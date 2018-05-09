bb8f12f831e32a8f4ba7650b3979d5f436d0142b
Jaco Greeff <jacogr@gmail.com>
2018-05-09 11:37:25 +0200

    Node operations moved to cli completely

--- packages/app-vanitygen/src/generator/cli.js
+++ packages/app-vanitygen/src/generator/cli.js
@@ -4,6 +4,9 @@
 // of the ISC license. See the LICENSE file for details.
 
 const yargs = require('yargs');
+const chalk = require('chalk');
+const u8aToHex = require('@polkadot/util/u8a/toHex');
+
 const generator = require('./index.js');
 
 const argv = yargs
@@ -13,6 +16,27 @@ const argv = yargs
   })
   .argv;
 
-generator({
-  match: argv.match
-});
+const options = {
+  match: argv.match,
+  runs: 1000
+};
+const startAt = Date.now();
+let best = { count: -1 };
+let total = 0;
+
+while (true) {
+  best = generator(options).found.reduce((best, match) => {
+    if ((match.count > best.count) || ((match.count === best.count) && (match.offset < best.offset))) {
+      return match;
+    }
+
+    return best;
+  }, best);
+
+  total += options.runs;
+
+  const elapsed = Date.now() - startAt;
+  const { address, count, offset, seed } = best;
+
+  console.log(`${total} in ${(elapsed / 1000).toFixed(2)}s, ${(elapsed / total).toFixed(3)}ms/key, count=${count}, offset=${offset} :: ${address.slice(0, offset)}${chalk.cyan(address.slice(offset, count + offset))}${address.slice(count + offset)} <= ${u8aToHex(seed)}`);
+}
