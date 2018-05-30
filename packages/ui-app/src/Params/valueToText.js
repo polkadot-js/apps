85c34daccbf3bb0d5960f57eafb91d1af256aff4
Jaco Greeff <jacogr@gmail.com>
2018-05-30 11:58:33 +0200

    Display decoded Proposals as text

--- packages/ui-app/src/Params/valueToText.js
+++ packages/ui-app/src/Params/valueToText.js
@@ -4,7 +4,7 @@
 // @flow
 
 import type BN from 'bn.js';
-import type { Param$Types, Param$Type$Array } from '@polkadot/params/types';
+import type { Param$Types, Param$Type$Array, ExtrinsicDecoded } from '@polkadot/params/types';
 
 import numberFormat from '@polkadot/ui-react-rx/util/numberFormat';
 import addressEncode from '@polkadot/util-keyring/address/encode';
@@ -12,23 +12,35 @@ import u8aToHex from '@polkadot/util/u8a/toHex';
 import isBn from '@polkadot/util/is/bn';
 import isU8a from '@polkadot/util/is/u8a';
 
+function proposalToText ({ extrinsic, params }: ExtrinsicDecoded): string {
+  if (!extrinsic) {
+    return '<unknown>';
+  }
+
+  const inputs = extrinsic.params.map(({ name, type }, index) =>
+    `${name}=${valueToText(type, params[index])}`
+  ).join(', ');
+
+  return `${extrinsic.section}.${extrinsic.name}(${inputs})`;
+}
+
 // flowlint-next-line unclear-type:off
-function arrayToText (type: Param$Type$Array, value: Array<any>): string {
+function arrayToText (type: Param$Type$Array, value: Array<any>, withBound: boolean = true): string {
   if (value.length === 0) {
     return 'empty';
   }
 
   const values = type.map((_type, index) =>
-    valueToText(_type, value[index])
+    valueToText(_type, value[index], false)
   ).join(', ');
 
-  return type.length === 1
+  return withBound && type.length === 1
     ? `(${values})`
     : values;
 }
 
 // flowlint-next-line unclear-type:off
-function valueToText (type: Param$Types, value: any): string {
+function valueToText (type: Param$Types, value: any, swallowError: boolean = true): string {
   try {
     if (type === 'bool') {
       return value ? 'Yes' : 'No';
@@ -46,6 +58,10 @@ function valueToText (type: Param$Types, value: any): string {
       return addressEncode((value: Uint8Array));
     }
 
+    if (type === 'Proposal') {
+      return proposalToText((value: ExtrinsicDecoded));
+    }
+
     if (isU8a(value)) {
       return u8aToHex((value: Uint8Array), 256);
     }
@@ -54,7 +70,11 @@ function valueToText (type: Param$Types, value: any): string {
       return numberFormat((value: BN));
     }
   } catch (error) {
-    console.log('valueToText', type, value, error);
+    if (!swallowError) {
+      throw error;
+    } else {
+      console.log('valueToText', type, value, error);
+    }
   }
 
   return value.toString();
