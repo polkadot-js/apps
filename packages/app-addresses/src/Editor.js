8b440256796be8bac3b8e4555a3f475c53d936f7
Luke Schoen <ltfschoen@gmail.com>
2018-06-12 19:44:49 +0200

    fix: Resolve linting CI errors

--- packages/app-addresses/src/Editor.js
+++ packages/app-addresses/src/Editor.js
@@ -67,7 +67,7 @@ class Editor extends React.PureComponent<Props, State> {
 
     return (
       <Button.Group>
-        <Button.Group.Divider style={{ padding: '0em'}}></Button.Group.Divider>
+        <Button.Group.Divider style={{ padding: '0em' }} />
         <Button
           isNegative
           onClick={this.onForget}
