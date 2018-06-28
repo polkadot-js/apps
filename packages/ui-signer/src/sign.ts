6939ff7ae9756eb4dbb30d0e653e11b772d5810e
Jaco Greeff <jacogr@gmail.com>
2018-06-28 13:48:00 +0200

    Cater for correct "Bytes" for submitExtrinsic

--- packages/ui-signer/src/sign.ts
+++ packages/ui-signer/src/sign.ts
@@ -9,23 +9,32 @@ import { Signed } from './types';
 import encodeCall from '@polkadot/extrinsics/codec/encode/call';
 import prefixes from '@polkadot/extrinsics/codec/encode/prefixes';
 import keyring from '@polkadot/ui-keyring/index';
+import bnToU8a from '@polkadot/util/bn/toU8a';
 import u8aConcat from '@polkadot/util/u8a/concat';
 import u8aToHex from '@polkadot/util/u8a/toHex';
 
 export default function signMessage (publicKey: Uint8Array, nonce: BN | number, value: Uint8Array, apiSupport: EncodingVersions): Signed {
   const message = encodeCall(publicKey, nonce, value, apiSupport);
   const signature = keyring.getPair(publicKey).sign(message);
+  const data = u8aConcat(
+    apiSupport === 'poc-1'
+      ? new Uint8Array([])
+      : prefixes.publicKey,
+    message,
+    signature
+  );
 
   console.log(`signMessage :   message :: ${u8aToHex(message)}`);
   console.log(`signMessage : signature :: ${u8aToHex(signature)}`);
+  console.log(`signMessage :      data :: ${u8aToHex(data)}`);
 
   return {
     data: u8aConcat(
+      // TODO We append the length here, but it probably needs to be done in the actual formatter - however there are differences between different encodings, here the extrinsic requires a length, where some other 'Bytes' types do not
       apiSupport === 'poc-1'
         ? new Uint8Array([])
-        : prefixes.publicKey,
-      message,
-      signature
+        : bnToU8a(data.length, 32, true),
+      data
     ),
     message,
     signature
