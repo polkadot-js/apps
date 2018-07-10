6196225fddeda8387735eee94bd4e8aa116e5103
Jaco Greeff <jacogr@gmail.com>
2018-07-10 17:16:23 +0200

    Start of override KeyValueStorage

---
+++ packages/ui-app/src/Params/Param/KeyValueStorageArray.tsx
@@ -0,0 +1,15 @@
+// Copyright 2017-2018 @polkadot/ui-app authors & contributors
+// This software may be modified and distributed under the terms
+// of the ISC license. See the LICENSE file for details.
+
+import { Props } from '../types';
+
+import React from 'react';
+
+export default class KeyValueStorageArray extends React.PureComponent<Props> {
+  render () {
+    return (
+      <div>WIP KeyValueStorage[]</div>
+    );
+  }
+}
