2cdc440f48ab16c310584e854121830384f3815d
Luke Schoen <ltfschoen@gmail.com>
2018-07-13 18:25:02 +0200

    fix: Add internationalisation, maxLength to input, validate depending on chain used
    - add internationalisation
    - add maxLength to input field depending on whether poc-1 (64bit) or latest (128bit) chain used
    - pass down api chain data via props
    - add unit tests for balance input field, new tests not all passing but don't understand why. since using unsigned 64bit and 128bit
    - add balance input field validation to only accept numbers
    - when i add `&& maxBN64Bit.gte(inputBN)` or `&& maxBN128Bit.gte(inputBN)` it doesn't appear
    to work correctly as in the UI it doesn't allow a length as much as expected

--- packages/ui-app/src/util/isValidBalance.ts
+++ packages/ui-app/src/util/isValidBalance.ts
@@ -2,9 +2,33 @@
 // This software may be modified and distributed under the terms
 // of the ISC license. See the LICENSE file for details.
 
-// RegEx Pattern: http://regexlib.com/REDetails.aspx?regexp_id=330
-const re = RegExp('^[0-9 ]+[0-9 ]*$');
+import BN from 'bn.js';
+import sizes from '@polkadot/params/sizes';
+import { EncodingVersions } from '@polkadot/params/types';
 
-export default function isValidBalance (input: string): boolean {
-  return input.trim().length !== 0 && re.test(input.trim());
+// RegEx Pattern (positive int): http://regexlib.com/REDetails.aspx?regexp_id=330
+const re = RegExp('^[0-9]+[0-9]*$');
+
+export default function isValidBalance (input: string, chain: string): boolean {
+  // apiSupport.chain is either 'poc-1' (64 bit) or 'latest' (128 bit)
+  const balanceSize: any = sizes.Balance.get(chain as EncodingVersions) || 64;
+  const max64Bit = 18446744073709551615;
+  const max128Bit = 340282366920938463463374607431768211455;
+  const maxBN64Bit = new BN(String(max64Bit));
+  const maxBN128Bit = new BN(String(max128Bit));
+  const inputBN = new BN(String(input));
+
+  if (input.trim().length === 0 || !re.test(input.trim())) {
+    return false;
+  }
+
+  if (chain === 'poc-1' && balanceSize === 64) { // && maxBN64Bit.gte(inputBN)
+    return true;
+  }
+
+  if (chain === 'latest' && balanceSize === 128) { // && maxBN128Bit.gte(inputBN)
+    return true;
+  }
+
+  return false;
 }
