77d449f1bdb7e629038c4b7ff8002315181697cd
Luke Schoen <ltfschoen@gmail.com>
2018-07-16 15:16:28 +0200

    refactor: Rename with HOC postfix

--- packages/app-governance/src/CouncilVotingProposalsHOC.tsx
+++ packages/app-governance/src/CouncilVotingProposalsHOC.tsx
@@ -1,3 +1,9 @@
+// Copyright 2017-2018 @polkadot/app-governance authors & contributors
+// This software may be modified and distributed under the terms
+// of the ISC license. See the LICENSE file for details.
+
+import BN from 'bn.js';
+
 import React from 'react';
 
 import storage from '@polkadot/storage';
@@ -5,13 +11,15 @@ import withStorageDiv from '@polkadot/ui-react-rx/with/storageDiv';
 import encodeAddress from '@polkadot/util-keyring/address/encode';
 import isUndefined from '@polkadot/util/is/undefined';
 
+type StorageCouncilVotingProposal = [BN, Uint8Array];
+
 const method = storage.councilVoting.public.proposals;
 
-const CouncilVotingProposals: React.ComponentType<any> = withStorageDiv(method)(
-  (value: any): string => {
-    console.log('value: ', value);
+const CouncilVotingProposalsHOC: React.ComponentType<any> = withStorageDiv(method)(
+  (value: Array<StorageCouncilVotingProposal>): string => {
+    console.log('value cv: ', value);
     if (isUndefined(value) || !value.length) {
-      return 'No council voting proposals found';
+      return 'No council voting proposals found using HOC';
     }
 
     // FIXME - why only one proposal returned when multiple created in UI?
@@ -28,4 +36,4 @@ const CouncilVotingProposals: React.ComponentType<any> = withStorageDiv(method)(
   }
 );
 
-export default CouncilVotingProposals;
+export default CouncilVotingProposalsHOC;
--- packages/app-governance/src/DemocracyProposalsHOC.tsx
+++ packages/app-governance/src/DemocracyProposalsHOC.tsx
@@ -1,3 +1,9 @@
+// Copyright 2017-2018 @polkadot/app-governance authors & contributors
+// This software may be modified and distributed under the terms
+// of the ISC license. See the LICENSE file for details.
+
+import BN from 'bn.js';
+
 import React from 'react';
 
 import storage from '@polkadot/storage';
@@ -5,13 +11,15 @@ import withStorageDiv from '@polkadot/ui-react-rx/with/storageDiv';
 import encodeAddress from '@polkadot/util-keyring/address/encode';
 import isUndefined from '@polkadot/util/is/undefined';
 
+type StorageDemocracyProposal = [BN, any, Uint8Array];
+
 const method = storage.democracy.public.proposals;
 
-const DemocracyProposals: React.ComponentType<any> = withStorageDiv(method)(
-  (value: any): string => {
-    console.log('value: ', value);
+const DemocracyProposalsHOC: React.ComponentType<any> = withStorageDiv(method)(
+  (value: Array<StorageDemocracyProposal>): string => {
+    console.log('value d: ', value);
     if (isUndefined(value) || !value.length) {
-      return 'No democracy proposals found';
+      return 'No democracy proposals found using HOC';
     }
 
     let democracyProposals: Array<string> = [];
@@ -27,4 +35,4 @@ const DemocracyProposals: React.ComponentType<any> = withStorageDiv(method)(
   }
 );
 
-export default DemocracyProposals;
+export default DemocracyProposalsHOC;
