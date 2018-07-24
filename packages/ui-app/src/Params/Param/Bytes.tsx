8bd553f7b5f03ec4ac8ef8ccb30bccd38b64a3af
Jaco Greeff <jacogr@gmail.com>
2018-07-24 17:16:10 +0200

    Update types

--- packages/ui-app/src/Params/Param/Bytes.tsx
+++ packages/ui-app/src/Params/Param/Bytes.tsx
@@ -2,7 +2,7 @@
 // This software may be modified and distributed under the terms
 // of the ISC license. See the LICENSE file for details.
 
-import { Props, RawParam$OnChange$Value } from '../types';
+import { Props } from '../types';
 
 import React from 'react';
 
