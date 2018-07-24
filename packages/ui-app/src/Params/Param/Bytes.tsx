b9f951c7915bd67ba5820a4bd131ec8205dd86fb
Jaco Greeff <jacogr@gmail.com>
2018-07-24 17:08:28 +0200

    Add length prefix to bytes

--- packages/ui-app/src/Params/Param/Bytes.tsx
+++ packages/ui-app/src/Params/Param/Bytes.tsx
@@ -2,26 +2,31 @@
 // This software may be modified and distributed under the terms
 // of the ISC license. See the LICENSE file for details.
 
-import { Props } from '../types';
+import { Props, RawParam$OnChange$Value } from '../types';
 
 import React from 'react';
 
 import BaseBytes from './BaseBytes';
 
-export default function Bytes ({ className, defaultValue, isDisabled, isError, label, name, onChange, style, withLabel }: Props) {
-  return (
-    <BaseBytes
-      className={className}
-      defaultValue={defaultValue}
-      isDisabled={isDisabled}
-      isError={isError}
-      label={label}
-      length={-1}
-      name={name}
-      onChange={onChange}
-      size='full'
-      style={style}
-      withLabel={withLabel}
-    />
-  );
+export default class Bytes extends React.PureComponent<Props> {
+  render () {
+    const { className, defaultValue, isDisabled, isError, label, name, onChange, style, withLabel } = this.props;
+
+    return (
+      <BaseBytes
+        className={className}
+        defaultValue={defaultValue}
+        isDisabled={isDisabled}
+        isError={isError}
+        label={label}
+        length={-1}
+        name={name}
+        onChange={onChange}
+        size='full'
+        style={style}
+        withLabel={withLabel}
+        withLength
+      />
+    );
+  }
 }
