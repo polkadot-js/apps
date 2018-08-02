1cdbe7264df7f1ed49269e9916b460b2032fd290
Luke Schoen <ltfschoen@gmail.com>
2018-08-02 21:12:45 +0200

    fix: Fix tslint errors

--- packages/app-accounts/src/util/showUploadButton.tsx
+++ packages/app-accounts/src/util/showUploadButton.tsx
@@ -2,11 +2,13 @@
 // This software may be modified and distributed under the terms
 // of the ISC license. See the LICENSE file for details.
 
+import { Button$Sizes } from '@polkadot/ui-app/Button/types';
+
 import React from 'react';
 
 import UploadButton from '@polkadot/ui-app/UploadButton';
 
-export default function showUploadButton (size?: string): any {
+export default function showUploadButton (size?: Button$Sizes): any {
   return (
     <div className='accounts--Address-file'>
       <UploadButton size={size}/>
