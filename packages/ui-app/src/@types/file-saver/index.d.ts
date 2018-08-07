1dc265e5d250394745c0d5edab05547a4484e9c3
Luke Schoen <ltfschoen@gmail.com>
2018-08-07 13:49:34 +0200

    fix: Add missing license headers

--- packages/ui-app/src/@types/file-saver/index.d.ts
+++ packages/ui-app/src/@types/file-saver/index.d.ts
@@ -1,3 +1,7 @@
+// Copyright 2017-2018 @polkadot/ui-app authors & contributors
+// This software may be modified and distributed under the terms
+// of the ISC license. See the LICENSE file for details.
+
 // https://www.typescriptlang.org/docs/handbook/namespaces-and-modules.html
 declare module 'file-saver' {
   export function saveAs(blob: Blob, filename: string): any;
