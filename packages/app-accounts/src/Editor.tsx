be6498d8d223d5e5793b8382c319591e0b87113c
Luke Schoen <ltfschoen@gmail.com>
2018-08-10 00:29:39 +0200

    fix: UploadButton props multi lines and value without {'big'}

--- packages/app-accounts/src/Editor.tsx
+++ packages/app-accounts/src/Editor.tsx
@@ -105,7 +105,10 @@ class Editor extends React.PureComponent<Props, State> {
           <div>{t('editor.nosaved', { defaultValue: 'There are no saved accounts. Create an account or upload a JSON file of a saved account.' })}</div>
           <div className={'accounts--Address-wrapper'}>
             <div className={'accounts--Address-file'}>
-              <UploadButton size={'big'} onChangeAccount={this.onChangeAccount} />
+              <UploadButton
+                size='big'
+                onChangeAccount={this.onChangeAccount}
+              />
             </div>
           </div>
         </div>
