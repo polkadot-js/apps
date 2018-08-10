405707ad67c3139ead78520628de284c8665ff51
Jaco Greeff <jacogr@gmail.com>
2018-08-10 13:40:00 +0200

    Cleanup previous hacks for null

--- packages/ui-react-rx/src/util/numberFormat.ts
+++ packages/ui-react-rx/src/util/numberFormat.ts
@@ -6,7 +6,7 @@ import BN from 'bn.js';
 
 const NUMBER_REGEX = new RegExp('(\\d+?)(?=(\\d{3})+(?!\\d)|$)', 'g');
 
-export default function numberFormat (value?: BN | number): string {
+export default function numberFormat (value?: BN | number | null): string {
   if (value === undefined || value === null) {
     return '0';
   }
