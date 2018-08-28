7ecbd4bff95cfb6586c4c17759c498b1d1fb43f3
Luke Schoen <ltfschoen@gmail.com>
2018-08-28 22:45:46 +0300

    test: Modal opens when toggled by prop

---
+++ packages/app-accounts/src/ChangePasswordModal.spec.js
@@ -0,0 +1,39 @@
+// Copyright 2017-2018 @polkadot/app-accounts authors & contributors
+// This software may be modified and distributed under the terms
+// of the ISC license. See the LICENSE file for details.
+
+import React from 'react';
+import { shallow } from '../../../test/enzyme';
+
+import { ChangePasswordModal } from './ChangePasswordModal';
+
+const mockT = (key, options) => (key);
+
+describe('ChangePasswordModal', () => {
+  let wrapper, emptyErrorPropFixture;
+
+  beforeEach(() => {
+    emptyErrorPropFixture = {
+      props: {
+        props: {
+          inputError: {
+            password: '',
+            newPassword: ''
+          },
+          formError: ''
+        }
+      }
+    };
+
+    wrapper = shallow(<ChangePasswordModal error={emptyErrorPropFixture} t={mockT} />, {});
+  });
+
+  it('creates the element', () => {
+    expect(wrapper).toHaveLength(1);
+  });
+
+  it('should only render Change Password Modal when prop values set for isPasswordModalOpen modal toggler', () => {
+    wrapper.setProps({ isPasswordModalOpen: true });
+    expect(wrapper.find('.ui--Accounts-change-password-Signer')).toHaveLength(1);
+  });
+});
