ae678e7344c4e084038861a0e6f195e292cbf2b9
Marni <marniduffy@gmail.com>
2018-09-03 14:43:46 -0400

    fixed travis recs

--- packages/app-extrinsics/src/index.tsx
+++ packages/app-extrinsics/src/index.tsx
@@ -1,8 +1,8 @@
 // Copyright 2017-2018 @polkadot/app-extrinsics authors & contributors
 // This software may be modified and distributed under the terms
 // of the ISC license. See the LICENSE file for details.
-import { BareProps } from '@polkadot/ui-app/types';
 import { QueueProps } from '@polkadot/ui-signer/types';
+import { I18nProps } from '@polkadot/ui-app/types';
 
 import './index.css';
 
@@ -15,35 +15,37 @@ import withObservableBase from '@polkadot/ui-react-rx/with/observableBase';
 
 import Selection from './Selection';
 
-type Props = BareProps & {
-  basePath: string
+type Props = I18nProps & {
+  basePath: string,
+  accountAll?: Array<any>
 };
 
 class ExtrinsicsApp extends React.PureComponent<Props> {
   render () {
 
-    const { allAccounts, t } = this.props;
-    const accountLength = allAccounts ? Object.keys(allAccounts).length : 0;
+    const { accountAll } = this.props;
+
+    if (!accountAll || !Object.keys(accountAll).length) {
+      return (
+        <div>
+          <p>There are no saved accounts. Add some first.</p>
+          <Link to='/accounts'>
+            <Button
+              isPrimary
+            >Add Accounts
+            </Button>
+          </Link>
+        </div>
+      );
+    }
 
     return (
       <main className='extrinsics--App'>
-        {  accountLength > 0 ?
-          <QueueConsumer>
-            {({ queueAdd }: QueueProps) => (
-              <Selection queueAdd={queueAdd} />
-            )}
-          </QueueConsumer>
-          :
-          <div>
-            <p>There are no saved accounts. Add some first.</p>
-            <Link to="/accounts">
-              <Button
-                isPrimary
-              >Add Accounts
-              </Button>
-            </Link>
-          </div>
-        }
+        <QueueConsumer>
+          {({ queueAdd }: QueueProps) => (
+            <Selection queueAdd={queueAdd} />
+          )}
+        </QueueConsumer>
       </main>
     );
   }
@@ -51,4 +53,4 @@ class ExtrinsicsApp extends React.PureComponent<Props> {
 
 export { ExtrinsicsApp };
 
-export default withObservableBase(accountsObservable.subject, { propName: 'allAccounts' })(ExtrinsicsApp);
+export default withObservableBase(accountsObservable.subject, { propName: 'accountAll' })(ExtrinsicsApp);
