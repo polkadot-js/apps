5069046062825db3be93e1f8d8ffe3c52592b320
Luke Schoen <ltfschoen@gmail.com>
2018-09-04 12:42:12 +0200

    fix: Change to Array<any> for accountAll like in app-accounts index

--- packages/app-accounts/src/util/accounts.ts
+++ packages/app-accounts/src/util/accounts.ts
@@ -2,21 +2,21 @@
 // This software may be modified and distributed under the terms
 // of the ISC license. See the LICENSE file for details.
 
-let accountsQty: (accountAll?: any[]) => number;
-let isAccounts: (accountAll?: any[]) => boolean;
-let isNoAccounts: (accountAll?: any[]) => boolean;
-let showIsOrAre: (accountAll?: any[]) => string;
-let showPlural: (accountAll?: any[]) => string;
+let accountsQty: (accountAll?: Array<any>) => number;
+let isAccounts: (accountAll?: Array<any>) => boolean;
+let isNoAccounts: (accountAll?: Array<any>) => boolean;
+let showIsOrAre: (accountAll?: Array<any>) => string;
+let showPlural: (accountAll?: Array<any>) => string;
 
-accountsQty = (accountAll?: any[]): number => isAccounts(accountAll) && accountAll ? Object.keys(accountAll).length : 0;
+accountsQty = (accountAll?: Array<any>): number => isAccounts(accountAll) && accountAll ? Object.keys(accountAll).length : 0;
 
-isAccounts = (accountAll?: any[]): boolean => !isNoAccounts(accountAll);
+isAccounts = (accountAll?: Array<any>): boolean => !isNoAccounts(accountAll);
 
-isNoAccounts = (accountAll?: any[]): boolean => !accountAll || !Object.keys(accountAll).length;
+isNoAccounts = (accountAll?: Array<any>): boolean => !accountAll || !Object.keys(accountAll).length;
 
-showIsOrAre = (accountAll?: any[]): string => accountsQty(accountAll) === 0 || accountsQty(accountAll) > 1 ? 'are' : 'is';
+showIsOrAre = (accountAll?: Array<any>): string => accountsQty(accountAll) === 0 || accountsQty(accountAll) > 1 ? 'are' : 'is';
 
-showPlural = (accountAll?: any[]): string => accountsQty(accountAll) === 0 || accountsQty(accountAll) > 1 ? 's' : '';
+showPlural = (accountAll?: Array<any>): string => accountsQty(accountAll) === 0 || accountsQty(accountAll) > 1 ? 's' : '';
 
 export {
   accountsQty,
