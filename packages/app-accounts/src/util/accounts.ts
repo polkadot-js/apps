3b700b61a07866bbae509bad320fbd98833f0e9a
Luke Schoen <ltfschoen@gmail.com>
2018-09-04 11:28:25 +0200

    fix: Remove unnecessary undefined for optional param

--- packages/app-accounts/src/util/accounts.ts
+++ packages/app-accounts/src/util/accounts.ts
@@ -2,21 +2,21 @@
 // This software may be modified and distributed under the terms
 // of the ISC license. See the LICENSE file for details.
 
-let accountsQty: (accountAll?: any[] | undefined) => number;
-let isAccounts: (accountAll?: any[] | undefined) => boolean;
-let isNoAccounts: (accountAll?: any[] | undefined) => boolean;
-let showIsOrAre: (accountAll?: any[] | undefined) => string;
-let showPlural: (accountAll?: any[] | undefined) => string;
+let accountsQty: (accountAll?: any[]) => number;
+let isAccounts: (accountAll?: any[]) => boolean;
+let isNoAccounts: (accountAll?: any[]) => boolean;
+let showIsOrAre: (accountAll?: any[]) => string;
+let showPlural: (accountAll?: any[]) => string;
 
-accountsQty = (accountAll?: any[] | undefined): number => isAccounts(accountAll) && accountAll ? Object.keys(accountAll).length : 0;
+accountsQty = (accountAll?: any[]): number => isAccounts(accountAll) && accountAll ? Object.keys(accountAll).length : 0;
 
-isAccounts = (accountAll?: any[] | undefined): boolean => !isNoAccounts(accountAll);
+isAccounts = (accountAll?: any[]): boolean => !isNoAccounts(accountAll);
 
-isNoAccounts = (accountAll?: any[] | undefined): boolean => !accountAll || !Object.keys(accountAll).length;
+isNoAccounts = (accountAll?: any[]): boolean => !accountAll || !Object.keys(accountAll).length;
 
-showIsOrAre = (accountAll?: any[] | undefined): string => accountsQty(accountAll) === 0 || accountsQty(accountAll) > 1 ? 'are' : 'is';
+showIsOrAre = (accountAll?: any[]): string => accountsQty(accountAll) === 0 || accountsQty(accountAll) > 1 ? 'are' : 'is';
 
-showPlural = (accountAll?: any[] | undefined): string => accountsQty(accountAll) === 0 || accountsQty(accountAll) > 1 ? 's' : '';
+showPlural = (accountAll?: any[]): string => accountsQty(accountAll) === 0 || accountsQty(accountAll) > 1 ? 's' : '';
 
 export {
   accountsQty,
