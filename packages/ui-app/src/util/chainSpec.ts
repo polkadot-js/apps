878e5439a535d42052bc9443a89a45fa76c50d6a
Luke Schoen <ltfschoen@gmail.com>
2018-09-07 18:07:55 +0200

    fix: Split function body onto new line for readability

--- packages/ui-app/src/util/chainSpec.ts
+++ packages/ui-app/src/util/chainSpec.ts
@@ -8,9 +8,8 @@ import { BIT_LENGTH_128 } from '../constants';
 
 // fixes error when enter value like 0.1111111111111111111111111111111, which causes
 // bn.js error in toU8a.js of 'byte array longer than desired length'
-const isValidBitLength = (value: BN, bitLength?: number): boolean => {
-  return value.bitLength() <= (bitLength || BIT_LENGTH_128) ? true : false;
-};
+const isValidBitLength = (value: BN, bitLength?: number): boolean =>
+  value.bitLength() <= (bitLength || BIT_LENGTH_128);
 
 /* chain specification 'latest' is 128 bit and supports max value of 2^128-1, which is ~3.40×10^38
  * and may be entered as 3.4e38, 3.4e+38 or 340282366920938463463374607431768211455. we may
