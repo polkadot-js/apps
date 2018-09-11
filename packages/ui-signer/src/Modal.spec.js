f9d5c8063258a179eb1f8f9f8d16e21844c86586
Luke Schoen <ltfschoen@gmail.com>
2018-09-11 11:04:28 +0200

    WIP

--- packages/ui-signer/src/Modal.spec.js
+++ packages/ui-signer/src/Modal.spec.js
@@ -3,10 +3,15 @@
 // of the ISC license. See the LICENSE file for details.
 
 import React from 'react';
-import { shallow } from '../../../test/enzyme';
+import { mount, shallow } from '../../../test/enzyme';
 import { Signer } from './Modal';
 
 const mockT = (key, options) => (key);
+
+// jest.mock('react-i18next', () => ({
+//   // this mock makes sure any components using the translate HoC receive the t function as a prop
+//   translate: () => Component => props => <Component t={k => k} {...props} />
+// }));
 
 describe('Signer', () => {
   let wrapper, expectedNextCurrentItemState, fixtureCurrentItemState, fixtureQueueProp, inputPassword;
@@ -36,13 +41,49 @@ describe('Signer', () => {
       <Signer
         t={mockT}
         queue={[]}
-      />
+      />, {}
     );
   });
 
-  it('creates the element', () => {
-    expect(wrapper).toHaveLength(1);
-    console.log(wrapper.debug());
+  it('creates the element', async () => {
+    try {
+      await wrapper.setState({
+        currentItem: fixtureCurrentItemState,
+        password: '123',
+        unlockError: null
+      });
+
+      await wrapper.setProps({
+        queue: fixtureQueueProp
+      });
+
+      wrapper.update();
+
+      expect(wrapper).toHaveLength(1);
+
+      // console.log(wrapper.debug());
+    } catch (error) {
+      console.error(error);
+    }
+  });
+
+  it('testing', (done) => {
+    try {
+      wrapper.setState({ currentItem: fixtureCurrentItemState }, () => {
+        wrapper.setProps({
+          queue: fixtureQueueProp
+        }, () => {
+          wrapper.update();
+          expect(wrapper.state('currentItem')).toEqual(expectedNextCurrentItemState);
+          expect(wrapper.find('.ui--signer-Signer')).toHaveLength(1);
+
+          console.log(wrapper.debug());
+          done();
+        });
+      });
+    } catch (error) {
+      console.error(error);
+    }
   });
 
   // Resolution of Enzyme issue: https://github.com/airbnb/enzyme/issues/1794
@@ -58,17 +99,20 @@ describe('Signer', () => {
         queue: fixtureQueueProp
       });
 
-      expect(wrapper.update().state('currentItem')).toEqual(expectedNextCurrentItemState);
-      wrapper.children().debug();
-      expect(wrapper.update().find('.ui--signer-Signer')).toHaveLength(1);
-      expect(wrapper.update().find('.ui--signer-Signer-Unlock')).toHaveLength(1);
-      // check no password input field validation error
-      expect(wrapper.update().find('.ui--signer-Signer-Unlock').closest('input').parent().hasClass('ui action input')).toBe(true);
-      inputPassword = wrapper.find('.ui--signer-Signer-Unlock').find('input');
-      const enterKey = 'Enter';
-      inputPassword.simulate('keyDown', {key: enterKey});
-      // check password input field validation error when press enter without password
-      expect(wrapper.update().find('.ui--signer-Signer-Unlock').closest('input').parent().hasClass('ui error action input')).toBe(true);
+      wrapper.update();
+
+      expect(wrapper.state('currentItem')).toEqual(expectedNextCurrentItemState);
+      expect(wrapper.find('.ui--signer-Signer')).toHaveLength(1);
+      // expect(wrapper.find('.ui--signer-Signer-Unlock')).toHaveLength(1);
+
+      console.log(wrapper.find('.ui--signer-Signer').debug());
+      // // check no password input field validation error
+      // expect(wrapper.update().find('.ui--signer-Signer-Unlock').closest('input').parent().hasClass('ui action input')).toBe(true);
+      // inputPassword = wrapper.find('.ui--signer-Signer-Unlock').find('input');
+      // const enterKey = 'Enter';
+      // inputPassword.simulate('keyDown', {key: enterKey});
+      // // check password input field validation error when press enter without password
+      // expect(wrapper.update().find('.ui--signer-Signer-Unlock').closest('input').parent().hasClass('ui error action input')).toBe(true);
     } catch (error) {
       console.error(error);
     }
