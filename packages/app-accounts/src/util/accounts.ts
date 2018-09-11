5b4c2707ea0ab3dfe9bbc1fbf4c2f405034487f5
Luke Schoen <ltfschoen@gmail.com>
2018-09-11 23:59:43 +0200

    fix: Fix linting by not using variable before declaration

--- packages/app-accounts/src/util/accounts.ts
+++ packages/app-accounts/src/util/accounts.ts
@@ -2,14 +2,14 @@
 // This software may be modified and distributed under the terms
 // of the ISC license. See the LICENSE file for details.
 
-const accountsQty = (allAccounts?: Array<any>): number =>
-  isAccounts(allAccounts) && allAccounts ? Object.keys(allAccounts).length : 0;
+const isNoAccounts = (allAccounts?: Array<any>): boolean =>
+  !allAccounts || !Object.keys(allAccounts).length;
 
 const isAccounts = (allAccounts?: Array<any>): boolean =>
   !isNoAccounts(allAccounts);
 
-const isNoAccounts = (allAccounts?: Array<any>): boolean =>
-  !allAccounts || !Object.keys(allAccounts).length;
+const accountsQty = (allAccounts?: Array<any>): number =>
+  isAccounts(allAccounts) && allAccounts ? Object.keys(allAccounts).length : 0;
 
 export {
   accountsQty,
