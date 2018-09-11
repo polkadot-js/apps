218beda37f2f0f493ccac9d457f15f56262c9040
Luke Schoen <ltfschoen@gmail.com>
2018-09-11 15:37:08 +0200

    test: Add test to check that DownloadButton appears in Editor when account exists

--- packages/app-accounts/src/Editor.spec.js
+++ packages/app-accounts/src/Editor.spec.js
@@ -6,17 +6,45 @@ import React from 'react';
 import { shallow } from '../../../test/enzyme';
 
 import { Editor } from './Editor';
+import { DownloadButton } from './DownloadButton';
 
+const sampleAddress = '5Fpq7JD6W441h1NFt3ayHrj6jGeacuK4d6EYq7GcfvQuAHuP';
 const mockT = (key, options) => (key);
 
 describe('Editor', () => {
-  let wrapper;
+  let fixtureState, wrapper;
 
   beforeEach(() => {
+    fixtureState = {
+      current: {
+        address: jest.fn(() => sampleAddress)
+      },
+      editedName: 'my account',
+      isEdited: false,
+      isForgetOpen: false,
+      previous: {
+        address: jest.fn(() => sampleAddress)
+      }
+    };
+
     wrapper = shallow(<Editor t={mockT} />, {});
   });
 
   it('creates the element', () => {
     expect(wrapper).toHaveLength(1);
+  });
+
+  it('should display download button when a current address exists', (done) => {
+    try {
+      wrapper.setState(fixtureState, () => {
+        wrapper.update();
+        expect(wrapper.state('editedName')).toEqual(fixtureState.editedName);
+        // console.log(wrapper.debug());
+        expect(wrapper.find(DownloadButton)).toHaveLength(1);
+        done();
+      });
+    } catch (error) {
+      console.error(error);
+    }
   });
 });
