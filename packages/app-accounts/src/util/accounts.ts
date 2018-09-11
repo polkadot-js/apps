b21d63bbaa168c9e1558a6e63bb4529c2a8f12ec
Luke Schoen <ltfschoen@gmail.com>
2018-09-11 23:26:49 +0200

    fix: Recombine account util function declarations with their implementation

--- packages/app-accounts/src/util/accounts.ts
+++ packages/app-accounts/src/util/accounts.ts
@@ -2,17 +2,13 @@
 // This software may be modified and distributed under the terms
 // of the ISC license. See the LICENSE file for details.
 
-let accountsQty: (allAccounts?: Array<any>) => number;
-let isAccounts: (allAccounts?: Array<any>) => boolean;
-let isNoAccounts: (allAccounts?: Array<any>) => boolean;
-
-accountsQty = (allAccounts?: Array<any>): number =>
+const accountsQty = (allAccounts?: Array<any>): number =>
   isAccounts(allAccounts) && allAccounts ? Object.keys(allAccounts).length : 0;
 
-isAccounts = (allAccounts?: Array<any>): boolean =>
+const isAccounts = (allAccounts?: Array<any>): boolean =>
   !isNoAccounts(allAccounts);
 
-isNoAccounts = (allAccounts?: Array<any>): boolean =>
+const isNoAccounts = (allAccounts?: Array<any>): boolean =>
   !allAccounts || !Object.keys(allAccounts).length;
 
 export {
