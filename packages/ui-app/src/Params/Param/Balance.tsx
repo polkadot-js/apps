055b68c7561e220f138d9e5e2663a0df62d1f871
Luke Schoen <ltfschoen@gmail.com>
2018-09-21 10:56:50 +1000

    refactor: Remove disused withApi and translate from Balance and simplify defaultValue

--- packages/ui-app/src/Params/Param/Balance.tsx
+++ packages/ui-app/src/Params/Param/Balance.tsx
@@ -2,24 +2,20 @@
 // This software may be modified and distributed under the terms
 // of the ISC license. See the LICENSE file for details.
 
-import { ApiProps } from '@polkadot/ui-react-rx/types';
-import { I18nProps } from '../../types';
 import { Props as BareProps } from '../types';
 
 import BN from 'bn.js';
 import React from 'react';
-import withApi from '@polkadot/ui-react-rx/with/api';
 
 import InputNumber from '../../InputNumber';
-import translate from '../../translate';
 import Bare from './Bare';
 
-type Props = I18nProps & ApiProps & BareProps;
+type Props = BareProps;
 
 class Balance extends React.PureComponent<Props> {
   render () {
     const { className, defaultValue: { value }, isError, label, style, withLabel } = this.props;
-    const defaultValue = new BN((value as BN).toString(10) || '0').toString(10);
+    const defaultValue = new BN(value as BN || '0').toString(10);
 
     return (
       <Bare
@@ -56,4 +52,4 @@ export {
   Balance
 };
 
-export default withApi(translate(Balance));
+export default Balance;
