db9d9b48d77d544d5be4a304588e8c94695ced63
Luke Schoen <ltfschoen@gmail.com>
2018-09-21 13:20:13 +1000

    test: Add more isValidNumber tests. Add maxValue and isValidBitLength tests

--- packages/ui-app/src/InputNumber.spec.js
+++ packages/ui-app/src/InputNumber.spec.js
@@ -2,6 +2,7 @@
 // This software may be modified and distributed under the terms
 // of the ISC license. See the LICENSE file for details.
 
+import BN from 'bn.js';
 import React from 'react';
 
 import { mount } from '../../../test/enzyme';
@@ -11,6 +12,9 @@ import { BIT_LENGTH_128, InputNumber } from './InputNumber';
 const mockT = (key, options) => (key);
 
 describe('InputNumber', () => {
+  const max128BN = new BN(2).pow(new BN(128)).subn(1);
+  const aboveMax128BN = max128BN.addn(1);
+  const belowMax128BN = max128BN.subn(1);
   let wrapper, divInputNumber, defaultValue;
 
   beforeEach(() => {
@@ -31,6 +35,12 @@ describe('InputNumber', () => {
   it('should display InputNumber wrapper and Input component', () => {
     expect(divInputNumber).toHaveLength(1);
     expect(wrapper.find(Input)).toHaveLength(1);
+  });
+
+  describe('maxValue', () => {
+    it('returns max BN value for given bitlength or uses 128 bit bitlength fallback', () => {
+      expect(wrapper.instance().maxValue(128)).toEqual(max128BN);
+    });
   });
 
   describe('isNonDecimal', () => {
@@ -63,6 +73,13 @@ describe('InputNumber', () => {
     });
   });
 
+  describe('isValidBitLength', () => {
+    it('returns whether value in BN is valid for given bitlength or uses 128 bit bitlength fallback', () => {
+      expect(wrapper.instance().isValidBitLength(aboveMax128BN, BIT_LENGTH_128)).toBe(false);
+      expect(wrapper.instance().isValidBitLength(max128BN, BIT_LENGTH_128)).toBe(true);
+    });
+  });
+
   describe('isValidNumber', () => {
     describe('checks extrinsic balance', () => {
       it('throws an error if input value for comparison is not a string', () => {
@@ -70,6 +87,21 @@ describe('InputNumber', () => {
 
         expect(() => { wrapper.instance().isValidNumber(invalidInputValueType, '0', BIT_LENGTH_128); }).toThrow();
       });
+    });
+
+    it('should not be valid when user enters a small decimal value greater than max bit length when using 128 bit', () => {
+      const invalidValue = '0.00000000000000000000000000000001';
+      const validValue = '0.0000000000000000000000000000001';
+      expect(wrapper.instance().isValidNumber(invalidValue, BIT_LENGTH_128)).toBe(false);
+      expect(wrapper.instance().isValidNumber(validValue, BIT_LENGTH_128)).toBe(true);
+    });
+
+    it('should not be valid when user enters positive value greater than or equal to the 128 bit max for latest chain', () => {
+      expect(wrapper.instance().isValidNumber(max128BN.toString(10), BIT_LENGTH_128)).toBe(false);
+    });
+
+    it('should be valid when user enters positive value less than the 128 bit max for latest chain', () => {
+      expect(wrapper.instance().isValidNumber(belowMax128BN.toString(10), BIT_LENGTH_128)).toBe(true);
     });
   });
 });
