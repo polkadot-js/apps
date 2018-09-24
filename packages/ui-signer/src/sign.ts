97fe02341f3daf95c25b1c42685d81c4f0b024eb
YJ <yjkimjunior@gmail.com>
2018-09-24 12:22:28 +0100

    ui-signer signMessage with ss58 address
    derive public key by decoding ss58 input

--- packages/ui-signer/src/sign.ts
+++ packages/ui-signer/src/sign.ts
@@ -7,13 +7,15 @@ import { EncodingVersions } from '@polkadot/params/types';
 import { Signed } from './types';
 
 import encodeCall from '@polkadot/extrinsics/codec/encode/call';
+import addressDecode from '@polkadot/util-keyring/address/decode';
 import prefixes from '@polkadot/extrinsics/codec/encode/prefixes';
 import keyring from '@polkadot/ui-keyring/index';
 import bnToU8a from '@polkadot/util/bn/toU8a';
 import u8aConcat from '@polkadot/util/u8a/concat';
 import u8aToHex from '@polkadot/util/u8a/toHex';
 
-export default function signMessage (publicKey: Uint8Array, nonce: BN | number, value: Uint8Array, apiSupport: EncodingVersions): Signed {
+export default function signMessage (ss58: string, nonce: BN | number, value: Uint8Array, apiSupport: EncodingVersions): Signed {
+  const publicKey = addressDecode(ss58);
   const message = encodeCall(publicKey, nonce, value, apiSupport);
   const signature = keyring.getPair(publicKey).sign(message);
   const data = u8aConcat(
