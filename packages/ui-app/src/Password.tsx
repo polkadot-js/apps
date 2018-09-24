fc59ea7dff7d5b273e970735f72726c9452ac339
Luke Schoen <ltfschoen@gmail.com>
2018-09-24 12:02:14 +1000

    feat: Add maxLength of 32 to Password component and pass prop to Input

--- packages/ui-app/src/Password.tsx
+++ packages/ui-app/src/Password.tsx
@@ -46,6 +46,7 @@ export default class Password extends React.PureComponent<Props, State> {
         isDisabled={isDisabled}
         isError={isError}
         label={label}
+        maxLength={32}
         name={name}
         onChange={onChange}
         onKeyDown={onKeyDown}
