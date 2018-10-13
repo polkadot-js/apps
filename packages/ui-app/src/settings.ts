55e7bb36cffd0aa2e4bc2e15ffc924376e4dcd0d
Jaco Greeff <jacogr@gmail.com>
2018-10-13 22:21:59 +0200

    Update deps, cleanups

--- packages/ui-app/src/settings.ts
+++ packages/ui-app/src/settings.ts
@@ -2,11 +2,14 @@
 // This software may be modified and distributed under the terms
 // of the ISC license. See the LICENSE file for details.
 
+import { Prefix } from '@polkadot/keyring/address/types';
+
 import store from 'store';
+import setAddressPrefix from '@polkadot/keyring/address/setPrefix';
 
 export interface SettingsStruct {
   apiUrl: string;
-  chainId: number;
+  chainPrefix: Prefix;
   i18nLang: string;
   uiMode: string;
   uiTheme: string;
@@ -14,7 +17,7 @@ export interface SettingsStruct {
 
 class Settings implements SettingsStruct {
   private _apiUrl: string;
-  private _chainId: number;
+  private _chainPrefix: Prefix;
   private _i18nLang: string;
   private _uiMode: string;
   private _uiTheme: string;
@@ -25,18 +28,20 @@ class Settings implements SettingsStruct {
     // FIXME Here we have the defaults for BBQ, swap to Polkadot as soon as poc-3 is there
     // FIXME WS_URL first, then substrate-rpc
     this._apiUrl = settings.apiUrl || 'wss://substrate-rpc.parity.io/' || process.env.WS_URL;
-    this._chainId = settings.chainId || 68;
+    this._chainPrefix = settings.chainPrefix || 68;
     this._i18nLang = settings.i18nLang || 'default';
     this._uiMode = settings.uiMode || process.env.UI_MODE || 'full';
     this._uiTheme = settings.uiTheme || process.env.UI_THEME || 'substrate';
+
+    setAddressPrefix(this._chainPrefix);
   }
 
   get apiUrl (): string {
     return this._apiUrl;
   }
 
-  get chainId (): number {
-    return this._chainId;
+  get chainPrefix (): Prefix {
+    return this._chainPrefix;
   }
 
   get i18nLang (): string {
@@ -81,7 +86,7 @@ class Settings implements SettingsStruct {
   get (): SettingsStruct {
     return {
       apiUrl: this._apiUrl,
-      chainId: this._chainId,
+      chainPrefix: this._chainPrefix,
       i18nLang: this._i18nLang,
       uiMode: this._uiMode,
       uiTheme: this._uiTheme
@@ -90,7 +95,7 @@ class Settings implements SettingsStruct {
 
   set (settings: Partial<SettingsStruct>): void {
     this._apiUrl = settings.apiUrl || this._apiUrl;
-    this._chainId = settings.chainId || this._chainId;
+    this._chainPrefix = settings.chainPrefix || this._chainPrefix;
     this._i18nLang = settings.i18nLang || this._i18nLang;
     this._uiMode = settings.uiMode || this._uiMode;
     this._uiTheme = settings.uiTheme || this._uiTheme;
