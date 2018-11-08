208d738bf37049979fba6f87918dbc24bd40b424
Jaco Greeff <jacogr@gmail.com>
2018-11-08 18:14:17 +0100

    Apache-2.0 in headers

--- packages/ui-identicon/src/Polkadot.tsx
+++ packages/ui-identicon/src/Polkadot.tsx
@@ -1,7 +1,7 @@
 // Copyright 2018 Paritytech via paritytech/oo7/polkadot-identicon
 // Copyright 2018 @polkadot/ui-identicon authors & contributors
 // This software may be modified and distributed under the terms
-// of the APL2 license. See the LICENSE file for details.
+// of the Apache-2.0 license. See the LICENSE file for details.
 
 // This has been converted from the original version that can be found at
 //
