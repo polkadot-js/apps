2ead595f5b3f3d380a6d90a949d82b7f22eed69b
Jaco Greeff <jacogr@gmail.com>
2018-12-14 13:26:03 +0100

    Pull svg from ui-assets

--- packages/apps/src/SideBar/index.tsx
+++ packages/apps/src/SideBar/index.tsx
@@ -8,10 +8,10 @@ import './SideBar.css';
 
 import React from 'react';
 import { Icon, Menu } from '@polkadot/ui-app/index';
+import polkadotLogo from '@polkadot/ui-assets/polkadot-white.svg';
+import substrateLogo from '@polkadot/ui-assets/parity-substrate-white.svg';
 import settings from '@polkadot/ui-settings';
 
-import polkadotLogo from '../static/polkadot-white.svg';
-import substrateLogo from '../static/parity-substrate-white.svg';
 import routing from '../routing';
 import translate from '../translate';
 import Item from './Item';
