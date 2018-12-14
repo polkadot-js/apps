62c1ded7e1d7d201995b58abb37070b43cff0ad0
Jaco Greeff <jacogr@gmail.com>
2018-12-14 12:48:04 +0100

    Expand status timeout

--- packages/ui-app/src/Status/Queue.tsx
+++ packages/ui-app/src/Status/Queue.tsx
@@ -25,7 +25,7 @@ const defaultState = {
 
 let nextId = 0;
 
-const REMOVE_TIMEOUT = 5000;
+const REMOVE_TIMEOUT = 7500;
 const SUBMIT_RPC = jsonrpc.author.methods.submitAndWatchExtrinsic;
 const STATUS_COMPLETE: Array<QueueTx$Status> = [
   // status from subscription
