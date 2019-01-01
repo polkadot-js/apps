12120473f388ba689d5674a47b69c054de6e2365
Jaco Greeff <jacogr@gmail.com>
2019-01-01 17:32:16 +0100

    derive lookup for Id -> Index mappings

--- packages/ui-react-rx/src/derive/balances/accountIndexes.ts
+++ packages/ui-react-rx/src/derive/balances/accountIndexes.ts
@@ -9,9 +9,11 @@ import ApiPromise from '@polkadot/api/promise';
 import { AccountId, AccountIndex } from '@polkadot/types';
 import { ENUMSET_SIZE } from '@polkadot/types/AccountIndex';
 
+type AccountIndexes = { [index: string]: AccountIndex };
+
 export default function accountIndexes (api: ApiPromise): DeriveSubscription {
   return {
-    subscribe: async (cb: (indexes: Array<AccountIndex>) => any): Promise<number> => {
+    subscribe: async (cb: (indexes: AccountIndexes) => any): Promise<number> => {
       return api.query.balances.nextEnumSet((nextIndex: AccountIndex) => {
         const lastIndex = nextIndex || new BN(0);
         const setSize = ENUMSET_SIZE.toNumber();
@@ -29,10 +31,10 @@ export default function accountIndexes (api: ApiPromise): DeriveSubscription {
               });
 
               return result;
-            }, {} as { [index: string]: AccountIndex })
+            }, {} as AccountIndexes)
           );
         });
-      }) as any as Promise<number>;
+      });
     },
     unsubscribe: (subscriptionId: number): Promise<any> =>
       api.query.balances.nextEnumSet.unsubscribe(subscriptionId)
