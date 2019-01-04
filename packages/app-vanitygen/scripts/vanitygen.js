82fc199bc27ebeb025f677b1ab3fb1733eec8f7b
Jaco Greeff <jacogr@gmail.com>
2019-01-04 13:13:25 +0100

    2019

--- packages/app-vanitygen/scripts/vanitygen.js
+++ packages/app-vanitygen/scripts/vanitygen.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
-// Copyright 2017-2018 @polkadot/client authors & contributors
+// Copyright 2017-2019 @polkadot/client authors & contributors
 // This software may be modified and distributed under the terms
 // of the Apache-2.0 license. See the LICENSE file for details.
 
