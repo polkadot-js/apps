854737e025230a31ef0b248bb33c5bb2ec4a1738
Jaco Greeff <jacogr@gmail.com>
2019-01-10 14:55:37 +0100

    Update jest config

--- jest.config.js
+++ jest.config.js
@@ -3,7 +3,7 @@ const config = require('@polkadot/dev-react/config/jest');
 module.exports = Object.assign({}, config, {
   moduleNameMapper: {
     '@polkadot/app-(accounts|addresses|democracy|explorer|extrinsics|rpc|settings|staking|storage|toolbox|transfer|vanitygen)(.*)$': '<rootDir>/packages/app-$1/src/$2',
-    '@polkadot/ui-(app|react-rx|signer)(.*)$': '<rootDir>/packages/ui-$1/src/$2',
+    '@polkadot/ui-(app|params|react-rx|signer)(.*)$': '<rootDir>/packages/ui-$1/src/$2',
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': 'empty/object',
     '\\.(css|less)$': 'empty/object'
   }
