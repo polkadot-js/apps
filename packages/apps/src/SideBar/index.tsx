1a7de5c8035893f8d1fd948cb66907cf8ca03dbb
Jeff Reiner <theundeadghost@gmail.com>
2019-01-11 19:30:55 +0100

    adding in Wiki link to sidebar (#556)
    Closes #444

--- packages/apps/src/SideBar/index.tsx
+++ packages/apps/src/SideBar/index.tsx
@@ -73,6 +73,14 @@ class SideBar extends React.PureComponent<Props> {
               <Icon name='github' /> GitHub
             </a>
           </Menu.Item>
+          <Menu.Item className='apps--SideBar-Item'>
+            <a
+              className='apps--SideBar-Item-NavLink'
+              href='https://github.com/w3f/Web3-wiki/wiki/Polkadot'
+            >
+              <Icon name='book' /> Wiki
+            </a>
+          </Menu.Item>
           <Menu.Divider hidden />
           {children}
         </Menu>
