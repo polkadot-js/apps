b276786995e6ce7ad21d3302b060d29578ca92f1
Jaco Greeff <jacogr@gmail.com>
2019-01-12 10:58:54 +0100

    Display authorities for block generation

--- packages/app-explorer/src/BlockHeader/index.tsx
+++ packages/app-explorer/src/BlockHeader/index.tsx
@@ -2,13 +2,15 @@
 // This software may be modified and distributed under the terms
 // of the Apache-2.0 license. See the LICENSE file for details.
 
-import { Header } from '@polkadot/types';
+import { AccountId, Header } from '@polkadot/types';
 import { BareProps } from '@polkadot/ui-app/types';
 
 import './BlockHeader.css';
 
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { withCall } from '@polkadot/ui-api/index';
+import { AddressMini } from '@polkadot/ui-app/index';
 import numberFormat from '@polkadot/ui-reactive/util/numberFormat';
 
 // FIXME 7 Nov 2018 Due to mismatches with block hashes between Substrate and BBQ,
@@ -17,27 +19,38 @@ import BlockHash from './BlockHash';
 import Extrinsics from './Extrinsics';
 
 type Props = BareProps & {
+  query_session_validators?: Array<AccountId>,
   value?: Header,
   withExtrinsics?: boolean,
   withLink?: boolean
 };
 
-export default class BlockHeader extends React.PureComponent<Props> {
+class BlockHeader extends React.PureComponent<Props> {
   render () {
-    const { value, withExtrinsics = false, withLink = false } = this.props;
+    const { query_session_validators = [], value, withExtrinsics = false, withLink = false } = this.props;
 
     if (!value) {
       return null;
     }
 
-    const { blockNumber, extrinsicsRoot, parentHash, stateRoot } = value;
+    const { blockNumber, digest: { logs }, extrinsicsRoot, parentHash, stateRoot } = value;
     const parentHex = parentHash.toHex();
+    const seal = logs.filter(({ type }) => type === 'Seal');
+    const validator = seal && query_session_validators.length
+      ? query_session_validators[seal[0].toNumber() % query_session_validators.length]
+      : undefined;
 
     return (
       <article className='explorer--BlockHeader'>
         <div className='details'>
           <div className='header'>
-            <div className='number'>{numberFormat(blockNumber)}&nbsp;</div>
+            <div className='number'>
+              <div>{numberFormat(blockNumber)}&nbsp;</div>
+              <div className='validator'>{validator
+                ? <AddressMini value={validator} />
+                : undefined
+              }</div>
+            </div>
             <div className='hash'>
               <BlockHash
                 blockNumber={blockNumber}
@@ -46,7 +59,8 @@ export default class BlockHeader extends React.PureComponent<Props> {
             </div>
           </div>
           <div className='contains'>
-            <div>
+
+            <div className='info'>
               <div className='type'>parentHash</div>
               <div className='hash'>{
                 value.blockNumber.gtn(1)
@@ -54,11 +68,11 @@ export default class BlockHeader extends React.PureComponent<Props> {
                   : parentHex
               }</div>
             </div>
-            <div>
+            <div className='info'>
               <div className='type'>extrinsicsRoot</div>
               <div className='hash'>{extrinsicsRoot.toHex()}</div>
             </div>
-            <div>
+            <div className='info'>
               <div className='type'>stateRoot</div>
               <div className='hash'>{stateRoot.toHex()}</div>
             </div>
@@ -72,3 +86,5 @@ export default class BlockHeader extends React.PureComponent<Props> {
     );
   }
 }
+
+export default withCall('query.session.validators')(BlockHeader);
