1a8da504c868ce942ecc26fca8c031d388823c58
Jaco Greeff <jacogr@gmail.com>
2019-01-12 19:04:40 +0100

    Fix balance formatting (#593)

--- packages/ui-api/src/util/index.ts
+++ packages/ui-api/src/util/index.ts
@@ -2,23 +2,16 @@
 // This software may be modified and distributed under the terms
 // of the Apache-2.0 license. See the LICENSE file for details.
 
-import balanceFormat, { calcSi } from './balanceFormat';
-import decimalFormat from './decimalFormat';
 import intervalObservable from './intervalObservable';
 import intervalTimer from './intervalTimer';
 import isEqual from './isEqual';
 import isTestChain from './isTestChain';
-import numberFormat from './numberFormat';
 import triggerChange from './triggerChange';
 
 export {
-  balanceFormat,
-  calcSi,
-  decimalFormat,
   intervalObservable,
   intervalTimer,
   isEqual,
   isTestChain,
-  numberFormat,
   triggerChange
 };
