0ed655aa4b414d7caedb6ea93f31068d4cbafdf4
Stefanie Doll <stefie.doll@googlemail.com>
2019-02-09 11:42:36 +0100

    More examples and removed deprecated examples

--- packages/app-js/src/snippets/extrinsics-examples.ts
+++ packages/app-js/src/snippets/extrinsics-examples.ts
@@ -1,40 +1,37 @@
-import { ApiPromise } from '@polkadot/api';
-import { Keyring } from '@polkadot/keyring';
-import { stringToU8a } from '@polkadot/util';
+// Copyright 2017-2019 @polkadot/app-js authors & contributors
+// This software may be modified and distributed under the terms
+// of the Apache-2.0 license. See the LICENSE file for details.
+import { Snippet } from '@polkadot/app-js/types';
 
-import {
-  BOB, createButton, createLog, createError, createWrapper
-} from '../commons';
+export const extrinsicMakeTransfer: Snippet = {
+  value: 'extrinsicMakeTransfer',
+  text: 'Extrinsics - Make transfer and listen to events',
+  label: { color: 'grey', children: 'Extrinsics', size: 'tiny' },
+  code: `// Make a transfer from Alice to Bob and listen to system events.
+// You need to be connected to a development chain for this example to work.
+const BOB = '5Gw3s7q4QLkSWwknsiPtjujPv3XM4Trxi5d4PgKMMk3gfGTE';
 
-// https://polkadot.js.org/api/examples/promise/06_make_transfer/
-export default (provider) => {
-  const wrapper = createWrapper('make-transfer', 'Promise - Make Transfer');
-  const makeTransfer = async (provider) => {
-    try {
-      // Create our API with a connection to the node
-      const api = await ApiPromise.create(provider);
-      // Get a random number between 1 and 100000
-      const randomAmount = Math.floor((Math.random() * 100000) + 1);
-      // Get Alice seed
-      const ALICE_SEED = 'Alice'.padEnd(32, ' ');
-      // Create an instance of the keyring
-      const keyring = new Keyring();
-      // Add Alice to our keyring (with the known seed for the account)
-      const alice = keyring.addFromSeed(stringToU8a(ALICE_SEED));
-      // Create a extrinsic, transferring randomAmount units to Bob.
-      // We can also create sign and send in two operations.
-      const transfer = api.tx.balances.transfer(BOB, randomAmount);
-      // Sign and Send the transaction
-      transfer.signAndSend(alice, ({ status, type }) => {
-        if (type === 'Finalised') {
-          createLog(`Successful transfer of ${randomAmount} from <b>Alice</b> to <b>Bob</b> with hash ${status.asFinalised.toHex()}`, wrapper);
-        } else {
-          createLog(`Status of transfer: ${type}`, wrapper);
-        }
-      });
-    } catch (e) {
-      createError(e, wrapper);
-    }
-  };
-  createButton(makeTransfer, wrapper, 'Initialize transfer');
+// Get a random number between 1 and 100000
+const randomAmount = Math.floor((Math.random() * 100000) + 1);
+
+// Get Alice seed
+const ALICE_SEED = 'Alice'.padEnd(32, ' ');
+
+// Add Alice to our keyring (with the known seed for the account)
+const alice = keyring.addFromSeed(util.stringToU8a(ALICE_SEED));
+
+// Create a extrinsic, transferring randomAmount units to Bob.
+const transfer = api.tx.balances.transfer(BOB, randomAmount);
+
+// Sign and Send the transaction
+transfer.signAndSend(alice, ({ events = [], status, type }) => {
+  if (type === 'Finalised') {
+    console.log('Successful transfer of ' + randomAmount + ' with hash ' + status.asFinalised.toHex());
+  } else {
+    console.log('Status of transfer: ' + type);
+  }
+  events.forEach(({ phase, event: { data, method, section } }) => {
+    console.log(phase.toString() + ' : ' + section + '.' + method + ' ' + data.toString());
+  });
+});`
 };
