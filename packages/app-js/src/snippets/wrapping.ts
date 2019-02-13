16a3ca785848fd9ed9ba330603cfb7022b424bac
Stefanie Doll <stefie.doll@googlemail.com>
2019-02-13 18:02:34 +0100

    Fine tuning examples

--- packages/app-js/src/snippets/wrapping.ts
+++ packages/app-js/src/snippets/wrapping.ts
@@ -2,7 +2,9 @@
 // This software may be modified and distributed under the terms
 // of the Apache-2.0 license. See the LICENSE file for details.
 
-export default `// All code is wrapped within an async closure, allowing access to api, hashing, keyring, util.
+export default `// All code is wrapped within an async closure,
+// allowing access to api, hashing, keyring, util.
+//
 // (async ({ api, hashing, keyring, util }) => {
 //   ... any user code is executed here ...
 // })();
