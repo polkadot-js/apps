83d3897f6b7dc4a3d36b8beab35b17de987c4e14
Stefanie Doll <stefie.doll@googlemail.com>
2019-03-05 15:21:12 +0100

    Remove leftover BareProps import

--- packages/apps/src/index.tsx
+++ packages/apps/src/index.tsx
@@ -3,7 +3,6 @@
 // of the Apache-2.0 license. See the LICENSE file for details.
 
 import settings from '@polkadot/ui-settings';
-import { BareProps } from '@polkadot/ui-app/types';
 import '@polkadot/ui-app/i18n';
 import '@polkadot/ui-app/styles';
 
