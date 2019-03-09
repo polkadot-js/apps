d4e53f02e2266f949f0fec7ed91167c72b08e7dc
Jaco Greeff <jacogr@gmail.com>
2019-03-09 08:54:25 +0100

    Add extrinsic hash to detail display

--- packages/ui-app/src/Call.tsx
+++ packages/ui-app/src/Call.tsx
@@ -6,6 +6,7 @@ import { IExtrinsic, IMethod } from '@polkadot/types/types';
 import { BareProps } from './types';
 
 import React from 'react';
+import styled from 'styled-components';
 import { Method, getTypeDef } from '@polkadot/types';
 import Params from '@polkadot/ui-params/index';
 
@@ -15,6 +16,15 @@ export type Props = BareProps & {
   children?: React.ReactNode,
   value: IExtrinsic | IMethod
 };
+
+const Wrapper = styled.div`
+  .hash {
+    opacity: 0.5;
+    overflow: hidden;
+    padding: 0 0.5rem 0.75rem 1.75rem;
+    text-overflow: ellipsis;
+  }
+`;
 
 export default class Call extends React.PureComponent<Props> {
   render () {
@@ -27,19 +37,29 @@ export default class Call extends React.PureComponent<Props> {
       isValid: true,
       value
     }));
+    const hash = (value as IExtrinsic).hash;
 
     return (
-      <div
+      <Wrapper
         className={classes('ui--Extrinsic', className)}
         style={style}
       >
         {children}
+        {
+          hash
+            ? (
+              <div className='hash'>
+                {hash.toHex()}
+              </div>
+            )
+            : null
+        }
         <Params
           isDisabled
           params={params}
           values={values}
         />
-      </div>
+      </Wrapper>
     );
   }
 }
