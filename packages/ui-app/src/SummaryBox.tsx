112a82eae3967e2736d96b5950b78f867ba3ac84
Ross Bulat <ross@jkrbinvestments.com>
2019-03-22 22:56:35 +0800

    remove comment

--- packages/ui-app/src/SummaryBox.tsx
+++ packages/ui-app/src/SummaryBox.tsx
@@ -2,7 +2,6 @@
 // This software may be modified and distributed under the terms
 // of the Apache-2.0 license. See the LICENSE file for details.
 
-// Unused atm, experiment as a replacement for NodeInfo on the SideBar
 import { BareProps } from './types';
 
 import React from 'react';
