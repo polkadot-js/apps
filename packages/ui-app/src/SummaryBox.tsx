ec46cbe072bad42aa97dbc8b3b229fc5bfd8a056
Ross Bulat <ross@jkrbinvestments.com>
2019-03-23 09:40:31 +0800

    styled SummaryBox

--- packages/ui-app/src/SummaryBox.tsx
+++ packages/ui-app/src/SummaryBox.tsx
@@ -5,17 +5,55 @@
 import { BareProps } from './types';
 
 import React from 'react';
+import styled from 'styled-components';
 
 type Props = BareProps & {
   children?: React.ReactNode
 };
 
+const StyledSummary = styled.div`
+  align-items: stretch;
+  margin-bottom: 2em;
+  text-align: center;
+  display: flex;
+  justify-content: space-between;
+  flex-wrap: wrap;
+
+	article {
+		color: rgba(0, 0, 0, 0.6);
+		flex: 0 1 auto;
+		text-align: left;
+	}
+
+	> section {
+	display: flex;
+	flex: 0 1 auto;
+	text-align: left;
+	}
+
+	details & {
+		display: block;
+		margin: 0.5rem 0.25rem;
+		opacity: 0.75;
+		outline: none;
+		overflow: hidden;
+		text-align: left;
+		text-overflow: ellipsis;
+		vertical-align: middle;
+		white-space: nowrap;
+
+		+ div {
+			margin-top: 0.75rem;
+		}
+	}
+  `;
+
 export default class SummaryBox extends React.PureComponent<Props> {
   render () {
     return (
-      <div className='ui--summary'>
+      <StyledSummary>
         {this.props.children}
-      </div>
+      </StyledSummary>
     );
   }
 }
