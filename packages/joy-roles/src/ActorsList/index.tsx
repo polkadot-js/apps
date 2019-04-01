226c2878015560add6f1383ab9de30cd04c4b957
Mokhtar Naamani <mokhtar.naamani@gmail.com>
2019-04-01 15:12:16 +0300

    only show unstake button for members

--- packages/joy-roles/src/ActorsList/index.tsx
+++ packages/joy-roles/src/ActorsList/index.tsx
@@ -3,7 +3,7 @@ import { BareProps } from '@polkadot/ui-app/types';
 import { ComponentProps } from '../props';
 import { withCalls } from '@polkadot/ui-api/index';
 import { Table } from 'semantic-ui-react';
-import { Option} from '@polkadot/types';
+import { Option, AccountId} from '@polkadot/types';
 import AddressMini from '@polkadot/ui-app/AddressMiniJoy';
 import { Actor } from '../types';
 import { MemberId } from '@polkadot/joy-members/types';
@@ -28,7 +28,7 @@ class ActorsList extends React.PureComponent<Props> {
                         <Table.HeaderCell>Member Id</Table.HeaderCell>
                         <Table.HeaderCell>Role</Table.HeaderCell>
                         <Table.HeaderCell>Actor Account</Table.HeaderCell>
-                        <Table.HeaderCell>Action</Table.HeaderCell>
+                        <Table.HeaderCell></Table.HeaderCell>
                     </Table.Row>
                 </Table.Header>
                 <Table.Body>{ actorAccountIds.map((account: string) =>
@@ -55,26 +55,21 @@ class ActorInner extends React.PureComponent<ActorProps> {
         const actor = actorOpt.unwrap();
 
         const memberId = memberIdOpt.isSome ? memberIdOpt.unwrap() : undefined;
+        const memberIsActor = memberId && (memberId.toString() == actor.member_id.toString());
 
         return (
             <Table.Row>
                 <Table.Cell>{actor.member_id.toString()}</Table.Cell>
                 <Table.Cell>{actor.role.toString()}</Table.Cell>
                 <Table.Cell><AddressMini value={actor.account} isShort={false} isPadded={false} withBalance={true} withName={true} withMemo={true} size={36} /></Table.Cell>
-                <Table.Cell>{this.renderMemberCell(memberId, actor)}</Table.Cell>
+                { memberIsActor ? <Table.Cell>{this.renderUnstakeTxButton(actor.account)}</Table.Cell> : null }
             </Table.Row>
         )
     }
 
-    private renderMemberCell(memberId: MemberId | undefined, actor: Actor) {
-        const memberIsActor = memberId && (memberId.toString() == actor.member_id.toString());
-
-        if (memberIsActor) {
-            return <TxButton tx={'actors.unstake'} params={[actor.account]} label={'Unstake'}
-                type='submit' size='large' isDisabled={false} />
-        } else {
-            return actor.member_id.toString()
-        }
+    private renderUnstakeTxButton(account: AccountId) {
+        return <TxButton tx={'actors.unstake'} params={[account]} label={'Unstake'}
+            type='submit' size='large' isDisabled={false} />
     }
 }
 
