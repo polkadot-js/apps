a301030e9f96f789cd51e50ffca511eb5214e992
Ross Bulat <ross@jkrbinvestments.com>
2019-04-08 12:10:09 +0800

    bye bye styled

--- packages/ui-app/src/styles/theme.ts
+++ packages/ui-app/src/styles/theme.ts
@@ -2,7 +2,6 @@
 /* This software may be modified and distributed under the terms
 /* of the Apache-2.0 license. See the LICENSE file for details. */
 
-import styled from 'styled-components';
 import theme from 'styled-theming';
 
 export const primaryColor = theme('theme', {
