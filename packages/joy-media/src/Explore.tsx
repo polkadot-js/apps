21550dd37d5ab342ed58aeb8d92e717e44f9bab3
Alex Siman <aleksandr.siman@gmail.com>
2019-04-09 16:46:29 +0300

    Use new preview component on Explore page

--- packages/joy-media/src/Explore.tsx
+++ packages/joy-media/src/Explore.tsx
@@ -1,73 +1,42 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import axios from 'axios';
 
 import { ApiProps } from '@polkadot/ui-api/types';
 import { I18nProps } from '@polkadot/ui-app/types';
+import { withCalls } from '@polkadot/ui-api/with';
+import { Vector } from '@polkadot/types/codec';
 
 import translate from './translate';
-import { Asset, buildApiUrl } from './utils';
-import metamocks from './metamocks';
+import { ContentId } from './types';
+import { View } from './View';
 
-// TODO delete all this mocking stuff once integrated w/ runtime module for storage:
-let mockIdx = 0;
-function nextMock () {
-  const mock = metamocks[mockIdx];
-  mockIdx++;
-  if (mockIdx === metamocks.length) {
-    mockIdx = 0;
-  }
-  return mock;
-}
-
-type Props = ApiProps & I18nProps & {};
-
-type State = {
-  assets?: Asset[]
+type Props = ApiProps & I18nProps & {
+  contentIds?: Vector<ContentId>
 };
 
-class Component extends React.PureComponent<Props, State> {
-
-  state: State = {};
-
-  componentDidMount () {
-    axios
-      .get<{ contents: Asset[] }>(buildApiUrl())
-      .then(res => {
-        const { contents: assets } = res.data;
-        this.setState({ assets });
-      });
-  }
+class Component extends React.PureComponent<Props> {
 
   render () {
-    mockIdx = 0;
-    const { assets } = this.state;
-    if (assets === undefined) return <em>Loading...</em>;
-    else if (assets.length === 0) return <em>No assets found.</em>;
-    else return this.renderAssets(assets);
+    const { contentIds } = this.props;
+    if (contentIds === undefined) return <em>Loading...</em>;
+    else if (contentIds.length === 0) return <em>No content found.</em>;
+    else return this.renderPreviews(contentIds);
   }
 
-  private renderAssets (assets: Asset[]) {
+  private renderPreviews (contentIds: Vector<ContentId>) {
     return (
       <div>
         <div className='MediaGrid'>
-        {assets.map(({ name, size }, i) => {
-          const mock = nextMock();
-          return (
-            <Link className='MediaCell' key={i} to={`/media/play/${name}`}>
-              <div className='CellContent'>
-                <div className='ThumbBox'>
-                  <img className='ThumbImg' src={mock.thumbnail} />
-                </div>
-                <div><b>{name}</b> ({size} bytes)</div>
-              </div>
-            </Link>
-          );
-        })}
+        {contentIds.map((contentId, i) =>
+          <View key={i} contentId={contentId} preview={true} />
+        )}
         </div>
       </div>
     );
   }
 }
 
-export default translate(Component);
+export default translate(
+  withCalls<Props>(
+    ['query.dataDirectory.knownContentIds', { propName: 'contentIds' } ]
+  )(Component)
+);
