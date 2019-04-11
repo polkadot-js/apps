db21bb51e9b1a37f6c236b0b0298585ae40c9ed7
Jaco Greeff <jacogr@gmail.com>
2019-04-12 00:01:58 +0200

    Align labels to the top (#989)
    * Align labels to the top
    
    * Oops.
    
    * Another oops. (last 2 related, unsaved edits...)
    
    * Small CSS adjustments (misc.)

--- packages/ui-app/src/Labelled.tsx
+++ packages/ui-app/src/Labelled.tsx
@@ -24,7 +24,7 @@ const defaultLabel: any = (// node?
 );
 
 const Wrapper = styled.div`
-  align-items: center;
+  align-items: flex-start;
   display: flex;
   flex: 1 1;
   text-align: left;
@@ -46,7 +46,11 @@ const Wrapper = styled.div`
   }
 
   > label {
+    align-items: center;
+    display: flex;
     flex: 0 0 15rem;
+    justify-content: flex-end;
+    min-height: 2.715rem; /* more-or-less 2 lines with adjustments, 38px as per input box */
     min-width: 15rem;
     padding-right: 0.5rem;
     position: relative;
