786c4fb01b6f4126d20e23aef9978dc225789d67
Jaco Greeff <jacogr@gmail.com>
2019-04-11 09:30:45 +0200

    Remove sidebar toggle button jumps (#961)
    * Remove sidebar toggle button jumps
    
    * Fix width

--- packages/apps/src/SideBar/index.tsx
+++ packages/apps/src/SideBar/index.tsx
@@ -96,8 +96,8 @@ class SideBar extends React.PureComponent<Props> {
                   : <NodeInfo />
               }
             </div>
-            {this.renderCollapse()}
           </Menu>
+          {this.renderCollapse()}
           {this.renderToggleBar()}
         </div>
       </Responsive>
