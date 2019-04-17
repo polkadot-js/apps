89fa2215ce2154743f392f42f2bbbaa6d6ed9ab9
Alex Siman <aleksandr.siman@gmail.com>
2019-04-17 12:16:48 +0200

    Show count of applicants / candidacy limit in a title of Applicants page

--- packages/joy-election/src/Applicants.tsx
+++ packages/joy-election/src/Applicants.tsx
@@ -1,26 +1,26 @@
 import React from 'react';
 import { Table } from 'semantic-ui-react';
+import BN from 'bn.js';
 
 import { I18nProps } from '@polkadot/ui-app/types';
 import { ApiProps } from '@polkadot/ui-api/types';
 import { withCalls } from '@polkadot/ui-api/with';
 import { AccountId } from '@polkadot/types';
+import { formatNumber } from '@polkadot/util';
 
 import translate from './translate';
 import Applicant from './Applicant';
 import ApplyForm from './ApplyForm';
 import Section from '@polkadot/joy-utils/Section';
+import { queryToProp } from '@polkadot/joy-utils/index';
 import { withMyAccount, MyAccountProps } from '@polkadot/joy-utils/MyAccount';
 
 type Props = ApiProps & I18nProps & MyAccountProps & {
+  candidacyLimit?: BN,
   applicants?: Array<AccountId>
 };
 
-type State = {};
-
-class Applicants extends React.PureComponent<Props, State> {
-
-  state: State = {};
+class Applicants extends React.PureComponent<Props> {
 
   private renderTable = (applicants: Array<AccountId>) => (
     <Table celled selectable compact>
@@ -39,12 +39,12 @@ class Applicants extends React.PureComponent<Props, State> {
   )
 
   render () {
-    const { myAddress, applicants = [] } = this.props;
-    // console.log({ applicants });
+    const { myAddress, applicants = [], candidacyLimit = new BN(0) } = this.props;
+    const title = <span>Applicants <sup>{applicants.length}/{formatNumber(candidacyLimit)}</sup></span>;
 
     return <>
       <ApplyForm myAddress={myAddress} />
-      <Section title='Applicants'>
+      <Section title={title}>
       {!applicants.length
         ? <em>No applicants yet</em>
         : this.renderTable(applicants)
@@ -57,6 +57,7 @@ class Applicants extends React.PureComponent<Props, State> {
 // inject the actual API calls automatically into props
 export default translate(
   withCalls<Props>(
-    ['query.councilElection.applicants', { propName: 'applicants' }]
+    queryToProp('query.councilElection.candidacyLimit'),
+    queryToProp('query.councilElection.applicants')
   )(withMyAccount(Applicants))
 );
