f7a760b0cb689a8d840277ebf595d9b421baec2c
Stefanie Doll <stefie.doll@googlemail.com>
2019-04-30 14:33:01 +0200

    Poke travis

--- packages/app-settings/src/Developer.tsx
+++ packages/app-settings/src/Developer.tsx
@@ -56,7 +56,7 @@ class Developer extends React.PureComponent<Props, State> {
           <div className='full'>
             <InputFile
               clearContent={typesHasNoEntries && isTypesValid}
-              help={t('Save the type definitions for your custom structures as key-value pairs in a valid JSON file. The key should be the name of your custom structure and the value an object containing your custom type definitions.')}
+              help={t('Save the type definitions for your custom structures as key-value pairs in a valid JSON file. The key should be the name of your custom structure and the value an object containing your type definitions.')}
               isError={!isTypesValid}
               label={t('Upload your additional type definitions as a JSON file')}
               onChange={this.onChangeTypes}
