c35ba2d68f64477740688f507ed8b9672fcb61f8
Thibaut <thibaut.sardan@gmail.com>
2019-05-03 18:51:16 +0200

    implement available according to the rust code

--- packages/ui-reactive/src/Available.tsx
+++ packages/ui-reactive/src/Available.tsx
@@ -4,7 +4,7 @@
 
 import { BareProps, CallProps } from '@polkadot/ui-api/types';
 
-import { AccountId, AccountIndex, Address, Balance, BalanceLock } from '@polkadot/types';
+import { AccountId, AccountIndex, Address, Balance, BalanceLock, BlockNumber } from '@polkadot/types';
 import { formatBalance } from '@polkadot/util';
 import React from 'react';
 import { withCalls } from '@polkadot/ui-api';
@@ -12,20 +12,33 @@ import { withCalls } from '@polkadot/ui-api';
 type Props = BareProps & CallProps & {
   balances_freeBalance?: Balance,
   balances_locks?: Array<BalanceLock>,
+  chain_bestNumber?: BlockNumber,
   children?: React.ReactNode,
   label?: string,
   params?: AccountId | AccountIndex | Address | string | Uint8Array | null
 };
 
 export class AvailableDisplay extends React.PureComponent<Props> {
-  render () {
-    const { balances_freeBalance, balances_locks, children, className, label = '', style } = this.props;
-    const available = balances_freeBalance && balances_locks && balances_locks[0] && balances_locks[0].amount && balances_freeBalance.sub(balances_locks[0].amount);
 
-//    console.log('params', params && params.toString());
-//    console.log('balances_freeBalance',balances_freeBalance && balances_freeBalance.toString());
-//    console.log('balancesLocksAmount',balances_locks && balances_locks.amount && balances_locks.amount.toString());
-//    console.log('available', available && available.toString());
+  maxBN (items: Balance[]) {
+    return items.reduce((acc: Balance, val: Balance) => {
+      return (val.gt(acc) ? val : acc);
+    }, new Balance(0));
+  }
+
+  render () {
+    const { balances_freeBalance, balances_locks, chain_bestNumber, children, className, label = '', style } = this.props;
+    let maxLock = new Balance(0);
+
+    if (Array.isArray(balances_locks)) {
+      // only get the locks that are valid until passed the current block
+      const totals = balances_locks.filter((value, index) => chain_bestNumber && value.until.gt(chain_bestNumber));
+      // get the maximum of the locks according to https://github.com/paritytech/substrate/blob/master/srml/balances/src/lib.rs#L699
+      maxLock = this.maxBN(totals.map(function (o) { return o.amount; }));
+    }
+
+    const available = maxLock.eq(new BlockNumber('0xffffffffffffffffffffffffffffffff')) ? new Balance(0) : balances_freeBalance && balances_freeBalance.sub(maxLock) ;
+
     return (
       <div
         className={className}
@@ -34,7 +47,7 @@ export class AvailableDisplay extends React.PureComponent<Props> {
         {label}{
           available
             ? formatBalance(available)
-            : '0'
+            : formatBalance(balances_freeBalance)
           }{children}
       </div>
     );
@@ -43,5 +56,6 @@ export class AvailableDisplay extends React.PureComponent<Props> {
 
 export default withCalls<Props>(
   ['query.balances.freeBalance', { paramName: 'params' }],
-  ['query.balances.locks', { paramName: 'params' }]
+  ['query.balances.locks', { paramName: 'params' }],
+  'derive.chain.bestNumber'
 )(AvailableDisplay);
