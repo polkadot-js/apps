70225e5b800e580cc364c709b3c5c9d517b89d27
Jaco Greeff <jacogr@gmail.com>
2019-05-11 20:15:14 +0200

    Update account forgetting

--- packages/app-accounts/src/Forgetting.tsx
+++ packages/app-accounts/src/Forgetting.tsx
@@ -5,7 +5,7 @@
 import { I18nProps } from '@polkadot/ui-app/types';
 
 import React from 'react';
-import { AddressSummary, Button, Modal } from '@polkadot/ui-app';
+import { AddressRow, Button, Modal } from '@polkadot/ui-app';
 
 import translate from './translate';
 
@@ -22,7 +22,6 @@ class Forgetting extends React.PureComponent<Props> {
         className='accounts--Forgetting-Modal'
         dimmer='inverted'
         open
-        size='tiny'
       >
         {this.renderContent()}
         {this.renderButtons()}
@@ -61,7 +60,13 @@ class Forgetting extends React.PureComponent<Props> {
           {t('Confirm account removal')}
         </Modal.Header>
         <Modal.Content>
-          <AddressSummary value={address} />
+          <AddressRow
+            isInline
+            value={address}
+          >
+            <p>{t('You are about to remove this account from your list of available accounts. Once completed, should you need access again, you need to re-create the account either via seed or via a backup file.')}</p>
+            <p>{t('This operaion does not remove the history of the account from the chain, not any associated funds from the account. The forget operation only limits your access to the account on this local machine.')}</p>
+          </AddressRow>
         </Modal.Content>
       </>
     );
--- packages/ui-app/src/util/getAddrName.ts
+++ packages/ui-app/src/util/getAddrName.ts
@@ -6,11 +6,17 @@ import keyring from '@polkadot/ui-keyring';
 import toShortAddress from './toShortAddress';
 
 export default function getAddrName (address: string, withShort?: boolean): string | undefined {
-  const pair = keyring.getAccount(address).isValid()
-    ? keyring.getAccount(address)
-    : keyring.getAddress(address);
+  let pair;
 
-  const name = pair.isValid()
+  try {
+    pair = keyring.getAccount(address).isValid()
+      ? keyring.getAccount(address)
+      : keyring.getAddress(address);
+  } catch (error) {
+    // all-ok, we have empty fallbacks
+  }
+
+  const name = pair && pair.isValid()
     ? pair.getMeta().name
     : undefined;
 
