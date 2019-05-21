37670fed9e759494e48dbfed7905863e55286e62
Thibaut <thibaut.sardan@gmail.com>
2019-05-21 20:52:09 +0200

    forgot the useless spacer

--- packages/ui-app/src/AddressSummary.tsx
+++ packages/ui-app/src/AddressSummary.tsx
@@ -356,7 +356,6 @@ class AddressSummary extends React.PureComponent<Props, State> {
 
     const resultingDom = isEditingTags ?
       <>
-        <div className='ui--AddressSummary-tags-spacer'/>
         <InputTags
           className='ui--AddressSummary-tags-input'
           onBlur={this.saveTags}
