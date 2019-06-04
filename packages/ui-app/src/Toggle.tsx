62b6cec6ad56b84a56e88d673ded3dbec8593791
Jaco Greeff <jacogr@gmail.com>
2019-06-04 15:34:53 +0200

    Use toggle for custom endpoint

---
+++ packages/ui-app/src/Toggle.tsx
@@ -0,0 +1,54 @@
+// Copyright 2017-2019 @polkadot/ui-app authors & contributors
+// This software may be modified and distributed under the terms
+// of the Apache-2.0 license. See the LICENSE file for details.
+
+import { BareProps } from './types';
+
+import React from 'react';
+import SUICheckbox from 'semantic-ui-react/dist/commonjs/modules/Checkbox';
+import styled from 'styled-components';
+
+type Props = BareProps & {
+  defaultValue?: boolean,
+  label: React.ReactNode,
+  onChange?: (isChecked: boolean) => void
+};
+
+class Toggle extends React.PureComponent<Props> {
+  render () {
+    const { className, defaultValue, label } = this.props;
+
+    return (
+      <div className={className}>
+        <SUICheckbox
+          defaultChecked={defaultValue}
+          onChange={this.onChange}
+          toggle
+        />
+        <label>{label}</label>
+      </div>
+    );
+  }
+
+  private onChange = (event: React.FormEvent<HTMLInputElement>, { checked }: any): void => {
+    const { onChange } = this.props;
+
+    onChange && onChange(checked);
+  }
+}
+
+export default styled(Toggle)`
+  > label {
+    display: inline-block;
+    margin-left: 0.5rem;
+  }
+
+  > label,
+  > .ui.checkbox {
+    vertical-align: middle;
+  }
+
+  .ui.checkbox + label {
+    color: rgba(78, 78, 78, 0.75);
+  }
+`;
