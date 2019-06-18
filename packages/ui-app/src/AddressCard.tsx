2bde89c407426a2c4d508051e8166fd9dc4ea68b
Jaco Greeff <jacogr@gmail.com>
2019-06-18 16:11:54 +0200

    lint hack

--- packages/ui-app/src/AddressCard.tsx
+++ packages/ui-app/src/AddressCard.tsx
@@ -3,6 +3,8 @@
 // of the Apache-2.0 license. See the LICENSE file for details.
 // import { I18nProps } from '@polkadot/ui-app/types';
 
+// @ts-ignore This line needed for the styled export... don't ask why
+import BN from 'bn.js';
 import React from 'react';
 import styled from 'styled-components';
 
