867abbea0b9f72f7f4cc45f74bf75b1ce4c6e59a
Keith Ingram <kwingram25@gmail.com>
2019-07-01 17:22:02 +0100

    rearrange

--- packages/ui-app/src/ActionItem.tsx
+++ packages/ui-app/src/ActionItem.tsx
@@ -2,10 +2,11 @@
 // This software may be modified and distributed under the terms
 // of the Apache-2.0 license. See the LICENSE file for details.
 
+import { Method, Proposal } from '@polkadot/types';
+
 import BN from 'bn.js';
 import React from 'react';
 import styled from 'styled-components';
-import { Method, Proposal } from '@polkadot/types';
 import { formatNumber } from '@polkadot/util';
 
 import Call from './Call';
