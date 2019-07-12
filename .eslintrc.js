a3457fe528c56d9601a5517a76aad41e4d9c0bad
Jaco Greeff <jacogr@gmail.com>
2019-07-12 23:39:23 +0200

    Add lint:new rule

---
+++ .eslintrc.js
@@ -0,0 +1,1 @@
+module.exports = require('@polkadot/dev-react/config/eslint');
--- packages/app-accounts/src/worker-loader.d.ts
+++ packages/app-accounts/src/worker-loader.d.ts
@@ -1,3 +1,4 @@
+/* eslint-disable */
 declare module 'worker-loader!*' {
   class WebpackWorker extends Worker {
     constructor();
