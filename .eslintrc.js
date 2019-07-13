ef22d76ef03aea2c319c7f54daf9e42c3801e57f
Jaco Greeff <jacogr@gmail.com>
2019-07-13 23:15:04 +0200

    1674 problems (1181 errors, 493 warnings) (no any... for now)

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,1 +1,10 @@
-module.exports = require('@polkadot/dev-react/config/eslint');
+const base = require('@polkadot/dev-react/config/eslint');
+
+// add override for any (a metric ton of them, initial conversion)
+module.exports = {
+  ...base,
+  rules: {
+    ...base.rules,
+    '@typescript-eslint/no-explicit-any': 'off'
+  }
+};
