bb9e54caada4215b95a54fae9aaec1caf1ebe05a
Jaco Greeff <jacogr@gmail.com>
2019-07-18 19:15:44 +0200

    remove wrapped component, only QR{Display, Scan}

--- packages/ui-qr/src/index.tsx
+++ packages/ui-qr/src/index.tsx
@@ -2,57 +2,5 @@
 // This software may be modified and distributed under the terms
 // of the Apache-2.0 license. See the LICENSE file for details.
 
-import { BaseProps } from './types';
-
-import React from 'react';
-
-import QrDisplay from './Display';
-import QrScan from './Scan';
-
-interface Props extends BaseProps {
-  isScanning: boolean;
-  onError?: (error: Error) => void;
-  onScan?: (data: string) => void;
-  size?: number;
-  value?: Record<string, any>;
-}
-
-export default class Qr extends React.PureComponent<Props> {
-  public render (): React.ReactNode {
-    const { isScanning } = this.props;
-
-    return isScanning
-      ? this.renderScan()
-      : this.renderDisplay();
-  }
-
-  private renderDisplay (): React.ReactNode {
-    const { className, size, value } = this.props;
-
-    return (
-      <QrDisplay
-        className={className}
-        size={size}
-        value={value}
-      />
-    );
-  }
-
-  private renderScan (): React.ReactNode {
-    const { className, onError, onScan, size } = this.props;
-
-    return (
-      <QrScan
-        className={className}
-        onError={onError}
-        onScan={onScan}
-        size={size}
-      />
-    );
-  }
-}
-
-export {
-  QrDisplay,
-  QrScan
-};
+export { default as QrDisplay } from './Display';
+export { default as QrScan } from './Scan';
