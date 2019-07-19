786b8c703cc8cec03b0b7be7bbc712ed6b966716
Stefanie Doll <stefie.doll@googlemail.com>
2019-07-19 12:28:06 +0200

    drop pre 1.0 support

--- packages/ui-reactive/src/TimePeriod.tsx
+++ packages/ui-reactive/src/TimePeriod.tsx
@@ -13,33 +13,20 @@ import { formatNumber } from '@polkadot/util';
 type Props = BareProps & CallProps & {
   children?: React.ReactNode;
   label?: React.ReactNode;
-  timestamp_minimumPeriod?: Moment | Option<Moment>; // Type Option<Moment> for pre 1.0 support
-};
+  timestamp_minimumPeriod?: Moment;
+}
 
 export class TimePeriod extends React.PureComponent<Props> {
   public render (): React.ReactNode {
     const { children, className, label = '', style, timestamp_minimumPeriod } = this.props;
-
-    // const period = timestamp_minimumPeriod && timestamp_minimumPeriod as Moment).toNumber
-    // ? (timestamp_minimumPeriod as Moment)
-    // : (timestamp_minimumPeriod as Option<Moment>).unwrapOr(null);
-
-    const period = timestamp_minimumPeriod
-      ? !(timestamp_minimumPeriod as Moment).toNumber
-        ? (timestamp_minimumPeriod as Option<Moment>).unwrapOr(null)
-        : timestamp_minimumPeriod
-      : null;
-
-    console.log('Period', period);
-
     return (
       <div
         className={className}
         style={style}
       >
         {label}{
-          period
-            ? `${formatNumber(period.toNumber() * 2)}s`
+          timestamp_minimumPeriod
+            ? `${formatNumber(timestamp_minimumPeriod.toNumber() * 2)}s`
             : '-'
         }{children}
       </div>
@@ -48,6 +35,6 @@ export class TimePeriod extends React.PureComponent<Props> {
 }
 
 export default withCalls<Props>(
-  // substrate 1.x: 'query.timestamp.blockPeriod',  pre-substrate 1.x:   'query.timestamp.blockPeriod'
-  ['consts.timestamp.minimumPeriod', { fallbacks: ['query.timestamp.minimumPeriod', 'query.timestamp.blockPeriod'] }]
+  // substrate 1.x: 'query.timestamp.blockPeriod'
+  ['consts.timestamp.minimumPeriod', { fallbacks: ['query.timestamp.minimumPeriod'] }]
 )(TimePeriod);
