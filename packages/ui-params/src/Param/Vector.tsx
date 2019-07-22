2e4102620c0655b20a17416240ae1e732afee2f5
Keith Ingram <kwingram25@gmail.com>
2019-07-22 19:08:11 +0100

    rv console

--- packages/ui-params/src/Param/Vector.tsx
+++ packages/ui-params/src/Param/Vector.tsx
@@ -119,7 +119,6 @@ class Vector extends React.PureComponent<Props, State> {
     console.log(type, values[index]);
 
     if (type.type === 'Vec<bool>' && isDisabled && values[index].value === false) {
-      console.log('HIDE');
       return true;
     }
     return false;
