7c900544ad1b337d34f5d227f561307c6da6b881
Keith Ingram <kwingram25@gmail.com>
2019-07-26 23:06:16 +0100

    caps

--- packages/app-explorer/src/BlockInfo/Extrinsics.tsx
+++ packages/app-explorer/src/BlockInfo/Extrinsics.tsx
@@ -83,7 +83,7 @@ class Extrinsics extends React.PureComponent<Props> {
                   : t('mortal')
                 : t('immortal')
             }
-            tip={extrinsic.tip.toBN()}
+            tip={extrinsic.tip.toBn()}
             value={extrinsic}
             withHash
           />
