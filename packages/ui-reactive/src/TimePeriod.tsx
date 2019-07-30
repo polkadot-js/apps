34aedc8af6427f67a801c5c1ec8539a5cb0bb220
Jaco Greeff <jacogr@gmail.com>
2019-07-30 08:48:56 +0200

    Author and expected block period display (Babe)

--- packages/ui-reactive/src/TimePeriod.tsx
+++ packages/ui-reactive/src/TimePeriod.tsx
@@ -11,6 +11,7 @@ import { withCalls } from '@polkadot/ui-api';
 import { formatNumber } from '@polkadot/util';
 
 type Props = BareProps & CallProps & {
+  babe_expectedBlockTime?: Moment;
   children?: React.ReactNode;
   label?: React.ReactNode;
   timestamp_minimumPeriod?: Moment;
@@ -18,23 +19,34 @@ type Props = BareProps & CallProps & {
 
 export class TimePeriod extends React.PureComponent<Props> {
   public render (): React.ReactNode {
-    const { children, className, label = '', style, timestamp_minimumPeriod } = this.props;
+    const { babe_expectedBlockTime, children, className, label = '', style, timestamp_minimumPeriod } = this.props;
     return (
       <div
         className={className}
         style={style}
       >
         {label}{
-          timestamp_minimumPeriod
-            ? `${formatNumber(timestamp_minimumPeriod.toNumber() * 2)}s`
-            : '-'
+          babe_expectedBlockTime
+            ? `${formatNumber(babe_expectedBlockTime.toNumber() / 1000)}s`
+            : timestamp_minimumPeriod
+              ? `${formatNumber(timestamp_minimumPeriod.toNumber() * 2)}s`
+              : '-'
         }{children}
       </div>
     );
   }
 }
 
+// NOTE we are not combining this into one with a fallback, it becomes slightly tricky.
+// There must be a cleaner way with this logic, but as of now it works on Alex & Kusama
+//  - babe bas the exact value in miliseconds
+//  - non-babe on  2.x has the value in miliseconds (not catered for here)
+//  - anything non-babe, i.e. Aura, needs to be multiplied by 2
 export default withCalls<Props>(
+  // substrate 2.x: consts.babe.expectedBlockTime
+  'consts.babe.expectedBlockTime',
   // substrate 1.x: 'query.timestamp.blockPeriod'
-  ['consts.timestamp.minimumPeriod', { fallbacks: ['query.timestamp.minimumPeriod'] }]
+  ['consts.timestamp.minimumPeriod', {
+    fallbacks: ['query.timestamp.minimumPeriod']
+  }]
 )(TimePeriod);
