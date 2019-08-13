a41ec55d145c3b44b96a6cd8ff2ec3eb6df875f3
Keith Ingram <kwingram25@gmail.com>
2019-08-13 19:01:59 +0000

    fix changed event sig

--- packages/app-staking/src/index.tsx
+++ packages/app-staking/src/index.tsx
@@ -3,15 +3,16 @@
 // This software may be modified and distributed under the terms
 // of the Apache-2.0 license. See the LICENSE file for details.
 
-import { AccountId, BlockNumber, EventRecord } from '@polkadot/types/interfaces';
+import { AccountId, AuthorityId, BlockNumber, EventRecord } from '@polkadot/types/interfaces';
 import { AppProps, I18nProps } from '@polkadot/react-components/types';
 import { ApiProps } from '@polkadot/react-api/types';
 import { SubjectInfo } from '@polkadot/ui-keyring/observable/types';
 import { ComponentProps } from './types';
 
+import BN from 'bn.js';
 import React from 'react';
 import { Route, Switch } from 'react-router';
-import { Option } from '@polkadot/types';
+import { createType, Option } from '@polkadot/types';
 import { HelpOverlay } from '@polkadot/react-components';
 import Tabs, { TabItem } from '@polkadot/react-components/Tabs';
 import { withCalls, withMulti, withObservable } from '@polkadot/react-api';
@@ -27,8 +28,9 @@ import translate from './translate';
 type Props = AppProps & ApiProps & I18nProps & {
   allAccounts?: SubjectInfo;
   allStashesAndControllers?: [AccountId[], Option<AccountId>[]];
+  chain_bestNumber?: BlockNumber;
   currentValidatorsControllersV1OrStashesV2?: AccountId[];
-  recentlyOnline?: Record<string, BlockNumber>;
+  recentlyOnline?: AuthorityId[];
 };
 
 interface State {
@@ -67,11 +69,17 @@ class App extends React.PureComponent<Props, State> {
   }
 
   public static getDerivedStateFromProps (props: Props, state: State): Pick<State, never> {
-    const { allStashesAndControllers = [[], []], currentValidatorsControllersV1OrStashesV2 = [] } = props;
+    const { allStashesAndControllers = [[], []], chain_bestNumber, currentValidatorsControllersV1OrStashesV2 = [] } = props;
 
-    const recentlyOnline = {
-      ...state.recentlyOnline || {},
-      ...props.recentlyOnline || {}
+    const recentlyOnline: Record<string, BlockNumber> = {
+      ...(state.recentlyOnline || {}),
+      ...(props.recentlyOnline || []).reduce(
+        (result: Record<string, BlockNumber>, authorityId): Record<string, BlockNumber> => ({
+          ...result,
+          [authorityId.toString()]: chain_bestNumber || createType('BlockNumber', new BN(0))
+        }),
+        {}
+      )
     };
 
     return {
@@ -137,27 +145,21 @@ export default withMulti(
   App,
   translate,
   withCalls<Props>(
+    'derive.chain.bestNumber',
     ['derive.staking.controllers', { propName: 'allStashesAndControllers' }],
     ['query.session.validators', { propName: 'currentValidatorsControllersV1OrStashesV2' }],
     [
       'query.system.events',
       {
         propName: 'recentlyOnline',
-        transform: (value?: EventRecord[]): Record<string, BlockNumber> => {
-          return (value || [])
-            .filter(({ event: { method, section } }): boolean => {
-              return section === 'imOnline' && method === 'HeartbeatReceived';
-            })
-            .reduce(
-              (result: Record<string, BlockNumber>, { event: { data: [blockNumber, authorityId] } }): Record<string, BlockNumber> => {
-                return {
-                  ...result,
-                  [authorityId.toString()]: blockNumber as BlockNumber
-                };
-              },
-              {}
-            );
-        }
+        transform: (value?: EventRecord[]): AuthorityId[] =>
+          (value || [])
+            .filter(({ event: { method, section } }): boolean =>
+              section === 'imOnline' && method === 'HeartbeatReceived'
+            )
+            .map(
+              ({ event: { data: [authorityId] } }): AuthorityId => authorityId as AuthorityId
+            )
       }
     ]
   ),
