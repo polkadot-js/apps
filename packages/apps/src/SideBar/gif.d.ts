23cac4cd2bff66bdb1662b1872a39aa313a9f151
Jaco Greeff <jacogr@gmail.com>
2019-08-13 11:57:58 +0200

    Allow chain-specific logo overrides (#1499)
    * Allow chain-specific logo overrides
    
    * Round-out available logos (current state)

---
+++ packages/apps/src/SideBar/gif.d.ts
@@ -0,0 +1,9 @@
+// Copyright 2017-2019 @polkadot/ui-assets authors & contributors
+// This software may be modified and distributed under the terms
+// of the Apache-2.0 license. See the LICENSE file for details.
+
+declare module '*.gif' {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const content: any;
+  export default content;
+}
--- packages/apps/src/SideBar/logos.ts
+++ packages/apps/src/SideBar/logos.ts
@@ -2,11 +2,33 @@
 // This software may be modified and distributed under the terms
 // of the Apache-2.0 license. See the LICENSE file for details.
 
-import polkadotSmall from '@polkadot/ui-assets/notext-polkadot.svg';
-import substrateSmall from '@polkadot/ui-assets/notext-parity-substrate-white.svg';
+// the imports here as a bit all-over, non-aphabetical - since we expect this to grow,
+// rather organise based on type, grouping chains and nodes as opposed to location
 
-export default function getLogo (node?: string): any {
-  return (node || '').indexOf('polkadot') !== -1
-    ? polkadotSmall
-    : substrateSmall;
+// last-resort fallback, just something empty
+import EMPTY from '@polkadot/ui-assets/empty.svg';
+
+// anything fopr a specific chain, most items will probably go in here
+import chainKusama from '@polkadot/ui-assets/chains/kusama-128.gif';
+
+// defaults for the node type, assuming we don't have a specific chain
+import polkadot from '@polkadot/ui-assets/polkadot-circle.svg';
+import polkadotJs from '@polkadot/ui-assets/polkadot-js.svg';
+import substrate from '@polkadot/ui-assets/substrate-white.svg';
+
+// overrides based on the actual matched chain name
+const CHAINS: Record<string, any> = {
+  Kusama: chainKusama
+};
+
+// overrides based on the actual software node type
+const NODES: Record<string, any> = {
+  'node-template': substrate,
+  'parity-polkadot': polkadot,
+  'polkadot-js': polkadotJs,
+  'substrate-node': substrate
+};
+
+export default function getLogo (node: string = '', chain: string = ''): any {
+  return CHAINS[chain] || NODES[node] || EMPTY;
 }
