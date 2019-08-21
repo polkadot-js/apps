a4dff716357e6098bef3e445fc24d33f3fd114b7
Jaco Greeff <jacogr@gmail.com>
2019-08-21 22:11:01 +0200

    Ok, well, it does something... theoretically...

--- packages/app-explorer/src/Forks.tsx
+++ packages/app-explorer/src/Forks.tsx
@@ -9,6 +9,7 @@ import { Header } from '@polkadot/types/interfaces';
 
 import React from 'react';
 import { withCalls, withMulti } from '@polkadot/react-api';
+import { formatNumber, hexToU8a, u8aToHex } from '@polkadot/util';
 
 import translate from './translate';
 
@@ -20,19 +21,71 @@ interface Props extends ApiProps, I18nProps {
 }
 
 interface ForkHeader {
+  hash: string;
   header: Header;
   isFinalized: boolean;
 }
 
+type ForkHeaders = Record<string, ForkHeader[]>;
+
 interface State {
-  all: Record<number, ForkHeader[]>;
+  all: ForkHeaders;
   finHeads: Header[];
   newHeads: Header[];
 }
 
+function mapToColumns (newHeads: Header[]): ForkHeaders {
+  return newHeads.reduce((all: ForkHeaders, header, index): ForkHeaders => {
+    const num = formatNumber(header.number);
+    const hash = header.hash.toHex();
+    const parent = header.parentHash.toHex();
+
+    // this is our first entry, so just add it to a column, get out
+    if (index === 0) {
+      all[num] = [{ hash, header, isFinalized: false }];
+    } else {
+      if (!all[num]) {
+        all[num] = [];
+      }
+
+      const bns = Object.values(all);
+      const maxCols = bns.reduce((maxCols, columns): number =>
+        Math.max(maxCols, columns.length), 0
+      );
+      let column = -1;
+
+      for (let row = bns.length - 1; column === -1 && row >= 0; row--) {
+        bns[row].forEach((entry, index): void => {
+          if (parent === entry.hash) {
+            column = index;
+          }
+        });
+      }
+
+      all[num][column === -1 ? maxCols : column] = { hash, header, isFinalized: false };
+    }
+
+    return all;
+  }, {});
+}
+
+function markFinalized (all: ForkHeaders, finHeads: Header[]): void {
+  finHeads.forEach((header): void => {
+    const hash = header.hash.toHex();
+
+    Object.values(all).forEach((columns): void => {
+      columns.forEach((column): void => {
+        if (column.hash === hash) {
+          column.isFinalized = true;
+        }
+      });
+    });
+  });
+}
+
 class Forks extends React.PureComponent<Props, State> {
   public state: State = {
-    all: [],
+    all: {},
     newHeads: [],
     finHeads: []
   };
@@ -45,17 +98,36 @@ class Forks extends React.PureComponent<Props, State> {
     subscribeFinalizedHeads && prevState.finHeads.push(subscribeFinalizedHeads);
     subscribeNewHead && prevState.newHeads.push(subscribeNewHead);
 
-    const finHeads = prevState.finHeads.slice(0, MAX_HEADS);
-    const newHeads = prevState.newHeads.slice(0, MAX_HEADS);
+    const finHeads = prevState.finHeads.slice(0, Math.min(prevState.finHeads.length, MAX_HEADS));
+    const newHeads = prevState.newHeads.slice(0, Math.min(prevState.newHeads.length, MAX_HEADS));
+    const all = mapToColumns(newHeads);
+
+    markFinalized(all, finHeads);
 
     return {
+      all,
       finHeads,
       newHeads
     };
   }
 
   public render (): React.ReactNode {
-    return 'check the console';
+    const { all } = this.state;
+
+    return (
+      <table>
+        <tbody>
+          {Object.entries(all).map(([bn, columns]): React.ReactNode => (
+            <tr key={bn}>
+              <td key='blockNumber'>{bn}</td>
+              {columns.map((column, index): React.ReactNode => (
+                <td key={index}>{column && u8aToHex(hexToU8a(column.hash), 64)}</td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    );
   }
 }
 
