4948f36ecc4d1728f45e8fd2a9d2a488da2d206f
Jaco Greeff <jacogr@gmail.com>
2019-08-31 11:22:20 +0200

    Setup for typescript-eslint 2

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,6 +3,13 @@ const base = require('@polkadot/dev-react/config/eslint');
 // add override for any (a metric ton of them, initial conversion)
 module.exports = {
   ...base,
+  parserOptions: {
+    ...base.parserOptions,
+    extraFileExtensions: ['*.d.ts'],
+    project: [
+      './tsconfig.eslint.json'
+    ]
+  },
   rules: {
     ...base.rules,
     '@typescript-eslint/no-explicit-any': 'off'
