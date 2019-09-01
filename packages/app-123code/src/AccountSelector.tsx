75147d0c92b760c33417d28b9835b8b36bda0e77
Jaco Greeff <jacogr@gmail.com>
2019-09-01 14:44:34 +0200

    Some func hooks in 123-code

--- packages/app-123code/src/AccountSelector.tsx
+++ packages/app-123code/src/AccountSelector.tsx
@@ -2,7 +2,7 @@
 // This software may be modified and distributed under the terms
 // of the Apache-2.0 license. See the LICENSE file for details.
 
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import { Bubble, InputAddress } from '@polkadot/react-components';
 import { AccountIndex, Balance, Nonce } from '@polkadot/react-query';
@@ -12,47 +12,32 @@ interface Props {
   onChange: (accountId?: string) => void;
 }
 
-interface State {
-  accountId?: string;
-}
+function AccountSelector ({ className, onChange }: Props): React.ReactElement<Props> {
+  const [accountId, setAccountId] = useState<string | undefined>();
 
-class AccountSelector extends React.PureComponent<Props, State> {
-  public state: State = {};
+  useEffect((): void => onChange(accountId), [accountId]);
 
-  public render (): React.ReactNode {
-    const { className } = this.props;
-    const { accountId } = this.state;
-
-    return (
-      <section className={`template--AccountSelector ui--row ${className}`}>
-        <InputAddress
-          className='medium'
-          label='my default account'
-          onChange={this.onChange}
-          type='account'
-        />
-        <div className='medium'>
-          <Bubble color='teal' icon='address card' label='index'>
-            <AccountIndex params={accountId} />
-          </Bubble>
-          <Bubble color='yellow' icon='adjust' label='balance'>
-            <Balance params={accountId} />
-          </Bubble>
-          <Bubble color='yellow' icon='target' label='transactions'>
-            <Nonce params={accountId} />
-          </Bubble>
-        </div>
-      </section>
-    );
-  }
-
-  private onChange = (accountId?: string): void => {
-    const { onChange } = this.props;
-
-    this.setState({ accountId }, (): void =>
-      onChange(accountId)
-    );
-  }
+  return (
+    <section className={`template--AccountSelector ui--row ${className}`}>
+      <InputAddress
+        className='medium'
+        label='my default account'
+        onChange={setAccountId}
+        type='account'
+      />
+      <div className='medium'>
+        <Bubble color='teal' icon='address card' label='index'>
+          <AccountIndex params={accountId} />
+        </Bubble>
+        <Bubble color='yellow' icon='adjust' label='balance'>
+          <Balance params={accountId} />
+        </Bubble>
+        <Bubble color='yellow' icon='target' label='transactions'>
+          <Nonce params={accountId} />
+        </Bubble>
+      </div>
+    </section>
+  );
 }
 
 export default styled(AccountSelector)`
