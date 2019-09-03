fbcb0f3001ed2d907e3574aa7435d3dd38c7dd91
Jaco Greeff <jacogr@gmail.com>
2019-09-03 14:43:00 +0200

    Detect periods as secs or miliseconds (#1580)
    * Detect periods as secs or miliseconds
    
    * Cleanup timer unmount
    
    * Additional NodeInfo cleanups

--- packages/app-explorer/src/NodeInfo/index.tsx
+++ packages/app-explorer/src/NodeInfo/index.tsx
@@ -2,13 +2,12 @@
 // This software may be modified and distributed under the terms
 // of the Apache-2.0 license. See the LICENSE file for details.
 
-import { Health, PeerInfo, Extrinsic } from '@polkadot/types/interfaces';
 import { I18nProps } from '@polkadot/react-components/types';
 import { Info } from './types';
 
 import React, { useContext, useEffect, useState } from 'react';
+import { ApiPromise } from '@polkadot/api';
 import { ApiContext } from '@polkadot/react-api';
-import { Vec } from '@polkadot/types';
 
 import './index.css';
 
@@ -21,39 +20,45 @@ const POLL_TIMEOUT = 9900;
 
 type Props = I18nProps;
 
+async function retrieveInfo (api: ApiPromise): Promise<Partial<Info>> {
+  try {
+    const [blockNumber, health, peers, extrinsics] = await Promise.all([
+      api.derive.chain.bestNumber(),
+      api.rpc.system.health(),
+      api.rpc.system.peers(),
+      api.rpc.author.pendingExtrinsics()
+    ]);
+
+    return { blockNumber, extrinsics, health, peers };
+  } catch (error) {
+    return {};
+  }
+}
+
 function NodeInfo ({ t }: Props): React.ReactElement<Props> {
   const { api } = useContext(ApiContext);
   const [info, setInfo] = useState<Partial<Info>>({});
   const [nextRefresh, setNextRefresh] = useState(Date.now());
   const [timerId, setTimerId] = useState(0);
 
-  const executeStatus = (): void => {
+  const executeStatus = (isFirstRun = false): void => {
     // eslint-disable-next-line @typescript-eslint/no-use-before-define
-    getStatus().catch((): void => {});
+    getStatus(isFirstRun).catch((): void => {});
   };
-  const getStatus = async (): Promise<void> => {
-    try {
-      const [blockNumber, health, peers, extrinsics] = await Promise.all([
-        api.derive.chain.bestNumber(),
-        api.rpc.system.health<Health>(),
-        api.rpc.system.peers<Vec<PeerInfo>>(),
-        api.rpc.author.pendingExtrinsics<Vec<Extrinsic>>()
-      ]);
-
-      setInfo({ blockNumber, extrinsics, health, peers });
-    } catch (error) {
-      setInfo({});
+  const getStatus = async (isFirstRun: boolean): Promise<void> => {
+    if (isFirstRun || timerId) {
+      setInfo(await retrieveInfo(api));
+      setNextRefresh(Date.now() + POLL_TIMEOUT);
+      setTimerId(window.setTimeout(executeStatus, POLL_TIMEOUT));
     }
-
-    setNextRefresh(Date.now() + POLL_TIMEOUT);
-    setTimerId(window.setTimeout(executeStatus, POLL_TIMEOUT));
   };
 
   useEffect((): () => void => {
-    executeStatus();
+    executeStatus(true);
 
     return (): void => {
       window.clearTimeout(timerId);
+      setTimerId(0);
     };
   }, []);
 
