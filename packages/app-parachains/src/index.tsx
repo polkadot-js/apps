bf392874760cad6b675b5c3981f0fec629ff8f7f
Jaco Greeff <jacogr@gmail.com>
2019-09-06 17:59:31 +0200

    App wrappers -> func

--- packages/app-parachains/src/index.tsx
+++ packages/app-parachains/src/index.tsx
@@ -3,7 +3,6 @@
 // of the Apache-2.0 license. See the LICENSE file for details.
 
 import { AppProps, BareProps, I18nProps } from '@polkadot/react-components/types';
-import { TabItem } from '@polkadot/react-components/Tabs';
 
 import React from 'react';
 import { Route, Switch } from 'react-router';
@@ -14,45 +13,26 @@ import translate from './translate';
 
 interface Props extends AppProps, BareProps, I18nProps {}
 
-interface State {
-  tabs: TabItem[];
-}
-
-class App extends React.PureComponent<Props, State> {
-  public constructor (props: Props) {
-    super(props);
-
-    const { t } = props;
-
-    this.state = {
-      tabs: [
-        {
-          isRoot: true,
-          name: 'overview',
-          text: t('Parachains overview')
-        }
-      ]
-    };
-  }
-
-  public render (): React.ReactNode {
-    const { basePath } = this.props;
-    const { tabs } = this.state;
-
-    return (
-      <main>
-        <header>
-          <Tabs
-            basePath={basePath}
-            items={tabs}
-          />
-        </header>
-        <Switch>
-          <Route component={Overview} />
-        </Switch>
-      </main>
-    );
-  }
+function App ({ basePath, t }: Props): React.ReactElement<Props> {
+  return (
+    <main>
+      <header>
+        <Tabs
+          basePath={basePath}
+          items={[
+            {
+              isRoot: true,
+              name: 'overview',
+              text: t('Parachains overview')
+            }
+          ]}
+        />
+      </header>
+      <Switch>
+        <Route component={Overview} />
+      </Switch>
+    </main>
+  );
 }
 
 export default translate(App);
