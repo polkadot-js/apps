3b15601d2b184ca9cd6ac7be76ec2d74d56b5e32
Jaco Greeff <jacogr@gmail.com>
2019-09-15 08:48:16 +0200

    Edgeware mainnet RPC & updated types (#1615)

--- packages/react-api/src/overrides/spec/edgeware.ts
+++ packages/react-api/src/overrides/spec/edgeware.ts
@@ -4,10 +4,12 @@
 
 import { IdentityTypes } from 'edgeware-node-types/dist/identity';
 import { SignalingTypes } from 'edgeware-node-types/dist/signaling';
+import { TreasuryRewardTypes } from 'edgeware-node-types/dist/treasuryReward';
 import { VotingTypes } from 'edgeware-node-types/dist/voting';
 
 export default {
   ...IdentityTypes,
   ...SignalingTypes,
+  ...TreasuryRewardTypes,
   ...VotingTypes
 };
