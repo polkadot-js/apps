352fbff91d1e7672974842629477324d5ba3183c
Jaco Greeff <jacogr@gmail.com>
2019-11-07 21:25:19 +0100

    Slice logs

--- packages/react-query/src/Events.tsx
+++ packages/react-query/src/Events.tsx
@@ -42,7 +42,7 @@ function Events ({ children }: Props): React.ReactElement<Props> {
         const newEventHash = xxhashAsHex(stringToU8a(JSON.stringify(newEvents)));
 
         if (newEventHash !== prevEventHash) {
-          setState(newEvents.concat(...state).filter((_, index): boolean => index < MAX_EVENTS));
+          setState(newEvents.concat(...state).slice(0, MAX_EVENTS));
         }
       });
     });
