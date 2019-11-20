8dfd36ffa127e2f761704f722a584cb8fef75d59
Keith Ingram <kwingram25@gmail.com>
2019-11-19 22:47:45 -0800

    Fix Vec<u8> issue (#1916)
    * fix vec<u8>
    
    * api version
    
    * console
    
    * lint

--- packages/react-params/src/Param/Struct.tsx
+++ packages/react-params/src/Param/Struct.tsx
@@ -6,7 +6,6 @@ import { TypeDef } from '@polkadot/types/types';
 import { ParamDef, Props, RawParam } from '../types';
 
 import React, { useEffect, useState } from 'react';
-import { createType, getTypeDef } from '@polkadot/types';
 
 import Params from '../';
 import Base from './Base';
@@ -17,10 +16,7 @@ export default function StructParam (props: Props): React.ReactElement<Props> {
   const [params, setParams] = useState<ParamDef[]>([]);
 
   useEffect((): void => {
-    const rawType = createType(type.type as any).toRawType();
-    const typeDef = getTypeDef(rawType);
-
-    setParams((typeDef.sub as TypeDef[]).map((type): ParamDef => ({ name: type.name, type })));
+    setParams((type.sub as TypeDef[]).map((subType): ParamDef => ({ name: subType.name, type: subType })));
   }, [type]);
 
   if (isDisabled) {
