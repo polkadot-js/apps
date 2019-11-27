d944bae0b5e6681f2a0a079c84c3d1c9c3a4090c
Jaco Greeff <jacogr@gmail.com>
2019-11-27 14:38:58 +0100

    Nomination target dashboard

---
+++ packages/app-staking/src/Targets/types.ts
@@ -0,0 +1,25 @@
+// Copyright 2017-2019 @polkadot/app-staking authors & contributors
+// This software may be modified and distributed under the terms
+// of the Apache-2.0 license. See the LICENSE file for details.
+
+import { AccountId, Balance } from '@polkadot/types/interfaces';
+
+import BN from 'bn.js';
+
+export interface ValidatorInfo {
+  accountId: AccountId;
+  bondOther: BN;
+  bondOwn: Balance;
+  bondShare: number;
+  bondTotal: Balance;
+  commission: Balance;
+  isFavorite: boolean;
+  key: string;
+  numNominators: number;
+  rankBonded: number;
+  rankCommission: number;
+  rankOverall: number;
+  rankReward: number;
+  rewardPayout: BN;
+  rewardSplit: BN;
+}
