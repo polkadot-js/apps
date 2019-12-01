fcf22a6c05333ab8f2521b27121e5ca4c36681c4
Xiliang Chen <xlchen1291@gmail.com>
2019-12-02 10:59:59 +1300

    add acala types (#2)

--- packages/apps/src/index.tsx
+++ packages/apps/src/index.tsx
@@ -40,6 +40,48 @@ const wsEndpoint = _wsEndpoint.split('#')[0];
 
 console.log('WS endpoint=', wsEndpoint);
 
+// Acala Types
+// TODO: import from acala.js instead
+registry.register({
+  AuctionId: 'u32',
+  AuctionIdOf: 'AuctionId',
+  CurrencyId: 'u8',
+  CurrencyIdOf: 'CurrencyId',
+  Amount: 'i128',
+  AmountOf: 'Amount',
+  DebitAmount: 'Amount',
+  DebitAmountOf: 'DebitAmount',
+  DebitBalanceOf: 'Balance',
+  AuctionInfo: {
+    bid: 'Option<(AccountId, Balance)>',
+    start: 'BlockNumber',
+    end: 'Option<BlockNumber>'
+  },
+  AuctionIdLinkedItem: {
+    prev: 'Option<AuctionId>',
+    next: 'Option<AuctionId>'
+  },
+  FixedU128: 'u128',
+  ExchangeRate: 'FixedU128',
+  Price: 'FixedU128',
+  OracleKey: 'CurrencyId',
+  OracleValue: 'Price',
+  Rate: 'FixedU128',
+  Ratio: 'FixedU128',
+  TimestampedValue: {
+    value: 'OracleValue',
+    timestamp: 'Moment'
+  },
+  TimestampedValueOf: 'TimestampedValue',
+  AuctionItem: {
+    owner: 'AccountId',
+    currencyId: 'CurrencyId',
+    amount: 'Balance',
+    target: 'Balance',
+    startTime: 'BlockNumber'
+  }
+});
+
 try {
   const types = store.get('types') || {};
   const names = Object.keys(types);
