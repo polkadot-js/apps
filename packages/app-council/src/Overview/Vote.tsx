516a46ebc51baea9dae954f1e37bbe8f721aedbf
Jaco Greeff <jacogr@gmail.com>
2019-12-09 23:17:24 +0100

    Bump API, fixed electionsPhragmen

--- packages/app-council/src/Overview/Vote.tsx
+++ packages/app-council/src/Overview/Vote.tsx
@@ -2,17 +2,15 @@
 // This software may be modified and distributed under the terms
 // of the Apache-2.0 license. See the LICENSE file for details.
 
+import { ApiProps } from '@polkadot/react-api/types';
 import { AccountId, VoteIndex } from '@polkadot/types/interfaces';
 import { Codec } from '@polkadot/types/types';
-import { DerivedVoterPositions } from '@polkadot/api-derive/types';
-import { ApiProps } from '@polkadot/react-api/types';
 import { ComponentProps } from './types';
 
 import BN from 'bn.js';
 import React from 'react';
+import { withApi } from '@polkadot/react-api';
 import styled from 'styled-components';
-import { createType } from '@polkadot/types';
-import { registry, withCalls, withMulti } from '@polkadot/react-api';
 import { AddressMini, Button, Toggle } from '@polkadot/react-components';
 import TxModal, { TxModalState, TxModalProps } from '@polkadot/react-components/TxModal';
 
@@ -21,31 +19,14 @@ import VoteValue from './VoteValue';
 
 type VoteType = 'member' | 'runnerup' | 'candidate';
 
-interface Props extends ApiProps, ComponentProps, TxModalProps {
-  voterPositions?: DerivedVoterPositions;
-}
+interface Props extends ApiProps, ComponentProps, TxModalProps {}
 
 interface State extends TxModalState {
   votes: Record<string, boolean>;
   voteValue: BN;
-  // voterPositions: DerivedVoterPositions;
 }
 
 // const MAX_VOTES = 16;
-
-// const AlreadyVoted = styled.article`
-//   display: flex;
-//   align-items: center;
-//   margin: 0.5rem 0;
-
-//   & > :first-child {
-//     flex: 1 1;
-//   }
-
-//   & > :not(:first-child) {
-//     margin: 0;
-//   }
-// `;
 
 const Candidates = styled.div`
   display: flex;
@@ -121,29 +102,17 @@ class Vote extends TxModal<Props, State> {
   protected txMethod = (): string =>
     this.props.api.tx.electionsPhragmen
       ? 'electionsPhragmen.vote'
-      : 'elections.setApprovals';
+      : 'elections.vote';
 
   protected txParams = (): [boolean[] | null, VoteIndex, BN | null] | [string[], BN] => {
-    const { api, electionsInfo: { candidates, nextVoterSet, voteCount }, voterPositions } = this.props;
-    const { accountId, votes, voteValue } = this.state;
-
-    if (api.tx.electionsPhragmen) {
-      return [
-        Object.entries(votes).filter(([, vote]): boolean => vote).map(([accountId]): string => accountId),
-        voteValue
-      ];
-    }
-
-    const approvals = candidates.map((accountId): boolean => votes[accountId.toString()] === true);
+    const { votes, voteValue } = this.state;
 
     return [
-      approvals
-        ? approvals.slice(0, 1 + approvals.lastIndexOf(true))
-        : [],
-      createType(registry, 'VoteIndex', voteCount),
-      voterPositions && accountId && voterPositions[accountId]
-        ? voterPositions[accountId].setIndex
-        : nextVoterSet || null
+      Object
+        .entries(votes)
+        .filter(([, vote]): boolean => vote)
+        .map(([accountId]): string => accountId),
+      voteValue
     ];
   }
 
@@ -155,13 +124,11 @@ class Vote extends TxModal<Props, State> {
   }
 
   protected renderTrigger = (): React.ReactNode => {
-    const { api, electionsInfo: { candidates, members, runnersUp }, t } = this.props;
-    const available = api.tx.electionsPhragmen
-      ? members
-        .map(([accountId]): AccountId => accountId)
-        .concat(runnersUp.map(([accountId]): AccountId => accountId))
-        .concat(candidates)
-      : candidates;
+    const { electionsInfo: { candidates, members, runnersUp }, t } = this.props;
+    const available = members
+      .map(([accountId]): AccountId => accountId)
+      .concat(runnersUp.map(([accountId]): AccountId => accountId))
+      .concat(candidates);
 
     return (
       <Button
@@ -175,43 +142,20 @@ class Vote extends TxModal<Props, State> {
   }
 
   protected renderContent = (): React.ReactNode => {
-    const { api, electionsInfo: { candidates, members, runnersUp }, t } = this.props;
+    const { electionsInfo: { candidates, members, runnersUp }, t } = this.props;
     const { accountId, votes } = this.state;
     const _candidates = candidates.map((accountId): [AccountId, VoteType] => [accountId, 'candidate']);
-    const available = api.tx.electionsPhragmen
-      ? members
-        .map(([accountId]): [AccountId, VoteType] => [accountId, 'member'])
-        .concat(runnersUp.map(([accountId]): [AccountId, VoteType] => [accountId, 'runnerup']))
-        .concat(_candidates)
-      : _candidates;
+    const available = members
+      .map(([accountId]): [AccountId, VoteType] => [accountId, 'member'])
+      .concat(runnersUp.map(([accountId]): [AccountId, VoteType] => [accountId, 'runnerup']))
+      .concat(_candidates);
 
     return (
       <>
-        {api.tx.electionsPhragmen && (
-          <VoteValue
-            accountId={accountId}
-            onChange={this.setVoteValue}
-          />
-        )}
-        {/* {(oldApprovals && accountId && voterPositions && voterPositions[accountId]) && (
-          <AlreadyVoted className='warning padded'>
-            <div>
-              <Icon name='warning sign' />
-              {t('You have already voted in this round')}
-            </div>
-            <Button.Group>
-              <TxButton
-                accountId={accountId}
-                isNegative
-                label={t('Retract vote')}
-                icon='delete'
-                onSuccess={this.onRetractVote}
-                params={[voterPositions[accountId].globalIndex]}
-                tx='elections.retractVoter'
-              />
-            </Button.Group>
-          </AlreadyVoted>
-        )} */}
+        <VoteValue
+          accountId={accountId}
+          onChange={this.setVoteValue}
+        />
         <Candidates>
           {available.map(([accountId, type]): React.ReactNode => {
             const key = accountId.toString();
@@ -246,50 +190,20 @@ class Vote extends TxModal<Props, State> {
     this.setState({ voteValue });
   }
 
-  private fetchApprovals = (accountId: string | null): void => {
-    const { api, electionsInfo: { candidates, voteCount } } = this.props;
-
-    if (!accountId || !voteCount) {
-      return;
-    }
-
-    // FIXME This any is a mismatch in api-derive
-    api.derive.elections
-      .approvalsOfAt(accountId as any, voteCount)
-      .then((approvals: boolean[]): void => {
-        this.setState({
-          votes: candidates.reduce((votes: Record<string, boolean>, accountId, index): Record<string, boolean> => ({
-            ...votes,
-            [accountId.toString()]: approvals[index] || false
-          }), {})
-        });
-      });
-  }
-
-  private fetchVotes = (accountId: string | null): void => {
-    const { api } = this.props;
-
-    if (!accountId || !api.tx.electionsPhragmen) {
-      return;
-    }
-
-    api.query.electionsPhragmen
-      .votesOf<[AccountId[]] & Codec>(accountId)
-      .then(([existingVotes]): void => {
-        existingVotes.forEach((accountId): void => {
-          this.onChangeVote(accountId.toString())(true);
-        });
-      });
-  }
-
   protected onChangeAccount = (accountId: string | null): void => {
     const { api } = this.props;
 
     this.setState({ accountId });
 
-    api.tx.electionsPhragmen
-      ? this.fetchVotes(accountId)
-      : this.fetchApprovals(accountId);
+    if (accountId) {
+      (api.query.electionsPhragmen || api.query.elections)
+        .votesOf<[AccountId[]] & Codec>(accountId)
+        .then(([existingVotes]): void => {
+          existingVotes.forEach((accountId): void => {
+            this.onChangeVote(accountId.toString())(true);
+          });
+        });
+    }
   }
 
   private onChangeVote = (accountId: string): (isChecked: boolean) => void =>
@@ -303,10 +217,4 @@ class Vote extends TxModal<Props, State> {
     }
 }
 
-export default withMulti(
-  Vote,
-  translate,
-  withCalls<Props>(
-    ['derive.elections.voterPositions', { propName: 'voterPositions' }]
-  )
-);
+export default translate(withApi(Vote));
