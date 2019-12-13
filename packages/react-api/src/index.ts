e54e652ab50919a31640ea017a6f7b71a756fa1f
Jaco Greeff <jacogr@gmail.com>
2019-12-13 11:16:45 +0100

    More

--- packages/react-api/src/index.ts
+++ packages/react-api/src/index.ts
@@ -2,12 +2,15 @@
 // This software may be modified and distributed under the terms
 // of the Apache-2.0 license. See the LICENSE file for details.
 
+import { TypeRegistry } from '@polkadot/types';
+
 import Api, { api } from './Api';
 import ApiContext from './ApiContext';
-import registry from './typeRegistry';
 
 import { withApi, withCalls, withCallDiv, withMulti, withObservable } from './hoc';
 import { isLedgerCapable, isLedger, clearLedger, getLedger } from './ledger';
+
+const registry = new TypeRegistry();
 
 export default Api;
 
