e728842d00794602b110678a4f537350b96b1d5f
Jaco Greeff <jacogr@gmail.com>
2019-12-13 12:57:45 +0100

    Cleanup imports

--- packages/react-api/src/index.ts
+++ packages/react-api/src/index.ts
@@ -2,23 +2,24 @@
 // This software may be modified and distributed under the terms
 // of the Apache-2.0 license. See the LICENSE file for details.
 
-import { TypeRegistry } from '@polkadot/types';
-
 import Api, { api } from './Api';
 import ApiContext from './ApiContext';
-
 import { withApi, withCalls, withCallDiv, withMulti, withObservable } from './hoc';
-import { isLedgerCapable, isLedger, clearLedger, getLedger } from './ledger';
-
-const registry = new TypeRegistry();
-
-export default Api;
+import { clearLedger, getLedger, isLedger, isLedgerCapable } from './ledger';
+import registry from './typeRegistry';
 
 export {
   api,
   Api,
   ApiContext,
   registry,
-  withApi, withCalls, withCallDiv, withMulti, withObservable,
-  isLedgerCapable, isLedger, clearLedger, getLedger
+  withApi,
+  withCalls,
+  withCallDiv,
+  withMulti,
+  withObservable,
+  clearLedger,
+  getLedger,
+  isLedger,
+  isLedgerCapable
 };
--- packages/react-query/src/LockedVote.tsx
+++ packages/react-query/src/LockedVote.tsx
@@ -1,9 +1,8 @@
-/* eslint-disable @typescript-eslint/camelcase */
 // Copyright 2017-2019 @polkadot/react-query authors & contributors
 // This software may be modified and distributed under the terms
 // of the Apache-2.0 license. See the LICENSE file for details.
 
-import { BareProps, CallProps } from '@polkadot/react-api/types';
+import { BareProps } from '@polkadot/react-api/types';
 import { AccountId, AccountIndex, Address, BalanceOf } from '@polkadot/types/interfaces';
 
 import React from 'react';
@@ -11,7 +10,7 @@ import { useApi, useCall } from '@polkadot/react-hooks';
 
 import FormatBalance from './FormatBalance';
 
-interface Props extends BareProps, CallProps {
+interface Props extends BareProps {
   children?: React.ReactNode;
   label?: React.ReactNode;
   params?: AccountId | AccountIndex | Address | string | Uint8Array | null;
