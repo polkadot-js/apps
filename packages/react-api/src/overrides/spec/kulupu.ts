1ccc6b2baed1778c438345a02925f7b9eeb244b3
Jaco Greeff <jacogr@gmail.com>
2020-01-06 17:28:02 +0100

    Kulupu

--- packages/react-api/src/overrides/spec/kulupu.ts
+++ packages/react-api/src/overrides/spec/kulupu.ts
@@ -2,10 +2,12 @@
 // This software may be modified and distributed under the terms
 // of the Apache-2.0 license. See the LICENSE file for details.
 
-import edgeware from './edgeware';
-import kulupu from './kulupu';
-
 export default {
-  edgeware,
-  kulupu
+  Difficulty: 'U256',
+  DifficultyAndTimestamp: {
+    difficulty: 'Difficulty',
+    timestamp: 'Momemt'
+  },
+  DispatchError: 'DispatchErrorTo198',
+  DispatchResult: 'DispatchResultTo198'
 };
