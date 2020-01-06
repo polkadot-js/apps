33d063c3bb4e7571dbaaac3f77713db207471215
Jaco Greeff <jacogr@gmail.com>
2020-01-06 16:16:50 +0100

    Bump deps (#2091)

--- packages/react-components/src/Status/Context.ts
+++ packages/react-components/src/Status/Context.ts
@@ -1,4 +1,4 @@
-// Copyright 2017-2019 @polkadot/react-components authors & contributors
+// Copyright 2017-2020 @polkadot/react-components authors & contributors
 // This software may be modified and distributed under the terms
 // of the Apache-2.0 license. See the LICENSE file for details.
 
