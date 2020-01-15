c66224b2f23ccce1c0d4f952eece1fa646a3a4be
Jaco Greeff <jacogr@gmail.com>
2020-01-15 21:39:03 +0100

    Unused import

--- packages/apps/src/overlays/Connecting.tsx
+++ packages/apps/src/overlays/Connecting.tsx
@@ -2,8 +2,6 @@
 // This software may be modified and distributed under the terms
 // of the Apache-2.0 license. See the LICENSE file for details.
 
-import { ApiProps } from '@polkadot/react-api/types';
-
 import React from 'react';
 import styled from 'styled-components';
 import { useApi } from '@polkadot/react-hooks';
