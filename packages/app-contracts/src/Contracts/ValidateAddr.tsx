56ebc980e52e31b49d823b56370fc5b076a68cd6
Jaco Greeff <jacogr@gmail.com>
2020-01-17 13:55:14 +0100

    No withCall for all functional (#2042)
    * No withCall for all functional
    
    * Linting
    
    * Remove HOC, unneeded eslint override
    
    * Cleanups

--- packages/app-contracts/src/Contracts/ValidateAddr.tsx
+++ packages/app-contracts/src/Contracts/ValidateAddr.tsx
@@ -1,27 +1,26 @@
-/* eslint-disable @typescript-eslint/camelcase */
 // Copyright 2017-2020 @polkadot/app-contracts authors & contributors
 // This software may be modified and distributed under the terms
 // of the Apache-2.0 license. See the LICENSE file for details.
 
 import { ContractInfo } from '@polkadot/types/interfaces';
-import { ApiProps } from '@polkadot/react-api/types';
 
 import React, { useEffect, useState } from 'react';
 import { Option } from '@polkadot/types';
-import { withCalls } from '@polkadot/react-api/hoc';
+import { useApi, useCall } from '@polkadot/react-hooks';
 import { InfoForInput } from '@polkadot/react-components';
 import keyring from '@polkadot/ui-keyring';
 
 import { useTranslation } from '../translate';
 
-interface Props extends ApiProps {
+interface Props {
   address?: string | null;
-  contracts_contractInfoOf?: Option<ContractInfo>;
   onChange: (isValid: boolean) => void;
 }
 
-function ValidateAddr ({ address, contracts_contractInfoOf, onChange }: Props): React.ReactElement<Props> | null {
+export default function ValidateAddr ({ address, onChange }: Props): React.ReactElement<Props> | null {
   const { t } = useTranslation();
+  const { api } = useApi();
+  const contractInfo = useCall<Option<ContractInfo>>(api.query.contracts.contractInfoOf, [address]);
   const [isAddress, setIsAddress] = useState(false);
   const [isStored, setIsStored] = useState(false);
 
@@ -35,8 +34,8 @@ function ValidateAddr ({ address, contracts_contractInfoOf, onChange }: Props): 
   }, [address]);
 
   useEffect((): void => {
-    setIsStored(!!contracts_contractInfoOf && contracts_contractInfoOf.isSome);
-  }, [contracts_contractInfoOf]);
+    setIsStored(!!contractInfo?.isSome);
+  }, [contractInfo]);
 
   useEffect((): void => {
     onChange(isAddress && isStored);
@@ -48,18 +47,10 @@ function ValidateAddr ({ address, contracts_contractInfoOf, onChange }: Props): 
 
   return (
     <InfoForInput type='error'>
-      {
-        isAddress
-          ? t('Unable to find deployed contract code at the specified address')
-          : t('The value is not in a valid address format')
+      {isAddress
+        ? t('Unable to find deployed contract code at the specified address')
+        : t('The value is not in a valid address format')
       }
     </InfoForInput>
   );
 }
-
-export default withCalls<Props>(
-  ['query.contracts.contractInfoOf', {
-    fallbacks: ['query.contract.contractInfoOf'],
-    paramName: 'address'
-  }]
-)(ValidateAddr);
