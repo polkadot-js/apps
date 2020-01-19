7328dbd1f156f7f24f83f116e1f90a39324b3fc1
Jaco Greeff <jacogr@gmail.com>
2020-01-19 20:45:12 +0100

    Remove IdentityInfo override

--- packages/react-params/src/Param/findComponent.ts
+++ packages/react-params/src/Param/findComponent.ts
@@ -19,7 +19,6 @@ import DispatchError from './DispatchError';
 import Enum from './Enum';
 import Hash256 from './Hash256';
 import Hash512 from './Hash512';
-import IdentityInfo from './IdentityInfo';
 import KeyValue from './KeyValue';
 import KeyValueArray from './KeyValueArray';
 import Moment from './Moment';
@@ -52,7 +51,6 @@ const components: ComponentMap = ([
   { c: Enum, t: ['Enum'] },
   { c: Hash256, t: ['BlockHash', 'CodeHash', 'Hash', 'H256', 'SeedOf'] },
   { c: Hash512, t: ['H512', 'Signature'] },
-  { c: IdentityInfo, t: ['IdentityInfo'] },
   { c: KeyValue, t: ['KeyValue'] },
   { c: KeyValueArray, t: ['Vec<KeyValue>'] },
   { c: Moment, t: ['Moment', 'MomentOf'] },
