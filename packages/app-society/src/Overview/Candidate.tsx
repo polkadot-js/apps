5cab16d1f06fc535caec22248660405cb5f68eca
Jaco Greeff <jacogr@gmail.com>
2020-01-28 10:13:44 +0100

    Society vouching (#2204)
    * Society vouching
    
    * Bump API, decode votes
    
    * Inline count
    
    * Remove unused style

--- packages/app-society/src/Overview/Candidate.tsx
+++ packages/app-society/src/Overview/Candidate.tsx
@@ -3,19 +3,53 @@
 // of the Apache-2.0 license. See the LICENSE file for details.
 
 import { DeriveSocietyCandidate } from '@polkadot/api-derive/types';
+import { AccountId, SocietyVote } from '@polkadot/types/interfaces';
 
-import React from 'react';
-import { AddressSmall } from '@polkadot/react-components';
+import React, { useEffect, useState } from 'react';
+import { AddressMini, AddressSmall } from '@polkadot/react-components';
+import { useApi, useCall } from '@polkadot/react-hooks';
 import { FormatBalance } from '@polkadot/react-query';
+import { Option } from '@polkadot/types';
 
 import { useTranslation } from '../translate';
+import CandidateVoting from './CandidateVoting';
 
 interface Props {
+  allMembers: string[];
+  isMember: boolean;
+  ownMembers: string[];
   value: DeriveSocietyCandidate;
 }
 
-export default function Candidate ({ value: { accountId, kind, value } }: Props): React.ReactElement<Props> {
+type VoteType = [string, SocietyVote];
+
+interface VoteSplit {
+  allAye: VoteType[];
+  allNay: VoteType[];
+  allSkeptic: VoteType[];
+}
+
+export default function Candidate ({ allMembers, isMember, ownMembers, value: { accountId, kind, value } }: Props): React.ReactElement<Props> {
   const { t } = useTranslation();
+  const { api } = useApi();
+  const [{ allAye, allNay, allSkeptic }, setVoteSplit] = useState<VoteSplit>({ allAye: [], allNay: [], allSkeptic: [] });
+  const votes = useCall<VoteType[]>(api.query.society.votes.multi as any, [allMembers.map((memberId): [AccountId, string] => [accountId, memberId])] as any, {
+    transform: (voteOpts: Option<SocietyVote>[]): VoteType[] =>
+      voteOpts
+        .map((voteOpt, index): [string, Option<SocietyVote>] => [allMembers[index], voteOpt])
+        .filter(([, voteOpt]): boolean => voteOpt.isSome)
+        .map(([accountId, voteOpt]): VoteType => [accountId, voteOpt.unwrap()])
+  });
+
+  useEffect((): void => {
+    if (votes) {
+      setVoteSplit({
+        allAye: votes.filter(([, vote]): boolean => vote.isApprove),
+        allNay: votes.filter(([, vote]): boolean => vote.isReject),
+        allSkeptic: votes.filter(([, vote]): boolean => vote.isSkeptic)
+      });
+    }
+  }, [votes]);
 
   return (
     <tr>
@@ -30,6 +64,58 @@ export default function Candidate ({ value: { accountId, kind, value } }: Props)
         <FormatBalance
           label={<label>{t('value')}</label>}
           value={value}
+        />
+      </td>
+      <td className='top padtop'>
+        {allSkeptic.length !== 0 && (
+          <details>
+            <summary>
+              {t('Skeptics ({{count}})', { replace: { count: allSkeptic.length } })}
+            </summary>
+            {allSkeptic.map(([who]): React.ReactNode =>
+              <AddressMini
+                key={who.toString()}
+                value={who}
+              />
+            )}
+          </details>
+        )}
+      </td>
+      <td className='top padtop'>
+        {allAye.length !== 0 && (
+          <details>
+            <summary>
+              {t('Approvals ({{count}})', { replace: { count: allAye.length } })}
+            </summary>
+            {allAye.map(([who]): React.ReactNode =>
+              <AddressMini
+                key={who.toString()}
+                value={who}
+              />
+            )}
+          </details>
+        )}
+      </td>
+      <td className='top padtop'>
+        {allNay.length !== 0 && (
+          <details>
+            <summary>
+              {t('Rejections ({{count}})', { replace: { count: allNay.length } })}
+            </summary>
+            {allNay.map(([who]): React.ReactNode =>
+              <AddressMini
+                key={who.toString()}
+                value={who}
+              />
+            )}
+          </details>
+        )}
+      </td>
+      <td className='number together top'>
+        <CandidateVoting
+          candidateId={accountId.toString()}
+          isMember={isMember}
+          ownMembers={ownMembers}
         />
       </td>
     </tr>
---
+++ packages/app-society/src/useMembers.ts
@@ -0,0 +1,35 @@
+// Copyright 2017-2020 @polkadot/app-society authors & contributors
+// This software may be modified and distributed under the terms
+// of the Apache-2.0 license. See the LICENSE file for details.
+
+import { DeriveSocietyMember } from '@polkadot/api-derive/types';
+
+import { useEffect, useState } from 'react';
+import { useAccounts, useApi, useCall } from '@polkadot/react-hooks';
+
+interface OwnMembers {
+  allMembers: string[];
+  isMember: boolean;
+  ownMembers: string[];
+}
+
+export default function useMembers (): OwnMembers {
+  const { api } = useApi();
+  const { allAccounts } = useAccounts();
+  const members = useCall<DeriveSocietyMember[]>(api.derive.society.members, []);
+  const [ownState, setOwnState] = useState<OwnMembers>({ allMembers: [], isMember: false, ownMembers: [] });
+
+  useEffect((): void => {
+    if (allAccounts && members) {
+      const allMembers = members
+        .filter((member): boolean => !member.isSuspended)
+        .map((member): string => member.accountId.toString());
+      const ownMembers = allMembers
+        .filter((address): boolean => allAccounts.includes(address));
+
+      setOwnState({ allMembers, isMember: ownMembers.length !== 0, ownMembers });
+    }
+  }, [allAccounts, members]);
+
+  return ownState;
+}
