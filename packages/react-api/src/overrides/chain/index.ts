0e009dcb79fc4ed4c9203093c234e4bdd9f872fb
Philip Stanislaus <6912756+philipstanislaus@users.noreply.github.com>
2020-02-11 14:23:00 +0100

    Fix type overwrites

--- packages/react-api/src/overrides/chain/index.ts
+++ packages/react-api/src/overrides/chain/index.ts
@@ -2,11 +2,11 @@
 // This software may be modified and distributed under the terms
 // of the Apache-2.0 license. See the LICENSE file for details.
 
-import amber from './amber';
+import CentrifugeChainAmber from './centrifuge-chain-amber';
 import Westend from './westend';
 
 export default {
-  amber,
-  Amber: amber,
+  'Flint Testnet CC2': CentrifugeChainAmber,
+  'Centrifuge Testnet Amber': CentrifugeChainAmber,
   Westend,
 };
