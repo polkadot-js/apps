45bc406ced7d067a691f910fe07571620760c8f0
Philip Stanislaus <6912756+philipstanislaus@users.noreply.github.com>
2020-02-11 14:34:26 +0100

    Move types to spec overrides

--- packages/react-api/src/overrides/chain/index.ts
+++ packages/react-api/src/overrides/chain/index.ts
@@ -2,11 +2,8 @@
 // This software may be modified and distributed under the terms
 // of the Apache-2.0 license. See the LICENSE file for details.
 
-import CentrifugeChainAmber from './centrifuge-chain-amber';
 import Westend from './westend';
 
 export default {
-  'Flint Testnet CC2': CentrifugeChainAmber,
-  'Centrifuge Testnet Amber': CentrifugeChainAmber,
-  Westend,
+  Westend
 };
