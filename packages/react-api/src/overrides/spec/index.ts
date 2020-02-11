73f27a0d773ba01c6909677c1f3b59cbfb5f361b
philipstanislaus <6912756+philipstanislaus@users.noreply.github.com>
2020-02-11 20:49:25 +0100

    Add centrifuge chain types (#2268)

--- packages/react-api/src/overrides/spec/index.ts
+++ packages/react-api/src/overrides/spec/index.ts
@@ -2,10 +2,12 @@
 // This software may be modified and distributed under the terms
 // of the Apache-2.0 license. See the LICENSE file for details.
 
+import centrifugeChain from './centrifuge-chain';
 import edgeware from './edgeware';
 import kulupu from './kulupu';
 
 export default {
+  'centrifuge-chain': centrifugeChain,
   edgeware,
   kulupu
 };
