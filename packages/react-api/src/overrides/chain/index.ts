4d2f5ae424d1ce63a1605a0b0f3132f33dee95b2
Philip Stanislaus <6912756+philipstanislaus@users.noreply.github.com>
2020-02-11 14:04:41 +0100

    Add type overwrites

--- packages/react-api/src/overrides/chain/index.ts
+++ packages/react-api/src/overrides/chain/index.ts
@@ -2,8 +2,11 @@
 // This software may be modified and distributed under the terms
 // of the Apache-2.0 license. See the LICENSE file for details.
 
+import amber from './amber';
 import Westend from './westend';
 
 export default {
-  Westend
+  amber,
+  Amber: amber,
+  Westend,
 };
