91024206aa1cc7afb38c1e73166084b1c9d47b6c
Jaco Greeff <jacogr@gmail.com>
2020-02-13 17:10:10 +0100

    Bump dev

--- packages/apps/webpack.config.js
+++ packages/apps/webpack.config.js
@@ -83,7 +83,7 @@ function createWebpack ({ alias = {}, context, name = 'index' }) {
             require.resolve('thread-loader'),
             {
               loader: require.resolve('babel-loader'),
-              options: require('@polkadot/dev-react/config/babel')
+              options: require('@polkadot/dev/config/babel')
             }
           ]
         },
