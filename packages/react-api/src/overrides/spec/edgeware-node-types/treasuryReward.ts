16478d63d177052b0bae1bb213fa314abc3ed404
Jaco Greeff <jacogr@gmail.com>
2020-02-20 14:04:07 +0100

    Adjust Edgeware types (imports)

---
+++ packages/react-api/src/overrides/spec/edgeware-node-types/treasuryReward.ts
@@ -0,0 +1,7 @@
+import { u128 } from '@polkadot/types';
+
+export class Balance2 extends u128 { }
+
+export const TreasuryRewardTypes = {
+  Balance2
+};
--- packages/react-api/src/overrides/spec/edgeware.ts
+++ packages/react-api/src/overrides/spec/edgeware.ts
@@ -2,7 +2,8 @@
 // This software may be modified and distributed under the terms
 // of the Apache-2.0 license. See the LICENSE file for details.
 
-import { EdgewareTypes } from 'edgeware-node-types/dist';
+// import { EdgewareTypes } from 'edgeware-node-types/dist';
+import { EdgewareTypes } from './edgeware-node-types';
 
 export default {
   ...EdgewareTypes,
