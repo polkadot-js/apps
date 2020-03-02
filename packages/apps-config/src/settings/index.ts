aaa2b178b0b9396183461fec32701377a4d7e978
Jaco Greeff <jacogr@gmail.com>
2020-03-02 09:08:08 +0100

    Pull in prefix settings in apps-config

--- packages/apps-config/src/settings/index.ts
+++ packages/apps-config/src/settings/index.ts
@@ -2,10 +2,12 @@
 // This software may be modified and distributed under the terms
 // of the Apache-2.0 license. See the LICENSE file for details.
 
-import availableLanguages from './languages';
 import availableEndpoints from './endpoints';
+import availableLanguages from './languages';
+import availableSs58 from './ss58';
 
 export {
   availableEndpoints,
-  availableLanguages
+  availableLanguages,
+  availableSs58
 };
---
+++ packages/apps-config/src/settings/ss58.ts
@@ -0,0 +1,38 @@
+// Copyright 2017-2020 @polkadot/ui-settings authors & contributors
+// This software may be modified and distributed under the terms
+// of the Apache-2.0 license. See the LICENSE file for details.
+
+import { Option } from './types';
+
+export default [
+  {
+    info: 'default',
+    text: 'Default for the connected node',
+    value: -1
+  },
+  {
+    info: 'substrate',
+    text: 'Substrate (generic)',
+    value: 42
+  },
+  {
+    info: 'polkadot',
+    text: 'Polkadot (live)',
+    value: 0
+  },
+  {
+    info: 'kusama',
+    text: 'Kusama (canary)',
+    value: 2
+  },
+  {
+    info: 'edgeware',
+    text: 'Edgeware (live)',
+    value: 7
+  },
+  {
+    info: 'centrifuge',
+    text: 'Centrifuge (live)',
+    value: 36
+  }
+].map((option): Option => ({ ...option, withI18n: true }));
