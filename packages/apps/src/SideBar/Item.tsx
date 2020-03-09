ef8e78a44a40f828195a73324456d06d796508cf
Jaco Greeff <jacogr@gmail.com>
2020-03-09 08:59:02 +0100

    Add staking payout alert counter

--- packages/apps/src/SideBar/Item.tsx
+++ packages/apps/src/SideBar/Item.tsx
@@ -103,7 +103,11 @@ export default function Item ({ route, isCollapsed, onClick }: Props): React.Rea
       <Icon name={icon} />
       <span className='text'>{t(`sidebar.${name}`, i18n)}</span>
       {count !== 0 && (
-        <Badge isInline info={count} type='counter' />
+        <Badge
+          isInline
+          info={count}
+          type='counter'
+        />
       )}
       <Tooltip
         offset={TOOLTIP_OFFSET}
--- packages/page-staking/src/Actions/Account/index.tsx
+++ packages/page-staking/src/Actions/Account/index.tsx
@@ -10,7 +10,7 @@ import { Codec, ITuple } from '@polkadot/types/types';
 import BN from 'bn.js';
 import React, { useContext, useEffect, useState } from 'react';
 import styled from 'styled-components';
-import { AddressInfo, AddressMini, AddressSmall, Button, Menu, Popup, StatusContext, TxButton } from '@polkadot/react-components';
+import { AddressInfo, AddressMini, AddressSmall, Badge, Button, Menu, Popup, StatusContext, TxButton } from '@polkadot/react-components';
 import { useAccounts, useApi, useCall, useToggle } from '@polkadot/react-hooks';
 import { u8aConcat, u8aToHex } from '@polkadot/util';
 
@@ -168,6 +168,17 @@ function Account ({ allStashes, className, isOwnStash, next, onUpdateType, staki
 
   return (
     <tr className={className}>
+      <td>
+        {payoutEras.length !== 0 && (
+          <Badge
+            hover={t('payouts for {{count}} eras pending', { replace: { count: payoutEras.length } })}
+            info={payoutEras.length}
+            isInline
+            isTooltip
+            type='counter'
+          />
+        )}
+      </td>
       <td className='top'>
         <BondExtra
           controllerId={controllerId}
