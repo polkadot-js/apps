51d51a7291cca9fd7fbca7e63f1ea8d81035d867
Jaco Greeff <jacogr@gmail.com>
2020-03-14 15:06:24 +0100

    Chain-specific types (Berlin) (#2385)
    * Chain-specific types (Berlin)
    
    * file case

--- packages/apps-config/src/api/chain/berlin.ts
+++ packages/apps-config/src/api/chain/berlin.ts
@@ -2,10 +2,7 @@
 // This software may be modified and distributed under the terms
 // of the Apache-2.0 license. See the LICENSE file for details.
 
-import Berlin from './berlin';
-import Westend from './westend';
-
 export default {
-  Berlin,
-  Westend
+  BalanceLock: 'BalanceLockTo212',
+  StakingLedger: 'StakingLedgerTo223'
 };
