0cff9a324b352c504be658ac1b1248a92884439e
Jaco Greeff <jacogr@gmail.com>
2020-03-18 10:17:20 +0100

    Re-add React.memo for linked inputs (#2407)

--- packages/react-components/src/InputRpc/SelectSection.tsx
+++ packages/react-components/src/InputRpc/SelectSection.tsx
@@ -18,8 +18,7 @@ interface Props extends BareProps {
   options: DropdownOptions;
   value: RpcMethod;
 }
-
-export default function SelectSection ({ className, defaultValue, isError, onChange, options, style, value }: Props): React.ReactElement<Props> {
+function SelectSection ({ className, defaultValue, isError, onChange, options, style, value }: Props): React.ReactElement<Props> {
   return (
     <Dropdown
       className={classes('ui--DropdownLinked-Sections', className)}
@@ -33,3 +32,5 @@ export default function SelectSection ({ className, defaultValue, isError, onCha
     />
   );
 }
+
+export default React.memo(SelectSection);
