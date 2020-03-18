c9129748688ffe21dcd9c60bfc843807d44cb3f0
Jaco Greeff <jacogr@gmail.com>
2020-03-18 01:06:07 +0100

    Revert Kusama hack (#2409)

--- packages/react-signer/src/Modal.tsx
+++ packages/react-signer/src/Modal.tsx
@@ -629,7 +629,7 @@ class Signer extends React.PureComponent<Props, State> {
         api.setSigner(injected.signer);
         params.push(address);
       } else {
-        params.push(pair, { genesisHash: '0x00' });
+        params.push(pair);
       }
     }
 
