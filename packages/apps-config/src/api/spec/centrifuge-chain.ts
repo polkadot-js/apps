4791adefbe19cf9c01b9dcf8ec715aea39839470
Philip Stanislaus <6912756+philipstanislaus@users.noreply.github.com>
2020-03-20 19:27:32 +0100

    Make linter happy

--- packages/apps-config/src/api/spec/centrifuge-chain.ts
+++ packages/apps-config/src/api/spec/centrifuge-chain.ts
@@ -3,26 +3,26 @@
 // of the Apache-2.0 license. See the LICENSE file for details.
 
 export default {
-   // chain-specific
+  // chain-specific
   AnchorData: {
     id: 'H256',
     docRoot: 'H256',
-    anchoredBlock: 'u64',
+    anchoredBlock: 'u64'
   },
   Fee: {
     key: 'Hash',
-    price: 'Balance',
+    price: 'Balance'
   },
   PreCommitData: {
     signingRoot: 'H256',
     identity: 'H256',
-    expirationBlock: 'u64',
+    expirationBlock: 'u64'
   },
   Proof: {
     leafHash: 'H256',
-    sortedHashes: 'H256',
+    sortedHashes: 'H256'
   },
   // Overwrites to Substrate types
-  Address: "AccountId",
-  LookupSource: "AccountId",
+  Address: 'AccountId',
+  LookupSource: 'AccountId'
 };
