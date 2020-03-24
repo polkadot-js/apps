d07903b6705da32ed76748a6d0a8f7fe0485cdd8
KarishmaBothara <bothara.karishma@gmail.com>
2020-03-25 12:46:54 +1300

    Removed all endpoints except the local node (#16)

--- packages/app-settings/src/available/endpoints.ts
+++ packages/app-settings/src/available/endpoints.ts
@@ -6,41 +6,6 @@ import { Option } from '../types';
 
 export default [
   {
-    info: 'kusama',
-    text: 'Kusama (Polkadot Canary, hosted by Parity)',
-    value: 'wss://kusama-rpc.polkadot.io/'
-  },
-  {
-    info: 'kusama',
-    text: 'Kusama (Polkadot Canary, hosted by Web3 Foundation)',
-    value: 'wss://cc3-5.kusama.network/'
-  },
-  {
-    info: 'westend',
-    text: 'Westend (Polkadot Testnet, hosted by Parity)',
-    value: 'wss://westend-rpc.polkadot.io'
-  },
-  {
-    info: 'edgeware',
-    text: 'Edgeware (Edgeware Mainnet, hosted by Commonwealth Labs)',
-    value: 'wss://mainnet1.edgewa.re'
-  },
-  {
-    info: 'edgeware',
-    text: 'Berlin (Edgeware Testnet, hosted by Commonwealth Labs)',
-    value: 'wss://berlin1.edgewa.re'
-  },
-  {
-    info: 'substrate',
-    text: 'Flaming Fir (Substrate Testnet, hosted by Parity)',
-    value: 'wss://substrate-rpc.parity.io/'
-  },
-  {
-    info: 'substrate',
-    text: 'Kulupu (Kulupu Mainnet, hosted by Kulupu)',
-    value: 'wss://rpc.kulupu.network/ws'
-  },
-  {
     info: 'local',
     text: 'Local Node (Own, 127.0.0.1:9944)',
     value: 'ws://127.0.0.1:9944/'
