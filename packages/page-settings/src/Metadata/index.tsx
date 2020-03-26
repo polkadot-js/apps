c957b1291af904086c76db68dfacd9ea3ae64cdd
Hanwen Cheng <heawen.cheng@gmail.com>
2020-03-26 21:16:25 +0100

    Update packages/page-settings/src/Metadata/index.tsx
    Co-Authored-By: Jaco Greeff <jacogr@gmail.com>

--- packages/page-settings/src/Metadata/index.tsx
+++ packages/page-settings/src/Metadata/index.tsx
@@ -1,3 +1,7 @@
+// Copyright 2017-2020 @polkadot/app-settings authors & contributors
+// This software may be modified and distributed under the terms
+// of the Apache-2.0 license. See the LICENSE file for details.
+
 import { useTranslation } from '@polkadot/app-settings/translate';
 import useCounter from '@polkadot/app-settings/useCounter';
 import React, { useMemo } from 'react';
