31796b87f30337e8fcf78dd354c7a663f0124cdf
Jaco Greeff <jacogr@gmail.com>
2020-04-06 12:28:29 +0200

    ChangePass from TxComponent (#2520)
    * ChangePass from TxComponent
    
    * Import as functional
    
    * Cleanups (vanity)
    
    * Use bundled node

--- packages/page-accounts/src/Accounts/modals/Import.tsx
+++ packages/page-accounts/src/Accounts/modals/Import.tsx
@@ -3,62 +3,108 @@
 // of the Apache-2.0 license. See the LICENSE file for details.
 
 import { KeyringPair$Json } from '@polkadot/keyring/types';
-import { I18nProps } from '@polkadot/react-components/types';
 import { ActionStatus } from '@polkadot/react-components/Status/types';
 import { ModalProps } from '../../types';
 
-import React from 'react';
-import { AddressRow, Button, InputAddress, InputFile, Modal, Password, TxComponent } from '@polkadot/react-components';
+import React, { useCallback, useState } from 'react';
+import { AddressRow, Button, InputAddress, InputFile, Modal, Password } from '@polkadot/react-components';
 import { isHex, isObject, u8aToString } from '@polkadot/util';
 import keyring from '@polkadot/ui-keyring';
 
-import translate from '../../translate';
+import { useTranslation } from '../../translate';
 
-interface Props extends ModalProps, I18nProps {}
+interface Props extends ModalProps {
+  className?: string;
+}
 
-interface State {
+interface FileState {
   address: string | null;
   isFileValid: boolean;
-  isPassValid: boolean;
   json: KeyringPair$Json | null;
+}
+
+interface PassState {
+  isPassValid: boolean;
   password: string;
 }
 
-class Import extends TxComponent<Props, State> {
-  public state: State = {
-    address: null,
-    isFileValid: false,
-    isPassValid: false,
-    json: null,
-    password: ''
-  };
+const acceptedFormats = ['application/json', 'text/plain'].join(', ');
 
-  public render (): React.ReactNode {
-    const { onClose, t } = this.props;
-    const { isFileValid, isPassValid } = this.state;
-
-    return (
-      <Modal header={t('Add via backup file')}>
-        {this.renderInput()}
-        <Modal.Actions onCancel={onClose}>
-          <Button
-            icon='sync'
-            isDisabled={!isFileValid || !isPassValid}
-            isPrimary
-            label={t('Restore')}
-            onClick={this.onSave}
-          />
-        </Modal.Actions>
-      </Modal>
+function parseFile (file: Uint8Array): FileState {
+  try {
+    const json = JSON.parse(u8aToString(file));
+    const publicKey = keyring.decodeAddress(json.address, true);
+    const address = keyring.encodeAddress(publicKey);
+    const isFileValid = publicKey.length === 32 && isHex(json.encoded) && isObject(json.meta) && (
+      Array.isArray(json.encoding.content)
+        ? json.encoding.content[0] === 'pkcs8'
+        : json.encoding.content === 'pkcs8'
     );
+
+    return { address, isFileValid, json };
+  } catch (error) {
+    console.error(error);
   }
 
-  private renderInput (): React.ReactNode {
-    const { t } = this.props;
-    const { address, isFileValid, isPassValid, json, password } = this.state;
-    const acceptedFormats = ['application/json', 'text/plain'].join(', ');
+  return { address: null, isFileValid: false, json: null };
+}
 
-    return (
+function Import ({ className, onClose, onStatusChange }: Props): React.ReactElement<Props> {
+  const { t } = useTranslation();
+  const [{ address, isFileValid, json }, setFile] = useState<FileState>({ address: null, isFileValid: false, json: null });
+  const [{ isPassValid, password }, setPass] = useState<PassState>({ isPassValid: false, password: '' });
+
+  const _onChangeFile = useCallback(
+    (file: Uint8Array): void =>
+      setFile(parseFile(file)),
+    []
+  );
+
+  const _onChangePass = useCallback(
+    (password: string): void =>
+      setPass({ isPassValid: keyring.isPassValid(password), password }),
+    []
+  );
+
+  const _onSave = useCallback(
+    (): void => {
+      if (!json) {
+        return;
+      }
+
+      const status: Partial<ActionStatus> = { action: 'restore' };
+
+      try {
+        const pair = keyring.restoreAccount(json, password);
+        const { address } = pair;
+
+        status.status = pair ? 'success' : 'error';
+        status.account = address;
+        status.message = t('account restored');
+
+        InputAddress.setLastValue('account', address);
+      } catch (error) {
+        setPass((state: PassState) => ({ ...state, isPassValid: false }));
+
+        status.status = 'error';
+        status.message = error.message;
+        console.error(error);
+      }
+
+      onStatusChange(status as ActionStatus);
+
+      if (status.status !== 'error') {
+        onClose();
+      }
+    },
+    [json, onClose, onStatusChange, password, t]
+  );
+
+  return (
+    <Modal
+      className={className}
+      header={t('Add via backup file')}
+    >
       <Modal.Content>
         <AddressRow
           defaultName={isFileValid && json ? json.meta.name : null}
@@ -71,7 +117,7 @@ class Import extends TxComponent<Props, State> {
             help={t('Select the JSON key file that was downloaded when you created the account. This JSON file contains your private key encrypted with your password.')}
             isError={!isFileValid}
             label={t('backup file')}
-            onChange={this.onChangeFile}
+            onChange={_onChangeFile}
             withLabel
           />
           <Password
@@ -80,81 +126,23 @@ class Import extends TxComponent<Props, State> {
             help={t('Type the password chosen at the account creation. It was used to encrypt your account\'s private key in the backup file.')}
             isError={!isPassValid}
             label={t('password')}
-            onChange={this.onChangePass}
-            onEnter={this.submit}
+            onChange={_onChangePass}
+            onEnter={_onSave}
             value={password}
           />
         </AddressRow>
       </Modal.Content>
-    );
-  }
-
-  private onChangeFile = (file: Uint8Array): void => {
-    try {
-      const json = JSON.parse(u8aToString(file));
-      const publicKey = keyring.decodeAddress(json.address, true);
-      const address = keyring.encodeAddress(publicKey);
-      const isFileValid = publicKey.length === 32 && isHex(json.encoded) && isObject(json.meta) && (
-        Array.isArray(json.encoding.content)
-          ? json.encoding.content[0] === 'pkcs8'
-          : json.encoding.content === 'pkcs8'
-      );
-
-      this.setState({
-        address,
-        isFileValid,
-        json
-      });
-    } catch (error) {
-      this.setState({
-        address: null,
-        isFileValid: false,
-        json: null
-      });
-      console.error(error);
-    }
-  }
-
-  private onChangePass = (password: string): void => {
-    this.setState({
-      isPassValid: keyring.isPassValid(password),
-      password
-    });
-  }
-
-  private onSave = (): void => {
-    const { onClose, onStatusChange, t } = this.props;
-    const { json, password } = this.state;
-
-    if (!json) {
-      return;
-    }
-
-    const status: Partial<ActionStatus> = { action: 'restore' };
-
-    try {
-      const pair = keyring.restoreAccount(json, password);
-      const { address } = pair;
-
-      status.status = pair ? 'success' : 'error';
-      status.account = address;
-      status.message = t('account restored');
-
-      InputAddress.setLastValue('account', address);
-    } catch (error) {
-      this.setState({ isPassValid: false });
-
-      status.status = 'error';
-      status.message = error.message;
-      console.error(error);
-    }
-
-    onStatusChange(status as ActionStatus);
-
-    if (status.status !== 'error') {
-      onClose();
-    }
-  }
+      <Modal.Actions onCancel={onClose}>
+        <Button
+          icon='sync'
+          isDisabled={!isFileValid || !isPassValid}
+          isPrimary
+          label={t('Restore')}
+          onClick={_onSave}
+        />
+      </Modal.Actions>
+    </Modal>
+  );
 }
 
-export default translate(Import);
+export default React.memo(Import);
