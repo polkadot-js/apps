425250745e34241f8f9a24b4be7ea617ad8238a7
Jaco Greeff <jacogr@gmail.com>
2020-04-07 09:57:32 +0200

    Cleanup Table styles

--- packages/react-components/src/Table/Body.tsx
+++ packages/react-components/src/Table/Body.tsx
@@ -3,6 +3,7 @@
 // of the Apache-2.0 license. See the LICENSE file for details.
 
 import React from 'react';
+import styled from 'styled-components';
 import { isString } from '@polkadot/util';
 
 import Spinner from '../Spinner';
@@ -31,4 +32,143 @@ function Body ({ children, className, empty, isEmpty }: Props): React.ReactEleme
   );
 }
 
-export default React.memo(Body);
+export default React.memo(styled(Body)`
+  td {
+    border-top: 1px solid #e6e6e6;
+    padding: 0.75rem 1rem;
+    text-align: left;
+    vertical-align: middle;
+
+    &:first-child {
+      border-left: 1px solid #e6e6e6;
+    }
+
+    &:last-child {
+      border-right: 1px solid #e6e6e6;
+    }
+
+    label {
+      display: block !important;
+      white-space: nowrap;
+    }
+
+    i.icon {
+      cursor: pointer;
+    }
+
+    div.empty {
+      opacity: 0.6;
+      padding: 0.25rem;
+    }
+
+    .ui--Spinner {
+      margin: 0 auto;
+
+      .text {
+        margin-bottom: 0;
+      }
+    }
+
+    &:hover label {
+      opacity: 1;
+    }
+
+    &.address {
+      min-width: 11rem;
+      padding: 0.85rem 1rem;
+    }
+
+    &.button {
+      text-align: right;
+      vertical-align: middle;
+      white-space: nowrap;
+    }
+
+    &.combined {
+      border-top-width: 0;
+    }
+
+    &.hash {
+      font-family: monospace;
+    }
+
+    &.number {
+      text-align: right;
+    }
+
+    &.relative {
+      position: relative;
+    }
+
+    &.overflow {
+      max-width: 0;
+      overflow: hidden;
+      text-overflow: ellipsis;
+    }
+
+    &.together {
+      white-space: nowrap;
+    }
+
+    &.top {
+      vertical-align: top;
+    }
+
+    &.middle {
+      text-align: center;
+    }
+
+    &.mini {
+      padding: 0 0.75rem 0 0;
+      white-space: nowrap;
+    }
+
+    &.favorite i.icon.isSelected {
+      color: darkorange;
+    }
+  }
+
+  tr {
+    background: white;
+
+    &.isHighlight td {
+      background: #ffffed;
+    }
+
+    &:last-child td {
+      border-bottom: 1px solid #e6e6e6;
+    }
+
+    &:first-child {
+      td:first-child {
+        border-radius: 0.25rem 0 0 0;
+      }
+
+      td:last-child {
+        border-radius: 0 0.25rem 0 0;
+      }
+    }
+
+    &:last-child {
+      td:first-child {
+        border-radius: 0 0 0 0.25rem;
+      }
+
+      td:last-child {
+        border-radius: 0 0 0.25rem 0;
+      }
+    }
+
+    &:not(:hover) {
+      .ui.button:not(.isIcon):not(.disabled) {
+        background: #eee !important;
+        color: #555 !important;
+      }
+
+      .ui.toggle.checkbox input:checked~.box:before,
+      .ui.toggle.checkbox input:checked~label:before {
+        background-color: #eee !important;
+      }
+    }
+  }
+`);
