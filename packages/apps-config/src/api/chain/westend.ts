ad9337331f47d4262891b692d84297e4e534bb69
Jaco Greeff <jacogr@gmail.com>
2020-04-07 20:31:04 +0200

    Westend (again)

--- packages/apps-config/src/api/chain/westend.ts
+++ packages/apps-config/src/api/chain/westend.ts
@@ -3,5 +3,7 @@
 // of the Apache-2.0 license. See the LICENSE file for details.
 
 export default {
-  Keys: 'SessionKeys5'
+  Address: 'AccountId',
+  Keys: 'SessionKeys5',
+  LookupSource: 'AccountId'
 };
