e29cd6e7c00c68a7e8ceb0edd55c1a5749c24401
kpozdnikin <kpozdnikin@usetech.ru>
2020-04-10 14:11:09 +0300

    simple nomination/ era to time

--- packages/page-staking/src/Nomination/eraToTime.tsx
+++ packages/page-staking/src/Nomination/eraToTime.tsx
@@ -1,17 +1,28 @@
 import React from 'react';
-import {useApi} from '@polkadot/react-hooks/index';
+import {useApi, useCall} from '@polkadot/react-hooks/index';
 import {useTranslation} from '@polkadot/app-accounts/translate';
 import {BareProps} from "@polkadot/react-api/types";
 import {BlockToTime} from "@polkadot/react-query/index";
 import { formatNumber } from '@polkadot/util';
+import { DeriveSessionProgress } from '@polkadot/api-derive/types';
 
 function eraToTime({ className, style }: BareProps) {
   const { api } = useApi();
   const { t } = useTranslation();
+  const sessionInfo = useCall<DeriveSessionProgress>(api.derive.session?.progress, []);
   const bondedDuration = api.consts.staking.bondingDuration;
   const epochDuration = api.consts.babe.epochDuration;
-  const sessionsPerEra = api.consts.staking.sessionsPerEra;
-  const eraLength = sessionsPerEra.mul(epochDuration).mul(bondedDuration);
+  // const sessionsPerEra = api.consts.stacking.sessionsPerEra;
+  const genesisSlot = api.query.babe.genesisSlot;
+  let eraLength = null;
+  if (genesisSlot) {
+    const sessionsPerEra = api.consts.staking.sessionsPerEra;
+    eraLength = sessionsPerEra.mul(epochDuration).mul(bondedDuration);
+  }
+
+  if (!eraLength) {
+    return null;
+  }
 
   return (
     <span
