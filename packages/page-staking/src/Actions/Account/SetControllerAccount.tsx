52c04fc29d8faecc2a868dd200c81db8dd178a61
Jaco Greeff <jacogr@gmail.com>
2020-04-11 09:05:00 +0200

    Modal conversion, functional non-TxComponent (#2548)
    * Modal conversion, functional non-TxComponent
    
    * Fix duration calc

--- packages/page-staking/src/Actions/Account/SetControllerAccount.tsx
+++ packages/page-staking/src/Actions/Account/SetControllerAccount.tsx
@@ -2,108 +2,76 @@
 // This software may be modified and distributed under the terms
 // of the Apache-2.0 license. See the LICENSE file for details.
 
-import { ApiProps } from '@polkadot/react-api/types';
-import { I18nProps } from '@polkadot/react-components/types';
+import React, { useState } from 'react';
+import { Icon, InputAddress, Modal, TxButton } from '@polkadot/react-components';
 
-import React from 'react';
-import { Icon, InputAddress, Modal, TxButton, TxComponent } from '@polkadot/react-components';
-import { withApi, withMulti } from '@polkadot/react-api/hoc';
+import { useTranslation } from '../../translate';
+import InputValidationController from './InputValidationController';
 
-import translate from '../../translate';
-import InputValidationController from '../Account/InputValidationController';
-
-interface Props extends ApiProps, I18nProps {
+interface Props {
   defaultControllerId: string;
   isValidating?: boolean;
   onClose: () => void;
   stashId: string;
 }
 
-interface State {
-  controllerError: string | null;
-  controllerId: string | null;
+function SetControllerAccount ({ defaultControllerId, isValidating, onClose, stashId }: Props): React.ReactElement<Props> {
+  const { t } = useTranslation();
+  const [, setControllerError] = useState<string | null>(null);
+  const [controllerId, setControllerId] = useState<string | null>(null);
+
+  return (
+    <Modal
+      className='staking--SetControllerAccount'
+      header={t('Change controller account')}
+      size='small'
+    >
+      <Modal.Content className='ui--signer-Signer-Content'>
+        {isValidating && (
+          <article className='warning'>
+            <div className='warning'>
+              <Icon name='warning sign' />
+              {t('Warning - Changing the controller while validating will modify the associated session account. It is advised to stop validating before changing the controller account.')}
+            </div>
+          </article>
+        )}
+        <InputAddress
+          className='medium'
+          isDisabled
+          label={t('stash account')}
+          value={stashId}
+        />
+        <InputAddress
+          className='medium'
+          defaultValue={defaultControllerId}
+          help={t('The controller is the account that will be used to control any nominating or validating actions. Should not match another stash or controller.')}
+          label={t('controller account')}
+          onChange={setControllerId}
+          type='account'
+          value={controllerId}
+        />
+        <InputValidationController
+          accountId={stashId}
+          controllerId={controllerId}
+          defaultController={defaultControllerId}
+          onError={setControllerError}
+        />
+      </Modal.Content>
+      <Modal.Actions onCancel={onClose}>
+        <TxButton
+          accountId={stashId}
+          icon='sign-in'
+          isDisabled={!controllerId}
+          isPrimary
+          label={t('Set controller')}
+          onStart={onClose}
+          params={[controllerId]}
+          tx='staking.setController'
+          withSpinner={false}
+        />
+      </Modal.Actions>
+    </Modal>
+  );
 }
 
-class SetControllerAccount extends TxComponent<Props, State> {
-  constructor (props: Props) {
-    super(props);
-
-    this.state = {
-      controllerError: null,
-      controllerId: null
-    };
-  }
-
-  public render (): React.ReactNode {
-    const { defaultControllerId, isValidating, onClose, stashId, t } = this.props;
-    const { controllerId } = this.state;
-    const canSubmit = !!controllerId;
-
-    return (
-      <Modal
-        className='staking--SetControllerAccount'
-        header={t('Change controller account')}
-        size='small'
-      >
-        <Modal.Content className='ui--signer-Signer-Content'>
-          {isValidating && (
-            <article className='warning'>
-              <div className='warning'>
-                <Icon name='warning sign' />
-                {t('Warning - Changing the controller while validating will modify the associated session account. It is advised to stop validating before changing the controller account.')}
-              </div>
-            </article>
-          )}
-          <InputAddress
-            className='medium'
-            isDisabled
-            label={t('stash account')}
-            value={stashId}
-          />
-          <InputAddress
-            className='medium'
-            defaultValue={defaultControllerId}
-            help={t('The controller is the account that will be used to control any nominating or validating actions. Should not match another stash or controller.')}
-            label={t('controller account')}
-            onChange={this.onChangeController}
-            type='account'
-            value={controllerId}
-          />
-          <InputValidationController
-            accountId={stashId}
-            controllerId={controllerId}
-            defaultController={defaultControllerId}
-            onError={this.onControllerError}
-          />
-        </Modal.Content>
-        <Modal.Actions onCancel={onClose}>
-          <TxButton
-            accountId={stashId}
-            icon='sign-in'
-            isDisabled={!canSubmit}
-            isPrimary
-            label={t('Set controller')}
-            onStart={onClose}
-            params={[controllerId]}
-            tx='staking.setController'
-            withSpinner
-          />
-        </Modal.Actions>
-      </Modal>
-    );
-  }
-
-  private onChangeController = (controllerId: string | null): void => {
-    this.setState({ controllerId });
-  }
-
-  private onControllerError = (controllerError: string | null): void => {
-    this.setState({ controllerError });
-  }
-}
-
-export default withMulti(
-  SetControllerAccount,
-  translate,
-  withApi
-);
+export default React.memo(SetControllerAccount);
--- packages/page-staking/src/Actions/Account/Unbond.tsx
+++ packages/page-staking/src/Actions/Account/Unbond.tsx
@@ -2,93 +2,39 @@
 // This software may be modified and distributed under the terms
 // of the Apache-2.0 license. See the LICENSE file for details.
 
-/* eslint-disable @typescript-eslint/camelcase */
-
 import { AccountId, StakingLedger } from '@polkadot/types/interfaces';
-import { I18nProps } from '@polkadot/react-components/types';
-import { ApiProps } from '@polkadot/react-api/types';
 
 import BN from 'bn.js';
-import React from 'react';
+import React, { useState } from 'react';
+import { Trans } from 'react-i18next';
 import styled from 'styled-components';
-import { Option } from '@polkadot/types';
-import { AddressInfo, InputAddress, InputBalance, Modal, TxButton, TxComponent } from '@polkadot/react-components';
-import { withCalls, withApi, withMulti } from '@polkadot/react-api/hoc';
+import { AddressInfo, InputAddress, InputBalance, Modal, TxButton } from '@polkadot/react-components';
+import { BlockToTime } from '@polkadot/react-query';
+import { formatNumber } from '@polkadot/util';
 
-import translate from '../../translate';
+import { useTranslation } from '../../translate';
+import useUnbondDuration from '../useUnbondDuration';
 
-interface Props extends I18nProps, ApiProps {
+interface Props {
+  className?: string;
   controllerId?: AccountId | null;
-  isOpen: boolean;
   onClose: () => void;
+  stakingLedger?: StakingLedger;
   stashId: string;
-  staking_ledger?: Option<StakingLedger>;
 }
 
-interface State {
-  maxBalance?: BN;
-  maxUnbond?: BN;
-}
+function Unbond ({ className, controllerId, onClose, stakingLedger, stashId }: Props): React.ReactElement<Props> {
+  const { t } = useTranslation();
+  const duration = useUnbondDuration();
+  const [maxBalance] = useState<BN | null>(stakingLedger?.active.unwrap() || null);
+  const [maxUnbond, setMaxUnbond] = useState<BN | null>(null);
 
-const BalanceWrapper = styled.div`
-  & > div {
-    justify-content: flex-end;
-
-    & .column {
-      flex: 0;
-    }
-  }
-`;
-
-class Unbond extends TxComponent<Props, State> {
-  public state: State = {};
-
-  public componentDidUpdate (prevProps: Props): void {
-    const { staking_ledger } = this.props;
-
-    if (staking_ledger !== prevProps.staking_ledger) {
-      this.setMaxBalance();
-    }
-  }
-
-  public render (): React.ReactNode {
-    const { controllerId, isOpen, onClose, t } = this.props;
-    const { maxUnbond } = this.state;
-    const canSubmit = !!maxUnbond && maxUnbond.gtn(0);
-
-    if (!isOpen) {
-      return null;
-    }
-
-    return (
-      <Modal
-        className='staking--Unbond'
-        header={t('Unbond funds')}
-        size='small'
-      >
-        {this.renderContent()}
-        <Modal.Actions onCancel={onClose}>
-          <TxButton
-            accountId={controllerId}
-            icon='sign-out'
-            isDisabled={!canSubmit}
-            isPrimary
-            label={t('Unbond')}
-            onStart={onClose}
-            params={[maxUnbond]}
-            tx='staking.unbond'
-            withSpinner={false}
-          />
-        </Modal.Actions>
-      </Modal>
-    );
-  }
-
-  private renderContent (): React.ReactNode {
-    const { controllerId, stashId, t } = this.props;
-    const { maxBalance } = this.state;
-
-    return (
+  return (
+    <Modal
+      className={`staking--Unbond ${className}`}
+      header={t('Unbond funds')}
+      size='small'
+    >
       <Modal.Content className='ui--signer-Signer-Content'>
         <InputAddress
           className='medium'
@@ -96,63 +42,56 @@ class Unbond extends TxComponent<Props, State> {
           isDisabled
           label={t('controller account')}
         />
-        <BalanceWrapper>
+        <div className='staking--Unbond--max'>
           <AddressInfo
             accountId={stashId}
             withBalance={{
               bonded: true
             }}
           />
-        </BalanceWrapper>
+        </div>
         <InputBalance
           autoFocus
           className='medium'
           help={t('The amount of funds to unbond, this is adjusted using the bonded funds on the stash account.')}
           label={t('unbond amount')}
           maxValue={maxBalance}
-          onChange={this.onChangeValue}
-          onEnter={this.sendTx}
+          onChange={setMaxUnbond}
           withMax
         />
+        {duration?.gtn(0) && (
+          <article className='warning'>
+            <Trans key='unlockDuration'>Once unbonded, funds will only be available for withdrawal in <BlockToTime blocks={duration} /> ({formatNumber(duration)} blocks)</Trans>
+          </article>
+        )}
       </Modal.Content>
-    );
-  }
-
-  private nextState (newState: Partial<State>): void {
-    this.setState((prevState: State): State => {
-      const { maxUnbond = prevState.maxUnbond, maxBalance = prevState.maxBalance } = newState;
-
-      return {
-        maxBalance,
-        maxUnbond
-      };
-    });
-  }
-
-  private setMaxBalance = (): void => {
-    const { staking_ledger } = this.props;
-
-    if (!staking_ledger || staking_ledger.isNone) {
-      return;
-    }
-
-    const { active } = staking_ledger.unwrap();
-
-    this.nextState({
-      maxBalance: active.unwrap()
-    });
-  }
-
-  private onChangeValue = (maxUnbond?: BN): void => {
-    this.nextState({ maxUnbond });
-  }
+      <Modal.Actions onCancel={onClose}>
+        <TxButton
+          accountId={controllerId}
+          icon='sign-out'
+          isDisabled={!maxUnbond?.gtn(0)}
+          isPrimary
+          label={t('Unbond')}
+          onStart={onClose}
+          params={[maxUnbond]}
+          tx='staking.unbond'
+          withSpinner={false}
+        />
+      </Modal.Actions>
+    </Modal>
+  );
 }
 
-export default withMulti(
-  Unbond,
-  translate,
-  withApi,
-  withCalls<Props>(
-    ['query.staking.ledger', { paramName: 'controllerId' }]
-  )
-);
+export default React.memo(styled(Unbond)`
+  article.warning > div {
+    display: inline-block;
+  }
+
+  .staking--Unbond--max > div {
+    justify-content: flex-end;
+
+    & .column {
+      flex: 0;
+    }
+  }
+`);
