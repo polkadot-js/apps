ed6282d3602555340773eeeeed6699810f316047
Jaco Greeff <jacogr@gmail.com>
2020-04-22 21:27:15 +0200

    Re-add extension load init

--- packages/react-api/src/Api.tsx
+++ packages/react-api/src/Api.tsx
@@ -45,7 +45,11 @@ interface ChainData {
   systemVersion: string;
 }
 
-const injectedPromise = web3Enable('polkadot-js/apps');
+const injectedPromise = new Promise<InjectedExtension[]>((resolve): void => {
+  window.addEventListener('load', (): void => {
+    resolve(web3Enable('polkadot-js/apps'));
+  });
+});
 
 const DEFAULT_DECIMALS = registry.createType('u32', 12);
 const DEFAULT_SS58 = registry.createType('u32', addressDefaults.prefix);
