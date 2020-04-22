3b424148ddaa5cd538678ae6ee954f0bacb3fca5
Greg Zaitsev <greg.zaitsev@gmail.com>
2020-04-22 23:40:28 +0300

    Revert version of Substrate and API to 1.10

--- docker/run.sh
+++ docker/run.sh
@@ -5,6 +5,7 @@ then
     chmod 777 -R /chain-data
 fi
 
-# ls /chain-data/chains
+# dump local chain spec
+#cargo run --release -- build-spec --dev --chain=local > /chain-data/spec_1_10.json
 
 cargo run --release -- --ws-external --rpc-external --dev --base-path /chain-data --chain=chainspec.json
