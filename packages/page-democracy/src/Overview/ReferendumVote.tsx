865ac836c44c6b310e7ad06b58d3ece66f2144a0
Jaco Greeff <jacogr@gmail.com>
2020-04-24 11:16:13 +0200

    Voting delegations (#2619)
    * Add vote delegations
    
    * Add handling for democracy delegations
    
    * Bump API
    
    * Remove wrong prop

--- packages/page-democracy/src/Overview/ReferendumVote.tsx
+++ packages/page-democracy/src/Overview/ReferendumVote.tsx
@@ -14,15 +14,15 @@ interface Props {
 
 const sizing = ['0.1x', '1x', '2x', '3x', '4x', '5x', '6x'];
 
-function voteLabel (vote: Vote): string {
-  return `${sizing[vote.conviction.toNumber()]} - `;
+function voteLabel ({ conviction }: Vote, isDelegating: boolean): string {
+  return `${sizing[conviction.toNumber()]}${isDelegating ? '/d' : ''} - `;
 }
 
-function ReferendumVote ({ vote: { accountId, balance, vote } }: Props): React.ReactElement<Props> {
+function ReferendumVote ({ vote: { accountId, balance, isDelegating, vote } }: Props): React.ReactElement<Props> {
   return (
     <AddressMini
       balance={balance}
-      labelBalance={voteLabel(vote)}
+      labelBalance={voteLabel(vote, isDelegating)}
       value={accountId}
       withBalance
     />
