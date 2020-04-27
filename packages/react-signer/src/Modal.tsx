e27754e37935a9b6bd0bf2c9c1bb1079af798497
Jaco Greeff <jacogr@gmail.com>
2020-04-27 20:38:38 +0200

    Qr fix, scanning display

--- packages/react-signer/src/Modal.tsx
+++ packages/react-signer/src/Modal.tsx
@@ -753,8 +753,6 @@ class Signer extends React.PureComponent<Props, State> {
       txStartCb();
     }
 
-    queueSetTxStatus(id, 'sending');
-
     try {
       const unsubscribe = await extrinsicCall.apply(extrinsic, [
         ...params,
@@ -790,6 +788,8 @@ class Signer extends React.PureComponent<Props, State> {
           }
         }
       ]);
+
+      queueSetTxStatus(id, 'sending');
     } catch (error) {
       console.error('makeExtrinsicCall: error:', error);
       queueSetTxStatus(id, 'error', {}, error);
