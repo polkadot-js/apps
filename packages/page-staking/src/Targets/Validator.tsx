405d5c81640550bf9ea25bec37954b699d5b1fa8
Jaco Greeff <jacogr@gmail.com>
2020-04-27 23:54:01 +0200

    Fix type import

--- packages/page-staking/src/Targets/Validator.tsx
+++ packages/page-staking/src/Targets/Validator.tsx
@@ -2,7 +2,7 @@
 // This software may be modified and distributed under the terms
 // of the Apache-2.0 license. See the LICENSE file for details.v
 
-import { ValidatorInfo } from './types';
+import { ValidatorInfo } from '../types';
 
 import React, { useCallback } from 'react';
 import { AddressSmall, Icon, Toggle } from '@polkadot/react-components';
