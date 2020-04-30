1f29e7bb8f097810754dca881e6df2e222405862
Eliott Teissonniere <git.eliott@teissonniere.org>
2020-04-30 11:34:54 -0700

    add alpha.6 types for arcadia

--- packages/apps-config/src/api/chain/arcadia.ts
+++ packages/apps-config/src/api/chain/arcadia.ts
@@ -2,10 +2,7 @@
 // This software may be modified and distributed under the terms
 // of the Apache-2.0 license. See the LICENSE file for details.
 
-import Arcadia from './arcadia';
-import Berlin from './berlin';
-
 export default {
-  Arcadia,
-  Berlin
+  // previous substrate versions
+  Weight: 'u32'
 };
