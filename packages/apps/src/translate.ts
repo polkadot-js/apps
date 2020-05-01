3aea9fa4da33d87c06b2c9be1b52e8d17166c0c5
Jaco Greeff <jacogr@gmail.com>
2020-05-01 22:12:12 +0200

    Adjust i18next options for current

--- packages/apps/src/translate.ts
+++ packages/apps/src/translate.ts
@@ -5,5 +5,5 @@
 import { useTranslation as useTranslationBase, UseTranslationResponse } from 'react-i18next';
 
 export function useTranslation (): UseTranslationResponse {
-  return useTranslationBase('apps');
+  return useTranslationBase(['apps', 'apps-routing']);
 }
