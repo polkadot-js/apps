f21ae871f3351a8d9c21e0a62294e14ce59819cd
Jaco Greeff <jacogr@gmail.com>
2020-05-03 08:53:59 +0200

    Load all namespaces up-front (no indv. queries) (#2680)
    * Move i18n app to settings (discoverability)
    
    * Load all namespecas up-front (no indv. queries)

--- packages/react-components/src/i18n/Backend.ts
+++ packages/react-components/src/i18n/Backend.ts
@@ -6,26 +6,43 @@ import languageCache from './cache';
 
 type Callback = (error: string | null, data: any) => void;
 
+type LoadResult = [string | null, any];
+
+const loaders: Record<string, Promise<LoadResult>> = {};
+
 export default class Backend {
   type = 'backend'
 
   static type: 'backend' = 'backend'
 
   async read (lng: string, _namespace: string, responder: Callback): Promise<void> {
-    if (!languageCache[lng]) {
-      try {
-        const response = await fetch(`locales/${lng}/translation.json`, {});
-
-        if (!response.ok) {
-          return responder(`i18n: failed loading ${lng}`, response.status >= 500 && response.status < 600);
-        }
-
-        languageCache[lng] = await response.json();
-      } catch (error) {
-        return responder(error.message, false);
-      }
+    if (languageCache[lng]) {
+      return responder(null, languageCache[lng]);
     }
 
-    return responder(null, languageCache[lng]);
+    // eslint-disable-next-line @typescript-eslint/no-misused-promises
+    if (!loaders[lng]) {
+      loaders[lng] = this.createLoader(lng);
+    }
+
+    const [error, data] = await loaders[lng];
+
+    return responder(error, data);
+  }
+
+  async createLoader (lng: string): Promise<LoadResult> {
+    try {
+      const response = await fetch(`locales/${lng}/translation.json`, {});
+
+      if (!response.ok) {
+        return [`i18n: failed loading ${lng}`, response.status >= 500 && response.status < 600];
+      } else {
+        languageCache[lng] = await response.json();
+
+        return [null, languageCache[lng]];
+      }
+    } catch (error) {
+      return [error.message, false];
+    }
   }
 }
