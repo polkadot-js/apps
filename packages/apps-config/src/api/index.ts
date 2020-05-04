3c0843ee7249c5a366d1bcc4fb766dcc20eddc80
Eliott Teissonniere <git.eliott@teissonniere.org>
2020-05-04 11:09:32 -0700

    don't add types twice

--- packages/apps-config/src/api/index.ts
+++ packages/apps-config/src/api/index.ts
@@ -8,7 +8,6 @@ import typesSpec from './spec';
 export function getChainTypes (specName: string, chainName: string): Record<string, string | object> {
   return {
     ...(typesSpec[specName as 'edgeware'] || {}),
-    ...(typesChain[chainName as 'Arcadia Nodle Network'] || {}),
     ...(typesChain[chainName as 'Berlin'] || {})
   };
 }
