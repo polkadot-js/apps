910c348e7e5d4ed066f3011468f23f026ba3bdc8
Drew Stone <drewstone329@gmail.com>
2020-05-05 14:09:33 -0600

    Update edgeware types

--- packages/apps-config/src/api/chain/berlin.ts
+++ packages/apps-config/src/api/chain/berlin.ts
@@ -2,10 +2,22 @@
 // This software may be modified and distributed under the terms
 // of the Apache-2.0 license. See the LICENSE file for details.
 
+import * as edgewareDefinitions from 'edgeware-node-types/dist/definitions';
+
+const edgTypes = Object.values(edgewareDefinitions)
+  .reduce((res, { default: { types } }): object => ({ ...res, ...types }), {});
+
 export default {
-  // previous substrate versions
-  BalanceLock: 'BalanceLockTo212',
-  ReferendumInfo: 'ReferendumInfoTo239',
+  ...edgTypes,
+  // aliases that don't do well as part of interfaces
+  'voting::VoteType': 'VoteType',
+  'voting::TallyType': 'TallyType',
+  'voting::Tally': 'VotingTally',
+  // chain-specific overrides
+  Address: 'GenericAddress',
+  Keys: 'SessionKeys4',
   StakingLedger: 'StakingLedgerTo223',
-  Weight: 'u32'
+  Votes: 'VotesTo230',
+  ReferendumInfo: 'ReferendumInfoTo239',
+  Weight: 'u32',
 };
