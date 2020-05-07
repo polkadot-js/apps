7c6ecfcd61deff47b731d7f37e1574a892a2a115
Joshy Orndorff <admin@joshyorndorff.com>
2020-05-07 10:05:57 -0400

    update node template weight type back to default u64

--- packages/apps-config/src/api/spec/node-template.ts
+++ packages/apps-config/src/api/spec/node-template.ts
@@ -8,6 +8,4 @@
 export default {
   Address: 'AccountId',
   LookupSource: 'AccountId',
-  // Remove when https://github.com/substrate-developer-hub/substrate-node-template/ moves to alpha-v7
-  Weight: 'u32'
 };
