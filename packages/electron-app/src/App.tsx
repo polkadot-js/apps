dea359ae966304eeb3a5e89fdf949348ea5a38e8
jozwiak <kubencki@gmail.com>
2020-05-12 23:07:25 +0200

    Integrate electron with react components from apps workspace

--- packages/electron-app/src/App.tsx
+++
@@ -1,6 +0,0 @@
-import React from "react"
-
-const App = () =>
-    <p>Hello React</p>
-
-export default App
--- packages/electron-app/src/index.tsx
+++ packages/electron-app/src/index.tsx
@@ -1,5 +1,50 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './App'
+// Copyright 2017-2020 @polkadot/apps authors & contributors
+// This software may be modified and distributed under the terms
+// of the Apache-2.0 license. See the LICENSE file for details.
 
-ReactDOM.render(<App />, document.getElementById('root'))
+import React, {Suspense} from 'react';
+import ReactDOM from 'react-dom';
+
+// setup these right at front
+import './initSettings';
+import 'semantic-ui-css/semantic.min.css';
+import '@polkadot/react-components/i18n';
+
+import {ThemeProvider} from 'styled-components';
+
+import settings from '@polkadot/ui-settings';
+import Queue from '@polkadot/react-components/Status/Queue';
+import {BlockAuthors, Events} from '@polkadot/react-query';
+import AccountSidebar from '@polkadot/app-accounts/Sidebar';
+import {HashRouter} from 'react-router-dom';
+import Apps from '@polkadot/apps/Apps';
+import {Api} from '@polkadot/react-api';
+
+const rootId = 'root';
+const rootElement = document.getElementById(rootId);
+const theme = { theme: settings.uiTheme };
+
+if (!rootElement) {
+  throw new Error(`Unable to find element with id '${rootId}'`);
+}
+
+ReactDOM.render(
+  <Suspense fallback='...'>
+    <ThemeProvider theme={theme}>
+      <Queue>
+        <Api url={settings.apiUrl}>
+          <BlockAuthors>
+            <Events>
+              <AccountSidebar>
+                <HashRouter>
+                  <Apps />
+                </HashRouter>
+              </AccountSidebar>
+            </Events>
+          </BlockAuthors>
+        </Api>
+      </Queue>
+    </ThemeProvider>
+  </Suspense>,
+  rootElement
+);
