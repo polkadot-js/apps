aeee9acc8e030677d8c98bf5ae40cf024ccf995e
thib <thibaut.sardan@gmail.com>
2020-05-14 11:08:36 +0200

    show amount in attest

--- packages/page-claims/src/Attest.tsx
+++ packages/page-claims/src/Attest.tsx
@@ -2,33 +2,70 @@
 // This software may be modified and distributed under the terms
 // of the Apache-2.0 license. See the LICENSE file for details.
 
-import React from 'react';
+import React, { useEffect, useCallback, useState } from 'react';
 import styled from 'styled-components';
 import { Button, Card, TxButton } from '@polkadot/react-components';
+import { FormatBalance } from '@polkadot/react-query';
+import { Option } from '@polkadot/types';
+import { BalanceOf, EthereumAddress, StatementKind } from '@polkadot/types/interfaces';
 
 import { useTranslation } from './translate';
-import { addrToChecksum } from './util';
+import { useApi } from '@polkadot/react-hooks';
 
 interface Props {
   accountId: string;
   className?: string;
+  ethereumAddress: EthereumAddress | null;
+  statementKind?: StatementKind;
 }
 
-function Attest ({ accountId, className }: Props): React.ReactElement<Props> | null {
+function Attest ({ accountId, className, ethereumAddress, statementKind }: Props): React.ReactElement<Props> | null {
   const { t } = useTranslation();
+  const { api } = useApi();
+  const [claimValue, setClaimValue] = useState<BalanceOf | null>(null);
+  const [claimAddress, setClaimAddress] = useState<EthereumAddress | null>(null);
+  const [isBusy, setIsBusy] = useState(false);
+
+  const _fetchClaim = useCallback(
+    (address: EthereumAddress): void => {
+      setIsBusy(true);
+
+      api.query.claims
+        .claims<Option<BalanceOf>>(address)
+        .then((claim): void => {
+          setClaimValue(claim.unwrapOr(null));
+          setIsBusy(false);
+        })
+        .catch((): void => setIsBusy(false));
+    },
+    [api]
+  );
+
+  useEffect((): void => {
+    setClaimAddress(ethereumAddress);
+
+    ethereumAddress && _fetchClaim(ethereumAddress);
+  }, [_fetchClaim, ethereumAddress]);
+
+  if (isBusy || !claimAddress) {
+    return null;
+  }
+
+  const hasClaim = claimValue && claimValue.gten(0);
 
   return (
     <Card isSuccess>
       <div className={className}>
         {t('By signing this transaction, you agree with the attestation available at the following address: XXX')}
-        <h3>{addrToChecksum(accountId)}</h3>
+        {hasClaim &&
+          <h3>{t('Attestation for: ')} <FormatBalance value={claimValue} /></h3>}
         <Button.Group>
           <TxButton
             accountId={accountId}
             icon='send'
             isPrimary
             label={t('Attest')}
-            params={['dsafdsalkfj']} // Replace with actual
+            params={[statementKind]} // Replace with actual
             tx='claims.attest'
           />
         </Button.Group>
