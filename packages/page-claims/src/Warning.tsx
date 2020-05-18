f62c9627cbd4164d57141cdcde8d859b808467a0
Amaury Martiny <amaury.martiny@protonmail.com>
2020-05-18 13:31:07 +0200

    Update packages/page-claims/src/Warning.tsx
    Co-authored-by: Jaco Greeff <jacogr@gmail.com>

--- packages/page-claims/src/Warning.tsx
+++ packages/page-claims/src/Warning.tsx
@@ -2,10 +2,6 @@
 // This software may be modified and distributed under the terms
 // of the Apache-2.0 license. See the LICENSE file for details.
 
-// Copyright 2017-2020 @polkadot/app-settings authors & contributors
-// This software may be modified and distributed under the terms
-// of the Apache-2.0 license. See the LICENSE file for details.
-
 import { AddressMini, Card } from '@polkadot/react-components';
 
 import React from 'react';
