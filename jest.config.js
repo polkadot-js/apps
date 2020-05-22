95b847b78c36ba1e778c91eadcc9556828f761ae
thib <thibaut.sardan@gmail.com>
2020-05-22 14:00:14 +0200

    add md to jest

--- jest.config.js
+++ jest.config.js
@@ -18,7 +18,7 @@ module.exports = Object.assign({}, config, {
   moduleNameMapper: {
     ...internalModules,
     '\\.(css|less)$': 'empty/object',
-    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': 'empty/object'
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga|md)$': 'empty/object'
   },
   transformIgnorePatterns: [
     '<rootDir>/node_modules'
