daf7311cb6506e82a2bfad03f811b0c2baec05a3
jakub_jozwiak <kubencki@gmail.com>
2020-05-26 23:28:54 +0200

    Add dialogs

--- packages/apps-electron/src/electron.ts
+++ packages/apps-electron/src/electron.ts
@@ -2,11 +2,12 @@
 // This software may be modified and distributed under the terms
 // of the Apache-2.0 license. See the LICENSE file for details.
 
-import { BrowserWindow, app, screen } from 'electron';
+import { BrowserWindow, app, screen, dialog } from 'electron';
 import path from 'path';
 import { autoUpdater } from 'electron-updater';
 
-const environment = process.env.NODE_ENV || 'production';
+const ENV = process.env.NODE_ENV || 'production';
+const isDev = ENV === 'development';
 
 function createWindow (): Promise<unknown> {
   const { height, width } = screen.getPrimaryDisplay().workAreaSize;
@@ -19,7 +20,7 @@ function createWindow (): Promise<unknown> {
     width
   });
 
-  if (environment === 'development') {
+  if (isDev) {
     win.webContents.openDevTools();
 
     return win.loadURL('http://0.0.0.0:9000/');
@@ -33,6 +34,22 @@ function createWindow (): Promise<unknown> {
 const onReady = async () => {
   await createWindow();
   await autoUpdater.checkForUpdatesAndNotify();
+  // TODO: Check is promise above resolved
 };
 
 app.whenReady().then(onReady).catch(console.error);
+
+autoUpdater.on('update-not-available', () => {
+  // eslint-disable-next-line no-void
+  void dialog.showMessageBox({
+    message: 'Current version is up-to-date.',
+    title: 'No Updates'
+  });
+});
+
+if (isDev) {
+  autoUpdater.on('error', (error) => {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access,@typescript-eslint/no-unsafe-call
+    dialog.showErrorBox('Error: ', error == null ? 'unknown' : (error.stack || error).toString());
+  });
+}
