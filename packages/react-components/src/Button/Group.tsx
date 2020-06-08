f6b0fd7e85ca0bfb2c3cff0355d052070bee1d01
Jaco Greeff <jacogr@gmail.com>
2020-06-08 07:33:07 +0200

    Remove Button divider

--- packages/react-components/src/Button/Group.tsx
+++ packages/react-components/src/Button/Group.tsx
@@ -2,32 +2,26 @@
 // This software may be modified and distributed under the terms
 // of the Apache-2.0 license. See the LICENSE file for details.
 
-import { GroupProps, GroupType } from './types';
+import { GroupProps } from './types';
 
 import React from 'react';
 import SUIButton from 'semantic-ui-react/dist/commonjs/elements/Button/Button';
 import styled from 'styled-components';
 
-import Divider from './Divider';
-
 function ButtonGroup ({ children, className = '', isBasic, isCentered }: GroupProps): React.ReactElement<GroupProps> {
   return (
-    <div className={`ui--Button-Group ${isCentered ? 'centered' : ''} ${className}`}>
+    <div className={`${className} ui--Button-Group ${isCentered ? 'centered' : ''}`}>
       <SUIButton.Group
         basic={isBasic}
         size='small'
       >
-        {isBasic
-          ? null
-          : <Divider className='ui--Button-Group-Divider' />
-        }
         {children}
       </SUIButton.Group>
     </div>
   );
 }
 
-const Memo = React.memo(styled(ButtonGroup)`
+export default React.memo(styled(ButtonGroup)`
   text-align: right;
 
   :not(:first-child) {
@@ -46,12 +40,4 @@ const Memo = React.memo(styled(ButtonGroup)`
   &+.ui--Table {
     margin-top: 1.5rem;
   }
-
-  .ui--Buttn-Group-Divider {
-    padding: 0;
-  }
-`) as unknown as GroupType;
-
-Memo.Divider = Divider;
-
-export default Memo;
+`);
