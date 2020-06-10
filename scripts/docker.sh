c9104dbe324ba9683b6ee33304b73e27ffb83e81
Jaco Greeff <jacogr@gmail.com>
2020-06-10 21:35:04 +0200

    Docker build/push ... try #1

---
+++ scripts/docker.sh
@@ -0,0 +1,40 @@
+#!/user/bin/env bash
+# Copyright 2017-2020 @polkadot/apps authors & contributors
+# This software may be modified and distributed under the terms
+# of the Apache-2.0 license. See the LICENSE file for details.
+
+# fail fast on any non-zero exits
+set -e
+
+# the docker image name and dockerhub repo
+NAME="polkadot-js-apps"
+REPO="jacogr"
+
+# extract the current npm version from package.json
+VERSION=$(cat package.json \
+  | grep version \
+  | head -1 \
+  | awk -F: '{ print $2 }' \
+  | sed 's/[",]//g' \
+  | sed 's/ //g')
+
+# helper function for the build logic
+function build () {
+  echo "*** Building $NAME"
+  docker build -t $NAME .
+}
+
+# helper function for the publishing logic
+function publish () {
+  docker login -u $REPO -p $DOCKER_PASS
+
+  echo "*** Tagging $REPO/$NAME"
+  docker tag $NAME $REPO/$NAME:$VERSION
+  docker tag $NAME $REPO/$NAME
+
+  echo "*** Publishing $NAME"
+  docker push $REPO/$NAME
+}
+
+build
+publish
