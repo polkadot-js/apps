b0f5d9991b99dfb614af9917d5f8a6fcf0c6a89d
Jaco Greeff <jacogr@gmail.com>
2020-06-11 07:53:14 +0200

    Docker tag version on non-beta

--- scripts/docker.sh
+++ scripts/docker.sh
@@ -29,7 +29,9 @@ function publish () {
   docker login -u $REPO -p $DOCKER_PASS
 
   echo "*** Tagging $REPO/$NAME"
-  # docker tag $NAME $REPO/$NAME:$VERSION
+  if [[ $VERSION != *"beta"* ]]; then
+    docker tag $NAME $REPO/$NAME:$VERSION
+  fi
   docker tag $NAME $REPO/$NAME
 
   echo "*** Publishing $NAME"
