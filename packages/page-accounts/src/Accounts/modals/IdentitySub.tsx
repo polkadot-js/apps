02da7ceffecd3467f61514f38fdc3f09d5d313fd
Jaco Greeff <jacogr@gmail.com>
2020-07-05 16:19:31 +0200

    Add/Remove rows for subs

--- packages/page-accounts/src/Accounts/modals/IdentitySub.tsx
+++ packages/page-accounts/src/Accounts/modals/IdentitySub.tsx
@@ -5,9 +5,9 @@
 import { AccountId, Balance } from '@polkadot/types/interfaces';
 import { ITuple } from '@polkadot/types/types';
 
-import React, { useEffect, useState } from 'react';
-import { Modal, Spinner, TxButton } from '@polkadot/react-components';
-import { useApi, useCall } from '@polkadot/react-hooks';
+import React, { useCallback, useEffect, useState } from 'react';
+import { Button, Modal, Spinner, TxButton } from '@polkadot/react-components';
+import { useAccounts, useApi, useCall } from '@polkadot/react-hooks';
 import { Data, Option, Vec } from '@polkadot/types';
 import { u8aToString } from '@polkadot/util';
 
@@ -19,35 +19,49 @@ interface Props {
   onClose: () => void;
 }
 
+function extractInfo ([[ids], opts]: [[string[]], Option<ITuple<[AccountId, Data]>>[]]): [string, string][] {
+  return ids.reduce((result: [string, string][], id, index): [string, string][] => {
+    const opt = opts[index];
+
+    if (opt.isSome) {
+      const [, data] = opt.unwrap();
+
+      if (data.isRaw) {
+        result.push([id, u8aToString(data.asRaw)]);
+      }
+    }
+
+    return result;
+  }, []);
+}
+
 function IdentitySub ({ address, className, onClose }: Props): React.ReactElement<Props> {
   const { t } = useTranslation();
   const { api } = useApi();
+  const { allAccounts } = useAccounts();
   const queryIds = useCall<string[]>(api.query.identity.subsOf, [address], {
     transform: ([, ids]: ITuple<[Balance, Vec<AccountId>]>) => ids.map((a) => a.toString())
   });
-  const queryInfos = useCall<Record<string, string>>(queryIds && queryIds.length !== 0 && api.query.identity.superOf.multi, [queryIds], {
-    transform: (optInfos: Option<ITuple<[AccountId, Data]>>[]) =>
-      optInfos.reduce((result: Record<string, string>, optInfo): Record<string, string> => {
-        if (optInfo.isSome) {
-          const [accountId, data] = optInfo.unwrap();
-
-          if (data.isRaw) {
-            result[accountId.toString()] = u8aToString(data.asRaw);
-          }
-        }
-
-        return result;
-      }, {})
-  });
-  const [infos, setInfos] = useState<Record<string, string> | undefined>();
+  const queryInfos = useCall<[[string[]], Option<ITuple<[AccountId, Data]>>[]]>(queryIds && queryIds.length !== 0 && api.query.identity.superOf.multi, [queryIds], { withParams: true });
+  const [infos, setInfos] = useState<[string, string][] | undefined>();
 
   useEffect((): void => {
-    if (queryIds && !queryIds.length) {
-      setInfos({});
-    } else if (queryInfos) {
-      setInfos(queryInfos);
+    if (queryInfos) {
+      setInfos(extractInfo(queryInfos));
+    } else if (queryIds && !queryIds.length) {
+      setInfos([]);
     }
   }, [queryIds, queryInfos]);
+
+  const _rowAdd = useCallback(
+    () => setInfos((infos) => infos && infos.concat([[allAccounts[0], '']])),
+    [allAccounts]
+  );
+
+  const _rowRemove = useCallback(
+    () => setInfos((infos) => infos && infos.slice(0, infos.length - 1)),
+    []
+  );
 
   return (
     <Modal
@@ -56,21 +70,45 @@ function IdentitySub ({ address, className, onClose }: Props): React.ReactElemen
     >
       <Modal.Content>
         {!infos
-          ? <Spinner label={t<string>('retrieving sub-identities')} />
+          ? <Spinner label={t<string>('Retrieving sub-identities')} />
           : (
-            <div>something</div>
+            <div>
+              {infos.map(([address, name], index) =>
+                <div key={`${address}:${index}`}>{address}&nbsp;{name}</div>
+              )}
+              <Button.Group>
+                <Button
+                  icon='plus'
+                  isPrimary
+                  label={t<string>('Add sub')}
+                  onClick={_rowAdd}
+                />
+                <Button
+                  icon='minus'
+                  isDisabled={infos.length === 0}
+                  isNegative
+                  label={t<string>('Remove sub')}
+                  onClick={_rowRemove}
+                />
+              </Button.Group>
+            </div>
           )
         }
       </Modal.Content>
       <Modal.Actions onCancel={onClose}>
-        <TxButton
-          accountId={address}
-          isPrimary
-          label={t<string>('Set Subs')}
-          onStart={onClose}
-          params={[]}
-          tx='identity.setSubs'
-        />
+        {infos && (
+          <TxButton
+            accountId={address}
+            isDisabled={!infos.length}
+            isPrimary
+            label={t<string>('Set Subs')}
+            onStart={onClose}
+            params={[
+              infos.map(([address, raw]) => [address, { raw }])
+            ]}
+            tx='identity.setSubs'
+          />
+        )}
       </Modal.Actions>
     </Modal>
   );
