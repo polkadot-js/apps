f1277c3ede2beaf0a850061e93085024a53f2e9f
Jaco Greeff <jacogr@gmail.com>
2020-07-08 08:51:34 +0200

    Society generator (#3141)
    * Society generator
    
    * Modal popup
    
    * Log accountId & hex
    
    * Spacing
    
    * Reduce for addressToBits

--- packages/page-society/src/Overview/Member.tsx
+++ packages/page-society/src/Overview/Member.tsx
@@ -4,9 +4,11 @@
 
 import { DeriveSocietyMember } from '@polkadot/api-derive/types';
 
-import React from 'react';
-import { AddressSmall, Tag } from '@polkadot/react-components';
+import React, { useEffect, useRef, useState } from 'react';
+import { AddressSmall, Icon, Modal, Tag } from '@polkadot/react-components';
+import { useApi, useToggle } from '@polkadot/react-hooks';
 
+import drawCanary from '../draw/canary';
 import { useTranslation } from '../translate';
 
 interface Props {
@@ -15,8 +17,27 @@ interface Props {
   value: DeriveSocietyMember;
 }
 
+const CANVAS_STYLE = {
+  display: 'block',
+  margin: '0 auto'
+};
+
 function Member ({ className = '', isHead, value: { accountId, strikes } }: Props): React.ReactElement<Props> {
   const { t } = useTranslation();
+  const { api } = useApi();
+  const canvasRef = useRef<HTMLCanvasElement | null>(null);
+  const [canInk] = useState(api.genesisHash.eq('0xb0a8d493285c2df73290dfb7e61f870f17b41801197a149ca93654499ea3dafe'));
+  const [isInkShowing, toggleInk] = useToggle();
+
+  useEffect((): void => {
+    if (canvasRef.current) {
+      const ctx = canvasRef.current.getContext('2d');
+
+      if (ctx) {
+        drawCanary(ctx, accountId);
+      }
+    }
+  });
 
   return (
     <tr className={className}>
@@ -35,6 +56,35 @@ function Member ({ className = '', isHead, value: { accountId, strikes } }: Prop
       <td className='all'>&nbsp;</td>
       <td className='number top'>
         {strikes.toString()}
+      </td>
+      <td>
+        {canInk && (
+          <>
+            <Icon
+              icon='pen-nib'
+              onClick={toggleInk}
+            />
+            {isInkShowing && (
+              <Modal
+                header={t('design samples')}
+                size='large'
+              >
+                <Modal.Content>
+                  <canvas
+                    height={525}
+                    ref={canvasRef}
+                    style={CANVAS_STYLE}
+                    width={800}
+                  />
+                </Modal.Content>
+                <Modal.Actions
+                  cancelLabel={t<string>('Close')}
+                  onCancel={toggleInk}
+                />
+              </Modal>
+            )}
+          </>
+        )}
       </td>
     </tr>
   );
