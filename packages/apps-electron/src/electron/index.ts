69b85e2d5384f66c0bf37f98efc2f208396bd0d7
Krzysztof Jelski <kjelski@gmail.com>
2020-07-10 15:35:07 +0200

    Add Content Security Policy header. (#3158)

--- packages/apps-electron/src/electron/index.ts
+++ packages/apps-electron/src/electron/index.ts
@@ -5,12 +5,14 @@
 import { app, shell } from 'electron';
 import { registerAccountStoreHandlers } from '../main/account-store';
 import { setupAutoUpdater } from './autoUpdater';
+import { setupContentSecurityPolicy } from './contentSecurityPolicy';
 import { createWindow } from './window';
 
 const ENV = process.env.NODE_ENV || 'production';
 
 const onReady = async () => {
   registerAccountStoreHandlers();
+  setupContentSecurityPolicy(ENV);
   await createWindow(ENV);
   await setupAutoUpdater();
 };
