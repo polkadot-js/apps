3aa1e8f42b17ca4b97b2edcb0bd887bad3b1a07c
Krzysztof Jelski <kjelski@gmail.com>
2020-07-10 15:31:00 +0200

    Open external links (eg. to GitHub or Wiki) in default system browser instead of a new Electron window. (#3159)

--- packages/apps-electron/src/electron/index.ts
+++ packages/apps-electron/src/electron/index.ts
@@ -2,7 +2,7 @@
 // This software may be modified and distributed under the terms
 // of the Apache-2.0 license. See the LICENSE file for details.
 
-import { app } from 'electron';
+import { app, shell } from 'electron';
 import { registerAccountStoreHandlers } from '../main/account-store';
 import { setupAutoUpdater } from './autoUpdater';
 import { createWindow } from './window';
@@ -14,5 +14,12 @@ const onReady = async () => {
   await createWindow(ENV);
   await setupAutoUpdater();
 };
+
+app.on('web-contents-created', (e, webContents) => {
+  webContents.on('new-window', (e, url) => {
+    e.preventDefault();
+    shell.openExternal(url).catch(console.error);
+  });
+});
 
 app.whenReady().then(onReady).catch(console.error);
