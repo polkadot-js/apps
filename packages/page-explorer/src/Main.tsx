927c1d02f92e1b95daede7b87822901be8f95a18
Jaco Greeff <jacogr@gmail.com>
2020-07-15 16:11:21 +0200

    Flatten recurring events (same block) (#3201)
    * Flattn recurring events (same block)
    
    * Same-height event filter
    
    * Display adjust
    
    * Filter {balances, treasury}.Deposit

--- packages/page-explorer/src/Main.tsx
+++ packages/page-explorer/src/Main.tsx
@@ -2,7 +2,7 @@
 // This software may be modified and distributed under the terms
 // of the Apache-2.0 license. See the LICENSE file for details.
 
-import { KeyedEvent } from './types';
+import { KeyedEvent } from '@polkadot/react-query/types';
 
 import React from 'react';
 import { HeaderExtended } from '@polkadot/api-derive';
