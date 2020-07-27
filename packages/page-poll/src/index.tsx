7aab1e0467e2503491d8e6fcf88948ee95e96135
Jaco Greeff <jacogr@gmail.com>
2020-07-27 17:55:24 +0200

    Remove Toggle completely after poll

--- packages/page-poll/src/index.tsx
+++ packages/page-poll/src/index.tsx
@@ -116,19 +116,21 @@ function PollApp ({ className }: Props): React.ReactElement<Props> {
                 <Columar.Column className='option'>
                   <div className='optionName'>{label}</div>
                   <div className='optionDesc'>{desc}</div>
-                  <Toggle
-                    className='pollToggle'
-                    isDisabled={!canVote}
-                    label={
-                      canVote
-                        ? value
-                          ? t<string>('Aye, I support this')
-                          : t<string>('Nay, I do not support this')
-                        : t<string>('Voting closed')
-                    }
-                    onChange={onChange}
-                    value={canVote && value}
-                  />
+                  {canVote && (
+                    <Toggle
+                      className='pollToggle'
+                      isDisabled={!canVote}
+                      label={
+                        canVote
+                          ? value
+                            ? t<string>('Aye, I support this')
+                            : t<string>('Nay, I do not support this')
+                          : t<string>('Voting closed')
+                      }
+                      onChange={onChange}
+                      value={canVote && value}
+                    />
+                  )}
                 </Columar.Column>
                 <Columar.Column>
                   {totals[index].isZero()
