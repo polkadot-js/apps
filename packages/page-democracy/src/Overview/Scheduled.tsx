d048f06e606c9b9cb88ec75807830f564d37e6df
Jaco Greeff <jacogr@gmail.com>
2020-07-29 19:21:21 +0200

    Display schedule on democracy (#3287)

---
+++ packages/page-democracy/src/Overview/Scheduled.tsx
@@ -0,0 +1,49 @@
+// Copyright 2017-2020 @polkadot/app-democracy authors & contributors
+// This software may be modified and distributed under the terms
+// of the Apache-2.0 license. See the LICENSE file for details.
+
+import { BlockNumber } from '@polkadot/types/interfaces';
+import { ScheduledExt } from './types';
+
+import React from 'react';
+import { CallExpander } from '@polkadot/react-components';
+import { useApi, useCall } from '@polkadot/react-hooks';
+import { BlockToTime } from '@polkadot/react-query';
+import { formatNumber } from '@polkadot/util';
+
+interface Props {
+  className?: string;
+  value: ScheduledExt;
+}
+
+function Scheduled ({ className = '', value: { call, count, maybePeriodic, nextBlockNumber } }: Props): React.ReactElement<Props> {
+  const { api } = useApi();
+  const bestNumber = useCall<BlockNumber>(api.derive.chain.bestNumber, []);
+  const period = maybePeriodic.unwrapOr(null);
+
+  return (
+    <tr className={className}>
+      <td className='all'><CallExpander value={call} /></td>
+      <td className='number together'>
+        {bestNumber && (
+          <>
+            <BlockToTime blocks={nextBlockNumber.sub(bestNumber)} />
+            #{formatNumber(nextBlockNumber)}
+          </>
+        )}
+      </td>
+      <td className='number together'>
+        {period && (
+          formatNumber(period[0])
+        )}
+      </td>
+      <td className='number together'>
+        {period && (
+          formatNumber(count) + ' / ' + formatNumber(period[1])
+        )}
+      </td>
+    </tr>
+  );
+}
+
+export default React.memo(Scheduled);
