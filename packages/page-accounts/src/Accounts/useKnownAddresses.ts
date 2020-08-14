182671b539e9f7d21c85c54917c377218931e631
Jaco Greeff <jacogr@gmail.com>
2020-08-14 10:02:32 +0200

    Additional useMemo (no useEffect + useState) (#3409)
    * Additional useMemo  (no useEffect + useState)
    
    * Apply suggestions from code review

--- packages/page-accounts/src/Accounts/useKnownAddresses.ts
+++ packages/page-accounts/src/Accounts/useKnownAddresses.ts
@@ -2,19 +2,15 @@
 // This software may be modified and distributed under the terms
 // of the Apache-2.0 license. See the LICENSE file for details.
 
-import { useEffect, useState } from 'react';
+import { useMemo } from 'react';
 import { useAccounts, useAddresses } from '@polkadot/react-hooks';
 
 export default function useKnownAddresses (exclude?: string): string[] {
   const { allAccounts } = useAccounts();
   const { allAddresses } = useAddresses();
-  const [known, setKnown] = useState<string[]>([]);
 
-  useEffect((): void => {
-    allAccounts && allAddresses && setKnown(
-      [...allAccounts, ...allAddresses].filter((a): boolean => a !== exclude)
-    );
-  }, [allAccounts, allAddresses, exclude]);
-
-  return known;
+  return useMemo(
+    () => [...allAccounts, ...allAddresses].filter((a) => a !== exclude),
+    [allAccounts, allAddresses, exclude]
+  );
 }
