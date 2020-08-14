44023fe51fc24da27e266c1f2cb10514566750fd
mariopino <info@quequiereshacer.es>
2020-08-14 16:30:46 +0200

    alphabet. order

--- packages/apps-config/src/links/index.ts
+++ packages/apps-config/src/links/index.ts
@@ -7,15 +7,15 @@ import { ExternalDef } from './types';
 import Commonwealth from './commonwealth';
 import Polkascan from './polkascan';
 import Polkassembly from './polkassembly';
-import Subscan from './subscan';
 import Polkastats from './polkastats';
+import Subscan from './subscan';
 
 const externals: Record<string, ExternalDef> = {
   Commonwealth,
   Polkascan,
   Polkassembly,
-  Subscan,
-  Polkastats
+  Polkastats,
+  Subscan
 };
 
 export default externals;
