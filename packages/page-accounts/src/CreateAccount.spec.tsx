0ae2327fa0744100d28ab6752bd91d39546a1167
Jaco Greeff <jacogr@gmail.com>
2020-08-17 12:30:36 +0200

    Apply suggestions from code review

--- packages/page-accounts/src/CreateAccount.spec.tsx
+++ packages/page-accounts/src/CreateAccount.spec.tsx
@@ -1,6 +1,7 @@
 // Copyright 2017-2020 @polkadot/app-accounts authors & contributors
 // This software may be modified and distributed under the terms
 // of the Apache-2.0 license. See the LICENSE file for details.
+
 import AccountsApp from '@polkadot/app-accounts';
 import { MemoryStore } from '@polkadot/app-accounts/test-support/MemoryStore';
 import { Api } from '@polkadot/react-api';
