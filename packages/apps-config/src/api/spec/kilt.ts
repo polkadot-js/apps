028717d002208f029bea1ff2751179167a4da3fd
William Freudenberger <w.freude@icloud.com>
2020-08-18 12:05:13 +0200

    Add KILT spec (#3436)
    * Add KILT spec
    
    * fix: mashnet url
    
    * refactor: apply alphabetical endpoints ordering

---
+++ packages/apps-config/src/api/spec/kilt.ts
@@ -0,0 +1,16 @@
+// Copyright 2017-2020 @polkadot/apps-config authors & contributors
+// This software may be modified and distributed under the terms
+// of the Apache-2.0 license. See the LICENSE file for details.
+
+export default {
+  Address: 'AccountId',
+  BlockNumber: 'u64',
+  DelegationNodeId: 'Hash',
+  ErrorCode: 'u16',
+  Index: 'u64',
+  LookupSource: 'AccountId',
+  Permissions: 'u32',
+  PublicBoxKey: 'Hash',
+  PublicSigningKey: 'Hash',
+  Signature: 'MultiSignature'
+};
