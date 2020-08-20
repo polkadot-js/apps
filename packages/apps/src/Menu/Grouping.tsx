c35a3d0526f269de4ddbf58528224e6a7227198a
Jaco Greeff <jacogr@gmail.com>
2020-08-20 10:44:54 +0200

    Lighten

--- packages/apps/src/Menu/Grouping.tsx
+++ packages/apps/src/Menu/Grouping.tsx
@@ -56,7 +56,7 @@ export default React.memo(styled(Grouping)`
   }
 
   .groupMenu {
-    background: #5e5d5c;
+    background: #6e6d6c;
     border-radius: 0 0 0.25rem 0.25rem;
     display: none;
     margin: 0;
@@ -77,7 +77,7 @@ export default React.memo(styled(Grouping)`
   &:hover {
     .groupHdr,
     .groupMenu li {
-      background: rgba(78, 77, 76, 0.75);
+      background: rgba(94, 93, 92, 0.75); /* #5e5d5c */
     }
 
     .groupMenu {
--- packages/apps/src/Menu/index.tsx
+++ packages/apps/src/Menu/index.tsx
@@ -161,7 +161,7 @@ function Menu ({ className = '' }: Props): React.ReactElement<Props> {
 
 export default React.memo(styled(Menu)`
   align-items: center;
-  background: #5e5d5c;
+  background: #6e6d6c;
   border-top: 0.5rem solid transparent;
   display: flex;
   justify-content: space-between;
@@ -191,7 +191,7 @@ export default React.memo(styled(Menu)`
   }
 
   .menuItems {
-    background: #5e5d5c;
+    background: #6e6d6c;
     color: #f5f4f3;
     flex: 1 1;
     list-style: none;
