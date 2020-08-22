9957b7282993c6880dce2b94e156b4561b8dddcd
Jaco Greeff <jacogr@gmail.com>
2020-08-22 09:26:18 +0200

    Small threshold adjustments

--- packages/page-council/src/thresholds.ts
+++ packages/page-council/src/thresholds.ts
@@ -3,35 +3,36 @@
 // of the Apache-2.0 license. See the LICENSE file for details.
 
 import { ApiPromise } from '@polkadot/api';
-import { POLKADOT_GENESIS, KULUPU_GENESIS } from '@polkadot/apps-config/api/constants';
+import { POLKADOT_GENESIS, KULUPU_GENESIS, KUSAMA_GENESIS } from '@polkadot/apps-config/api/constants';
 
 const PROPOSE_THRESHOLDS: Record<string, number> = {
-  // Kulupu
   [KULUPU_GENESIS]: 1,
-  // Polkadot
-  [POLKADOT_GENESIS]: 0.6
+  [KUSAMA_GENESIS]: 0.5,
+  [POLKADOT_GENESIS]: 0.6,
+  default: 0.5
 };
 
 const SLASH_THRESHOLDS: Record<string, number> = {
-  // Polkadot
-  [POLKADOT_GENESIS]: 0.75
+  [KUSAMA_GENESIS]: 0.5,
+  [POLKADOT_GENESIS]: 0.75,
+  default: 0.5
 };
 
 const TREASURY_THRESHOLDS: Record<string, number> = {
-  // Kulupu
   [KULUPU_GENESIS]: 0.8,
-  // Polkadot
-  [POLKADOT_GENESIS]: 0.6
+  [KUSAMA_GENESIS]: 0.6,
+  [POLKADOT_GENESIS]: 0.6,
+  default: 0.6
 };
 
 export function getThreshold (api: ApiPromise): number {
-  return PROPOSE_THRESHOLDS[api.genesisHash.toHex()] || 0.5;
+  return PROPOSE_THRESHOLDS[api.genesisHash.toHex()] || PROPOSE_THRESHOLDS.default;
 }
 
 export function getSlashThreshold (api: ApiPromise): number {
-  return SLASH_THRESHOLDS[api.genesisHash.toHex()] || 0.5;
+  return SLASH_THRESHOLDS[api.genesisHash.toHex()] || SLASH_THRESHOLDS.default;
 }
 
 export function getTreasuryThreshold (api: ApiPromise): number {
-  return TREASURY_THRESHOLDS[api.genesisHash.toHex()] || 0.6;
+  return TREASURY_THRESHOLDS[api.genesisHash.toHex()] || TREASURY_THRESHOLDS.default;
 }
