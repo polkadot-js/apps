a35d98cc172f1caadd93b51819a44f3a71b8dd6a
Jaco Greeff <jacogr@gmail.com>
2020-08-23 23:46:49 +0200

    Split RPC (dejavu) into own nav item

--- packages/apps-routing/src/signing.ts
+++ packages/apps-routing/src/signing.ts
@@ -4,17 +4,18 @@
 
 import { Route } from './types';
 
-import Component from '@polkadot/app-toolbox';
+import Component from '@polkadot/app-signing';
 
 export default function create (t: <T = string> (key: string, text: string, options: { ns: string }) => T): Route {
   return {
     Component,
     display: {
+      needsAccounts: true,
       needsApi: []
     },
     group: 'developer',
-    icon: 'tools',
-    name: 'toolbox',
-    text: t<string>('nav.toolbox', 'Toolbox', { ns: 'apps-routing' })
+    icon: 'signature',
+    name: 'signing',
+    text: t<string>('nav.signing', 'Sign and verify', { ns: 'apps-routing' })
   };
 }
