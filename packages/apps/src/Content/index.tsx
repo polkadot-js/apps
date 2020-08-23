2ee65de48e88cc61c84fecfb8acc0c3baaff0eb4
Jaco Greeff <jacogr@gmail.com>
2020-08-23 10:18:05 +0200

    Basic settings when not connected

--- packages/apps/src/Content/index.tsx
+++ packages/apps/src/Content/index.tsx
@@ -37,6 +37,7 @@ function Content ({ className }: Props): React.ReactElement<Props> {
   const { t } = useTranslation();
   const { api, isApiConnected, isApiReady } = useApi();
   const { queueAction } = useContext(StatusContext);
+
   const { Component, display: { needsApi }, name } = useMemo(
     (): Route => {
       const app = location.pathname.slice(1) || '';
@@ -46,9 +47,14 @@ function Content ({ className }: Props): React.ReactElement<Props> {
     [location, t]
   );
 
+  const missingApis = useMemo(
+    () => findMissingApis(api, needsApi),
+    [api, needsApi]
+  );
+
   return (
     <div className={className}>
-      {(!isApiReady || !isApiConnected)
+      {needsApi && (!isApiReady || !isApiConnected)
         ? (
           <div className='connecting'>
             <Spinner label={t<string>('Initializing connection')} />
@@ -57,7 +63,7 @@ function Content ({ className }: Props): React.ReactElement<Props> {
         : (
           <>
             <Suspense fallback='...'>
-              {findMissingApis(api, needsApi).length
+              {missingApis.length
                 ? <NotFound />
                 : (
                   <ErrorBoundary trigger={name}>
--- packages/page-settings/src/index.tsx
+++ packages/page-settings/src/index.tsx
@@ -7,6 +7,7 @@ import { AppProps as Props } from '@polkadot/react-components/types';
 import React, { useMemo } from 'react';
 import { Route, Switch } from 'react-router';
 import { HelpOverlay, Tabs } from '@polkadot/react-components';
+import { useApi } from '@polkadot/react-hooks';
 
 import md from './md/basics.md';
 import { useTranslation } from './translate';
@@ -20,7 +21,9 @@ export { useCounter };
 
 function SettingsApp ({ basePath, onStatusChange }: Props): React.ReactElement<Props> {
   const { t } = useTranslation();
+  const { isApiConnected, isApiReady } = useApi();
   const numExtensions = useCounter();
+
   const items = useMemo(() => [
     {
       isRoot: true,
@@ -42,12 +45,18 @@ function SettingsApp ({ basePath, onStatusChange }: Props): React.ReactElement<P
     }
   ], [numExtensions, t]);
 
+  const hidden = useMemo(
+    () => (isApiConnected && isApiReady) ? [] : ['metadata', 'i18n'],
+    [isApiConnected, isApiReady]
+  );
+
   return (
     <main className='settings--App'>
       <HelpOverlay md={md as string} />
       <header>
         <Tabs
           basePath={basePath}
+          hidden={hidden}
           items={items}
         />
       </header>
