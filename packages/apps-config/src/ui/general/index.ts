0c5e1b31473700ccab111cfa9215aafa990b7dcc
aniiantt <nxiiun@gmail.com>
2020-08-24 17:38:40 +1200

    rococo acala (#3497)

--- packages/apps-config/src/ui/general/index.ts
+++ packages/apps-config/src/ui/general/index.ts
@@ -13,6 +13,7 @@ const chainRocco = '#6f36dc';
 const chainRoccoTick = '#22bb22';
 const chainRoccoTrack = '#bb2222';
 const chainRoccoTrick = '#2222bb';
+const chainRoccoAcala = '#173DC9';
 const chainWestend = '#da68a7';
 
 const nodeCentrifuge = '#fcc367';
@@ -34,6 +35,7 @@ const chainColors: Record<string, any> = [
   ['Tick', chainRoccoTick],
   ['Track', chainRoccoTrack],
   ['Trick', chainRoccoTrick],
+  ['acala mandala pc1', chainRoccoAcala],
   ['Westend', chainWestend]
 ].reduce((colors, [chain, color]): Record<string, any> => ({
   ...colors,
