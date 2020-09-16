b547e8969008ee18521723bdf373bd629102b1f6
Vincent Geddes <vincent@snowfork.com>
2020-09-16 02:12:59 +0200

    Update colors for Artemis node (#10)

--- packages/apps-config/src/ui/general/index.ts
+++ packages/apps-config/src/ui/general/index.ts
@@ -25,6 +25,7 @@ const chainWestend = '#da68a7';
 
 // based on node name
 // alphabetical
+const nodeArtemis = '#505049';
 const nodeCentrifuge = '#fcc367';
 const nodeEdgeware = '#0a95df';
 const nodeNodle = '#1ab394';
@@ -61,6 +62,7 @@ const chainColors: Record<string, any> = [
 // overrides based on the actual software node type (all '-' converted to ' ')
 // alphabetical
 const nodeColors: Record<string, any> = [
+  ['artemis node', nodeArtemis],
   ['centrifuge chain', nodeCentrifuge],
   ['Centrifuge Chain Node', nodeCentrifuge],
   ['edgeware node', nodeEdgeware],
