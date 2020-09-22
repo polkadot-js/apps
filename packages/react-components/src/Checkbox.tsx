7060ecceb22e4af45e02049ce3b75c61455a70ad
Michał Ziętalak <michal.z@ethworks.io>
2020-09-22 13:50:36 +0200

    Set checkbox label to unselectable

--- packages/react-components/src/Checkbox.tsx
+++ packages/react-components/src/Checkbox.tsx
@@ -55,6 +55,12 @@ export default React.memo(styled(Checkbox)`
     margin: 0 0.5rem;
     opacity: 1;
     cursor: pointer;
+    -webkit-touch-callout: none; /* iOS Safari */
+    -webkit-user-select: none; /* Safari */
+    -khtml-user-select: none; /* Konqueror HTML */
+    -moz-user-select: none; /* Old versions of Firefox */
+    -ms-user-select: none; /* Internet Explorer/Edge */
+    user-select: none;
   }
 
   > label,
