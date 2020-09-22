b2449546d0106a15df046085091637537fc6ed62
Thibaut Sardan <33178835+Tbaut@users.noreply.github.com>
2020-09-22 13:53:31 +0200

    Fix overflow in democracy proposal (polkadot) (#3739)
    * dix overflow
    
    * Update packages/react-components/src/Expander.tsx
    
    Co-authored-by: Jaco Greeff <jacogr@gmail.com>

--- packages/react-components/src/Expander.tsx
+++ packages/react-components/src/Expander.tsx
@@ -145,6 +145,10 @@ export default React.memo(styled(Expander)`
       text-overflow: ellipsis;
       vertical-align: middle;
       white-space: nowrap;
+
+      span {
+        white-space: normal;
+      }
     }
 
     .ui--Icon {
