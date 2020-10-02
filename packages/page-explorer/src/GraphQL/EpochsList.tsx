a87f63e3ac40933e77c000a628cf09d3b4ead5e8
Sam Hellawell <sshellawell@gmail.com>
2020-10-02 18:40:31 +0100

    add pagination to epochs list

--- packages/page-explorer/src/GraphQL/EpochsList.tsx
+++ packages/page-explorer/src/GraphQL/EpochsList.tsx
@@ -4,13 +4,14 @@ import Epoch from '../Epoch';
 import { useTranslation } from '../translate';
 
 import { HeaderExtended } from '@polkadot/api-derive';
-import { Table } from '@polkadot/react-components';
+import { Table, Button } from '@polkadot/react-components';
 
 import { getSub, getQuery, accountToPolkadot } from '../apollo-helpers';
 import { ApolloProvider, Query } from 'react-apollo';
 import gql from 'graphql-tag';
 
 const epochsPerPage = 16;
+const startPage = 1;
 
 const VALIDATORS_QUERY = gql`
   query validator {
@@ -30,9 +31,46 @@ const VALIDATORS_QUERY = gql`
   }
 `;
 
-// Take entries of map with numeric keys and sort them in ascending order of key
-function sortEntriesOfMapWithNumKey(entries) {
-  entries.sort((element1, element2) => element1[0]._args[0].toNumber() - element2[0]._args[0].toNumber());
+function Pagination ({ page, maxPages, maxIndex, onPageChange }): React.ReactElement<Props> {
+  const [ currentPage, setCurrentPage ] = useState(page || 0);
+  const { t } = useTranslation();
+
+  function handlePreviousClick() {
+    if (currentPage > 1) {
+      const targetPage = currentPage - 1;
+      setCurrentPage(targetPage);
+      onPageChange(targetPage, maxPages, maxIndex);
+    }
+  }
+
+  function handleNextClick() {
+    if (currentPage < maxPages) {
+      const targetPage = currentPage + 1;
+      setCurrentPage(targetPage);
+      onPageChange(targetPage, maxPages, maxIndex);
+    }
+  }
+
+  return (
+    <Button.Group>
+      <Button
+        icon='arrow-left'
+        isDisabled={currentPage <= 1}
+        label={t<string>('previous page')}
+        onClick={handlePreviousClick}
+      />
+      <Button
+        isDisabled={true}
+        label={'Page: ' + currentPage}
+      />
+      <Button
+        icon='arrow-right'
+        isDisabled={currentPage >= maxPages}
+        label={t<string>('next page')}
+        onClick={handleNextClick}
+      />
+    </Button.Group>
+  );
 }
 
 function EpochsList (): React.ReactElement<Props> {
@@ -40,21 +78,31 @@ function EpochsList (): React.ReactElement<Props> {
   const { t } = useTranslation();
   const [ totalRewards, setTotalRewards ] = useState({});
   const [ accounts, setAccounts ] = useState([]);
+  const [ { maxPages, maxIndex }, setMaxPages ] = useState([]);
   const headers = [
     [t('validators'), 'start', 5],
     [t('produced blocks'), 'expand'],
   ];
 
-  async function setEpochIndices() {
-    const currentEpoch = await api.query.poAModule.epoch();
+  function handlePageChange(index, max, maxIndex) {
+    const startIndex = maxIndex - (index - 1) * epochsPerPage;
     const promises = [];
-    for (let i = currentEpoch; i >= Math.max(currentEpoch - epochsPerPage, 1); i--) {
+    for (let i = startIndex; i >= Math.max(startIndex - epochsPerPage, 1); i--) {
       promises.push({
         index: i
       });
     }
-
     setAccounts(promises);
+  }
+
+  async function setEpochIndices() {
+    const currentEpoch = await api.query.poAModule.epoch();
+    const maxPages = Math.ceil(currentEpoch.toNumber() / epochsPerPage);
+    handlePageChange(startPage, maxPages, currentEpoch.toNumber());
+    setMaxPages({
+      maxPages,
+      maxIndex: currentEpoch.toNumber(),
+    });
   }
 
   useEffect(() => {
@@ -78,16 +126,19 @@ function EpochsList (): React.ReactElement<Props> {
   // TODO: add pagination
 
   return (
-    <Table
-      empty={t<string>('No epochs available')}
-      header={headerRef.current}
-    >
-      {accounts
-        .filter((header) => !!header)
-        .map((header): React.ReactNode => (
-          <Epoch key={header.index} value={header} />
-        ))}
-    </Table>
+    <>
+      <Table
+        empty={t<string>('No epochs available')}
+        header={headerRef.current}
+      >
+        {accounts
+          .filter((header) => !!header)
+          .map((header): React.ReactNode => (
+            <Epoch key={header.index} value={header} />
+          ))}
+      </Table>
+      <Pagination page={startPage} maxPages={maxPages} maxIndex={maxIndex} onPageChange={handlePageChange} />
+    </>
   );
 }
 
