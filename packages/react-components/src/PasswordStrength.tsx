9ac6e8ecb427b5ebd02b9c9f3d77264eb790eb00
Krzysztof Jelski <krzysztof@ethworks.io>
2020-10-19 13:13:25 +0200

    Organize imports. Configure StrengthTester once. Use color from theme.

--- packages/react-components/src/PasswordStrength.tsx
+++ packages/react-components/src/PasswordStrength.tsx
@@ -1,12 +1,22 @@
 // Copyright 2017-2020 @polkadot/react-components authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
-import { useTranslation } from '@polkadot/react-components/translate';
+import { ThemeProps } from './types';
+
 import React from 'react';
 import styled from 'styled-components';
 import strengthTester from 'owasp-password-strength-test';
 
+import { useTranslation } from './translate';
+
 const MAX_STRENGTH = 7; // equal to number of password tests in owasp strength tester
+
+strengthTester.config({
+  allowPassphrases: true,
+  maxLength: 128,
+  minLength: 8,
+  minPhraseLength: 20
+});
 
 interface Props {
   className?: string;
@@ -14,12 +24,6 @@ interface Props {
 }
 
 function calcStrength (password: string): number {
-  strengthTester.config({
-    allowPassphrases: true,
-    maxLength: 128,
-    minLength: 8,
-    minPhraseLength: 20
-  });
   const testResult = strengthTester.test(password);
   const passedTests = Math.max(0, testResult.passedTests.length - testResult.failedTests.length);
 
@@ -49,30 +53,28 @@ function PasswordStrength ({ className = '', value }: Props): React.ReactElement
   );
 }
 
-export default React.memo(styled(PasswordStrength)`
-align-items: center;
-margin-top: 0.5rem;
-margin-left: 2rem;
-font-weight: 800;
-font-size: 10px;
-line-height: 14px;
-text-transform: uppercase;
-color: #8B8B8B;
-& .ui--Strength-bar {
-  position: relative;
-  height: 0.6rem;
-  width: 14rem;
-  margin: 0 10px;
-  border: 1px solid #DFDFDF;
-  border-radius: 0.15rem;
-  background: #ECECEC;
-}
-& .ui--Strength-bar-highlighted {
-  position: absolute;
-  top: -0.07rem;
-  height: 0.6rem;
-  width: 100%;
-  border-radius: 0.15rem;
-  background: linear-gradient(90.43deg, #FF8B00 0%, #FFBB50 112.75%);
-}
-`);
+export default React.memo(styled(PasswordStrength)(({ theme }: ThemeProps) => `
+  align-items: center;
+  margin-top: 0.5rem;
+  margin-left: 2rem;
+  font-size: 1rem;
+  text-transform: uppercase;
+  color: ${theme.colorLabel};
+  .ui--Strength-bar {
+    position: relative;
+    height: 0.6rem;
+    width: 14rem;
+    margin: 0 10px;
+    border: 1px solid #DFDFDF;
+    border-radius: 0.15rem;
+    background: #ECECEC;
+  }
+  .ui--Strength-bar-highlighted {
+    position: absolute;
+    top: -0.07rem;
+    height: 0.6rem;
+    width: 100%;
+    border-radius: 0.15rem;
+    background: linear-gradient(90.43deg, #FF8B00 0%, #FFBB50 112.75%);
+  }
+`));
