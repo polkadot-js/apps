afadc33ea8150b659d6b03ffada10a38dec2aea2
Krzysztof Jelski <krzysztof@ethworks.io>
2020-10-19 13:38:16 +0200

    wip on text area

--- packages/page-accounts/src/modals/Create.tsx
+++ packages/page-accounts/src/modals/Create.tsx
@@ -10,8 +10,7 @@ import FileSaver from 'file-saver';
 import React, { useCallback, useMemo, useState } from 'react';
 import styled from 'styled-components';
 import { DEV_PHRASE } from '@polkadot/keyring/defaults';
-import { AddressRow, Button, Checkbox, CopyButton, Dropdown, Expander, Input, InputAddress, Modal } from '@polkadot/react-components';
-import TextAreaWithLabel from '@polkadot/react-components/TextAreaWithLabel';
+import { AddressRow, Button, Checkbox, CopyButton, Dropdown, Expander, Input, InputAddress, Modal, TextArea } from '@polkadot/react-components';
 import { useApi } from '@polkadot/react-hooks';
 import keyring from '@polkadot/ui-keyring';
 import uiSettings from '@polkadot/ui-settings';
@@ -294,8 +293,7 @@ function Create ({ className = '', onClose, onStatusChange, seed: propsSeed, typ
         {step === 1 && <>
           <Modal.Columns>
             <Modal.Column>
-              <TextAreaWithLabel
-                className='ui--TextArea-lineHeight'
+              <TextArea
                 help={t<string>('The private key for your account is derived from this seed. This seed must be kept secret as anyone in its possession has access to the funds of this account. If you validate, use the seed of the session account as the "--key" parameter of your node.')}
                 isAction
                 isError={!isSeedValid}
@@ -321,7 +319,7 @@ function Create ({ className = '', onClose, onStatusChange, seed: propsSeed, typ
                   isMnemonic
                   value={seed}
                 />
-              </TextAreaWithLabel>
+              </TextArea>
             </Modal.Column>
             <Modal.Column>
               <p>{t<string>('The secret seed value for this account. Ensure that you keep this in a safe place, with access to the seed you can re-create the account.')}</p>
--- packages/react-components/src/TextArea.tsx
+++ packages/react-components/src/TextArea.tsx
@@ -1,31 +1,131 @@
 // Copyright 2017-2020 @polkadot/react-components authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
-import styled, { css } from 'styled-components';
+import React, { TextareaHTMLAttributes, useCallback } from 'react';
+
+import { Labelled } from '@polkadot/react-components';
+import styled from 'styled-components';
 
 interface Props {
+  children?: React.ReactNode;
+  className?: string;
+  help?: React.ReactNode;
+  isError?: boolean;
+  isReadOnly?: boolean;
+  label?: React.ReactNode;
+  onChange?: (arg: string) => void;
+  seed?: string;
+  withLabel?: boolean;
+}
+
+interface TextInputProps extends TextareaHTMLAttributes<any>{
   withError?: boolean;
 }
 
-const TextInput = css(({ withError }: Props) => `
+const TextInput = ({ withError, ...textAreaProps }: TextInputProps) => <textarea
+  {...textAreaProps}
+  className={[textAreaProps.className, withError ? 'ui-textArea-withError' : ''].join(' ')}
+/>;
+
+const TextAreaInput = styled(TextInput)`
   background: #FAFAFA;
-  border-color: ${withError ? '#E42F2F' : '#DDE1EB'};
   border-radius: 4px;
   border: 1px solid #DDE1EB;
   box-sizing: border-box;
-  color: ${withError ? '#E42F2F' : '#242529'};
+  color: #242529;
   display: block;
   font-size: 16px;
   outline: none;
   padding: 1.75rem 3rem 0.75rem 1.5rem;
   resize: none;
   width: 100%;
+
   &:read-only {
-    background: '#1A1B20';
+    background: #1A1B20;
     box-shadow: none;
     outline: none;
   }
-`);
 
-export const TextArea = styled.textarea<Props>`${TextInput}`;
-export const Input = styled.input<Props>`${TextInput}`;
+  &.ui-textArea-withError {
+    border-color: #E42F2F;
+    color: #E42F2F;
+  }
+`;
+
+interface TextAreaWithDropdownProps {
+  children?: React.ReactNode;
+  isError?: boolean;
+  isFocused?: boolean;
+  isReadOnly?: boolean;
+  rowsCount?: number;
+  onChange?: (value: string) => void;
+  value?: string;
+  id: string;
+}
+
+function TextAreaWithDropdown ({ children, id, isError, isFocused, isReadOnly, onChange, rowsCount, value }: TextAreaWithDropdownProps): React.ReactElement<TextAreaWithDropdownProps> {
+  const _onChange = useCallback(
+    ({ target: { value } }: React.ChangeEvent<HTMLTextAreaElement>): void => {
+      onChange && onChange(value);
+    },
+    [onChange]
+  );
+
+  return (
+    <>
+      <TextAreaInput
+        autoCapitalize='off'
+        autoCorrect='off'
+        autoFocus={isFocused}
+        id={id}
+        onChange={_onChange}
+        readOnly={isReadOnly}
+        rows={rowsCount || 2}
+        spellCheck={false}
+        value={value}
+        withError={isError}
+      />
+      {children}
+    </>
+  );
+}
+
+function TextArea ({ children, className, help, isError, isReadOnly, label, onChange, seed, withLabel }: Props): React.ReactElement<Props> {
+  return (
+    <Labelled
+      className={className}
+      help={help}
+      label={label}
+      withLabel={withLabel}
+    >
+      <div className='TextAreaWithDropdown'>
+        <TextAreaWithDropdown
+          id='printJS-seed'
+          isError={isError}
+          isReadOnly={isReadOnly}
+          onChange={onChange}
+          value={seed}
+        >
+          {children}
+        </TextAreaWithDropdown>
+      </div>
+    </Labelled>
+  );
+}
+
+export default React.memo(styled(TextArea)`
+  .TextAreaWithDropdown {
+    display: flex;
+    textarea {
+      border-top-right-radius: 0;
+      border-bottom-right-radius: 0;
+      background: #fff;
+      border-color: #DFDFDF;
+      border-right: none;
+    }
+    .ui.selection.dropdown {
+      border-top-left-radius: 0;
+      border-bottom-left-radius: 0;
+    }
+  }
+`);
