b5721d0a93664feabef8786c0cf409459da8e7b7
Hal Smith Stevens <hally9k@gmail.com>
2020-10-29 10:27:08 +1300

    TANZ brand hack

--- packages/apps-config/src/ui/colors.ts
+++ packages/apps-config/src/ui/colors.ts
@@ -2,88 +2,92 @@
 // SPDX-License-Identifier: Apache-2.0
 
 // defaults
-const emptyColor = '#99999';
+const emptyColor = "#99999";
 
 // based on chain name
 // alphabetical
-const chainCrab = '#7C30DD';
-const chainCrust = '#ff8812';
-const chainHanonycash = '#0099CC';
-const chainKulupu = '#003366';
-const chainPhala = '#4dc56a';
-const chainPlasm = '#2096F3';
-const chainPolkadot = '#e6007a';
-const chainKusama = '#000000';
-const chainRocco = '#6f36dc';
-const chainRoccoAcala = '#173DC9';
-const chainRoccoDarwinia = '#7C30DD';
-const chainRococoLaminar = '#004FFF';
-const chainRoccoTick = '#22bb22';
-const chainRoccoTrack = '#bb2222';
-const chainRoccoTrick = '#2222bb';
-const chainWestend = '#da68a7';
+const chainCrab = "#7C30DD";
+const chainCrust = "#ff8812";
+const chainHanonycash = "#0099CC";
+const chainKulupu = "#003366";
+const chainPhala = "#4dc56a";
+const chainPlasm = "#2096F3";
+const chainPolkadot = "#e6007a";
+const chainKusama = "#000000";
+const chainRocco = "#6f36dc";
+const chainRoccoAcala = "#173DC9";
+const chainRoccoDarwinia = "#7C30DD";
+const chainRococoLaminar = "#004FFF";
+const chainRoccoTick = "#22bb22";
+const chainRoccoTrack = "#bb2222";
+const chainRoccoTrick = "#2222bb";
+const chainWestend = "#da68a7";
+const chainDevelopment = "#2A3612";
 
 // based on node name
 // alphabetical
-const nodeCanvas = '#c77cff';
-const nodeCentrifuge = '#fcc367';
-const nodeEdgeware = '#0a95df';
-const nodeEquilibrium = '#1792ff';
-const nodeNodle = '#1ab394';
-const nodeKilt = '#eb5b2a';
-const nodeStafi = '#00F3AB';
-const nodeSubsocial = '#b9018c';
+const nodeCanvas = "#c77cff";
+const nodeCentrifuge = "#fcc367";
+const nodeEdgeware = "#0a95df";
+const nodeEquilibrium = "#1792ff";
+const nodeNodle = "#1ab394";
+const nodeKilt = "#eb5b2a";
+const nodeStafi = "#00F3AB";
+const nodeSubsocial = "#b9018c";
 
 // overrides based on the actual matched chain name
 // alphabetical
 const chainColors: Record<string, any> = [
-  ['acala mandala pc1', chainRoccoAcala],
-  ['Crab', chainCrab],
-  ['crust maxwell cc2', chainCrust],
-  ['darwinia parachain', chainRoccoDarwinia],
-  ['hanonycash', chainHanonycash],
-  ['Kulupu', chainKulupu],
-  ['Kusama', chainKusama],
-  ['Kusama CC1', chainKusama],
-  ['Kusama CC2', chainKusama],
-  ['Kusama CC3', chainKusama],
-  ['laminar turbulence pc1', chainRococoLaminar],
-  ['phala poc 2', chainPhala],
-  ['Plasm', chainPlasm],
-  ['Polkadot', chainPolkadot],
-  ['Polkadot CC1', chainPolkadot],
-  ['Rococo', chainRocco],
-  ['Tick', chainRoccoTick],
-  ['Track', chainRoccoTrack],
-  ['Trick', chainRoccoTrick],
-  ['Westend', chainWestend]
-].reduce((colors, [chain, color]): Record<string, any> => ({
-  ...colors,
-  [chain.toLowerCase()]: color
-}), {});
+  ["acala mandala pc1", chainRoccoAcala],
+  ["Crab", chainCrab],
+  ["crust maxwell cc2", chainCrust],
+  ["darwinia parachain", chainRoccoDarwinia],
+  ["hanonycash", chainHanonycash],
+  ["Kulupu", chainKulupu],
+  ["Kusama", chainKusama],
+  ["Kusama CC1", chainKusama],
+  ["Kusama CC2", chainKusama],
+  ["Kusama CC3", chainKusama],
+  ["laminar turbulence pc1", chainRococoLaminar],
+  ["phala poc 2", chainPhala],
+  ["Plasm", chainPlasm],
+  ["Polkadot", chainPolkadot],
+  ["Polkadot CC1", chainPolkadot],
+  ["Rococo", chainRocco],
+  ["Tick", chainRoccoTick],
+  ["Track", chainRoccoTrack],
+  ["Trick", chainRoccoTrick],
+  ["Westend", chainWestend],
+  ["Development", chainDevelopment]
+].reduce(
+  (colors, [chain, color]): Record<string, any> => ({
+    ...colors,
+    [chain.toLowerCase()]: color
+  }),
+  {}
+);
 
 // overrides based on the actual software node type (all '-' converted to ' ')
 // alphabetical
 const nodeColors: Record<string, any> = [
-  ['Canvas Node', nodeCanvas],
-  ['centrifuge chain', nodeCentrifuge],
-  ['Centrifuge Chain Node', nodeCentrifuge],
-  ['edgeware node', nodeEdgeware],
-  ['Equilibrium node', nodeEquilibrium],
-  ['kilt node', nodeKilt],
-  ['nodle chain node', nodeNodle],
-  ['Stafi node', nodeStafi],
-  ['subsocial node', nodeSubsocial]
+  ["Canvas Node", nodeCanvas],
+  ["centrifuge chain", nodeCentrifuge],
+  ["Centrifuge Chain Node", nodeCentrifuge],
+  ["edgeware node", nodeEdgeware],
+  ["Equilibrium node", nodeEquilibrium],
+  ["kilt node", nodeKilt],
+  ["nodle chain node", nodeNodle],
+  ["Stafi node", nodeStafi],
+  ["subsocial node", nodeSubsocial]
   // ['node template', emptyColor],
   // ['parity polkadot', emptyColor],
   // ['substrate node', emptyColor]
-].reduce((colors, [node, color]): Record<string, any> => ({
-  ...colors,
-  [node.toLowerCase().replace(/-/g, ' ')]: color
-}), {});
+].reduce(
+  (colors, [node, color]): Record<string, any> => ({
+    ...colors,
+    [node.toLowerCase().replace(/-/g, " ")]: color
+  }),
+  {}
+);
 
-export {
-  chainColors,
-  emptyColor,
-  nodeColors
-};
+export { chainColors, emptyColor, nodeColors };
--- packages/apps/src/Apps.tsx
+++ packages/apps/src/Apps.tsx
@@ -1,31 +1,32 @@
 // Copyright 2017-2020 @polkadot/apps authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
-import { BareProps as Props, ThemeDef, ThemeProps } from '@polkadot/react-components/types';
+import { BareProps as Props, ThemeDef, ThemeProps } from "@polkadot/react-components/types";
 
-import React, { useContext, useMemo } from 'react';
-import styled, { ThemeContext } from 'styled-components';
-import AccountSidebar from '@polkadot/app-accounts/Sidebar';
-import { getSystemChainColor } from '@polkadot/apps-config/ui';
-import GlobalStyle from '@polkadot/react-components/styles';
-import { useApi } from '@polkadot/react-hooks';
-import Signer from '@polkadot/react-signer';
+import React, { useContext, useMemo } from "react";
+import styled, { ThemeContext } from "styled-components";
+import AccountSidebar from "@polkadot/app-accounts/Sidebar";
+import { getSystemChainColor } from "@polkadot/apps-config/ui";
+import GlobalStyle from "@polkadot/react-components/styles";
+import { useApi } from "@polkadot/react-hooks";
+import Signer from "@polkadot/react-signer";
 
-import ConnectingOverlay from './overlays/Connecting';
-import Content from './Content';
-import Menu from './Menu';
-import WarmUp from './WarmUp';
+import ConnectingOverlay from "./overlays/Connecting";
+import Content from "./Content";
+import Menu from "./Menu";
+import WarmUp from "./WarmUp";
 
-export const PORTAL_ID = 'portals';
+export const PORTAL_ID = "portals";
 
-function Apps ({ className = '' }: Props): React.ReactElement<Props> {
+function Apps({ className = "" }: Props): React.ReactElement<Props> {
   const { theme } = useContext<ThemeDef>(ThemeContext);
   const { systemChain, systemName } = useApi();
 
-  const uiHighlight = useMemo(
-    () => getSystemChainColor(systemChain, systemName),
-    [systemChain, systemName]
-  );
+  const uiHighlight = useMemo(() => {
+    console.log("woot:", systemChain, "|", systemName);
+
+    return getSystemChainColor(systemChain, systemName);
+  }, [systemChain, systemName]);
 
   return (
     <>
@@ -45,10 +46,14 @@ function Apps ({ className = '' }: Props): React.ReactElement<Props> {
   );
 }
 
-export default React.memo(styled(Apps)(({ theme }: ThemeProps) => `
+export default React.memo(
+  styled(Apps)(
+    ({ theme }: ThemeProps) => `
   background: ${theme.bgPage};
   box-sizing: border-box;
   display: flex;
   flex-direction: column;
   min-height: 100vh;
-`));
+`
+  )
+);
