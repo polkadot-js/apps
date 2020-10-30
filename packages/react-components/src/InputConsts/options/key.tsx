13f84234b7a492c746c84e1080e17afff9d66e29
Jaco Greeff <jacogr@gmail.com>
2020-10-30 11:32:55 +0100

    Type imports

--- packages/react-components/src/InputConsts/options/key.tsx
+++ packages/react-components/src/InputConsts/options/key.tsx
@@ -1,7 +1,7 @@
 // Copyright 2017-2020 @polkadot/react-components authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
-import { ConstantCodec } from '@polkadot/metadata/Decorated/types';
+import { ConstantCodec } from '@polkadot/metadata/decorate/types';
 import { DropdownOptions, DropdownOption } from '../../util/types';
 
 import React from 'react';
