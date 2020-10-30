27fbeef324725dc6a0b492b219a0f86e2ae9c088
Jaco Greeff <jacogr@gmail.com>
2020-10-30 11:38:54 +0100

    Bump deps (#3949)
    * Bump deps
    
    * Type imports

--- packages/page-storage/src/Selection/Consts.tsx
+++ packages/page-storage/src/Selection/Consts.tsx
@@ -1,7 +1,7 @@
 // Copyright 2017-2020 @polkadot/app-storage authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
-import { ConstantCodec } from '@polkadot/metadata/Decorated/types';
+import { ConstantCodec } from '@polkadot/metadata/decorate/types';
 import { ConstValue } from '@polkadot/react-components/InputConsts/types';
 import { ComponentProps as Props } from '../types';
 
