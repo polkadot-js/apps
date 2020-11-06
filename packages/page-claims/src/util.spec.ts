9bfa09ee368a5429eda46f128d166bfed10fd770
Jaco Greeff <jacogr@gmail.com>
2020-11-06 04:42:52 +0100

    Re-add

--- packages/page-claims/src/util.spec.ts
+++ packages/page-claims/src/util.spec.ts
@@ -5,7 +5,7 @@ import { hexToU8a } from '@polkadot/util';
 
 import { publicToAddr, recoverFromJSON } from './util';
 
-describe.skip('util', (): void => {
+describe('util', (): void => {
   it('converts a publicKey to address via publicToAddr', (): void => {
     expect(
       publicToAddr(
