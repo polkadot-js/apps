61552bfb5efe334ead3f32e03fbd3f649d4baf4c
joelamouche <estienne.antoine@gmail.com>
2020-11-09 10:54:34 +0100

    added color

--- packages/apps-config/src/api/spec/moonbeam.ts
+++ packages/apps-config/src/api/spec/moonbeam.ts
@@ -1,5 +1,5 @@
 // Copyright 2017-2020 @polkadot/apps-config authors & contributors
-// SPDX-License-Identifier": Apache-2.0
+// SPDX-License-Identifier: Apache-2.0
 
 // structs need to be in order
 /* eslint-disable sort-keys */
