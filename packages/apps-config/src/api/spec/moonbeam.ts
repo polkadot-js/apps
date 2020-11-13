f2f3d09d8976a3a4e47dacadcd96d8be30f5890d
Antoine Estienne <estienne.antoine@gmail.com>
2020-11-13 16:37:20 +0100

    Feature/add types and more compatibility moonbeam (#4023)
    * changed network
    
    * updated types
    
    * updated netowrk name in network bar

--- packages/apps-config/src/api/spec/moonbeam.ts
+++ packages/apps-config/src/api/spec/moonbeam.ts
@@ -21,7 +21,6 @@ export default {
     }
   },
   Transaction: {
-    action: 'String',
     block_hash: 'Option<H256>',
     block_number: 'Option<U256>',
     chain_id: 'Option<u64>',
@@ -30,25 +29,17 @@ export default {
     from: 'H160',
     gas: 'U256',
     gas_price: 'U256',
-    gas_limit: 'u64',
     hash: 'H256',
     input: 'Bytes',
     nonce: 'U256',
     public_key: 'Option<H512>',
     r: 'U256',
     raw: 'Bytes',
-    s: 'U256',
-    signature: 'Signature',
     standard_v: 'U256',
     to: 'Option<H160>',
     transaction_index: 'Option<U256>',
     v: 'U256',
     value: 'U256'
-  },
-  Signature: {
-    v: 'u64',
-    r: 'H256',
-    s: 'H256'
   },
   TransactionStatus: {
     transaction_hash: 'H256',
@@ -73,5 +64,11 @@ export default {
     state_root: 'Option<H256>',
     logs_bloom: 'H2048',
     status_code: 'Option<U64>'
-  }
+  },
+  ExitReason: { _enum: {
+    ExitSucceed: 'bool',
+    ExitError: 'bool',
+    ExitRevert: 'bool',
+    ExitFatal: 'bool'
+  } }
 };
