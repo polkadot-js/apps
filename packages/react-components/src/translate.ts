aaedb61e0dc9176b701a4ef884897363854fa4b3
Jaco Greeff <jacogr@gmail.com>
2020-11-26 10:23:52 +0100

    import type { UseTranslationResponse } (#4074)

--- packages/react-components/src/translate.ts
+++ packages/react-components/src/translate.ts
@@ -1,7 +1,8 @@
 // Copyright 2017-2020 @polkadot/react-components authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
-import { useTranslation as useTranslationBase, UseTranslationResponse, withTranslation } from 'react-i18next';
+import type { UseTranslationResponse } from 'react-i18next';
+import { useTranslation as useTranslationBase, withTranslation } from 'react-i18next';
 
 export function useTranslation (): UseTranslationResponse {
   return useTranslationBase('react-components');
