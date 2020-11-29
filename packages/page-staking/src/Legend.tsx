366c8804d091143f101cc7ad888ade18ad74fce3
Jaco Greeff <jacogr@gmail.com>
2020-11-29 12:56:13 +0100

    Cleanup targets pre-filtering on sort (#4098)

--- packages/page-staking/src/Legend.tsx
+++ packages/page-staking/src/Legend.tsx
@@ -11,7 +11,7 @@ interface Props {
   className?: string;
 }
 
-function Ledgend ({ className }: Props): React.ReactElement<Props> {
+function Legend ({ className }: Props): React.ReactElement<Props> {
   const { t } = useTranslation();
 
   return (
@@ -62,7 +62,7 @@ function Ledgend ({ className }: Props): React.ReactElement<Props> {
   );
 }
 
-export default React.memo(styled(Ledgend)`
+export default React.memo(styled(Legend)`
   font-size: 0.85rem;
   padding: 1rem 0.5rem;
   text-align: center;
--- packages/page-staking/src/Targets/index.tsx
+++ packages/page-staking/src/Targets/index.tsx
@@ -13,7 +13,7 @@ import { useApi, useAvailableSlashes } from '@polkadot/react-hooks';
 
 import ElectionBanner from '../ElectionBanner';
 import Filtering from '../Filtering';
-import Ledgend from '../Ledgend';
+import Legend from '../Legend';
 import { MAX_NOMINATIONS } from '../constants';
 import { useTranslation } from '../translate';
 import Nominate from './Nominate';
@@ -31,6 +31,8 @@ interface Props {
 }
 
 interface Flags {
+  maxPaid: BN | undefined;
+  withElected: boolean;
   withGroup: boolean;
   withIdentity: boolean;
   withoutComm: boolean;
@@ -46,18 +48,27 @@ const CLASSES: Record<string, string> = {
   rankBondOther: 'media--1600',
   rankNumNominators: 'media--1200'
 };
+const MAX_COMM_FILTER = 20;
+const SORT_KEYS = ['rankNumNominators', 'rankComm', 'rankBondTotal', 'rankBondOwn', 'rankBondOther', 'rankOverall'];
 
-function sort (sortBy: TargetSortBy, sortFromMax: boolean, validators: ValidatorInfo[]): number[] {
-  return [...Array(validators.length).keys()]
-    .sort((a, b) =>
-      sortFromMax
-        ? validators[a][sortBy] - validators[b][sortBy]
-        : validators[b][sortBy] - validators[a][sortBy]
+function isIncluded ({ commissionPer, hasIdentity, isElected, isFavorite, numNominators }: ValidatorInfo, { maxPaid, withElected, withIdentity, withoutComm, withoutOver }: Flags): boolean {
+  return isFavorite || (
+    (!withIdentity || hasIdentity) &&
+    (!withElected || isElected) &&
+    (!withoutComm || commissionPer < MAX_COMM_FILTER) &&
+    (!withoutOver || !maxPaid || maxPaid.gtn(numNominators))
+  );
+}
+
+function sort (sortBy: TargetSortBy, sortFromMax: boolean, validators: ValidatorInfo[]): ValidatorInfo[] {
+  return validators
+    .sort((a, b) => sortFromMax
+      ? a[sortBy] - b[sortBy]
+      : b[sortBy] - a[sortBy]
     )
-    .sort((a, b) =>
-      validators[a].isFavorite === validators[b].isFavorite
-        ? 0
-        : (validators[a].isFavorite ? -1 : 1)
+    .sort((a, b) => a.isFavorite === b.isFavorite
+      ? 0
+      : (a.isFavorite ? -1 : 1)
     );
 }
 
@@ -73,23 +84,18 @@ function extractNominees (ownNominators: StakerState[] = []): string[] {
   return myNominees;
 }
 
-function selectProfitable (maxPaid: BN |undefined, list: ValidatorInfo[], { withGroup, withIdentity, withoutComm, withoutOver }: Flags): string[] {
+function selectProfitable (list: ValidatorInfo[], flags: Flags): string[] {
   const parentIds: string[] = [];
   const result: string[] = [];
+  const { withGroup } = flags;
 
   for (let i = 0; i < list.length && result.length < MAX_NOMINATIONS; i++) {
-    const { commissionPer, hasIdentity, isElected, isFavorite, key, numNominators, parentId, stakedReturnCmp } = list[i];
+    const { isFavorite, key, parentId, stakedReturnCmp } = list[i];
 
-    if (
-      (!withIdentity || hasIdentity) &&
-      (isElected || isFavorite) &&
-      stakedReturnCmp &&
-      (isFavorite || (
-        (!withGroup || !parentId || !parentIds.includes(parentId)) &&
-        (!withoutComm || commissionPer < 20) &&
-        (!withoutOver || !maxPaid || maxPaid.gtn(numNominators))
-      ))
-    ) {
+    if (isFavorite || (
+      (stakedReturnCmp > 0) &&
+      (!withGroup || !parentId || !parentIds.includes(parentId))
+    )) {
       result.push(key);
       parentId && parentIds.push(parentId);
     }
@@ -111,14 +117,42 @@ function Targets ({ className = '', isInElection, ownStashes, targets: { avgStak
   const [withoutComm, setWithoutComm] = useState(true);
   const [withoutOver, setWithoutOver] = useState(true);
   const [{ sortBy, sortFromMax }, setSortBy] = useState<SortState>({ sortBy: 'rankOverall', sortFromMax: true });
-  const [sorted, setSorted] = useState<number[] | undefined>();
+  const [sorted, setSorted] = useState<ValidatorInfo[] | undefined>();
+
+  const labelsRef = useRef({
+    rankBondOther: t<string>('other stake'),
+    rankBondOwn: t<string>('own stake'),
+    rankBondTotal: t<string>('total stake'),
+    rankComm: t<string>('comm.'),
+    rankNumNominators: t<string>('nominators'),
+    rankOverall: t<string>('return')
+  });
+
+  const flags = useMemo(
+    () => ({
+      maxPaid: api.consts.staking?.maxNominatorRewardedPerValidator,
+      withElected,
+      withGroup,
+      withIdentity,
+      withoutComm,
+      withoutOver
+    }),
+    [api, withElected, withGroup, withIdentity, withoutComm, withoutOver]
+  );
+
+  const filtered = useMemo(
+    () => validators && validators.filter((info) => isIncluded(info, flags)),
+    [flags, validators]
+  );
 
   // We are using an effect here to get this async. Sorting will have a double-render, however it allows
   // the page to immediately display (with loading), whereas useMemo would have a laggy interface
   // (the same applies for changing the sort order, state here is more effective)
   useEffect((): void => {
-    validators && setSorted(sort(sortBy, sortFromMax, validators));
-  }, [sortBy, sortFromMax, validators]);
+    filtered && setSorted(
+      sort(sortBy, sortFromMax, filtered)
+    );
+  }, [filtered, sortBy, sortFromMax]);
 
   const myNominees = useMemo(
     () => extractNominees(ownNominators),
@@ -138,80 +172,66 @@ function Targets ({ className = '', isInElection, ownStashes, targets: { avgStak
   const _toggleSelected = useCallback(
     (address: string) => setSelected(
       selected.includes(address)
-        ? selected.filter((accountId): boolean => address !== accountId)
+        ? selected.filter((accountId) => address !== accountId)
         : [...selected, address]
     ),
     [selected]
   );
 
   const _selectProfitable = useCallback(
-    () => setSelected(selectProfitable(
-      api.consts.staking?.maxNominatorRewardedPerValidator,
-      validators || [],
-      { withGroup, withIdentity, withoutComm, withoutOver }
-    )),
-    [api, validators, withGroup, withIdentity, withoutComm, withoutOver]
+    () => filtered && setSelected(
+      selectProfitable(filtered, flags)
+    ),
+    [filtered, flags]
   );
-
-  const labelsRef = useRef({
-    rankBondOther: t<string>('other stake'),
-    rankBondOwn: t<string>('own stake'),
-    rankBondTotal: t<string>('total stake'),
-    rankComm: t<string>('comm.'),
-    rankNumNominators: t<string>('nominators'),
-    rankOverall: t<string>('return')
-  });
 
   const header = useMemo(() => [
     [t('validators'), 'start', 3],
-    ...(['rankNumNominators', 'rankComm', 'rankBondTotal', 'rankBondOwn', 'rankBondOther', 'rankOverall'] as (keyof typeof labelsRef.current)[])
-      .map((header) => [
-        <>{labelsRef.current[header]}<Icon icon={sortBy === header ? (sortFromMax ? 'chevron-down' : 'chevron-up') : 'minus'} /></>,
-        `${sorted ? `isClickable ${sortBy === header ? 'highlight--border' : ''} number` : 'number'} ${CLASSES[header] || ''}`,
-        1,
-        () => _sort(header as 'rankComm')
-      ]),
+    ...(SORT_KEYS as (keyof typeof labelsRef.current)[]).map((header) => [
+      <>{labelsRef.current[header]}<Icon icon={sortBy === header ? (sortFromMax ? 'chevron-down' : 'chevron-up') : 'minus'} /></>,
+      `${sorted ? `isClickable ${sortBy === header ? 'highlight--border' : ''} number` : 'number'} ${CLASSES[header] || ''}`,
+      1,
+      () => _sort(header as 'rankComm')
+    ]),
     [],
     []
   ], [_sort, labelsRef, sortBy, sorted, sortFromMax, t]);
 
   const filter = useMemo(() => (
-    sorted && (
-      <div>
-        <Filtering
-          nameFilter={nameFilter}
-          setNameFilter={setNameFilter}
-          setWithIdentity={setWithIdentity}
-          withIdentity={withIdentity}
-        >
-          <Toggle
-            className='staking--buttonToggle'
-            label={t<string>('limit single operator exposure')}
-            onChange={setWithGroup}
-            value={withGroup}
-          />
-          <Toggle
-            className='staking--buttonToggle'
-            label={t<string>('no 20%+ comm')}
-            onChange={setWithoutComm}
-            value={withoutComm}
-          />
-          <Toggle
-            className='staking--buttonToggle'
-            label={t<string>('no oversubscribed')}
-            onChange={setWithoutOver}
-            value={withoutOver}
-          />
-          <Toggle
-            className='staking--buttonToggle'
-            label={t<string>('only elected')}
-            onChange={setWithElected}
-            value={withElected}
-          />
-        </Filtering>
-      </div>
-    )
-  ), [nameFilter, sorted, t, withElected, withGroup, withIdentity, withoutComm, withoutOver]);
+    <div>
+      <Filtering
+        nameFilter={nameFilter}
+        setNameFilter={setNameFilter}
+        setWithIdentity={setWithIdentity}
+        withIdentity={withIdentity}
+      >
+        <Toggle
+          className='staking--buttonToggle'
+          label={t<string>('limit single operator exposure')}
+          onChange={setWithGroup}
+          value={withGroup}
+        />
+        <Toggle
+          className='staking--buttonToggle'
+          label={t<string>('no {{maxComm}}%+ comm', { replace: { maxComm: MAX_COMM_FILTER } })}
+          onChange={setWithoutComm}
+          value={withoutComm}
+        />
+        <Toggle
+          className='staking--buttonToggle'
+          label={t<string>('no oversubscribed')}
+          onChange={setWithoutOver}
+          value={withoutOver}
+        />
+        <Toggle
+          className='staking--buttonToggle'
+          label={t<string>('only elected')}
+          onChange={setWithElected}
+          value={withElected}
+        />
+      </Filtering>
+    </div>
+  ), [nameFilter, t, withElected, withGroup, withIdentity, withoutComm, withoutOver]);
 
   return (
     <div className={className}>
@@ -243,23 +263,20 @@ function Targets ({ className = '', isInElection, ownStashes, targets: { avgStak
         empty={sorted && t<string>('No active validators to check')}
         filter={filter}
         header={header}
-        ledgend={<Ledgend />}
+        legend={<Legend />}
       >
-        {validators && sorted && (validators.length === sorted.length) && sorted.map((index): React.ReactNode =>
+        {sorted?.map((info): React.ReactNode =>
           <Validator
             allSlashes={allSlashes}
             canSelect={selected.length < MAX_NOMINATIONS}
             filterName={nameFilter}
-            info={validators[index]}
-            isNominated={myNominees.includes(validators[index].key)}
-            isSelected={selected.includes(validators[index].key)}
-            key={validators[index].key}
+            info={info}
+            isNominated={myNominees.includes(info.key)}
+            isSelected={selected.includes(info.key)}
+            key={info.key}
             toggleFavorite={toggleFavorite}
             toggleSelected={_toggleSelected}
-            withElected={withElected}
             withIdentity={withIdentity}
-            withoutComm={withoutComm}
-            withoutOver={withoutOver}
           />
         )}
       </Table>
