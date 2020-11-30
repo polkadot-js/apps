82adfbc83700b1cf691eeb342fb75830c25a0620
Jaco Greeff <jacogr@gmail.com>
2020-11-30 17:53:50 +0100

    Derives are always subscriptions

--- packages/react-hooks/src/useAccountInfoCache.ts
+++ packages/react-hooks/src/useAccountInfoCache.ts
@@ -7,12 +7,14 @@ import type { AccountId, AccountIndex, Address } from '@polkadot/types/interface
 import { useEffect, useState } from 'react';
 
 import { useApi } from './useApi';
+import { useIsMountedRef } from './useIsMountedRef';
 
 type VoidFn = () => void;
 
 const CACHE: Record<string, DeriveAccountInfo> = {};
 
 export function useAccountInfoCache (value: AccountId | AccountIndex | Address | string | null | undefined, isCached: boolean): DeriveAccountInfo | undefined {
+  const mountedRef = useIsMountedRef();
   const { api } = useApi();
   const [state, setState] = useState<DeriveAccountInfo | undefined>(CACHE[value?.toString() || '']);
 
@@ -20,16 +22,13 @@ export function useAccountInfoCache (value: AccountId | AccountIndex | Address |
     const address = value?.toString() || '';
     let unsubscribe: VoidFn | undefined;
 
-    if (isCached) {
-      if (!CACHE[address]) {
-        api.derive.accounts
-          .info(value)
-          .then((state) => setState(CACHE[address] = state))
-          .catch(console.error);
-      }
-    } else {
+    if (!isCached || !CACHE[address]) {
       api.derive.accounts
-        .info(value, (state) => setState(CACHE[address] = state))
+        .info(value, (state): void => {
+          mountedRef.current && setState(
+            CACHE[address] = state
+          );
+        })
         .then((u): void => {
           unsubscribe = u;
         })
@@ -37,7 +36,7 @@ export function useAccountInfoCache (value: AccountId | AccountIndex | Address |
     }
 
     return () => unsubscribe && unsubscribe();
-  }, [api, isCached, value]);
+  }, [api, isCached, mountedRef, value]);
 
   return state;
 }
