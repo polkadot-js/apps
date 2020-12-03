65bef235cd9eecae89d79b4ed72bac6a1e2d5bb5
toxotguo <scut-guo@163.com>
2020-12-03 17:17:51 +0800

    Add polkadot and kusama endpoints hosted by PatractLabs (#4142)
    * add patractlabs endpoint for polkadot
    
    * add patractlabs endpoint for polkadot
    
    * add kusama endpoint hosted by patractlabs
    
    * add kusama endpoints hosted by patractlabs
    
    * fix indentations of 4 spaces
    
    * Update endpoints.ts

--- packages/apps-config/src/settings/endpoints.ts
+++ packages/apps-config/src/settings/endpoints.ts
@@ -75,6 +75,12 @@ function createLiveNetworks (t: TFunction): LinkOption[] {
       value: 'wss://polkadot.api.onfinality.io/public-ws'
     },
     {
+      info: 'polkadot',
+      text: t('rpc.polkadot.patractlabs', 'Polkadot', { ns: 'apps-config' }),
+      textBy: t('rpc.hosted.by', 'hosted by {{host}}', { ns: 'apps-config', replace: { host: 'Patract Elara' } }),
+      value: 'wss://polkadot.elara.patract.io'
+    },
+    {
       dnslink: 'kusama',
       info: 'kusama',
       text: t('rpc.kusama.parity', 'Kusama', { ns: 'apps-config' }),
@@ -92,6 +98,12 @@ function createLiveNetworks (t: TFunction): LinkOption[] {
       text: t('rpc.kusama.onfinality', 'Kusama', { ns: 'apps-config' }),
       textBy: t('rpc.hosted.by', 'hosted by {{host}}', { ns: 'apps-config', replace: { host: 'OnFinality' } }),
       value: 'wss://kusama.api.onfinality.io/public-ws'
+    },
+    {
+      info: 'kusama',
+      text: t('rpc.kusama.patractlabs', 'Kusama', { ns: 'apps-config' }),
+      textBy: t('rpc.hosted.by', 'hosted by {{host}}', { ns: 'apps-config', replace: { host: 'Patract Elara' } }),
+      value: 'wss://kusama.elara.patract.io'
     },
     {
       info: 'kusama',
