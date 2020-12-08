41d676029327d956a0099d6eead58fc36b97354a
Jaco Greeff <jacogr@gmail.com>
2020-12-08 17:58:31 +0100

    Detect system.blockWeights vs system.maximumBlockWeight

--- packages/page-staking/src/Payouts/PayButton.tsx
+++ packages/page-staking/src/Payouts/PayButton.tsx
@@ -2,7 +2,7 @@
 // SPDX-License-Identifier: Apache-2.0
 
 import type { SubmittableExtrinsic } from '@polkadot/api/types';
-import type { EraIndex } from '@polkadot/types/interfaces';
+import type { EraIndex, Weight } from '@polkadot/types/interfaces';
 import type { PayoutValidator } from './types';
 
 import React, { useEffect, useState } from 'react';
@@ -88,10 +88,12 @@ function PayButton ({ className, isAll, isDisabled, payout }: Props): React.Reac
       api.tx.staking
         .payoutStakers(validatorId, eras[0].era)
         .paymentInfo(allAccounts[0])
-        .then((info) => setMaxPayouts(Math.floor(
-          // 65% of the block weight on a single extrinsic (64 for safety)
-          api.consts.system.maximumBlockWeight.muln(64).div(info.weight).toNumber() / 100
-        )))
+        .then((info) => setMaxPayouts(Math.floor((
+          api.consts.system.blockWeights
+            ? api.consts.system.blockWeights.perClass.normal.maxExtrinsic
+            // 65% of the block weight on a single extrinsic (64 for safety)
+            : (api.consts.system.maximumBlockWeight as Weight).muln(64)
+        ).div(info.weight).toNumber() / 100)))
         .catch(console.error);
     } else {
       // at 64 payouts we can fit in 36 (as per tests)
