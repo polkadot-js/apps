593b42c27552d89c172a2f3ec10f2eb7e5dd60af
Jaco Greeff <jacogr@gmail.com>
2020-12-20 12:55:10 +0100

    Save target/overview toggles (#4249)

--- packages/react-hooks/src/index.ts
+++ packages/react-hooks/src/index.ts
@@ -29,6 +29,7 @@ export { useOwnStashes, useOwnStashIds } from './useOwnStashes';
 export { useOwnStashInfos } from './useOwnStashInfos';
 export { usePassword } from './usePassword';
 export { useRegistrars } from './useRegistrars';
+export { useSavedFlags } from './useSavedFlags';
 export { useSudo } from './useSudo';
 export { useToggle } from './useToggle';
 export { useVotingStatus } from './useVotingStatus';
