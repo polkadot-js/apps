069d823613e0954e6339dea5365c9a6dcb7a893c
Jaco Greeff <jacogr@gmail.com>
2020-12-20 16:43:18 +0100

    Remove unused TxModal components

--- packages/react-components/src/types.ts
+++ packages/react-components/src/types.ts
@@ -6,10 +6,7 @@ import type { WithTranslation } from 'react-i18next';
 import type { SubmittableExtrinsic } from '@polkadot/api/promise/types';
 import type { Abi } from '@polkadot/api-contract';
 import type { ActionStatus } from '@polkadot/react-components/Status/types';
-import type { TxState } from '@polkadot/react-hooks/types';
 import type { AccountId, Index } from '@polkadot/types/interfaces';
-import type { ButtonProps } from './Button/types';
-import type { InputAddressProps } from './InputAddress/types';
 import type { TxCallback, TxFailedCallback } from './Status/types';
 
 export type StringOrNull = string | null;
@@ -30,32 +27,11 @@ export interface AppProps {
 
 export type I18nProps = BareProps & WithTranslation;
 
-export type ConstructTxFn = () => any[];
-
-export interface TxTriggerProps {
-  onOpen: () => void;
-}
-
-export type TxTrigger = React.ComponentType<TxTriggerProps>;
-
-export interface TxProps {
-  extrinsic?: SubmittableExtrinsic | SubmittableExtrinsic[] | null;
-  tx?: string;
-  params?: any[] | ConstructTxFn;
-}
-
-export interface TxAccountProps {
-  className?: string;
-  filter?: string[];
-  label?: React.ReactNode;
-  help?: React.ReactNode;
-  onChange: (value: string | null) => void;
-}
-
-export interface TxButtonProps extends TxProps {
+export interface TxButtonProps {
   accountId?: AccountId | StringOrNull;
   accountNonce?: Index;
   className?: string;
+  extrinsic?: SubmittableExtrinsic | SubmittableExtrinsic[] | null;
   icon?: IconName;
   isBasic?: boolean;
   isBusy?: boolean;
@@ -70,38 +46,11 @@ export interface TxButtonProps extends TxProps {
   onStart?: VoidFn;
   onSuccess?: TxCallback;
   onUpdate?: TxCallback;
+  params?: any[] | (() => any[]);
   tooltip?: string;
+  tx?: string;
   withoutLink?: boolean;
   withSpinner?: boolean;
-}
-
-export interface TxModalProps extends I18nProps, TxState {
-  accountId?: StringOrNull;
-  header?: React.ReactNode;
-  isDisabled?: boolean;
-  isOpen?: boolean;
-  isUnsigned?: boolean;
-  children: React.ReactNode;
-  preContent?: React.ReactNode;
-  trigger?: TxTrigger;
-  onSubmit?: () => void;
-  onOpen?: () => void;
-  onClose?: () => void;
-  onSuccess?: () => void;
-  onFailed?: () => void;
-  modalProps?: {
-    [index: string]: any;
-  };
-  contentClassName?: string;
-  inputAddressHelp?: React.ReactNode;
-  inputAddressExtra?: React.ReactNode;
-  inputAddressLabel?: React.ReactNode;
-  inputAddressProps?: Pick<InputAddressProps, never>;
-  cancelButtonLabel?: React.ReactNode;
-  cancelButtonProps?: Pick<ButtonProps, never>;
-  submitButtonIcon?: IconName;
-  submitButtonLabel?: React.ReactNode;
-  submitButtonProps?: Pick<TxButtonProps, never>;
 }
 
 export type BitLength = 8 | 16 | 32 | 64 | 128 | 256;
