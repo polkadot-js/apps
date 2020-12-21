d1e13e82370bcc31d8af42e7c0a031360347cad0
lovesh <lovesh.bond@gmail.com>
2020-12-22 02:32:57 +0530

    Fix type
    Signed-off-by: lovesh <lovesh.bond@gmail.com>

--- packages/apps-config/src/api/spec/dock.ts
+++ packages/apps-config/src/api/spec/dock.ts
@@ -90,8 +90,8 @@ export default {
     starting_slot: 'SlotNo',
     expected_ending_slot: 'SlotNo',
     ending_slot: 'Option<SlotNo>',
-    emission_for_treasury: 'Option<Balance>',
-    emission_for_validators: 'Option<Balance>'
+    emission_for_validators: 'Option<Balance>',
+    emission_for_treasury: 'Option<Balance>'
   },
   ValidatorStatsPerEpoch: {
     block_count: 'EpochLen',
