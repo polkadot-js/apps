531e14dda4c5d897d1137e03040d6c3eda7e161c
Michał Ziętalak <michal.z@ethworks.io>
2020-12-29 09:23:55 +0100

    Extract useTreasury hook

--- packages/page-bounties/src/Summary.tsx
+++ packages/page-bounties/src/Summary.tsx
@@ -3,25 +3,21 @@
 
 import type { BountyIndex } from '@polkadot/types/interfaces';
 
-import BN from 'bn.js';
 import React from 'react';
 import styled from 'styled-components';
 
-import { DeriveBalancesAccount } from '@polkadot/api-derive/types';
 import { useTranslation } from '@polkadot/app-treasury/translate';
 import { CardSummary, SummaryBox } from '@polkadot/react-components';
 import Progress from '@polkadot/react-components/Progress';
 import { useApi, useCall } from '@polkadot/react-hooks';
+import { useTreasury } from '@polkadot/react-hooks/useTreasury';
 import { BlockToTime, FormatBalance } from '@polkadot/react-query';
 import { Balance } from '@polkadot/types/interfaces';
-import { stringToU8a } from '@polkadot/util';
 
 interface Props {
   activeBounties?: number;
   className?: string;
 }
-const TREASURY_ACCOUNT = stringToU8a('modlpy/trsry'.padEnd(32, '\0'));
-const PM_DIV = new BN(1000000);
 
 function Summary ({ activeBounties, className = '' }: Props): React.ReactElement<Props> {
   const { t } = useTranslation();
@@ -29,15 +25,9 @@ function Summary ({ activeBounties, className = '' }: Props): React.ReactElement
 
   const bountyIndex = useCall<BountyIndex>((api.query.bounties || api.query.treasury).bountyCount);
   const bestNumber = useCall<Balance>(api.derive.chain.bestNumber);
-  const treasuryBalance = useCall<DeriveBalancesAccount>(api.derive.balances.account, [TREASURY_ACCOUNT]);
   const spendPeriod = api.consts.treasury.spendPeriod;
 
-  const value = treasuryBalance?.freeBalance.gtn(0)
-    ? treasuryBalance.freeBalance
-    : null;
-  const burn = treasuryBalance?.freeBalance.gtn(0) && !api.consts.treasury.burn.isZero()
-    ? api.consts.treasury.burn.mul(treasuryBalance?.freeBalance).div(PM_DIV)
-    : null;
+  const { burn, value } = useTreasury();
 
   return (
     <SummaryBox className={`ui--BountySummary ${className}`}>
