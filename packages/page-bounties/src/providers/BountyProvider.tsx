eab7a81b942b8f1d45fce44aa0638bfacc76377a
Krzysztof Jelski <krzysztof@ethworks.io>
2020-12-29 10:10:28 +0100

    Extract local var

--- packages/page-bounties/src/providers/BountyProvider.tsx
+++ packages/page-bounties/src/providers/BountyProvider.tsx
@@ -13,10 +13,11 @@ export function BountyProvider ({ children }: { children: ReactNode }): ReactEle
   const { api } = useApi();
   const bounties = useCall<DeriveBounties>((api.derive.bounties || api.derive.treasury).bounties);
   const bestNumber = useCall<BlockNumber>(api.derive.chain.bestNumber);
-  const bountyDepositBase = ((api.consts.bounties || api.consts.treasury).bountyDepositBase as BalanceOf).toBn();
-  const bountyValueMinimum = ((api.consts.bounties || api.consts.treasury).bountyValueMinimum as BalanceOf).toBn();
-  const maximumReasonLength = (api.consts.bounties || api.consts.treasury).maximumReasonLength.toNumber();
-  const bountyDepositPerByte = ((api.consts.bounties || api.consts.treasury).bountyDepositPerByte as BalanceOf).toBn();
+  const constsBase = api.consts.bounties || api.consts.treasury;
+  const bountyDepositBase = (constsBase.bountyDepositBase as BalanceOf).toBn();
+  const bountyValueMinimum = (constsBase.bountyValueMinimum as BalanceOf).toBn();
+  const maximumReasonLength = constsBase.maximumReasonLength.toNumber();
+  const bountyDepositPerByte = (constsBase.bountyDepositPerByte as BalanceOf).toBn();
   const proposeBounty = (api.tx.bounties || api.tx.treasury).proposeBounty;
   const bountiesApi: BountyApi = {
     bestNumber,
