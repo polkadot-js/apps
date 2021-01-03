e6bcdb7fa9972a6c3b95d49b89fd103e65fe2f1f
Jaco Greeff <jacogr@gmail.com>
2021-01-03 11:26:36 +0100

    2021

--- packages/react-components/src/AddressMini.tsx
+++ packages/react-components/src/AddressMini.tsx
@@ -1,4 +1,4 @@
-// Copyright 2017-2020 @polkadot/app-staking authors & contributors
+// Copyright 2017-2021 @polkadot/app-staking authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
 import type { AccountId, AccountIndex, Address } from '@polkadot/types/interfaces';
