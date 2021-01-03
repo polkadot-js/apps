6dbb188a1acb55f66630cb718586910609205ba1
Jaco Greeff <jacogr@gmail.com>
2021-01-03 11:33:39 +0100

    2021 (#4310)

--- packages/react-components/src/InputAddress/types.ts
+++ packages/react-components/src/InputAddress/types.ts
@@ -1,4 +1,4 @@
-// Copyright 2017-2020 @polkadot/react-components authors & contributors
+// Copyright 2017-2021 @polkadot/react-components authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
 import type { KeyringOption$Type, KeyringSectionOption } from '@polkadot/ui-keyring/options/types';
--- packages/react-components/src/Tabs/index.tsx
+++ packages/react-components/src/Tabs/index.tsx
@@ -1,4 +1,4 @@
-// Copyright 2017-2020 @polkadot/react-components authors & contributors
+// Copyright 2017-2021 @polkadot/react-components authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
 import type { ThemeProps } from '../types';
@@ -67,7 +67,7 @@ export default React.memo(styled(Tabs)(({ theme }: ThemeProps) => `
 
   text-align: left;
   z-index: 1;
-  
+
   & .tabs-container {
     max-width: ${theme.contentMaxWidth};
     margin: 0 auto;
