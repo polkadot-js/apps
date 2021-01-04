c5bf0de631391154a1c6683ec41e9efc95a4b05a
Jaco Greeff <jacogr@gmail.com>
2021-01-04 14:46:14 +0100

    Parachains registration (#4318)
    * Parachains registration
    
    * Registrar dialog

--- packages/page-parachains/src/Overview/index.tsx
+++ packages/page-parachains/src/Overview/index.tsx
@@ -1,12 +1,14 @@
 // Copyright 2017-2021 @polkadot/app-parachains authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
+import type { bool } from '@polkadot/types';
 import type { ParaId } from '@polkadot/types/interfaces';
 
 import React from 'react';
 
 import { useApi, useCall } from '@polkadot/react-hooks';
 
+import Actions from './Actions';
 import Parachains from './Parachains';
 import Summary from './Summary';
 import Upcoming from './Upcoming';
@@ -15,20 +17,30 @@ interface Props {
   className?: string;
 }
 
+const transformRegister = {
+  transform: (value: bool): boolean => value.isTrue
+};
+
 function Overview (): React.ReactElement<Props> {
   const { api } = useApi();
   const paraIds = useCall<ParaId[]>(api.query.paras?.parachains);
   const upcomingIds = useCall<ParaId[]>(api.query.paras?.upcomingParas);
+  const canRegister = useCall<boolean>(api.query.registrar?.parathreadsRegistrationEnabled, [], transformRegister);
 
   return (
     <>
       <Summary
+        canRegister={canRegister}
         parachainCount={paraIds?.length}
         upcomingCount={upcomingIds?.length}
       />
+      <Actions canRegister={canRegister} />
       {api.query.paras && (
         <>
-          <Parachains ids={paraIds} />
+          <Parachains
+            canRegister={canRegister}
+            ids={paraIds}
+          />
           <Upcoming ids={upcomingIds} />
         </>
       )}
