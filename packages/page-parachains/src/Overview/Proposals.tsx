149abb51513ce3a055912cb2c82230a2e56fe6b0
Jaco Greeff <jacogr@gmail.com>
2021-01-12 01:13:07 +0100

    Retrievae proposals

--- packages/page-parachains/src/Overview/Proposals.tsx
+++ packages/page-parachains/src/Overview/Proposals.tsx
@@ -1,21 +1,34 @@
 // Copyright 2017-2021 @polkadot/app-parachains authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
+import type { Option } from '@polkadot/types';
+import type { ParachainProposal, ParaId } from '@polkadot/types/interfaces';
 import type { ProposalExt } from './types';
 
 import React, { useRef } from 'react';
 
 import { Table } from '@polkadot/react-components';
+import { useApi, useCall } from '@polkadot/react-hooks';
 
 import { useTranslation } from '../translate';
 import Proposal from './Proposal';
 
 interface Props {
-  proposals?: ProposalExt[];
+  className?: string;
 }
 
-function Proposals ({ proposals }: Props): React.ReactElement<Props> {
+const transformProposals = {
+  transform: (entries: [{ args: [ParaId] }, Option<ParachainProposal>][]): ProposalExt[] => {
+    return entries
+      .filter(([, opt]) => opt.isSome)
+      .map(([{ args: [id] }, optProposal]) => ({ id, proposal: optProposal.unwrap() }));
+  }
+};
+
+function Proposals (): React.ReactElement<Props> {
   const { t } = useTranslation();
+  const { api } = useApi();
+  const proposals = useCall<ProposalExt[]>(api.query.proposeParachain.proposals.entries, undefined, transformProposals);
 
   const headerRef = useRef([
     [t('proposals'), 'start', 2],
