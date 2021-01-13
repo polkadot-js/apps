705b046795236de965dfe4bbf67b5f8b114c4c15
Jaco Greeff <jacogr@gmail.com>
2021-01-13 10:06:07 +0100

    Show scheduled/approved

--- packages/page-parachains/src/Overview/types.tsx
+++ packages/page-parachains/src/Overview/types.tsx
@@ -1,11 +1,22 @@
 // Copyright 2017-2021 @polkadot/app-parachains authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
-import type { ParachainProposal, ParaId } from '@polkadot/types/interfaces';
+import type { ParachainProposal, ParaId, SessionIndex } from '@polkadot/types/interfaces';
 
 export interface ProposalExt {
   id: ParaId;
   isApproved: boolean;
   isScheduled: boolean;
-  proposal: ParachainProposal;
+  proposal?: ParachainProposal;
+}
+
+export interface ScheduledProposals {
+  scheduledIds: ParaId[];
+  sessionIndex: SessionIndex;
+}
+
+export interface Proposals {
+  approvedIds: ParaId[];
+  proposalIds: ParaId[];
+  scheduled: ScheduledProposals[];
 }
