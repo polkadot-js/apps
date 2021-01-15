400b4c3436324e1366c6aff7e121d118f6a60827
Alexander Krupenkin <mail@akru.me>
2021-01-15 08:21:02 +0300

    Added robonomics rococo parachain endpoint

--- packages/apps-config/src/endpoints/testingRococo.ts
+++ packages/apps-config/src/endpoints/testingRococo.ts
@@ -58,6 +58,14 @@ export function createRococo (t: TFunction): EndpointOption {
         providers: {
           PlasmNetwork: 'wss://rpc.rococo.plasmnet.io'
         }
+      },
+      {
+        info: 'rococoRobonomics',
+        paraId: 3000,
+        text: t('rpc.rococo.robonomics', 'Robonomics PC2', { ns: 'apps-config' }),
+        providers: {
+          Airalab: 'wss://rococo.parachain.robonomics.network'
+        }
       }
     ]
   };
