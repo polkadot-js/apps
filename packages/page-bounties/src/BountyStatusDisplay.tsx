9567472944c6dd68f5c1280bb90e5a2ce0ed8be8
Michał Ziętalak <michal.z@ethworks.io>
2021-01-18 18:30:59 +0100

    Implement help tooltip in BountyStatusDisplay

--- packages/page-bounties/src/BountyStatusDisplay.tsx
+++ packages/page-bounties/src/BountyStatusDisplay.tsx
@@ -4,9 +4,11 @@
 import type { DeriveCollectiveProposal } from '@polkadot/api-derive/types';
 import type { BountyStatus } from '@polkadot/types/interfaces';
 
-import React from 'react';
+import React, { useCallback, useMemo } from 'react';
 
+import { useTranslation } from '@polkadot/app-bounties/translate';
 import VotingDescription from '@polkadot/app-bounties/VotingDescription';
+import { LabelHelp } from '@polkadot/react-components';
 
 type Props = {
   bountyStatus: string;
@@ -15,9 +17,25 @@ type Props = {
 }
 
 function BountyStatusDisplay ({ bountyStatus, proposals, status }: Props) {
+  const { t } = useTranslation();
+
+  const statusHelpMessages: Record<string, string> = useMemo(() => ({
+    Active: t('This bounty has received general approval and is currently being implemented.'),
+    Approved: t("This bounty was approved by the council, it will get funded after treasury's send period."),
+    CuratorProposed: t('The voting for a curator is in progress.'),
+    Funded: t('This bounty was approved and funded by council.'),
+    PendingPayout: t('This bounty was completed and the beneficiary was awarded by the curator.'),
+    Proposed: t('After a bounty was proposed the council decides whether to fund it or not.')
+  }), [t]);
+
   return (
-    <div>
-      <div>{bountyStatus}</div>
+    <>
+      <div>
+        {bountyStatus}
+        <LabelHelp
+          help={statusHelpMessages[status.type]}
+        />
+      </div>
       {proposals && (
         <div>
           <VotingDescription
@@ -26,7 +44,7 @@ function BountyStatusDisplay ({ bountyStatus, proposals, status }: Props) {
           />
         </div>
       )}
-    </div>
+    </>
   );
 }
 
