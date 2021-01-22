5950b99da12b72e426725e4a4ee835e0677ea854
Jaco Greeff <jacogr@gmail.com>
2021-01-22 21:23:37 +0100

    Extend PolkaBtc definitions (balances modules)

--- packages/apps-config/src/api/specVersioned/polkabtc.ts
+++ packages/apps-config/src/api/specVersioned/polkabtc.ts
@@ -5,4 +5,10 @@ import type { OverrideBundleDefinition } from '@polkadot/types/types';
 
 import definitions from '@interlay/polkabtc-types';
 
-export default definitions as OverrideBundleDefinition;
+// This ia a temp work-around to add the interfaces key to the actual types
+// With the next release of the API this should be able tp be added to the base
+const extended = definitions as OverrideBundleDefinition;
+
+extended.interfaces = { balances: ['dot', 'polkaBtc'] };
+
+export default extended;
