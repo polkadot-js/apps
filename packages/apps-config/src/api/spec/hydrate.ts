5948f619c332f917bcfecfd4cbc63fdfc8c95165
lumir-mrkva <lumir.mrkva@topmonks.com>
2021-01-28 20:24:43 +0000

    silence linter

--- packages/apps-config/src/api/spec/hydrate.ts
+++ packages/apps-config/src/api/spec/hydrate.ts
@@ -1,6 +1,7 @@
 // Copyright 2017-2021 @polkadot/apps-config authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
+/* eslint-disable sort-keys */
 export default {
   Amount: 'i128',
   AmountOf: 'Amount',
