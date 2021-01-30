3c86dc62fdad48604d9f26b411eaa5df73de056d
Hayden <haosong0323@gmail.com>
2021-01-30 16:46:08 +0800

    Add types

--- packages/apps-config/src/api/spec/zenlink.ts
+++ packages/apps-config/src/api/spec/zenlink.ts
@@ -1,28 +1,34 @@
 // Copyright 2017-2021 @polkadot/apps-config authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
+/* eslint-disable sort-keys */
+
 export default {
+  Address: 'AccountId',
+  AccountInfo: 'AccountInfoWithRefCount',
   AssetId: {
     _enum: {
       NativeCurrency: null,
       ParaCurrency: 'u32'
     }
   },
+  LookupSource: 'AccountId',
   Pair: {
-    account: 'AccountId',
     token_0: 'AssetId',
     token_1: 'AssetId',
+    account: 'AccountId',
     total_liquidity: 'TokenBalance'
   },
   PairId: 'u32',
   PairInfo: {
-    account: 'AccountId',
-    holding_liquidity: 'TokenBalance',
-    reserve_0: 'TokenBalance',
-    reserve_1: 'TokenBalance',
     token_0: 'AssetId',
     token_1: 'AssetId',
-    total_liquidity: 'TokenBalance'
+    account: 'AccountId',
+    total_liquidity: 'TokenBalance',
+    holding_liquidity: 'TokenBalance',
+    reserve_0: 'TokenBalance',
+    reserve_1: 'TokenBalance'
   },
+  RefCount: 'u32',
   TokenBalance: 'u128'
 };
