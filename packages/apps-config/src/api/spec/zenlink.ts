e7e46f67add52c3e0f0b90f5080dc0799e75845c
Hayden <haosong0323@gmail.com>
2021-01-30 10:55:33 +0800

    Fix lint

--- packages/apps-config/src/api/spec/zenlink.ts
+++ packages/apps-config/src/api/spec/zenlink.ts
@@ -3,27 +3,27 @@
 // of the Apache-2.0 license. See the LICENSE file for details.
 
 export default {
-  PairId: 'u32',
-  Pair: {
-    token_0: 'AssetId',
-    token_1: 'AssetId',
-    account: 'AccountId',
-    total_liquidity: 'TokenBalance'
-  },
-  PairInfo: {
-    token_0: 'AssetId',
-    token_1: 'AssetId',
-    account: 'AccountId',
-    total_liquidity: 'TokenBalance',
-    holding_liquidity: 'TokenBalance',
-    reserve_0: 'TokenBalance',
-    reserve_1: 'TokenBalance'
-  },
   AssetId: {
     _enum: {
       NativeCurrency: null,
       ParaCurrency: 'u32'
     }
+  },
+  Pair: {
+    account: 'AccountId',
+    token_0: 'AssetId',
+    token_1: 'AssetId',
+    total_liquidity: 'TokenBalance'
+  },
+  PairId: 'u32',
+  PairInfo: {
+    account: 'AccountId',
+    holding_liquidity: 'TokenBalance',
+    reserve_0: 'TokenBalance',
+    reserve_1: 'TokenBalance',
+    token_0: 'AssetId',
+    token_1: 'AssetId',
+    total_liquidity: 'TokenBalance'
   },
   TokenBalance: 'u128'
 };
