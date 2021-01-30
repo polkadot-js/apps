0c3bc2f4d9204eb0ab8aab8d8cf07eba17160dc2
qinghuan-deep <1245816264@qq.com>
2021-01-30 17:21:36 +0800

    add types

--- packages/apps-config/src/api/spec/chainx.ts
+++ packages/apps-config/src/api/spec/chainx.ts
@@ -1,8 +1,12 @@
-/* eslint-disable header/header */
-/* eslint-disable sort-keys */
-// [object Object]
+// Copyright 2017-2021 @polkadot/apps-config authors & contributors
 // SPDX-License-Identifier: Apache-2.0
+
+// structs need to be in order
+/* eslint-disable sort-keys */
+
 export default {
+  Address: 'AccountId',
+  LookupSource: 'AccountId',
   Chain: {
     _enum: [
       'ChainX',
