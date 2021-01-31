f58cc526f9c83f517285f68e616279c36022b9eb
Jaco Greeff <jacogr@gmail.com>
2021-01-31 14:14:51 +0100

    extra

--- packages/react-components/src/InputStorage/index.tsx
+++ packages/react-components/src/InputStorage/index.tsx
@@ -30,7 +30,7 @@ function InputStorage ({ className = '', defaultValue, help, label, onChange, wi
   const { api } = useApi();
   const [optionsMethod, setOptionsMethod] = useState<DropdownOptions>(() => keyOptions(api, defaultValue.creator.section));
   const [optionsSection] = useState<DropdownOptions>(() => sectionOptions(api));
-  const [value, setValue] = useState<QueryableStorageEntry<'promise'>>(() => defaultValue);
+  const [value, setValue] = useState<QueryableStorageEntry<'promise'>>(defaultValue);
 
   const _onKeyChange = useCallback(
     (newValue: QueryableStorageEntry<'promise'>): void => {
