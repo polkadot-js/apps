360fdd424f14a4e7f8f863bc002a9b0fb8cc6fcb
Sam Hellawell <sshellawell@gmail.com>
2021-02-01 20:42:36 +0000

    update github/wiki lniks

--- packages/apps/src/Menu/index.tsx
+++ packages/apps/src/Menu/index.tsx
@@ -31,8 +31,8 @@ const disabledLog = new Map<string, string>();
 
 function createExternals (t: TFunction): ItemRoute[] {
   return [
-    { href: 'https://github.com/polkadot-js/apps', icon: 'code-branch', name: 'github', text: t<string>('nav.github', 'GitHub', { ns: 'apps-routing' }) },
-    { href: 'https://wiki.polkadot.network', icon: 'book', name: 'wiki', text: t<string>('nav.wiki', 'Wiki', { ns: 'apps-routing' }) }
+    { href: 'https://github.com/docknetwork', icon: 'code-branch', name: 'github', text: t<string>('nav.github', 'GitHub', { ns: 'apps-routing' }) },
+    { href: 'https://docs.dock.io', icon: 'book', name: 'wiki', text: t<string>('nav.wiki', 'Docs', { ns: 'apps-routing' }) }
   ];
 }
 
