aff8ee5e203d45fafd0a98f37a9de4519b41abac
Ewa Kowalska <ewcia.kowalska@gmail.com>
2021-02-01 11:57:26 +0100

    Remove styling from voting link

--- packages/page-bounties/src/Voting/VotingLink.tsx
+++ packages/page-bounties/src/Voting/VotingLink.tsx
@@ -2,52 +2,19 @@
 // SPDX-License-Identifier: Apache-2.0
 
 import React from 'react';
-import styled from 'styled-components';
-
-import { Icon } from '@polkadot/react-components';
 
 import Description from '../Description';
 import { useTranslation } from '../translate';
 
-interface Props {
-  className?: string;
-}
-
-function VotingLink ({ className }: Props): React.ReactElement<string> {
+function VotingLink (): React.ReactElement<string> {
   const { t } = useTranslation();
 
   return (
-    <>
-      <div className={className}>
-        <a className='voting-link'
-          href='#/council/motions'
-        >
-          <b>{t('Voting')}</b>
-          <Icon
-            className='voting-icon'
-            icon='chevron-right'
-          />
-        </a>
-        <Description description={t<string>('Go to motions panel')} />
-      </div>
-    </>
+    <div>
+      <a href='#/council/motions'>{t<string>('Voting')}</a>
+      <Description description={t<string>('Go to motions panel')} />
+    </div>
   );
 }
 
-export default React.memo(styled(VotingLink)`
-  && .voting-link {
-    font-size: 0.85rem;
-    line-height: 0.5rem;
-    color: currentColor;
-    :hover {
-      color: currentColor;
-    }
-  }
-
-  .voting-icon {
-    margin-left: 0.3rem;
-    margin-bottom: 0.2rem;
-    width: 0.4rem;
-    height: 0.4rem;
-  }
-`);
+export default React.memo(VotingLink);
