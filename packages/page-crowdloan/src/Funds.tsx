b28220968cb0401599279b3624fc1b88d7fe63fb
Jaco Greeff <jacogr@gmail.com>
2021-02-05 16:49:58 +0100

    Fund view expanded

--- packages/page-crowdloan/src/Funds.tsx
+++ packages/page-crowdloan/src/Funds.tsx
@@ -1,13 +1,14 @@
 // Copyright 2017-2021 @polkadot/app-crowdloan authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
+import type BN from 'bn.js';
 import type { Option } from '@polkadot/types';
 import type { FundIndex, FundInfo } from '@polkadot/types/interfaces';
 
 import React, { useEffect, useRef, useState } from 'react';
 
 import { Table } from '@polkadot/react-components';
-import { useApi, useCallMulti } from '@polkadot/react-hooks';
+import { useApi, useCall, useCallMulti } from '@polkadot/react-hooks';
 
 import Fund from './Fund';
 import { useTranslation } from './translate';
@@ -25,14 +26,18 @@ interface Fund {
 function Funds ({ className, fundIndexes }: Props): React.ReactElement<Props> {
   const { t } = useTranslation();
   const { api } = useApi();
+  const bestNumber = useCall<BN>(api.derive.chain.bestNumber);
   const optFunds = useCallMulti<Option<FundInfo>[]>(fundIndexes.map((id) => [api.query.crowdloan.funds, id]));
   const [funds, setFunds] = useState<Fund[] | undefined>();
 
   const headerRef = useRef([
-    [t('campaigns'), 'start'],
-    []
+    [t('campaigns'), 'start', 2],
+    [t('raised'), 'number'],
+    [t('cap'), 'number'],
+    [t('end'), 'number']
   ]);
 
+  // we actually want to split this further info completed and ongoing
   useEffect((): void => {
     optFunds && fundIndexes.length === optFunds.length && setFunds(
       fundIndexes
@@ -49,6 +54,7 @@ function Funds ({ className, fundIndexes }: Props): React.ReactElement<Props> {
     >
       {funds?.map(({ id, info }) => (
         <Fund
+          bestNumber={bestNumber}
           id={id}
           info={info}
           key={id.toString()}
