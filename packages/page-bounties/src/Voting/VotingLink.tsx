255783c3173250d604ae680fd2d4804002d19051
Ewa Kowalska <ewcia.kowalska@gmail.com>
2021-02-05 12:20:05 +0100

    Use react styled in Voting link

--- packages/page-bounties/src/Voting/VotingLink.tsx
+++ packages/page-bounties/src/Voting/VotingLink.tsx
@@ -2,19 +2,26 @@
 // SPDX-License-Identifier: Apache-2.0
 
 import React from 'react';
+import styled from 'styled-components';
 
 import Description from '../Description';
 import { useTranslation } from '../translate';
 
-function VotingLink (): React.ReactElement<string> {
+interface Props {
+  className?: string;
+}
+
+function VotingLink ({ className }: Props): React.ReactElement<Props> {
   const { t } = useTranslation();
 
   return (
-    <div style={{ margin: ' 0 1rem 0 0' }}>
+    <div className={className}>
       <a href='#/council/motions'>{t<string>('Voting')}</a>
       <Description description={t<string>('Go to motions panel')} />
     </div>
   );
 }
 
-export default React.memo(VotingLink);
+export default React.memo(styled(VotingLink)`
+  margin: 0 1rem 0 0
+`);
