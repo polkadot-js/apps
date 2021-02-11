f271be5fb7ed3cd5e891d46653d6cdb00c641a0e
Aleksandr Krupenkin <mail@akru.me>
2021-02-11 12:25:58 +0300

    Updated plasm parachain types (#4648)
    * Updated plasm parachain types
    
    * Fix lint
    
    * Fix lint

--- packages/apps-config/src/api/spec/plasm-parachain.ts
+++ packages/apps-config/src/api/spec/plasm-parachain.ts
@@ -6,5 +6,26 @@
 
 export default {
   Address: 'AccountId',
-  LookupSource: 'AccountId'
+  LookupSource: 'AccountId',
+  ChainId: {
+    _enum: {
+      RelayChain: null,
+      Parachain: 'ParaId'
+    }
+  },
+  XCurrencyId: {
+    chain_id: 'ChainId',
+    currency_id: 'Bytes'
+  },
+  CurrencyIdOf: 'CurrencyId',
+  CurrencyId: {
+    _enum: {
+      Token: 'TokenSymbol'
+    }
+  },
+  TokenSymbol: {
+    _enum: ['ACA', 'AUSD', 'DOT', 'XBTC', 'LDOT', 'RENBTC', 'SDN', 'PLM']
+  },
+  AmountOf: 'Amount',
+  Amount: 'i128'
 };
