0007925daa8f482e4992e67e01080b42af6bd70d
Greg Zaitsev <greg.zaitsev@gmail.com>
2021-02-18 15:53:48 +0300

    Lint unique types

--- packages/apps-config/src/api/spec/unique.ts
+++ packages/apps-config/src/api/spec/unique.ts
@@ -13,103 +13,103 @@ const definitions: OverrideBundleDefinition = {
       minmax: [0, undefined],
       types: {
         AccessMode: {
-            _enum: ['Normal', 'WhiteList']
+          _enum: ['Normal', 'WhiteList']
         },
         DecimalPoints: 'u8',
         CollectionMode: {
-            _enum: {
+          _enum: {
             Invalid: null,
             NFT: null,
             Fungible: 'DecimalPoints',
             ReFungible: null
-            }
+          }
         },
         Ownership: {
-            Owner: 'AccountId',
-            Fraction: 'u128'
+          Owner: 'AccountId',
+          Fraction: 'u128'
         },
         FungibleItemType: {
-            Value: 'u128'
+          Value: 'u128'
         },
         NftItemType: {
-            Owner: 'AccountId',
-            ConstData: 'Vec<u8>',
-            VariableData: 'Vec<u8>'
+          Owner: 'AccountId',
+          ConstData: 'Vec<u8>',
+          VariableData: 'Vec<u8>'
         },
         ReFungibleItemType: {
-            Owner: 'Vec<Ownership<AccountId>>',
-            ConstData: 'Vec<u8>',
-            VariableData: 'Vec<u8>'
+          Owner: 'Vec<Ownership<AccountId>>',
+          ConstData: 'Vec<u8>',
+          VariableData: 'Vec<u8>'
         },
         CollectionType: {
-            Owner: 'AccountId',
-            Mode: 'CollectionMode',
-            Access: 'AccessMode',
-            DecimalPoints: 'DecimalPoints',
-            Name: 'Vec<u16>',
-            Description: 'Vec<u16>',
-            TokenPrefix: 'Vec<u8>',
-            MintMode: 'bool',
-            OffchainSchema: 'Vec<u8>',
-            SchemaVersion: 'SchemaVersion',
-            Sponsor: 'AccountId',
-            SponsorConfirmed: 'bool',
-            Limits: 'CollectionLimits',
-            VariableOnChainSchema: 'Vec<u8>',
-            ConstOnChainSchema: 'Vec<u8>'
+          Owner: 'AccountId',
+          Mode: 'CollectionMode',
+          Access: 'AccessMode',
+          DecimalPoints: 'DecimalPoints',
+          Name: 'Vec<u16>',
+          Description: 'Vec<u16>',
+          TokenPrefix: 'Vec<u8>',
+          MintMode: 'bool',
+          OffchainSchema: 'Vec<u8>',
+          SchemaVersion: 'SchemaVersion',
+          Sponsor: 'AccountId',
+          SponsorConfirmed: 'bool',
+          Limits: 'CollectionLimits',
+          VariableOnChainSchema: 'Vec<u8>',
+          ConstOnChainSchema: 'Vec<u8>'
         },
         RawData: 'Vec<u8>',
         Address: 'AccountId',
         LookupSource: 'AccountId',
         Weight: 'u64',
         CreateNftData: {
-            const_data: 'Vec<u8>',
-            variable_data: 'Vec<u8>'
+          const_data: 'Vec<u8>',
+          variable_data: 'Vec<u8>'
         },
         CreateFungibleData: {
-            value: 'u128'
+          value: 'u128'
         },
         CreateReFungibleData: {
-            const_data: 'Vec<u8>',
-            variable_data: 'Vec<u8>',
-            pieces: 'u128'
+          const_data: 'Vec<u8>',
+          variable_data: 'Vec<u8>',
+          pieces: 'u128'
         },
         CreateItemData: {
-            _enum: {
+          _enum: {
             NFT: 'CreateNftData',
             Fungible: 'CreateFungibleData',
             ReFungible: 'CreateReFungibleData'
-            }
+          }
         },
         SchemaVersion: {
-            _enum: [
+          _enum: [
             'ImageURL',
             'Unique'
-            ]
+          ]
         },
         CollectionId: 'u32',
         TokenId: 'u32',
         ChainLimits: {
-            CollectionNumbersLimit: 'u32',
-            AccountTokenOwnershipLimit: 'u32',
-            CollectionAdminsLimit: 'u64',
-            CustomDataLimit: 'u32',
-            NftSponsorTimeout: 'u32',
-            FungibleSponsorTimeout: 'u32',
-            RefungibleSponsorTimeout: 'u32',
-            OffchainSchemaLimit: 'u32',
-            VariableOnChainSchemaLimit: 'u32',
-            ConstOnChainSchemaLimit: 'u32'
+          CollectionNumbersLimit: 'u32',
+          AccountTokenOwnershipLimit: 'u32',
+          CollectionAdminsLimit: 'u64',
+          CustomDataLimit: 'u32',
+          NftSponsorTimeout: 'u32',
+          FungibleSponsorTimeout: 'u32',
+          RefungibleSponsorTimeout: 'u32',
+          OffchainSchemaLimit: 'u32',
+          VariableOnChainSchemaLimit: 'u32',
+          ConstOnChainSchemaLimit: 'u32'
         },
         CollectionLimits: {
-            AccountTokenOwnershipLimit: 'u32',
-            SponsoredMintSize: 'u32',
-            TokenLimit: 'u32',
-            SponsorTimeout: 'u32',
-            OwnerCanTransfer: 'bool',
-            OwnerCanDestroy: 'bool'
+          AccountTokenOwnershipLimit: 'u32',
+          SponsoredMintSize: 'u32',
+          TokenLimit: 'u32',
+          SponsorTimeout: 'u32',
+          OwnerCanTransfer: 'bool',
+          OwnerCanDestroy: 'bool'
         }
-        }
+      }
     }
   ]
 };
