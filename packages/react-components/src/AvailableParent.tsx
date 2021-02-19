f8d9603ec2ee4cd37d1d939cc836e506da4b75c6
Achim <achim.schneider@equeo.de>
2021-02-19 18:32:52 +0100

    Available -> AvailableParent -> react-components

--- packages/react-components/src/AvailableParent.tsx
+++ packages/react-components/src/AvailableParent.tsx
@@ -8,7 +8,7 @@ import React from 'react';
 import { DeriveBalancesAll } from '@polkadot/api-derive/types';
 import { AccountId, AccountIndex, Address } from '@polkadot/types/interfaces';
 
-import FormatBalance from './FormatBalance';
+import FormatBalance from '../../react-query/src/FormatBalance';
 
 interface Props extends BareProps {
   children?: React.ReactNode;
