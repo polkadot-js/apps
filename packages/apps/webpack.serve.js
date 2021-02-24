cfdfea798c289ecdf3cab958fcbc024ca370b913
Jaco Greeff <jacogr@gmail.com>
2021-02-24 18:20:21 +0100

    Bump deps (common)

--- packages/apps/webpack.serve.js
+++ packages/apps/webpack.serve.js
@@ -24,6 +24,7 @@ module.exports = merge(
       }),
       new webpack.HotModuleReplacementPlugin()
     ],
+    watch: true,
     watchOptions: {
       ignored: ['.yarn', 'build', 'node_modules']
     }
