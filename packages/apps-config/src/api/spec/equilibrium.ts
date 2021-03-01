00f1d9c90b9c216768712e78b3a619d161ee6f86
Alex D <pr0fedt@gmail.com>
2021-03-01 11:40:50 +0300

    update equilibrium spec

--- packages/apps-config/src/api/spec/equilibrium.ts
+++ packages/apps-config/src/api/spec/equilibrium.ts
@@ -2,24 +2,17 @@
 // SPDX-License-Identifier: Apache-2.0
 
 import type { ApiInterfaceRx } from '@polkadot/api/types';
-import type { Enum, Struct } from '@polkadot/types';
-import type { Option } from '@polkadot/types/codec';
-import type { AccountId, AccountIndex, AccountInfo, Address, Balance, BlockNumber, Index } from '@polkadot/types/interfaces';
-import type { OverrideBundleDefinition } from '@polkadot/types/types';
-import type { Observable } from '@polkadot/x-rxjs';
+import type { Enum } from '@polkadot/types';
+import type { AccountData, AccountId, AccountIndex, Address, Balance, Index } from '@polkadot/types/interfaces';
+import type { Codec, OverrideBundleDefinition } from '@polkadot/types/types';
+
+import BN from 'bn.js';
 
 import { memo } from '@polkadot/api-derive/util/memo';
-import { combineLatest, of } from '@polkadot/x-rxjs';
-import { map, switchMap } from '@polkadot/x-rxjs/operators';
+import { map } from '@polkadot/x-rxjs/operators';
 
 // structs need to be in order
 /* eslint-disable sort-keys */
-
-interface VestingInfo extends Struct {
-  readonly locked: Balance;
-  readonly perBlock: Balance;
-  readonly startingBlock: BlockNumber;
-}
 
 interface SignedBalance extends Enum {
   readonly isPositive: boolean;
@@ -27,7 +20,6 @@ interface SignedBalance extends Enum {
   readonly isNegative: boolean;
   readonly asNegative: Balance;
 }
-
 export interface EQDeriveBalancesAll {
   additional: []; // fix until vesting instances are supported in api-derive
   freeBalance: Balance;
@@ -39,24 +31,17 @@ export interface EQDeriveBalancesAll {
   lockedBreakdown: unknown[];
 }
 
-type Result = [Balance, Balance, Balance, Index];
+const currencies = ['Unknown', 'Usd', 'EQ', 'Eth', 'Btc', 'Eos', 'Dot'];
 
-type RawResult = [
-  SignedBalance,
-  Option<Balance>,
-  Option<VestingInfo>,
-  AccountInfo
-];
-
-type EQDeriveBalancesAccountQuery = (
-  address: AccountIndex | AccountId | Address | string
-) => Observable<{ accountNonce: Index; }>;
-
-type EQDeriveBalancesAllQuery = (
-  address: AccountIndex | AccountId | Address | string
-) => Observable<EQDeriveBalancesAll>;
+const signedBalancePredicate = (raw: Codec): raw is SignedBalance =>
+  ['asNegative', 'asPositive', 'isNegative', 'isPositive']
+    .some((key) => Object.prototype.hasOwnProperty.call(raw, key));
 
 const definitions: OverrideBundleDefinition = {
+  instances: {
+    balances: currencies
+  },
+
   types: [
     {
       // on all versions
@@ -156,136 +141,37 @@ const definitions: OverrideBundleDefinition = {
     }
   ],
   derives: {
-    // TODO derive.democracy.locks
-    // TODO derice.staking.account
-    balances: {
-      account: (
-        // Compatibility with calc tx fee
-        instanceId: string,
-        api: ApiInterfaceRx
-      ): EQDeriveBalancesAccountQuery =>
-        memo(
-          instanceId,
-          (address: AccountId | AccountIndex | Address | string) =>
-            api.derive.accounts.accountId(address).pipe(
-              switchMap(
-                (accountId): Observable<[AccountId, [Index]]> =>
-                  accountId
-                    ? combineLatest([
-                      of(accountId),
-                      api
-                        .queryMulti([
-                          [api.query.system.account, accountId]
-                        ])
-                        .pipe(
-                          map((raw): [Index] => {
-                            if (raw.length < 1) {
-                              throw new Error('Data expected');
-                            }
+    ...currencies.reduce((all, cur) => ({ ...all,
+      [cur]: {
+        account: (instanceId: string, api: ApiInterfaceRx) =>
+          memo(
+            instanceId,
+            (address: AccountIndex | AccountId | Address | string) =>
+              api.query.balances.account(address, cur).pipe(map(
+                (balance): AccountData => {
+                  let free = api.registry.createType('Balance');
+                  const reserved = api.registry.createType('Balance');
+                  const miscFrozen = api.registry.createType('Balance');
+                  const feeFrozen = api.registry.createType('Balance');
 
-                            const data = raw as [AccountInfo];
+                  if (signedBalancePredicate(balance)) {
+                    if (balance.isPositive) {
+                      free = api.registry.createType('Balance', balance.asPositive);
+                    } else if (balance.isNegative) {
+                      free = api.registry.createType(
+                        'Balance',
+                        balance.asNegative.mul(new BN(-1))
+                      );
+                    }
+                  }
 
-                            return [data[0].nonce];
-                          })
-                        )
-                    ])
-                    : of([
-                      api.registry.createType('AccountId'),
-                      [api.registry.createType('Index')]
-                    ])
-              ),
-              map(([, [accountNonce]]) => ({ accountNonce }))
-            )
-        ),
-      all: (
-        // Compatibility for account balance in explorer
-        instanceId: string,
-        api: ApiInterfaceRx
-      ): EQDeriveBalancesAllQuery =>
-        memo(
-          instanceId,
-          (address: AccountIndex | AccountId | Address | string) =>
-            api.derive.accounts.accountId(address).pipe(
-              switchMap(
-                (accountId): Observable<[AccountId, Result]> =>
-                  accountId
-                    ? combineLatest([
-                      of(accountId),
-                      api
-                        .queryMulti([
-                          [api.query.balances.account, [accountId, 'EQ']],
-                          [api.query.eqVesting.vested, accountId],
-                          [api.query.eqVesting.vesting, accountId],
-                          [api.query.system.account, accountId]
-                        ])
-                        .pipe(
-                          map(
-                            (raw): Result => {
-                              if (raw.length < 4) {
-                                throw new Error('4 members expected');
-                              }
-
-                              const res = raw as RawResult;
-                              const freeBalance = res[0].asPositive;
-
-                              let reservedBalance = api.registry.createType(
-                                'Balance'
-                              );
-
-                              let vestingLocked = api.registry.createType(
-                                'Balance'
-                              );
-
-                              if (res[1].isSome && res[2].isSome) {
-                                const vested = res[1].unwrap();
-                                const info = res[2].unwrap();
-
-                                vestingLocked = info.locked;
-
-                                reservedBalance = api.registry.createType(
-                                  'Balance',
-                                  vestingLocked.sub(vested)
-                                );
-                              }
-
-                              return [
-                                freeBalance,
-                                reservedBalance,
-                                vestingLocked,
-                                res[3].nonce
-                              ];
-                            }
-                          )
-                        )
-                    ])
-                    : of([
-                      api.registry.createType('AccountId'),
-                      [
-                        api.registry.createType('Balance'),
-                        api.registry.createType('Balance'),
-                        api.registry.createType('Balance'),
-                        api.registry.createType('Index')
-                      ]
-                    ])
-              ),
-              map(
-                ([
-                  accountId,
-                  [freeBalance, reservedBalance, vestingLocked, accountNonce]
-                ]): EQDeriveBalancesAll => ({
-                  accountId,
-                  accountNonce,
-                  additional: [],
-                  freeBalance,
-                  lockedBalance: vestingLocked,
-                  lockedBreakdown: [],
-                  reservedBalance,
-                  vestingLocked
-                })
-              )
-            )
-        )
-    }
+                  return api.registry.createType(
+                    'AccountData',
+                    { free, reserved, miscFrozen, feeFrozen }
+                  );
+                }))
+          )
+      } }), {})
   }
 };
 
