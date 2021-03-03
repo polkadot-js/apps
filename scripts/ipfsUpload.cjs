7d2d1842d191b668858f4275557adebb91d03f43
Jaco Greeff <jacogr@gmail.com>
2021-03-03 11:52:39 +0100

    Update deps (& berry) (#4765)
    * Update deps (& berry)
    
    * linting
    
    * Additional
    
    * Bumps
    
    * jest babel helpers esm

--- scripts/ipfsUpload.cjs
+++ scripts/ipfsUpload.cjs
@@ -6,7 +6,7 @@ const pinataSDK = require('@pinata/sdk');
 const cloudflare = require('dnslink-cloudflare');
 const execSync = require('@polkadot/dev/scripts/execSync.cjs');
 
-const { createWsEndpoints } = require('../packages/apps-config/build/endpoints');
+const { createWsEndpoints } = require('../packages/apps-config/build/endpoints.cjs');
 const pkgJson = require('../package.json');
 
 // https://gateway.pinata.cloud/ipfs/
