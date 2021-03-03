cc7bf13b386da4481a9a15ceec873e43812b448b
Jaco Greeff <jacogr@gmail.com>
2021-03-03 08:49:50 +0100

    Update deps (& berry)

--- packages/apps-electron/webpack.renderer.cjs
+++ packages/apps-electron/webpack.renderer.cjs
@@ -1,26 +1,29 @@
 // Copyright 2017-2021 @polkadot/apps authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
-const fs = require('fs');
+/* eslint-disable camelcase */
+
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 const { merge } = require('webpack-merge');
 
-const baseConfig = require('./webpack.base.js');
+const baseConfig = require('../apps/webpack.base.cjs');
 
 const context = __dirname;
-const hasPublic = fs.existsSync(path.join(context, 'public'));
 
 module.exports = merge(
-  baseConfig(context),
+  baseConfig(context, 'development'),
   {
-    devtool: process.env.BUILD_ANALYZE ? 'source-map' : false,
     plugins: [
+      // It must be placed before HtmlWebpackPlugin
+      new CopyWebpackPlugin({ patterns: [{ from: '../apps/public' }] }),
       new HtmlWebpackPlugin({
         PAGE_TITLE: 'Polkadot/Substrate Portal',
         inject: true,
-        template: path.join(context, `${hasPublic ? 'public/' : ''}index.html`)
+        template: path.join(context, '../apps/public/index.html')
       })
-    ]
+    ],
+    target: 'web'
   }
 );
