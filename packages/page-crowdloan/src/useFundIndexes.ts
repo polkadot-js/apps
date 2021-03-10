568a57ba4ea5a0d7c453d77e22d84c3a03bd9dc3
Jaco Greeff <jacogr@gmail.com>
2021-03-10 22:53:18 +0100

    indexes

--- packages/page-crowdloan/src/useFundIndexes.ts
+++ packages/page-crowdloan/src/useFundIndexes.ts
@@ -1,22 +1,22 @@
 // Copyright 2017-2021 @polkadot/app-crowdloan authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
-import type { FundIndex } from '@polkadot/types/interfaces';
+import type { ParaId } from '@polkadot/types/interfaces';
 
 import { useEffect, useState } from 'react';
 
 import { useApi, useEventTrigger } from '@polkadot/react-hooks';
 
-export default function useFundIndexes (): FundIndex[] {
+export default function useFundIndexes (): ParaId[] {
   const { api } = useApi();
-  const [indexes, setIndexes] = useState<FundIndex[]>([]);
+  const [indexes, setIndexes] = useState<ParaId[]>([]);
   const trigger = useEventTrigger([api.events.crowdloan.Created]);
 
   useEffect((): void => {
     trigger &&
       api.query.crowdloan.funds
         .keys()
-        .then((indexes) => setIndexes(indexes.map(({ args }) => args[0] as FundIndex)))
+        .then((indexes) => setIndexes(indexes.map(({ args }) => args[0] as ParaId)))
         .catch(console.error);
   }, [api, trigger]);
 
