71497c57f8e8a670863c0b3df23a186dbe263044
Jaco Greeff <jacogr@gmail.com>
2021-03-11 14:21:34 +0100

    Bid action (no result data displays) (#4822)
    * Bid action (no rsult data displays)
    
    * allow last === first
    
    * Bump api

--- packages/page-parachains/src/Auctions/Bid.tsx
+++ packages/page-parachains/src/Auctions/Bid.tsx
@@ -2,6 +2,7 @@
 // SPDX-License-Identifier: Apache-2.0
 
 import type BN from 'bn.js';
+import type { AuctionIndex } from '@polkadot/types/interfaces';
 
 import React, { useState } from 'react';
 
@@ -12,53 +13,49 @@ import { BN_ZERO } from '@polkadot/util';
 import { useTranslation } from '../translate';
 
 interface Props {
-  bestNumber?: BN;
   className?: string;
+  id: AuctionIndex;
 }
 
-function FundAdd ({ bestNumber, className }: Props): React.ReactElement<Props> {
+function Bid ({ className, id }: Props): React.ReactElement<Props> {
   const { t } = useTranslation();
   const { api } = useApi();
   const { hasAccounts } = useAccounts();
   const [accountId, setAccountId] = useState<string | null>(null);
-  const [cap, setCap] = useState<BN | undefined>();
-  const [endBlock, setEndBlock] = useState<BN | undefined>();
+  const [amount, setAmount] = useState<BN | undefined>();
   const [firstSlot, setFirstSlot] = useState<BN | undefined>();
   const [lastSlot, setLastSlot] = useState<BN | undefined>();
   const [paraId, setParaId] = useState<BN | undefined>();
   const [isOpen, toggleOpen] = useToggle();
 
-  const isEndError = !bestNumber || !endBlock || endBlock.lt(bestNumber);
   const isLastError = !lastSlot || !firstSlot || lastSlot.lt(firstSlot) || lastSlot.gt(firstSlot.addn(3));
-
-  // TODO Add verifier
 
   return (
     <>
       <Button
         icon='plus'
         isDisabled={!hasAccounts}
-        label={t<string>('Add campaign')}
+        label={t<string>('Bid')}
         onClick={toggleOpen}
       />
       {isOpen && (
         <Modal
           className={className}
-          header={t<string>('Add campaign')}
+          header={t<string>('Place bid')}
           size='large'
         >
           <Modal.Content>
             <Modal.Columns>
               <Modal.Column>
                 <InputAddress
-                  label={t<string>('propose from')}
+                  label={t<string>('bid from')}
                   onChange={setAccountId}
                   type='account'
                   value={accountId}
                 />
               </Modal.Column>
               <Modal.Column>
-                {t<string>('This account will be associated with the fund and pay the deposit. This should match the registrar for the parachain.')}
+                {t<string>('This account will be associated with the bid. This should match the registrar for the parachain.')}
               </Modal.Column>
             </Modal.Columns>
             <Modal.Columns>
@@ -71,31 +68,19 @@ function FundAdd ({ bestNumber, className }: Props): React.ReactElement<Props> {
                 />
               </Modal.Column>
               <Modal.Column>
-                {t<string>('The parachain id this campaign refers to')}
+                {t<string>('The parachain id this bid is placed for')}
               </Modal.Column>
             </Modal.Columns>
             <Modal.Columns>
               <Modal.Column>
                 <InputBalance
                   isZeroable={false}
-                  label={t<string>('crowdfund cap')}
-                  onChange={setCap}
+                  label={t<string>('bid amount')}
+                  onChange={setAmount}
                 />
               </Modal.Column>
               <Modal.Column>
-                {t<string>('The amount to be raised in this funding campaign')}
-              </Modal.Column>
-            </Modal.Columns>
-            <Modal.Columns>
-              <Modal.Column>
-                <InputNumber
-                  isError={isEndError}
-                  label={t<string>('ending block')}
-                  onChange={setEndBlock}
-                />
-              </Modal.Column>
-              <Modal.Column>
-                {t<string>('The block until which this campaign is active for')}
+                {t<string>('The amount to to bid for this parachain slot')}
               </Modal.Column>
             </Modal.Columns>
             <Modal.Columns>
@@ -111,7 +96,7 @@ function FundAdd ({ bestNumber, className }: Props): React.ReactElement<Props> {
                 />
               </Modal.Column>
               <Modal.Column>
-                {t<string>('The first and last slots for this funding campaign. The last slot should be after the first and a maximum of 3 slots more than the first')}
+                {t<string>('The first and last slots for this bid. The last slot should be after the first and a maximum of 3 slots more than the first')}
               </Modal.Column>
             </Modal.Columns>
           </Modal.Content>
@@ -119,11 +104,11 @@ function FundAdd ({ bestNumber, className }: Props): React.ReactElement<Props> {
             <TxButton
               accountId={accountId}
               icon='plus'
-              isDisabled={!paraId?.gt(BN_ZERO) || !cap?.gt(BN_ZERO) || !firstSlot?.gte(BN_ZERO) || isEndError || isLastError}
-              label={t<string>('Add')}
+              isDisabled={!paraId?.gt(BN_ZERO) || !amount?.gt(BN_ZERO) || !firstSlot?.gte(BN_ZERO) || isLastError}
+              label={t<string>('Bid')}
               onStart={toggleOpen}
-              params={[paraId, cap, firstSlot, lastSlot, endBlock, null]}
-              tx={api.tx.crowdloan.create}
+              params={[paraId, id, firstSlot, lastSlot, amount]}
+              tx={api.tx.auctions.bid}
             />
           </Modal.Actions>
         </Modal>
@@ -132,4 +117,4 @@ function FundAdd ({ bestNumber, className }: Props): React.ReactElement<Props> {
   );
 }
 
-export default React.memo(FundAdd);
+export default React.memo(Bid);
