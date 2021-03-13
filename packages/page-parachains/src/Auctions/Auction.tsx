93538d55f631e54af29f405ec71fb23c6e4af2d4
Jaco Greeff <jacogr@gmail.com>
2021-03-13 10:58:48 +0100

    Rework auction list display (#4837)
    * Rework auction list display
    
    * Adjust

--- packages/page-parachains/src/Auctions/Auction.tsx
+++ packages/page-parachains/src/Auctions/Auction.tsx
@@ -6,7 +6,6 @@ import type { AuctionIndex, BlockNumber, LeasePeriodOf } from '@polkadot/types/i
 import React, { useRef } from 'react';
 
 import { Table } from '@polkadot/react-components';
-import { BN_ZERO } from '@polkadot/util';
 
 import { useTranslation } from '../translate';
 import useWinningData from './useWinningData';
@@ -14,14 +13,13 @@ import WinBlock from './WinBlock';
 
 interface Props {
   auctionInfo: [LeasePeriodOf, BlockNumber] | null;
-  bestNumber?: BlockNumber;
   className?: string;
   numAuctions: AuctionIndex | null;
 }
 
-function Auction ({ auctionInfo, bestNumber, className, numAuctions }: Props): React.ReactElement<Props> {
+function Auction ({ auctionInfo, className, numAuctions }: Props): React.ReactElement<Props> {
   const { t } = useTranslation();
-  const winningData = useWinningData();
+  const winningData = useWinningData(auctionInfo);
 
   const headerRef = useRef([
     [t('winners'), 'start', 4],
@@ -29,26 +27,21 @@ function Auction ({ auctionInfo, bestNumber, className, numAuctions }: Props): R
     [t('value'), 'number']
   ]);
 
-  const offset = auctionInfo && bestNumber && bestNumber.gt(auctionInfo[1])
-    ? bestNumber.sub(auctionInfo[1])
-    : BN_ZERO;
-
   return (
     <Table
       className={className}
-      empty={winningData && numAuctions && auctionInfo
-        ? t<string>('No winners in this auction')
-        : t<string>('No ongoing auction')
-      }
+      empty={numAuctions && winningData && (
+        auctionInfo && !winningData.length
+          ? t<string>('No winners in this auction')
+          : t<string>('No ongoing auction')
+      )}
       header={headerRef.current}
     >
-      {offset && bestNumber && winningData?.map((value, count) => (
+      {winningData?.map((value, count) => (
         <WinBlock
-          bestNumber={bestNumber}
           isEven={!!(count % 2)}
           isLatest={count === 0}
           key={value.blockNumber.toString()}
-          offset={offset}
           value={value}
         />
       ))}
