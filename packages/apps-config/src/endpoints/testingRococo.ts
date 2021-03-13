a51ab68f9707e0fa1110ae9da9ce97cca6230de0
Justin Pham <justin.phamnz@gmail.com>
2021-03-14 11:26:20 +1300

    update paraId for Bit.Country Parachain testnet
    We updated our paraId of our Bit.Country Parachain proposal

--- packages/apps-config/src/endpoints/testingRococo.ts
+++ packages/apps-config/src/endpoints/testingRococo.ts
@@ -72,7 +72,7 @@ export function createRococo (t: TFunction): LinkOption[] {
       },
       {
         info: 'rococoBitCountry',
-        paraId: 8888,
+        paraId: 8,
         text: t('rpc.rococo.bitcountry', 'Bit.Country PC1', { ns: 'apps-config' }),
         providers: {
           BitCountry: 'wss://tewai-parachain.bit.country:9955'
