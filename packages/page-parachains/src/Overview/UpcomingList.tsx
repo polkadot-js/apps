5e3ee838e8ba8ada1d9250c9c7c7ef7ee6543206
Jaco Greeff <jacogr@gmail.com>
2021-03-14 16:29:11 +0100

    lifecycle

--- packages/page-parachains/src/Overview/UpcomingList.tsx
+++ packages/page-parachains/src/Overview/UpcomingList.tsx
@@ -22,7 +22,7 @@ function UpcomingList ({ currentPeriod, ids }: Props): React.ReactElement<Props>
   const headerRef = useRef([
     [t('upcoming'), 'start', 2],
     [undefined, 'all'],
-    [t('status'), 'number'],
+    [t('lifecycle'), 'number'],
     [t('leases'), 'start'],
     [t('parachain'), 'start']
   ]);
