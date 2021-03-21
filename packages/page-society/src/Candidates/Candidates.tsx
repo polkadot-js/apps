29ac8ba6fb24d6df0d56f142777373b3395dc203
Jaco Greeff <jacogr@gmail.com>
2021-03-21 13:08:24 +0100

    Expand society bid kind displays

--- packages/page-society/src/Candidates/Candidates.tsx
+++ packages/page-society/src/Candidates/Candidates.tsx
@@ -23,7 +23,7 @@ function Candidates ({ allMembers, candidates, className = '', isMember, ownMemb
 
   const headerRef = useRef([
     [t('candidates'), 'start'],
-    [t('kind')],
+    [t('bid kind'), 'start', 2],
     [t('value')],
     [t('votes'), 'expand'],
     []
