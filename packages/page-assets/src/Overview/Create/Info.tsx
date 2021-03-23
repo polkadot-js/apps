5243576d473bbead6f5efa8e3e86d8b0caf759e0
Jaco Greeff <jacogr@gmail.com>
2021-03-23 20:53:55 +0100

    min balance

--- packages/page-assets/src/Overview/Create/Info.tsx
+++ packages/page-assets/src/Overview/Create/Info.tsx
@@ -24,6 +24,7 @@ function Info ({ assetIds, className = '', onChange }: Props): React.ReactElemen
   const { api } = useApi();
   const [accountId, setAccountId] = useState<string | null>(null);
   const [assetId, setAssetId] = useState<BN | null>(null);
+  const [minBalance, setMinBalance] = useState<BN | null>(null);
 
   const isIdValid = useMemo(
     () => !!assetId && assetId.gt(BN_ZERO) && !assetIds.some((a) => a.eq(assetId)),
@@ -32,21 +33,21 @@ function Info ({ assetIds, className = '', onChange }: Props): React.ReactElemen
 
   useEffect((): void => {
     onChange(
-      assetId && accountId && isIdValid
+      assetId && accountId && minBalance && isIdValid && !minBalance.isZero()
         ? {
           accountId,
           assetId,
-          createTx: api.tx.assets.create(assetId, accountId, 0)
+          createTx: api.tx.assets.create(assetId, accountId, minBalance)
         }
         : null
     );
-  }, [api, accountId, assetId, assetIds, isIdValid, onChange]);
+  }, [api, accountId, assetId, assetIds, isIdValid, minBalance, onChange]);
 
   return (
     <Modal.Content className={className}>
       <Modal.Columns hint={t<string>('The account that is to be used to create this asset and setup the initial metadata.')}>
         <InputAddress
-          label={t<string>('admin account')}
+          label={t<string>('creator account')}
           onChange={setAccountId}
           type='account'
         />
@@ -58,6 +59,14 @@ function Info ({ assetIds, className = '', onChange }: Props): React.ReactElemen
           isZeroable={false}
           label={t<string>('asset id')}
           onChange={setAssetId}
+        />
+      </Modal.Columns>
+      <Modal.Columns hint={t<string>('The minimum balance for the asset.')}>
+        <InputNumber
+          autoFocus
+          isZeroable={false}
+          label={t<string>('minimum balance')}
+          onChange={setMinBalance}
         />
       </Modal.Columns>
     </Modal.Content>
