0f3c44a9735a7a73220961361e7d061f3c7c00b9
Jaco Greeff <jacogr@gmail.com>
2021-03-26 11:30:05 +0100

    Sort cowdloan via status, no winner contribute

--- packages/page-parachains/src/Crowdloan/types.ts
+++ packages/page-parachains/src/Crowdloan/types.ts
@@ -1,11 +1,23 @@
 // Copyright 2017-2021 @polkadot/app-parachains authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
-import type { FundInfo, ParaId } from '@polkadot/types/interfaces';
+import type { AccountId, BalanceOf, FundInfo, ParaId } from '@polkadot/types/interfaces';
 
 export interface Campaign {
   info: FundInfo;
+  isCapped?: boolean;
   isEnded?: boolean;
+  isWinner?: boolean;
   key: string;
+  paraId: ParaId;
+}
+
+export interface LeaseLease {
+  accountId: AccountId;
+  balance: BalanceOf;
+}
+
+export interface Lease {
+  leases: LeaseLease[];
   paraId: ParaId;
 }
