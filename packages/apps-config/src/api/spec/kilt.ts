56c6550d8049094ef19fdeb72eaa3c4b94e622cf
William Freudenberger <w.freude@icloud.com>
2021-03-29 18:48:29 +0200

    feat: add KILT xcmp types (#4971)

--- packages/apps-config/src/api/spec/kilt.ts
+++ packages/apps-config/src/api/spec/kilt.ts
@@ -41,7 +41,50 @@ const definitions: OverrideBundleDefinition = {
         Permissions: 'u32',
         PublicBoxKey: 'Hash',
         PublicSigningKey: 'Hash',
-        Signature: 'MultiSignature'
+        Signature: 'MultiSignature',
+        XCurrencyId: {
+          chainId: 'ChainId',
+          currencyId: 'Vec<u8>'
+        },
+        ChainId: {
+          _enum: {
+            RelayChain: 'Null',
+            ParaChain: 'ParaId'
+          }
+        },
+        CurrencyIdOf: 'CurrencyId',
+        CurrencyId: {
+          _enum: {
+            DOT: 0,
+            KSM: 1,
+            KILT: 2
+          }
+        },
+        XcmError: {
+          _enum: {
+            Undefined: 0,
+            Unimplemented: 1,
+            UnhandledXcmVersion: 2,
+            UnhandledXcmMessage: 3,
+            UnhandledEffect: 4,
+            EscalationOfPrivilege: 5,
+            UntrustedReserveLocation: 6,
+            UntrustedTeleportLocation: 7,
+            DestinationBufferOverflow: 8,
+            CannotReachDestination: 9,
+            MultiLocationFull: 10,
+            FailedToDecode: 11,
+            BadOrigin: 12,
+            ExceedsMaxMessageSize: 13,
+            FailedToTransactAsset: 14
+          }
+        },
+        ReferendumInfo: {
+          _enum: {
+            Ongoing: 'ReferendumStatus',
+            Finished: 'ReferendumInfoFinished'
+          }
+        }
       }
     }
   ]
