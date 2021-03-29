572347bb95349aa51f74b894aa908e50fc51bd96
Kun <zikunvan@gmail.com>
2021-03-29 23:33:56 +0800

    [CI] Fix ipfsUpload Crust Pinner (#4970)
    * Fix abnormal account type
    
    * Fix crust logo invisible
    
    * Add Crust Rococo V1 Endpoint
    
    * Upgrade to Crust Maxwell
    
    * Apply crust type package and add DCF provider
    
    * Ipfs on crust (#4)
    
    * Change ipfs upload in a more decentralized way
    
    * Add CRUST_SEEDS env variable
    
    * Fix with Crust IPFS Upload

--- scripts/ipfsUpload.cjs
+++ scripts/ipfsUpload.cjs
@@ -3,7 +3,7 @@
 
 const fs = require('fs');
 const pinataSDK = require('@pinata/sdk');
-const crustPin = require('@crustio/crust-pin').default;
+const CrustPinner = require('@crustio/crust-pin').default;
 const cloudflare = require('dnslink-cloudflare');
 const execSync = require('@polkadot/dev/scripts/execSync.cjs');
 
@@ -20,7 +20,7 @@ const PINMETA = { name: DOMAIN };
 
 const repo = `https://${process.env.GH_PAT}@github.com/${process.env.GITHUB_REPOSITORY}.git`;
 const pinata = pinataSDK(process.env.PINATA_API_KEY, process.env.PINATA_SECRET_KEY);
-const crust = crustPin(process.env.CRUST_SEEDS);
+const crust = new CrustPinner(process.env.CRUST_SEEDS);
 
 function writeFiles (name, content) {
   [DST, SRC].forEach((root) =>
