232ece7d9cf2709c2cd75ef4b01f4dfc2ea31cad
achimcc <achim@parity.com>
2021-04-08 14:43:35 +0200

    removed apps folder

--- packages/app/src/types.ts
+++ packages/app/src/types.ts
@@ -2,7 +2,7 @@
 // and @canvas-ui/app-execute authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
-import { ActionStatus } from '@canvas-ui/react-components/Status/types';
+import { ActionStatus } from '@canvas-ui/react-api/Status/types';
 import { BareProps } from '@canvas-ui/react-components/types';
 import { VoidFn } from '@canvas-ui/react-util/types';
 
