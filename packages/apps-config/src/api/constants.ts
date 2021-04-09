08a71937fd563d1f0fa6e690832d7b0360e841b3
Jaco Greeff <jacogr@gmail.com>
2021-04-09 23:43:30 +0300

    Update Rococo genesis

--- packages/apps-config/src/api/constants.ts
+++ packages/apps-config/src/api/constants.ts
@@ -21,4 +21,4 @@ export const KUSAMA_GENESIS = getGenesis('kusama');
 export const POLKADOT_GENESIS = getGenesis('polkadot');
 export const POLKADOT_DENOM_BLOCK = new BN(1248328);
 
-export const ROCOCO_GENESIS = '0x343442f12fa715489a8714e79a7b264ea88c0d5b8c66b684a7788a516032f6b9';
+export const ROCOCO_GENESIS = '0x1611e1dbf0405379b861e2e27daa90f480b2e6d3682414a80835a52e8cb8a215';
