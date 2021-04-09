af6bd369dfb43a35086001cfeac484054db024a7
achimcc <achim@parity.com>
2021-04-09 16:51:11 +0200

    cleanup react-components v2 delete unused files

--- packages/react-components/src/styles/constants.ts
+++ packages/react-components/src/styles/constants.ts
@@ -6,29 +6,3 @@ export const ELEV_CSS = `
   box-sizing: border-box;
   border-radius: 4px;
 `;
-
-export const ELEV_0_CSS = `
-  ${ELEV_CSS}
-  background: var(--background);
-  border: 1px solid var(--grey20);
-`;
-
-export const ELEV_1_CSS = `
-  ${ELEV_CSS}
-  background: var(--grey10);
-`;
-
-export const ELEV_2_CSS = `
-  ${ELEV_CSS}
-  background: var(--grey15);
-`;
-
-export const ELEV_3_CSS = `
-  ${ELEV_CSS}
-  background: var(--grey20);
-`;
-
-export const ELEV_4_CSS = `
-  ${ELEV_CSS}
-  background: var(--grey30);
-`;
