8217c5556a3a706ade1e37ff788206cab270d2f9
achimcc <achim@parity.com>
2021-04-09 16:59:02 +0200

    cleanup react-components v5

--- packages/react-components/src/styles/constants.ts
+++ packages/react-components/src/styles/constants.ts
@@ -2,7 +2,7 @@
 // and @canvas-ui/react-components authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
-export const ELEV_CSS = `
+const ELEV_CSS = `
   box-sizing: border-box;
   border-radius: 4px;
 `;
