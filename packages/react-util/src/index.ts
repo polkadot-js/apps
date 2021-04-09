e885ccd1b4b9ad2d524a54aebe63bbe001a3d886
achimcc <achim@parity.com>
2021-04-09 14:43:47 +0200

    cleanup react-util

--- packages/react-util/src/index.ts
+++ packages/react-util/src/index.ts
@@ -2,12 +2,9 @@
 // SPDX-License-Identifier: Apache-2.0
 
 export { default as classes } from './classes';
-export { default as getAddressMeta } from './getAddressMeta';
 export { default as getAddressName } from './getAddressName';
-export { default as getAddressTags } from './getAddressTags';
 export { default as getContractAbi } from './getContractAbi';
 export { default as getContractForAddress } from './getContractForAddress';
-export { default as isTreasuryProposalVote } from './isTreasuryProposalVote';
 export { default as timeToString } from './timeToString';
 export { default as toAddress } from './toAddress';
 export { default as toShortAddress } from './toShortAddress';
