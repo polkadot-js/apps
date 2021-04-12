25b989e3c2e86e16314e72fed3b216a7e4265a3e
Jaco Greeff <jacogr@gmail.com>
2021-04-12 15:14:42 +0300

    0.87.1

--- packages/react-api/src/util/isEqual.ts
+++ packages/react-api/src/util/isEqual.ts
@@ -2,28 +2,15 @@
 // SPDX-License-Identifier: Apache-2.0
 
 function flatten (key: string | null, value?: unknown): unknown {
-  if (!value) {
-    return value;
-  }
-
-  if ((value as Record<string, unknown>).$$typeof) {
-    return '';
-  }
-
-  if (Array.isArray(value)) {
-    return value.map((item) => flatten(null, item));
-  }
-
-  return value;
+  return !value
+    ? value
+    : (value as Record<string, unknown>).$$typeof
+      ? ''
+      : Array.isArray(value)
+        ? value.map((item) => flatten(null, item))
+        : value;
 }
 
-export default function isEqual <T> (a?: T, b?: T, debug = false): boolean {
-  const jsonA = JSON.stringify({ test: a }, flatten);
-  const jsonB = JSON.stringify({ test: b }, flatten);
-
-  if (debug) {
-    console.log('jsonA', jsonA, 'jsonB', jsonB);
-  }
-
-  return jsonA === jsonB;
+export default function isEqual <T> (a?: T, b?: T): boolean {
+  return JSON.stringify({ test: a }, flatten) === JSON.stringify({ test: b }, flatten);
 }
