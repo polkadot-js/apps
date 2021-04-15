fc0babce235d10ec11804f326f22b74e870d9025
zhijun <zhijunsoh@gmail.com>
2021-04-15 18:24:14 +0800

    add prism parachain (#5077)
    * add prism parachain
    
    * prism parachain ascending order
    
    Co-authored-by: zhijunsoh <zhijunsho@gmail.com>

--- packages/apps-config/src/endpoints/testing.ts
+++ packages/apps-config/src/endpoints/testing.ts
@@ -206,6 +206,13 @@ export function createTesting (t: TFunction): LinkOption[] {
       }
     },
     {
+      info: 'prism',
+      text: t('rpc.prism', 'Prism', { ns: 'apps-config' }),
+      providers: {
+        Prism: 'wss://testnet.psm.link'
+      }
+    },
+    {
       info: 'realis',
       text: t('rpc.realis', 'Realis.Network', { ns: 'apps-config' }),
       providers: {
