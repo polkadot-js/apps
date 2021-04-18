1283ab190a2469c189ba1bcc66025b4bccd9524c
Igor Smirnov <igorsmirnov.gg@gmail.com>
2021-04-18 19:08:21 +0800

    updated logos and colors for spanner and hammer

--- packages/apps-config/src/ui/logos/index.ts
+++ packages/apps-config/src/ui/logos/index.ts
@@ -15,7 +15,7 @@ import chainRococo from './chains/rococo.svg';
 import chainRococoTick from './chains/rococo-tick.svg';
 import chainRococoTrack from './chains/rococo-track.svg';
 import chainRococoTrick from './chains/rococo-trick.svg';
-import chainSpanner from './chains/spanner.svg';
+import chainSpanner from './chains/spanner.png';
 import chainSnakenet from './chains/snakenet.svg';
 import chainUnique from './chains/unique.svg';
 import extensionPolkadotJs from './extensions/polkadot-js.svg';
