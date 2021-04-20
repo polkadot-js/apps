919dbcd8a734f5078078d1510db182ab44f09c9b
Jaco <jacogr@gmail.com>
2021-04-20 12:38:36 +0300

    Use batch management hook for asset create (#5102)

--- packages/page-assets/src/Overview/Create/Create.tsx
+++ packages/page-assets/src/Overview/Create/Create.tsx
@@ -1,14 +1,13 @@
 // Copyright 2017-2021 @polkadot/app-assets authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
-import type { SubmittableExtrinsic } from '@polkadot/api/types';
 import type { AssetId } from '@polkadot/types/interfaces';
 import type { InfoState, MetadataState, TeamState } from './types';
 
-import React, { useEffect, useState } from 'react';
+import React, { useMemo, useState } from 'react';
 
 import { Button, Modal, TxButton } from '@polkadot/react-components';
-import { useApi, useStepper } from '@polkadot/react-hooks';
+import { useStepper, useTxBatch } from '@polkadot/react-hooks';
 
 import { useTranslation } from '../../translate';
 import Info from './Info';
@@ -21,24 +20,22 @@ interface Props {
   onClose: () => void;
 }
 
+const BATCH_OPTIONS = {
+  isBatchAll: true
+};
+
 function Create ({ assetIds, className, onClose }: Props): React.ReactElement<Props> {
   const { t } = useTranslation();
-  const { api } = useApi();
   const [step, nextStep, prevStep] = useStepper();
   const [asset, setAsset] = useState<InfoState | null>(null);
   const [metadata, setMetadata] = useState<MetadataState | null>(null);
   const [team, setTeam] = useState<TeamState | null>(null);
-  const [extrinsic, setExtrinsic] = useState<SubmittableExtrinsic<'promise'> | null>(null);
-
-  useEffect((): void => {
-    setExtrinsic(
-      () => asset && metadata && team
-        ? team.teamTx
-          ? api.tx.utility.batchAll([asset.createTx, metadata.metadataTx, team.teamTx])
-          : api.tx.utility.batchAll([asset.createTx, metadata.metadataTx])
-        : null
-    );
-  }, [api, asset, metadata, team]);
+  const txs = useMemo(() => asset && metadata && team && (
+    team.teamTx
+      ? [asset.createTx, metadata.metadataTx, team.teamTx]
+      : [asset.createTx, metadata.metadataTx]
+  ), [asset, metadata, team]);
+  const extrinsic = useTxBatch(txs, BATCH_OPTIONS);
 
   return (
     <Modal
