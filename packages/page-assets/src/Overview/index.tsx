fcbb067c416ecf9012d7ca86a6ae7196d28ce81d
Jaco <jacogr@gmail.com>
2021-04-23 14:46:51 +0300

    Display asset balances (#5128)

--- packages/page-assets/src/Overview/index.tsx
+++ packages/page-assets/src/Overview/index.tsx
@@ -1,6 +1,9 @@
 // Copyright 2017-2021 @polkadot/app-assets authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
+import type { AssetId } from '@polkadot/types/interfaces';
+import type { AssetInfo } from '../types';
+
 import React from 'react';
 
 import { Button } from '@polkadot/react-components';
@@ -8,22 +11,21 @@ import { Button } from '@polkadot/react-components';
 import Assets from './Assets';
 import Create from './Create';
 import Summary from './Summary';
-import useAssetIds from './useAssetIds';
 
 interface Props {
   className?: string;
+  ids?: AssetId[];
+  infos?: AssetInfo[];
 }
 
-function Overview ({ className }: Props): React.ReactElement<Props> {
-  const assetIds = useAssetIds();
-
+function Overview ({ className, ids, infos }: Props): React.ReactElement<Props> {
   return (
     <div className={className}>
-      <Summary numAssets={assetIds?.length} />
+      <Summary numAssets={ids?.length} />
       <Button.Group>
-        <Create assetIds={assetIds} />
+        <Create assetIds={ids} />
       </Button.Group>
-      <Assets assetIds={assetIds} />
+      <Assets infos={infos} />
     </div>
   );
 }
