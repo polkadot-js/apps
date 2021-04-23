eabf66acab5d03e1d82055a5d287bd03f6df5914
Jaco <jacogr@gmail.com>
2021-04-23 11:54:51 +0300

    Asset modal rework (min balance with decimals) (#5126)
    * Asset modal rework (min balance with decimals)
    
    * Language cleanups

--- packages/page-assets/src/Overview/Create/Create.tsx
+++ packages/page-assets/src/Overview/Create/Create.tsx
@@ -2,16 +2,15 @@
 // SPDX-License-Identifier: Apache-2.0
 
 import type { AssetId } from '@polkadot/types/interfaces';
-import type { InfoState, MetadataState, TeamState } from './types';
+import type { InfoState, TeamState } from './types';
 
 import React, { useMemo, useState } from 'react';
 
 import { Button, Modal, TxButton } from '@polkadot/react-components';
-import { useStepper, useTxBatch } from '@polkadot/react-hooks';
+import { useApi, useStepper, useTxBatch } from '@polkadot/react-hooks';
 
 import { useTranslation } from '../../translate';
 import Info from './Info';
-import Metadata from './Metadata';
 import Team from './Team';
 
 interface Props {
@@ -26,21 +25,43 @@ const BATCH_OPTIONS = {
 
 function Create ({ assetIds, className, onClose }: Props): React.ReactElement<Props> {
   const { t } = useTranslation();
+  const { api } = useApi();
   const [step, nextStep, prevStep] = useStepper();
   const [asset, setAsset] = useState<InfoState | null>(null);
-  const [metadata, setMetadata] = useState<MetadataState | null>(null);
   const [team, setTeam] = useState<TeamState | null>(null);
-  const txs = useMemo(() => asset && metadata && team && (
-    team.teamTx
-      ? [asset.createTx, metadata.metadataTx, team.teamTx]
-      : [asset.createTx, metadata.metadataTx]
-  ), [asset, metadata, team]);
+
+  const [createTx, metadataTx] = useMemo(
+    () => asset
+      ? [
+        api.tx.assets.create(asset.assetId, asset.accountId, asset.minBalance),
+        api.tx.assets.setMetadata(asset.assetId, asset.assetName, asset.assetSymbol, asset.assetDecimals)
+      ]
+      : [null, null],
+    [api, asset]
+  );
+
+  const teamTx = useMemo(
+    () => asset && team && (team.adminId !== asset.accountId || team.freezerId !== asset.accountId || team.issuerId !== asset.accountId)
+      ? api.tx.assets.setTeam(asset.assetId, team.issuerId, team.adminId, team.freezerId)
+      : null,
+    [api, asset, team]
+  );
+
+  const txs = useMemo(
+    () => createTx && metadataTx && team && (
+      teamTx
+        ? [createTx, metadataTx, teamTx]
+        : [createTx, metadataTx]
+    ),
+    [createTx, metadataTx, team, teamTx]
+  );
+
   const extrinsic = useTxBatch(txs, BATCH_OPTIONS);
 
   return (
     <Modal
       className={className}
-      header={t<string>('create asset {{step}}/{{steps}}', { replace: { step, steps: 3 } })}
+      header={t<string>('create asset {{step}}/{{steps}}', { replace: { step, steps: 2 } })}
       size='large'
     >
       {step === 1 && (
@@ -50,17 +71,9 @@ function Create ({ assetIds, className, onClose }: Props): React.ReactElement<Pr
           onChange={setAsset}
         />
       )}
-      {step === 2 && (
-        <Metadata
-          assetId={asset?.assetId}
-          defaultValue={metadata}
-          onChange={setMetadata}
-        />
-      )}
-      {step === 3 && (
+      {step === 2 && asset && (
         <Team
-          accountId={asset?.accountId}
-          assetId={asset?.assetId}
+          accountId={asset.accountId}
           defaultValue={team}
           onChange={setTeam}
         />
@@ -75,21 +88,6 @@ function Create ({ assetIds, className, onClose }: Props): React.ReactElement<Pr
           />
         }
         {step === 2 && (
-          <>
-            <Button
-              icon='step-backward'
-              label={t<string>('Prev')}
-              onClick={prevStep}
-            />
-            <Button
-              icon='step-forward'
-              isDisabled={!metadata}
-              label={t<string>('Next')}
-              onClick={nextStep}
-            />
-          </>
-        )}
-        {step === 3 && (
           <>
             <Button
               icon='step-backward'
