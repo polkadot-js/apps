f1c309c41e217fee44cf39e239d32211266f532e
dudleyneedham <dudleyneedham1993@gmail.com>
2021-04-23 15:06:14 +0200

    removed spelling mistake

--- packages/apps-config/src/api/spec/kilt.ts
+++ packages/apps-config/src/api/spec/kilt.ts
@@ -3,8 +3,6 @@
 
 import type { OverrideBundleDefinition } from '@polkadot/types/types';
 
-import { typeBundleForPolkadot } from '@kiltprotocol/types-definitions';
-// structs need to be in order
-/* eslint-disable sort-keys */
+import { typeBundleForPolkadot } from '@kiltprotocol/type-definitions';
 
-export default typeBundleForPolkadot as OverrideBundleDefinition;
+export default typeBundleForPolkadot as unknown as OverrideBundleDefinition;
