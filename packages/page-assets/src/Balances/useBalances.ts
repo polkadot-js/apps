d37db5257f8b9f940e172b9a909acd252562e4f4
Jaco <jacogr@gmail.com>
2021-04-23 22:51:53 +0300

    Update API (Token DispatchError) (#5133)
    * Update API (Token DispatchError)
    
    * Cleanups

--- packages/page-assets/src/Balances/useBalances.ts
+++ packages/page-assets/src/Balances/useBalances.ts
@@ -2,7 +2,6 @@
 // SPDX-License-Identifier: Apache-2.0
 
 import type { AssetBalance, AssetId } from '@polkadot/types/interfaces';
-import type { AssetInfoComplete } from '../types';
 
 import { useEffect, useState } from 'react';
 
@@ -14,32 +13,32 @@ interface Result {
 }
 
 const queryOptions = {
-  transform: ([[params], results]: [[[AssetId, string][]], AssetBalance[]]): Result[] =>
-    results
-      .map((balance, index) => ({
-        accountId: params[index][1],
-        balance
+  transform: ([[params], balances]: [[[AssetId, string][]], AssetBalance[]]): Result[] =>
+    params
+      .map(([, accountId], index) => ({
+        accountId,
+        balance: balances[index]
       }))
       .filter(({ balance: { balance } }) => !balance.isZero()),
   withParamsTransform: true
 };
 
-export default function useBalances (info?: AssetInfoComplete | null): Result[] | null {
+export default function useBalances (id?: AssetId | null): Result[] | null {
   const { api } = useApi();
   const { allAccounts } = useAccounts();
   const mountedRef = useIsMountedRef();
   const [state, setState] = useState<Result[] | null>(null);
-  const query = useCall(info && api.query.assets.account.multi, info && [allAccounts.map((a) => [info.id, a])], queryOptions);
+  const query = useCall(id && api.query.assets.account.multi, [allAccounts.map((a) => [id, a])], queryOptions);
 
   useEffect((): void => {
     if (mountedRef.current) {
       setState(null);
 
-      if (info) {
+      if (id) {
         // dummy
       }
     }
-  }, [info, mountedRef]);
+  }, [id, mountedRef]);
 
   useEffect((): void => {
     if (mountedRef.current && query) {
