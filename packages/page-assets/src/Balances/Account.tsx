a2e3cec4df1c372c08f1cc9b5f92d9f9ff3e2697
Jaco <jacogr@gmail.com>
2021-04-24 08:08:50 +0300

    Show minimum balance on asset mint/transfer (#5134)
    * Show minimum blanace on asset mint/transfer
    
    * Bump API

--- packages/page-assets/src/Balances/Account.tsx
+++ packages/page-assets/src/Balances/Account.tsx
@@ -1,7 +1,7 @@
 // Copyright 2017-2021 @polkadot/app-assets authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
-import type { AssetBalance, AssetId } from '@polkadot/types/interfaces';
+import type { AssetBalance, AssetId, TAssetBalance } from '@polkadot/types/interfaces';
 
 import React from 'react';
 
@@ -16,10 +16,11 @@ interface Props {
   assetId: AssetId;
   balance: AssetBalance;
   className?: string;
+  minBalance: TAssetBalance;
   siFormat: [number, string];
 }
 
-function Account ({ accountId, assetId, balance: { balance, isFrozen, isSufficient }, className, siFormat }: Props): React.ReactElement<Props> {
+function Account ({ accountId, assetId, balance: { balance, isFrozen, isSufficient }, className, minBalance, siFormat }: Props): React.ReactElement<Props> {
   const { t } = useTranslation();
 
   return (
@@ -43,6 +44,7 @@ function Account ({ accountId, assetId, balance: { balance, isFrozen, isSufficie
         <Transfer
           accountId={accountId}
           assetId={assetId}
+          minBalance={minBalance}
           siFormat={siFormat}
         />
       </td>
