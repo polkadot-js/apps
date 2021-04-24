c2265a75e4138a2b2873c5b4c99bf0e1dd52fb75
Jaco <jacogr@gmail.com>
2021-04-24 12:04:52 +0300

    Bump deps (#5136)

--- packages/page-assets/src/useAssetInfos.ts
+++ packages/page-assets/src/useAssetInfos.ts
@@ -5,7 +5,7 @@ import type { Option } from '@polkadot/types';
 import type { AccountId, AssetDetails, AssetId, AssetMetadata } from '@polkadot/types/interfaces';
 import type { AssetInfo } from './types';
 
-import { useMemo } from 'react';
+import { useEffect, useState } from 'react';
 
 import { useAccounts, useApi, useCall } from '@polkadot/react-hooks';
 
@@ -47,13 +47,18 @@ function extractInfo (allAccounts: string[], id: AssetId, optDetails: Option<Ass
 export default function useAssetInfos (ids?: AssetId[]): AssetInfo[] | undefined {
   const { api } = useApi();
   const { allAccounts } = useAccounts();
-  const allMetadata = useCall<AssetMetadata[]>(api.query.assets.metadata.multi, [ids]);
-  const allDetails = useCall<Option<AssetDetails>[]>(api.query.assets.asset.multi, [ids]);
+  const metadata = useCall<AssetMetadata[]>(api.query.assets.metadata.multi, [ids]);
+  const details = useCall<Option<AssetDetails>[]>(api.query.assets.asset.multi, [ids]);
+  const [state, setState] = useState<AssetInfo[] | undefined>();
 
-  return useMemo(
-    () => allDetails && allMetadata && ids && (allDetails.length === ids.length) && (allMetadata.length === ids.length)
-      ? ids.map((id, index) => extractInfo(allAccounts, id, allDetails[index], allMetadata[index]))
-      : undefined,
-    [allAccounts, allDetails, allMetadata, ids]
-  );
+  useEffect((): void => {
+    details && metadata && ids && (details.length === ids.length) && (metadata.length === ids.length) &&
+      setState(
+        ids.map((id, index) =>
+          extractInfo(allAccounts, id, details[index], metadata[index])
+        )
+      );
+  }, [allAccounts, details, ids, metadata]);
+
+  return state;
 }
