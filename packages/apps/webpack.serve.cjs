1aa27a8b663f9efccb0ff8bc2f297cce24ce58e4
Ubuntu <ubuntu@ip-172-31-52-152.ec2.internal>
2021-04-28 10:04:00 +0000

    Changed explorer page header

--- packages/apps/webpack.serve.cjs
+++ packages/apps/webpack.serve.cjs
@@ -19,7 +19,7 @@ module.exports = merge(
     },
     plugins: [
       new HtmlWebpackPlugin({
-        PAGE_TITLE: 'Polkadot/Substrate Portal',
+        PAGE_TITLE: 'Polygon Avail Block Explorer',
         inject: true,
         template: path.join(__dirname, 'public/index.html')
       }),
