9675bd07f17c1cbb5f68ecaa8426330c586c75b7
Hoon Kim <mail@hoonkim.me>
2021-05-07 21:41:38 +0900

    add AccountInfo type for Dusty Plasm (#5228)

--- packages/apps-config/src/api/spec/dusty.ts
+++ packages/apps-config/src/api/spec/dusty.ts
@@ -12,6 +12,7 @@ const definitions: OverrideBundleDefinition = {
       // on all versions
       minmax: [0, undefined],
       types: {
+        AccountInfo: 'AccountInfoWithProviders',
         AuthorityId: 'AccountId',
         AuthorityVote: 'u32',
         ChallengeGameOf: {
