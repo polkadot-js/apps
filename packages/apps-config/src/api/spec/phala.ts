7c3188addf18ea874486c277e9c555cd409ae025
h4x3rotab <h4x3rotab@gmail.com>
2021-05-14 17:12:26 +0000

    Add Khala Network Kusama paraid

--- packages/apps-config/src/api/spec/phala.ts
+++ packages/apps-config/src/api/spec/phala.ts
@@ -3,6 +3,8 @@
 
 import type { OverrideBundleDefinition } from '@polkadot/types/types';
 
+import { poc4 } from '@phala/typedefs';
+
 // structs need to be in order
 /* eslint-disable sort-keys */
 
@@ -11,34 +13,7 @@ const definitions: OverrideBundleDefinition = {
     {
       // on all versions
       minmax: [0, undefined],
-      types: {
-        EcdsaSignature: '[u8; 65]',
-        EthereumAddress: 'H160',
-        EthereumTxHash: 'H256',
-        MiningInfo: {
-          isMining: 'bool',
-          startBlock: 'Option<BlockNumber>'
-        },
-        PayoutPrefs: {
-          commission: 'u32',
-          target: 'AccountId'
-        },
-        Score: {
-          features: 'Vec<u32>',
-          overallScore: 'u32'
-        },
-        StashInfo: {
-          controller: 'AccountId',
-          payoutPrefs: 'PayoutPrefs'
-        },
-        WorkerInfo: {
-          machineId: 'Vec<u8>',
-          pubkey: 'Vec<u8>',
-          lastUpdated: 'u64',
-          score: 'Option<Score>',
-          status: 'i32'
-        }
-      }
+      types: poc4
     }
   ]
 };
