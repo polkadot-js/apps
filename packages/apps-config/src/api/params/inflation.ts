7b57ed2c32a170f472f103ade292a0cde89605f1
Jaco Greeff <jacogr@gmail.com>
2021-05-19 10:20:03 +0300

    Inflation adjustment (based on auctions)

--- packages/apps-config/src/api/params/inflation.ts
+++ packages/apps-config/src/api/params/inflation.ts
@@ -6,6 +6,8 @@ import type { ApiPromise } from '@polkadot/api';
 import { KUSAMA_GENESIS, POLKADOT_GENESIS } from '../constants';
 
 interface InflationParams {
+  auctionAdjust: number;
+  auctionMax: number;
   falloff: number;
   idealStake: number;
   maxInflation: number;
@@ -13,6 +15,8 @@ interface InflationParams {
 }
 
 const DEFAULT_PARAMS: InflationParams = {
+  auctionAdjust: 0,
+  auctionMax: 0,
   falloff: 0.05,
   idealStake: 0.5,
   maxInflation: 0.1,
@@ -20,7 +24,7 @@ const DEFAULT_PARAMS: InflationParams = {
 };
 
 const KNOWN_PARAMS: Record<string, InflationParams> = {
-  [KUSAMA_GENESIS]: { ...DEFAULT_PARAMS, idealStake: 0.75 },
+  [KUSAMA_GENESIS]: { ...DEFAULT_PARAMS, auctionAdjust: 0.3, auctionMax: 60, idealStake: 0.75 },
   [POLKADOT_GENESIS]: { ...DEFAULT_PARAMS, idealStake: 0.75 }
 };
 
--- packages/react-hooks/src/useInflation.ts
+++ packages/react-hooks/src/useInflation.ts
@@ -8,19 +8,20 @@ import BN from 'bn.js';
 import { useEffect, useState } from 'react';
 
 import { getInflationParams } from '@polkadot/apps-config';
-import { BN_MILLION } from '@polkadot/util';
+import { BN_MILLION, BN_ZERO } from '@polkadot/util';
 
 import { useApi } from './useApi';
 import { useCall } from './useCall';
 
 const EMPTY: Inflation = { inflation: 0, stakedReturn: 0 };
 
-export function calcInflation (api: ApiPromise, totalStaked: BN, totalIssuance: BN): Inflation {
-  const { falloff, idealStake, maxInflation, minInflation } = getInflationParams(api);
+export function calcInflation (api: ApiPromise, totalStaked: BN, totalIssuance: BN, numAuctions: BN): Inflation {
+  const { auctionAdjust, auctionMax, falloff, idealStake, maxInflation, minInflation } = getInflationParams(api);
   const stakedFraction = totalStaked.isZero() || totalIssuance.isZero()
     ? 0
     : totalStaked.mul(BN_MILLION).div(totalIssuance).toNumber() / BN_MILLION.toNumber();
-  const idealInterest = maxInflation / idealStake;
+  const auctionCalc = Math.min(auctionMax, numAuctions.toNumber()) * (auctionAdjust / auctionMax);
+  const idealInterest = maxInflation / (idealStake - auctionCalc);
   const inflation = 100 * (minInflation + (
     stakedFraction <= idealStake
       ? (stakedFraction * (idealInterest - (minInflation / idealStake)))
@@ -38,13 +39,18 @@ export function calcInflation (api: ApiPromise, totalStaked: BN, totalIssuance: 
 export function useInflation (totalStaked?: BN): Inflation {
   const { api } = useApi();
   const totalIssuance = useCall<BN>(api.query.balances?.totalIssuance);
+  const auctionCounter = useCall<BN>(api.query.auctions?.auctionCounter);
   const [state, setState] = useState<Inflation>(EMPTY);
 
   useEffect((): void => {
-    totalIssuance && totalStaked && setState(
-      calcInflation(api, totalStaked, totalIssuance)
+    const numAuctions = api.query.auctions
+      ? auctionCounter
+      : BN_ZERO;
+
+    numAuctions && totalIssuance && totalStaked && setState(
+      calcInflation(api, totalStaked, totalIssuance, numAuctions)
     );
-  }, [api, totalIssuance, totalStaked]);
+  }, [api, auctionCounter, totalIssuance, totalStaked]);
 
   return state;
 }
