212ae5fc14ab60fb3971a6a7c2207fcc4b01a74e
Antoine Estienne <estienne.antoine@gmail.com>
2021-05-26 16:56:22 +0000

    Update moonbeam runtime specNames (#5376)
    * update moonbeam runtime specNames
    
    * lint

--- packages/apps-config/src/settings/ethereumChains.ts
+++ packages/apps-config/src/settings/ethereumChains.ts
@@ -4,10 +4,8 @@
 // The list of Ethereum networks, for these the UI will default to Ethereum-only accounts
 
 export const ethereumChains = [
-  'node-moonbeam',
-  'moonbase-alphanet',
-  'moonbeam-alphanet',
-  'moonbeam-standalone',
+  'moonbase',
   'moonbeam',
+  'moonriver',
   'moonshadow'
 ];
