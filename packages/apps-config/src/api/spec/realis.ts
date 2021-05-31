75414d9f1a219d39970a33b8b9a230fd1708c2b6
Nikita <63471940+Daelon02@users.noreply.github.com>
2021-05-31 08:44:09 +0300

    Added new types for Realis.Network (#5393)
    * Update logo for Realis Network
    
    * Added new types for Realis.Network

--- packages/apps-config/src/api/spec/realis.ts
+++ packages/apps-config/src/api/spec/realis.ts
@@ -12,7 +12,6 @@ const definitions: OverrideBundleDefinition = {
       // on all versions
       minmax: [0, undefined],
       types: {
-        TokenId: 'u32',
         Rarity: {
           _enum: [
             'Common',
@@ -42,6 +41,85 @@ const definitions: OverrideBundleDefinition = {
           rarity: 'Rarity',
           socket: 'Socket',
           params: 'Params'
+        },
+        AttributeTransaction: {
+          signature: 'Signature',
+          name: 'Vec<u8>',
+          value: 'Vec<u8>',
+          validity: 'u32',
+          signer: 'AccountId',
+          identity: 'AccountId'
+        },
+        Attribute: {
+          name: 'Vec<u8>',
+          value: 'Vec<u8>',
+          validity: 'BlockNumber',
+          creation: 'Moment',
+          nonce: 'u64'
+        },
+        SocialTokenBalance: 'u128',
+        RegistrarIndex: 'u32',
+        Judgement: {
+          _enum: [
+            'Requested',
+            'Approved'
+          ]
+        },
+        Registration: {
+          judgements: 'Vec<JudgementItem>',
+          account_id: 'AccountId'
+        },
+        Bloom: 'H256',
+        Log: {
+          address: 'H160',
+          topics: 'Vec<H256>',
+          data: 'Bytes'
+        },
+        Receipt: {
+          state_root: 'H256',
+          used_gas: 'U256',
+          logs_bloom: 'Bloom',
+          logs: 'Vec<Log>'
+        },
+        TransactionAction: {
+          _enum: {
+            Call: 'H160',
+            Create: 'Null'
+          }
+        },
+        TransactionRecoveryId: 'u64',
+        TransactionSignature: {
+          v: 'TransactionRecoveryId',
+          r: 'H256',
+          s: 'H256'
+        },
+        Transaction: {
+          nonce: 'U256',
+          gas_price: 'U256',
+          gas_limit: 'U256',
+          action: 'TransactionAction',
+          value: 'U256',
+          input: 'Bytes',
+          signature: 'TransactionSignature'
+        },
+        TransactionStatus: {
+          transaction_hash: 'H256',
+          transaction_index: 'u32',
+          from: 'H160',
+          to: 'Option<H160>',
+          contract_address: 'Option<H160>',
+          logs: 'Vec<Log>',
+          logs_bloom: 'Bloom'
+        },
+        Id: 'AuthorityId',
+        ChainId: 'u8',
+        ResourceId: 'Vec<u8>',
+        ExchangeId: 'u64',
+        CurrencyOf: 'Balance',
+        TokenId: 'U256',
+        RealisTokenId: 'u32',
+        Erc721Token: {
+          id: 'NftId'
         }
       }
     }
