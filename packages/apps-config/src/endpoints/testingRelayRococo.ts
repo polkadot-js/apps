27c35fa10ac48112055e6e9edeb5929bd96548fb
chuoikhungbo <quanvantruong73@gmail.cp>
2021-06-05 15:22:53 +0700

    fix lint

--- packages/apps-config/src/endpoints/testingRelayRococo.ts
+++ packages/apps-config/src/endpoints/testingRelayRococo.ts
@@ -200,7 +200,7 @@ export function createRococo (t: TFunction): EndpointOption {
       {
         info: 'rococoHalongbay',
         paraId: 2018,
-        text: t('rpc.rococo.halongbay', 'Halongbay', {ns: 'app-config'}),
+        text: t('rpc.rococo.halongbay', 'Halongbay', { ns: 'app-config' }),
         providers: {
           Halongbay: 'wss://halongbay.polkafoundry.com'
         }
