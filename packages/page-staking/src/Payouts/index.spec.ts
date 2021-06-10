77372f1e1675e8918e20b378704ff4f0106a01db
Marcin Wachulski <marcin@ethworks.io>
2021-06-10 12:01:33 +0200

    Payouts -> 'My validators' all 4/4 tests passing

--- packages/page-staking/src/Payouts/index.spec.ts
+++ packages/page-staking/src/Payouts/index.spec.ts
@@ -1,14 +1,13 @@
 // Copyright 2017-2021 @polkadot/app-staking authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
-import {ApiPromise} from '@polkadot/api';
 import i18next from '@polkadot/react-components/i18n';
-import {createAugmentedApi} from '@polkadot/test-support/api';
 import {mockHooks} from '@polkadot/test-support/hooks/mockHooks';
 import {MemoryStore} from '@polkadot/test-support/keyring';
 import {keyring} from '@polkadot/ui-keyring';
 
 import {PayoutsPage} from '../../test/pages/payoutsPage';
+import {StakerState} from "@polkadot/react-hooks/types";
 
 jest.mock('@polkadot/react-hooks/useTreasury', () => ({
   useTreasury: () => mockHooks.treasury
@@ -22,46 +21,56 @@ jest.mock('@polkadot/react-hooks/useBlockTime', () => ({
   useBlockTime: () => mockHooks.blockTime
 }));
 
-let augmentedApi: ApiPromise;
-
 describe('Payouts', () => {
   let payoutsPage: PayoutsPage;
+  const someOwnedValidator: StakerState = {
+    controllerId: 'someId',
+    hexSessionIdNext: '0x1112',
+    hexSessionIdQueue: 'sth',
+    isLoading: false,
+    isOwnController: false,
+    isOwnStash: false,
+    isStashNominating: false,
+    isStashValidating: false,
+    sessionIds: [],
+    stashId: 'someStashId'
+  }
 
   beforeAll(async () => {
     await i18next.changeLanguage('en');
     keyring.loadAll({isDevelopment: true, store: new MemoryStore()});
-    augmentedApi = createAugmentedApi();
   });
 
   beforeEach(() => {
-    payoutsPage = new PayoutsPage(augmentedApi);
+    payoutsPage = new PayoutsPage();
   });
 
   describe('empty view', () => {
     it('renders `My validators/stashes` toggle', async () => {
-      payoutsPage.renderMany();
+      payoutsPage.renderPage();
 
       await payoutsPage.expectText('My validators');
       await payoutsPage.expectText('My stashes');
     });
 
     it('when no own validators, then `My validators` is disabled and `My stashes` selected', async () => {
-      payoutsPage.renderMany();
+      payoutsPage.renderPage();
 
       await payoutsPage.expectMyValidators({isDisabled: true, isSelected: false});
     });
 
     it('when some own validators, then `My validators` is enabled and selected', async () => {
-      payoutsPage.renderMany();
+      payoutsPage.renderPage([someOwnedValidator]);
 
       await payoutsPage.expectMyValidators({isDisabled: false, isSelected: true});
     });
 
     it('when some own validators, but after accounts load and rerender, then `My validators` is selected',
       async () => {
-        payoutsPage.renderMany();
-
+        payoutsPage.renderPage();
+        await payoutsPage.expectMyValidators({isDisabled: true, isSelected: false});
+        payoutsPage.renderPage([someOwnedValidator])
         await payoutsPage.expectMyValidators({isDisabled: false, isSelected: true});
-    });
+      });
   });
 });
--- packages/page-staking/test/pages/payoutsPage.tsx
+++ packages/page-staking/test/pages/payoutsPage.tsx
@@ -1,35 +1,27 @@
 // Copyright 2017-2021 @polkadot/app-staking authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
-import { render } from '@testing-library/react';
-import React, { Suspense } from 'react';
-import { MemoryRouter } from 'react-router-dom';
-import { ThemeProvider } from 'styled-components';
+import {render,RenderResult} from '@testing-library/react';
+import React, {Suspense} from 'react';
+import {MemoryRouter} from 'react-router-dom';
+import {ThemeProvider} from 'styled-components';
 
-import { ApiPromise } from '@polkadot/api';
-import { DeriveCollectiveProposal } from '@polkadot/api-derive/types';
-import { BountyApi } from '@polkadot/app-bounties/hooks';
-import { lightTheme } from '@polkadot/apps/themes';
-import { POLKADOT_GENESIS } from '@polkadot/apps-config';
-import { ApiContext } from '@polkadot/react-api';
-import { ApiProps } from '@polkadot/react-api/types';
-import { QueueProvider } from '@polkadot/react-components/Status/Context';
-import { PartialQueueTxExtrinsic, QueueProps, QueueTxExtrinsicAdd } from '@polkadot/react-components/Status/types';
-import { BountyFactory } from '@polkadot/test-support/creation/bounties/bountyFactory';
-import { TypeRegistry } from '@polkadot/types/create';
-import { Bounty, BountyIndex, BountyStatus } from '@polkadot/types/interfaces';
+import {lightTheme} from '@polkadot/apps/themes';
+import {POLKADOT_GENESIS} from '@polkadot/apps-config';
+import {ApiContext} from '@polkadot/react-api';
+import {ApiProps} from '@polkadot/react-api/types';
+import {QueueProvider} from '@polkadot/react-components/Status/Context';
+import {PartialQueueTxExtrinsic, QueueProps, QueueTxExtrinsicAdd} from '@polkadot/react-components/Status/types';
+import {TypeRegistry} from '@polkadot/types/create';
 
 import Payouts from '@polkadot/app-staking/Payouts';
 import BN from "bn.js";
 import registry from "@polkadot/react-api/typeRegistry";
+import {StakerState} from "@polkadot/react-hooks/types";
 
 function aGenesisHash () {
   return new TypeRegistry().createType('Hash', POLKADOT_GENESIS);
 }
-
-type FindOne = (match: string) => Promise<HTMLElement>;
-type FindManyWithMatcher = (match: string | ((match: string) => boolean)) => Promise<HTMLElement[]>
-type GetMany = (match: string) => HTMLElement[];
 
 class NotYetRendered extends Error {
 
@@ -38,51 +30,14 @@ class NotYetRendered extends Error {
 let queueExtrinsic: (value: PartialQueueTxExtrinsic) => void;
 const propose = jest.fn().mockReturnValue('mockProposeExtrinsic');
 
-interface RenderedPayoutsPage {
-  findAllByTestId: FindManyWithMatcher;
-  findByText: FindOne;
-  findByRole: FindOne;
-  findByTestId: FindOne;
-  getAllByRole: GetMany;
-  queryAllByText: GetMany;
-}
-
 export class PayoutsPage {
-  aBounty: ({ status, value }?: Partial<Bounty>) => Bounty;
-  aBountyIndex: (index?: number) => BountyIndex;
-  aBountyStatus: (status: string) => BountyStatus;
-  bountyStatusWith: ({ curator, status }: { curator?: string, status?: string, }) => BountyStatus;
-  bountyWith: ({ status, value }: { status?: string, value?: number }) => Bounty;
+  renderResult?: RenderResult;
 
-  findByRole?: FindOne;
-  findByText?: FindOne;
-  findByTestId?: FindOne;
-  getAllByRole?: GetMany;
-  findAllByTestId?: FindManyWithMatcher;
-  queryAllByText?: GetMany;
-
-  constructor (api: ApiPromise) {
-    ({ aBounty: this.aBounty, aBountyIndex: this.aBountyIndex, aBountyStatus: this.aBountyStatus, bountyStatusWith: this.bountyStatusWith, bountyWith: this.bountyWith } = new BountyFactory(api));
+  renderPage (ownValidators: StakerState[] = []) {
+    this.renderPayouts(ownValidators);
   }
 
-  renderOne (bounty: Bounty, proposals: DeriveCollectiveProposal[] = [], description = '', index = this.aBountyIndex()): RenderedPayoutsPage {
-    return this.renderMany({ bounties: [{ bounty, description, index, proposals }] });
-  }
-
-  renderMany (bountyApi: Partial<BountyApi> = {}, { balance = 1 } = {}): RenderedPayoutsPage {
-    const { findAllByTestId, findByRole, findByTestId, findByText, getAllByRole, queryAllByText } = this.renderPayouts();
-
-    this.findByRole = findByRole;
-    this.findByText = findByText;
-    this.findByTestId = findByTestId;
-    this.getAllByRole = getAllByRole;
-    this.findAllByTestId = findAllByTestId;
-    this.queryAllByText = queryAllByText;
-
-    return { findAllByTestId, findByRole, findByTestId, findByText, getAllByRole, queryAllByText };
-  }
-
-  private renderPayouts () {
+  private renderPayouts (ownValidators: StakerState[]) {
     const mockApi: ApiProps = {
       api: {
         consts: {
@@ -129,38 +84,46 @@ export class PayoutsPage {
       queueExtrinsic
     } as QueueProps;
 
-    return render(
-      <>
-        <div id='tooltips'/>
-        <Suspense fallback='...'>
-          <QueueProvider value={queue}>
-            <MemoryRouter>
-              <ThemeProvider theme={lightTheme}>
-                <ApiContext.Provider value={mockApi}>
-                  <Payouts ownValidators={[]}/>
-                </ApiContext.Provider>
-              </ThemeProvider>
-            </MemoryRouter>
-          </QueueProvider>
-        </Suspense>
-      </>
-    );
+    let content = <>
+      <div id='tooltips'/>
+      <Suspense fallback='...'>
+        <QueueProvider value={queue}>
+          <MemoryRouter>
+            <ThemeProvider theme={lightTheme}>
+              <ApiContext.Provider value={mockApi}>
+                <Payouts ownValidators={ownValidators}/>
+              </ApiContext.Provider>
+            </ThemeProvider>
+          </MemoryRouter>
+        </QueueProvider>
+      </Suspense>
+    </>;
+
+    if (!this.hasBeenRendered()) {
+      this.renderResult = render(content);
+    } else {
+      this.renderResult!.rerender(content);
+    }
   }
 
-  private assertRendered (): asserts this is RenderedPayoutsPage {
-    if (this.findByText === undefined) {
+  private hasBeenRendered () {
+    return this.renderResult !== undefined;
+  }
+
+  private assertRendered () {
+    if (!this.hasBeenRendered()) {
       throw new NotYetRendered();
     }
   }
 
   async expectText (expected: string): Promise<void> {
     this.assertRendered();
-    expect(await this.findByText(expected)).toBeTruthy();
+    expect(await this.renderResult!.findByText(expected)).toBeTruthy();
   }
 
   async expectMyValidators (expected: {isDisabled: boolean, isSelected: boolean}): Promise<void> {
     this.assertRendered();
-    const validators = await this.findByText('My validators') as HTMLButtonElement;
+    const validators = await this.renderResult!.findByText('My validators') as HTMLButtonElement;
     expect(validators).toBeVisible();
 
     if (expected.isDisabled)
