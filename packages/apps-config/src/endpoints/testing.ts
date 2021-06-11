3c9800de43f4b86c842182733f7def6354227ae9
billjhlee <billjhlee@gmail.com>
2021-06-11 14:26:43 +0900

    Update testing.ts

--- packages/apps-config/src/endpoints/testing.ts
+++ packages/apps-config/src/endpoints/testing.ts
@@ -264,7 +264,7 @@ export function createTesting (t: TFunction, firstOnly?: boolean): LinkOption[] 
       info: 'opportunity',
       text: t('rpc.test.opportunity', 'Opportunity', { ns: 'apps-config' }),
       providers: {
-        Opportunity: 'wss://rpc.opportunity.standard.tech',
+        Opportunity: 'wss://rpc.opportunity.standard.tech'
       }
     },
     {
