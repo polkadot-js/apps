91e0e391404de086acd8d7e54b62c64b81a1cccc
Jaco <jacogr@gmail.com>
2021-06-14 08:35:00 +0300

    Remove unreachable endpoints (#5582)

--- packages/apps-config/src/endpoints/testingRelayWestend.ts
+++ packages/apps-config/src/endpoints/testingRelayWestend.ts
@@ -23,7 +23,7 @@ export function createWestend (t: TFunction): EndpointOption {
     text: t('rpc.westend', 'Westend', { ns: 'apps-config' }),
     providers: {
       Parity: 'wss://westend-rpc.polkadot.io',
-      'NodeFactory(Vedran)': 'wss://westend.vedran.nodefactory.io/ws',
+      // 'NodeFactory(Vedran)': 'wss://westend.vedran.nodefactory.io/ws', // https://github.com/polkadot-js/apps/issues/5580
       'Patract Elara': 'wss://westend.elara.patract.io',
       OnFinality: 'wss://westend.api.onfinality.io/public-ws'
     },
