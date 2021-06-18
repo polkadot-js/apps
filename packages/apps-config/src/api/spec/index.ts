5930aceb51c233ec4e5e7efd45a9385fc854c64b
Raymond Zhong <raykyri@users.noreply.github.com>
2021-06-17 21:59:44 -0700

    Add types for beresheet runtime (#5576)
    * Add types for beresheet runtime
    
    * Import beresheet types to spec listing

--- packages/apps-config/src/api/spec/index.ts
+++ packages/apps-config/src/api/spec/index.ts
@@ -6,6 +6,7 @@ import type { OverrideBundleDefinition } from '@polkadot/types/types';
 import acala from './acala';
 import apron from './apron';
 import aresParachain from './ares-parachain';
+import beresheet from './beresheet';
 import bifrost from './bifrost';
 import bifrostParachain from './bifrost-parachain';
 import bitcountry from './bitcountry';
@@ -109,6 +110,7 @@ const spec: Record<string, OverrideBundleDefinition> = {
   apron,
   'ares-parachain': aresParachain,
   basilisk: hydrate,
+  beresheet,
   bifrost: bifrost,
   'bifrost-parachain': bifrostParachain,
   'bitcountry-node': bitcountry,
