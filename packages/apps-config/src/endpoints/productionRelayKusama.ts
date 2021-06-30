28619b998fd62ce90b2fc162b0ac3043f66dc904
Miguel Hervas <miguel.hervas.lazaro@gmail.com>
2021-06-30 14:38:11 +0100

    link to altair site

--- packages/apps-config/src/endpoints/productionRelayKusama.ts
+++ packages/apps-config/src/endpoints/productionRelayKusama.ts
@@ -46,7 +46,7 @@ export function createKusama (t: TFunction): EndpointOption {
       // NOTE: Added alphabetical based on chain name
       {
         info: 'altair',
-        homepage: 'https://centrifuge.io/',
+        homepage: 'https://centrifuge.io/altair',
         paraId: 2021,
         text: t('rpc.kusama.altair', 'Altair', { ns: 'apps-config' }),
         providers: {
