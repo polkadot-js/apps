8629c7263591702a69d5fae03a86092ae97aefe1
Jaco Greeff <jacogr@gmail.com>
2021-07-02 07:53:52 +0300

    Unused type import

--- packages/page-tech-comm/src/App.tsx
+++ packages/page-tech-comm/src/App.tsx
@@ -1,7 +1,6 @@
 // Copyright 2017-2021 @polkadot/app-tech-comm authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
-import type { Option } from '@polkadot/types';
 import type { AccountId, Hash } from '@polkadot/types/interfaces';
 
 import React, { useMemo } from 'react';
