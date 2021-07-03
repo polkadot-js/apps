91d34c81b37864fe49798a8efc8ba3bebcbc638b
drewstone <drewstone329@gmail.com>
2021-07-03 15:02:24 -0400

    Update production.ts (#5757)
    @raykyri double check this is the right load-balanced RPC.

--- packages/apps-config/src/endpoints/production.ts
+++ packages/apps-config/src/endpoints/production.ts
@@ -61,7 +61,7 @@ export function createProduction (t: TFunction, firstOnly?: boolean): LinkOption
       info: 'edgeware',
       text: t('rpc.prod.edgeware', 'Edgeware', { ns: 'apps-config' }),
       providers: {
-        'Commonwealth Labs': 'wss://mainnet1.edgewa.re',
+        'Commonwealth Labs': 'wss://mainnet.edgewa.re',
         'Patract Elara': 'wss://edgeware.elara.patract.io',
         OnFinality: 'wss://edgeware.api.onfinality.io/public-ws'
       }
