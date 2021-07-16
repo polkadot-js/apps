aa6ea3a48631ac288defb63b33ece93da791b078
Jaco <jacogr@gmail.com>
2021-07-16 12:33:03 +0300

    Rework API urls (#5840)
    * Rework API urls
    
    * Adjust
    
    * Fix build

--- packages/apps/src/initSettings.ts
+++ packages/apps/src/initSettings.ts
@@ -7,18 +7,17 @@ import store from 'store';
 import { createWsEndpoints } from '@polkadot/apps-config';
 import { extractIpfsDetails } from '@polkadot/react-hooks/useIpfs';
 import { settings } from '@polkadot/ui-settings';
-import { Endpoint } from '@polkadot/ui-settings/types';
 import { assert } from '@polkadot/util';
 
-function networkOrUrl (apiType: Endpoint): void {
-  if (apiType.type === 'json-rpc') {
-    console.log('WS endpoint=', apiType.param);
-  } else if (apiType.type === 'substrate-connect') {
-    console.log('Chain of light client is =', apiType.param);
+function networkOrUrl (apiUrl: string): void {
+  if (apiUrl.startsWith('light://')) {
+    console.log('Light endpoint=', apiUrl.replace('light://', ''));
+  } else {
+    console.log('WS endpoint=', apiUrl);
   }
 }
 
-function getApiType (): Endpoint {
+function getApiUrl (): string {
   // we split here so that both these forms are allowed
   //  - http://localhost:3000/?rpc=wss://substrate-rpc.parity.io/#/explorer
   //  - http://localhost:3000/#/explorer?rpc=wss://substrate-rpc.parity.io
@@ -31,17 +30,9 @@ function getApiType (): Endpoint {
     // https://polkadot.js.org/apps/?rpc=ws://127.0.0.1:9944#/explorer;
     const url = decodeURIComponent(urlOptions.rpc.split('#')[0]);
 
-    assert(url.startsWith('ws://') || url.startsWith('wss://'), 'Non-prefixed ws/wss url');
+    assert(url.startsWith('ws://') || url.startsWith('wss://') || url.startsWith('light://'), 'Non-prefixed ws/wss/light url');
 
-    return { param: url, type: 'json-rpc' };
-  } else if (urlOptions.sc) {
-    assert(!Array.isArray(urlOptions.sc), 'Invalid network specified');
-
-    // https://polkadot.js.org/apps/?sc=kusama#/explorer;
-    const network = decodeURIComponent(urlOptions.sc.split('#')[0]);
-    const chain = network.split('-')[0];
-
-    return { param: chain, type: 'substrate-connect' };
+    return url;
   }
 
   const endpoints = createWsEndpoints(<T = string>(): T => ('' as unknown as T));
@@ -52,7 +43,7 @@ function getApiType (): Endpoint {
     const option = endpoints.find(({ dnslink }) => dnslink === ipnsChain);
 
     if (option) {
-      return { param: option.value, type: 'json-rpc' };
+      return option.value;
     }
   }
 
@@ -60,17 +51,17 @@ function getApiType (): Endpoint {
   const fallbackUrl = endpoints.find(({ value }) => !!value);
 
   // via settings, or the default chain
-  return [stored.apiType, process.env.WS_URL].includes(settings.apiType)
-    ? settings.apiType // keep as-is
+  return [stored.apiUrl, process.env.WS_URL].includes(settings.apiUrl)
+    ? settings.apiUrl // keep as-is
     : fallbackUrl
-      ? { param: fallbackUrl.value, type: 'json-rpc' } // grab the fallback
-      : { param: 'ws://127.0.0.1:9944', type: 'json-rpc' }; // nothing found, go local
+      ? fallbackUrl.value // grab the fallback
+      : 'ws://127.0.0.1:9944'; // nothing found, go local
 }
 
 // There cannot be a Substrate Connect light client default (expect only jrpc EndpointType)
-const apiType = getApiType();
+const apiUrl = getApiUrl();
 
 // set the default as retrieved here
-settings.set({ apiType });
+settings.set({ apiUrl });
 
-networkOrUrl(apiType);
+networkOrUrl(apiUrl);
