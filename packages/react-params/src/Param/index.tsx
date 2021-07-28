127c71aa908d5d34d135c9c2985eeed2d3999b3b
Jaco <jacogr@gmail.com>
2021-07-28 17:30:38 +0300

    Bump API (#5886)

--- packages/react-params/src/Param/index.tsx
+++ packages/react-params/src/Param/index.tsx
@@ -19,9 +19,9 @@ function Param ({ className = '', defaultValue, isDisabled, isInOption, isOption
 
   const label = useMemo(
     () => isUndefined(name)
-      ? `${isDisabled && isInOption ? 'Option<' : ''}${encodeTypeDef(type)}${isDisabled && isInOption ? '>' : ''}`
-      : `${name}: ${isDisabled && isInOption ? 'Option<' : ''}${encodeTypeDef(type)}${isDisabled && isInOption ? '>' : ''}`,
-    [isDisabled, isInOption, name, type]
+      ? `${isDisabled && isInOption ? 'Option<' : ''}${encodeTypeDef(registry, type)}${isDisabled && isInOption ? '>' : ''}`
+      : `${name}: ${isDisabled && isInOption ? 'Option<' : ''}${encodeTypeDef(registry, type)}${isDisabled && isInOption ? '>' : ''}`,
+    [isDisabled, isInOption, name, registry, type]
   );
 
   if (!Component) {
