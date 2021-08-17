f8892a87aac9d123a428944d13d7ac4018db6c49
MiZiet <michalzietalak@o2.pl>
2021-08-17 10:33:58 +0200

    Add useCallback in useScroll

--- packages/react-hooks/src/useScroll.ts
+++ packages/react-hooks/src/useScroll.ts
@@ -1,24 +1,22 @@
 // Copyright 2017-2021 @polkadot/react-hooks authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 
 export function useScroll (): number {
   const [scrollY, setScrollY] = useState(0);
 
-  function logit () {
-    setScrollY(window.pageYOffset);
-  }
+  const setYOffset = useCallback((): void => setScrollY(window.pageYOffset), []);
 
   useEffect(() => {
     function watchScroll () {
-      window.addEventListener('scroll', logit);
+      window.addEventListener('scroll', setYOffset);
     }
 
     watchScroll();
 
     return () => {
-      window.removeEventListener('scroll', logit);
+      window.removeEventListener('scroll', setYOffset);
     };
   }, []);
 
