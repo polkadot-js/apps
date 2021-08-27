9e7062420f9b9757a4684bbaed3da50beff7a027
Yongfeng LI <wliyongfeng@gmail.com>
2021-08-27 13:42:27 +0800

    Add dotreasury to external links

--- packages/apps-config/src/links/index.ts
+++ packages/apps-config/src/links/index.ts
@@ -4,6 +4,7 @@
 import type { ExternalDef } from './types';
 
 import Commonwealth from './commonwealth';
+import Dotreasury from './dotreasury';
 import DotScanner from './dotscanner';
 import Polkascan from './polkascan';
 import Polkassembly from './polkassembly';
@@ -13,6 +14,7 @@ import Subscan from './subscan';
 
 export const externalLinks: Record<string, ExternalDef> = {
   Commonwealth,
+  Dotreasury,
   DotScanner,
   Polkascan,
   Polkassembly,
