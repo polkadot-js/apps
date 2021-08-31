ecc056a31032db97dfc0e35468077d3fe6c3d7f2
Cheng JIANG <alex_cj96@foxmail.com>
2021-08-31 15:15:52 +0800

    make heiko reachable (#6029)
    * make heiko reachable
    
    * fix endpoint text order

--- packages/apps-config/src/endpoints/productionRelayKusama.ts
+++ packages/apps-config/src/endpoints/productionRelayKusama.ts
@@ -199,7 +199,6 @@ export function createKusama (t: TFunction): EndpointOption {
       {
         info: 'heiko',
         homepage: 'https://parallel.fi',
-        isUnreachable: true,
         paraId: 2085,
         text: t('rpc.kusama.heiko', 'Parallel Heiko', { ns: 'apps-config' }),
         providers: {
