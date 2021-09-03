4c725133e8370d138f410918b9e0068ff9b923b4
Jaco Greeff <jacogr@gmail.com>
2021-09-03 10:59:58 +0300

    Adjust Westend weights

--- packages/apps-config/src/api/params/teleport.ts
+++ packages/apps-config/src/api/params/teleport.ts
@@ -3,16 +3,17 @@
 
 import type { ApiPromise } from '@polkadot/api';
 
-import { KUSAMA_GENESIS } from '../constants';
+import { KUSAMA_GENESIS, WESTEND_GENESIS } from '../constants';
 
 // 3 * BaseXcmWeight on Kusama
 const KUSAMA_WEIGHT = 3 * 1_000_000_000;
 
-// On Rococo and Westend this is lower, but non-failing with the higher Kusama weights
+// On Rococo this is lower (3 * 100_000), but non-failing with the higher Kusama weights
 const DEFAULT_WEIGHT = KUSAMA_WEIGHT;
 
 const KNOWN_WEIGHTS: Record<string, number> = {
-  [KUSAMA_GENESIS]: KUSAMA_WEIGHT
+  [KUSAMA_GENESIS]: KUSAMA_WEIGHT,
+  [WESTEND_GENESIS]: 3 * 10_000_000
 };
 
 export function getTeleportWeight (api: ApiPromise): number {
