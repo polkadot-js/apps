e10a75aa006b1380b1061e218e3d21ba6397601b
Joakim Nyman <Joakim.nyman86@gmail.com>
2021-09-14 07:33:39 +0200

    Added Kusama RPC endpoint, hosted by Dwellir (#6083)

--- packages/apps-config/src/endpoints/productionRelayKusama.ts
+++ packages/apps-config/src/endpoints/productionRelayKusama.ts
@@ -23,7 +23,7 @@ export function createKusama (t: TFunction): EndpointOption {
       Parity: 'wss://kusama-rpc.polkadot.io',
       OnFinality: 'wss://kusama.api.onfinality.io/public-ws',
       'Patract Elara': 'wss://pub.elara.patract.io/kusama',
-      // Dwellir: 'wss://kusama-rpc.dwellir.com',
+      Dwellir: 'wss://kusama-rpc.dwellir.com',
       'light client': 'light://substrate-connect/kusama'
       // Pinknode: 'wss://rpc.pinknode.io/kusama/explorer' // https://github.com/polkadot-js/apps/issues/5721
     },
