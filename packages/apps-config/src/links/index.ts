d53b12ffd0ee020a00364eb6b592ca00e2c06e53
olehmell <oleh.melnechyk@gmail.com>
2021-09-16 13:36:47 +0300

    Revert "Change order"
    This reverts commit 2af96cc1153c07085ddc4b7d0cac94b2bf813b5a.

--- packages/apps-config/src/links/index.ts
+++ packages/apps-config/src/links/index.ts
@@ -21,8 +21,7 @@ export const externalLinks: Record<string, ExternalDef> = {
   PolkassemblyIo,
   PolkassemblyNetwork,
   Polkastats,
-  SubId,
-  // eslint-disable-next-line sort-keys
   Statescan,
+  SubId,
   Subscan
 };
