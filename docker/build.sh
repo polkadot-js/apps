ed1d224950350199ff574930bcd9b385ce8c40bd
filip <fbielejec@gmail.com>
2021-10-05 11:17:31 +0200

    docker build script

--- docker/build.sh
+++ docker/build.sh
@@ -7,27 +7,26 @@
 set -e
 
 # the docker image name and dockerhub repo
-NAME="polkadot-js-apps"
-REPO="jacogr"
+NAME="aleph-wallet"
+ECR_REGISTRY=public.ecr.aws/x2t8a1o3
+
+# -- BUIDL
 
 # extract the current npm version from package.json
 VERSION=$(cat package.json \
-  | grep version \
-  | head -1 \
-  | awk -F: '{ print $2 }' \
-  | sed 's/[",]//g' \
-  | sed 's/ //g')
+            | grep version \
+            | head -1 \
+            | awk -F: '{ print $2 }' \
+            | sed 's/[",]//g' \
+            | sed 's/ //g')
 
-echo "*** Building $NAME"
-docker build -t $NAME -f docker/Dockerfile .
+echo "*** Buidling $NAME"
+docker build -t $NAME:latest -f docker/Dockerfile .
 
-docker login -u $REPO -p $DOCKER_PASS
+docker tag aleph-node:latest $ECR_REGISTRY/$NAME:latest
+docker tag aleph-node:latest $ECR_REGISTRY/$NAME:$VERSION
+aws ecr-public get-login-password --region us-east-1 | docker login --username AWS --password-stdin $ECR_REGISTRY
+echo "*** Pushing to $ECR_REGISTRY/$NAME"
+docker push --all-tags $ECR_REGISTRY/$NAME
 
-echo "*** Tagging $REPO/$NAME"
-if [[ $VERSION != *"beta"* ]]; then
-  docker tag $NAME $REPO/$NAME:$VERSION
-fi
-docker tag $NAME $REPO/$NAME
-
-echo "*** Publishing $NAME"
-docker push $REPO/$NAME
+exit $?
