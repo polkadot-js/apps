4113b16e0bbbaccd6acf503f26abf55f780d3e47
Krzysztof Jelski <krzysztof@ethworks.io>
2021-10-06 16:00:16 +0200

    Restore the Westend interBTC chain

--- packages/apps-config/src/endpoints/testingRelayWestend.ts
+++ packages/apps-config/src/endpoints/testingRelayWestend.ts
@@ -72,6 +72,14 @@ export function createWestend (t: TFunction): EndpointOption {
         }
       },
       {
+        info: 'interBTC',
+        paraId: 2094,
+        text: t('rpc.westend.interbtc', 'InterBTC', { ns: 'apps-config' }),
+        providers: {
+          Interlay: 'wss://api-westend.interlay.io/parachain'
+        }
+      },
+      {
         info: 'moonshadow',
         isUnreachable: true, // https://github.com/polkadot-js/apps/issues/6181
         paraId: 2002,
