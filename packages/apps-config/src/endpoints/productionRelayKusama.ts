c335ede5d0e39f97e8a17ffe245b261e77deba40
Dominik Harz <dominik@interlay.io>
2021-10-13 17:52:59 +0200

    fix: kintsugi labs hosted parachain URL (#6280)

--- packages/apps-config/src/endpoints/productionRelayKusama.ts
+++ packages/apps-config/src/endpoints/productionRelayKusama.ts
@@ -164,12 +164,11 @@ export function createKusama (t: TFunction): EndpointOption {
       },
       {
         info: 'kintsugi',
-        isUnreachable: true, // https://github.com/polkadot-js/apps/issues/6101
         homepage: 'https://kintsugi.interlay.io/',
         paraId: 2092,
         text: t('rpc.kusama.kintsugi', 'Kintsugi BTC', { ns: 'apps-config' }),
         providers: {
-          'Kintsugi Labs': 'wss://api-kin.interlay.io/parachain',
+          'Kintsugi Labs': 'wss://api-kusama.interlay.io/parachain',
           OnFinality: 'wss://kintsugi.api.onfinality.io/public-ws'
         }
       },
