9e2e4a48ead1a25710b2234a3ee3a1d9529d2978
Hoon Kim <mail@hoonkim.me>
2021-10-14 06:17:14 +0200

    add dapps staking custom types to shiden network (#6287)
    * add dapps staking type
    
    * remove unused type
    
    * add era index

--- packages/apps-config/src/api/spec/shiden.ts
+++ packages/apps-config/src/api/spec/shiden.ts
@@ -13,9 +13,27 @@ const definitions: OverrideBundleDefinition = {
       minmax: [0, undefined],
       types: {
         Keys: 'AccountId',
+        Address: 'MultiAddress',
+        LookupSource: 'MultiAddress',
+        AmountOf: 'Amount',
+        Amount: 'i128',
         SmartContract: {
-          _enum: ['H160', 'AccountId']
-        }
+          _enum: {
+            Evm: 'H160',
+            Wasm: 'AccountId'
+          }
+        },
+        EraStakingPoints: {
+          total: 'Balance',
+          stakers: 'BTreeMap<AccountId, Balance>',
+          formerStakedEra: 'EraIndex',
+          claimedRewards: 'Balance'
+        },
+        EraRewardAndStake: {
+          rewards: 'Balance',
+          staked: 'Balance'
+        },
+        EraIndex: 'u32'
       }
     }
   ]
