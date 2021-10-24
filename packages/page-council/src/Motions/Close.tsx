051c0a7b70f45c20fe340608bde99164b8a3799f
Jaco <jacogr@gmail.com>
2021-10-24 17:25:20 +0300

    Require proposalWeight/Length for close

--- packages/page-council/src/Motions/Close.tsx
+++ packages/page-council/src/Motions/Close.tsx
@@ -57,6 +57,7 @@ function Close ({ hasFailed, hash, idNumber, proposal }: Props): React.ReactElem
           <Modal.Actions>
             <TxButton
               accountId={accountId}
+              isDisabled={!hasFailed && !proposalLength}
               onStart={toggleOpen}
               params={
                 api.tx[modLocation].close.meta.args.length === 4
