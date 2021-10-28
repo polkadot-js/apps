694c425366474b67e0bd461ae9e7947931729559
Satish Mohan <satish@dhiway.com>
2021-10-28 13:33:07 +0530

    adding cord favicon

--- packages/apps/webpack.config.cjs
+++ packages/apps/webpack.config.cjs
@@ -17,7 +17,7 @@ module.exports = merge(
     devtool: process.env.BUILD_ANALYZE ? 'source-map' : false,
     plugins: [
       new HtmlWebpackPlugin({
-        PAGE_TITLE: 'Polkadot/Substrate Portal',
+        PAGE_TITLE: 'CORD Apps Portal',
         inject: true,
         template: path.join(context, `${hasPublic ? 'public/' : ''}index.html`)
       })
