094a94449f65ff92e196c327d504362fa5c5f180
Jaco <jacogr@gmail.com>
2021-10-30 10:30:11 +0200

    Adjust error stack formatting (#6448)
    * Adjust error stack formatting
    
    * linting

--- packages/react-hooks/src/useNamedHook.ts
+++ packages/react-hooks/src/useNamedHook.ts
@@ -1,7 +1,7 @@
 // Copyright 2017-2021 @polkadot/react-hooks authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
-export function useNamedHook <T> (name: string, fn: (...args: any[]) => T, ...args: unknown[]): T {
+export function useNamedHook <T, F extends (...args: any[]) => T> (name: string, fn: F, ...args: Parameters<F>): T {
   try {
     return fn(...args);
   } catch (error) {
