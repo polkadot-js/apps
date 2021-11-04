c9ea22e64f69d9b8ae6c5b97d214ccf496c3c9ff
Sam <3194321+SamHellawell@users.noreply.github.com>
2021-11-04 04:31:43 +0000

    re-enable dock testnet endpoint (#6472)

--- packages/apps-config/src/endpoints/testing.ts
+++ packages/apps-config/src/endpoints/testing.ts
@@ -100,10 +100,9 @@ export function createTesting (t: TFunction, firstOnly: boolean, withSort: boole
     },
     {
       info: 'dock-testnet',
-      isDisabled: true, // https://github.com/polkadot-js/apps/issues/6469
       text: t('rpc.test.dock-pos-testnet', 'Dock', { ns: 'apps-config' }),
       providers: {
-        'Dock Association': 'wss://danforth-1.dock.io'
+        'Dock Association': 'wss://knox-1.dock.io'
       }
     },
     {
