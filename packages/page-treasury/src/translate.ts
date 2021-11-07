0a26f5210347da36ebebd2eaf569b2fe25b79357
Jaco <jacogr@gmail.com>
2021-11-07 11:09:50 +0100

    Bump @polkadot deps (#6514)
    * Bump @polkadot deps
    
    * Bumps

--- packages/page-treasury/src/translate.ts
+++ packages/page-treasury/src/translate.ts
@@ -5,6 +5,6 @@ import type { UseTranslationResponse } from 'react-i18next';
 
 import { useTranslation as useTranslationBase } from 'react-i18next';
 
-export function useTranslation (): UseTranslationResponse<'app-treasury'> {
+export function useTranslation (): UseTranslationResponse<'app-treasury', undefined> {
   return useTranslationBase('app-treasury');
 }
