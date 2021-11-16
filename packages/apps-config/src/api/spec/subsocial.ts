82e3db1f017203ef56ee6806ea954050a42fe51d
olehmell <oleh.melnechyk@gmail.com>
2021-11-16 16:52:22 +0200

    Update subsocial types

--- packages/apps-config/src/api/spec/subsocial.ts
+++ packages/apps-config/src/api/spec/subsocial.ts
@@ -1,23 +1,6 @@
 // Copyright 2017-2021 @polkadot/apps-config authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
-import type { OverrideBundleDefinition } from '@polkadot/types/types';
+import { types } from '@subsocial/types/substrate/preparedTypes';
 
-import * as subsocialDefinitions from '@subsocial/types/substrate/interfaces/definitions';
-
-import { typesFromDefs } from '../util';
-
-// structs need to be in order
-/* eslint-disable sort-keys */
-
-const definitions: OverrideBundleDefinition = {
-  types: [
-    {
-      // on all versions
-      minmax: [0, undefined],
-      types: typesFromDefs(subsocialDefinitions)
-    }
-  ]
-};
-
-export default definitions;
+export default { types };
