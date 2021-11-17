1b352ce287b1d95fac757980abadc05f10466047
estienne.antoine@gmail.com <estienne.antoine@gmail.com>
2021-11-17 15:03:17 +0100

    use the right bundle

--- packages/apps-config/src/api/spec/moonbeam.ts
+++ packages/apps-config/src/api/spec/moonbeam.ts
@@ -1,6 +1,6 @@
 // Copyright 2017-2021 @polkadot/apps-config authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
-import { moonbeamDefinitionsDeprecated as moonbeamDefinitions } from 'moonbeam-types-bundle';
+import { moonbeamDefinitions } from 'moonbeam-types-bundle';
 
 export default moonbeamDefinitions;
