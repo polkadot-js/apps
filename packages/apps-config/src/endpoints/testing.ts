43a36a62709718be0ebd3bae8c9b6e9b12675b2f
Henry Liu <50917820+Zombieliu@users.noreply.github.com>
2021-11-20 02:44:22 +0800

    add web3games testnet (#6575)
    * update Web3games information
    
    * update Web3games information again
    
    * web3games come back!
    
    * remove the isDisabled flag.

--- packages/apps-config/src/endpoints/testing.ts
+++ packages/apps-config/src/endpoints/testing.ts
@@ -483,10 +483,9 @@ export function createTesting (t: TFunction, firstOnly: boolean, withSort: boole
     },
     {
       info: 'web3games',
-      isDisabled: true, // https://github.com/polkadot-js/apps/runs/2755409009?check_suite_focus=true
       text: t('rpc.test.web3games', 'Web3Games', { ns: 'apps-config' }),
       providers: {
-        Web3Games: 'wss://substrate.org.cn:4443'
+        Web3Games: 'wss://testnet.web3games.org'
       }
     },
     {
