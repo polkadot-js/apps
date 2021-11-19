eef26df7dc5eed833b89b366d3823518c639ea63
frank <450595468@qq.com>
2021-11-20 05:29:30 +0800

    Add Ares Odyssey (#6577)
    * update json
    
    * Update for Odyssey
    
    * remove todo.
    
    * update Odyssey
    
    * Fix odyssey - named logo
    
    * update sort
    
    * update sort
    
    Co-authored-by: linhai <kami@cancanyou.com>

--- packages/apps-config/src/endpoints/productionRelayPolkadot.ts
+++ packages/apps-config/src/endpoints/productionRelayPolkadot.ts
@@ -56,6 +56,15 @@ export function createPolkadot (t: TFunction): EndpointOption {
         }
       },
       {
+        info: 'odyssey',
+        homepage: 'https://www.aresprotocol.io/',
+        paraId: 2028,
+        text: t('rpc.polkadot.odyssey', 'Ares Odyssey', { ns: 'apps-config' }),
+        providers: {
+          AresProtocol: 'wss://wss.odyssey.aresprotocol.io'
+        }
+      },
+      {
         info: 'astar',
         homepage: 'https://astar.network',
         paraId: 2006,
