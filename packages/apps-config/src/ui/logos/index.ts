61df63c9f207aa54d47f89e23ac100c9d3b8d36a
Yongfeng LI <wliyongfeng@gmail.com>
2021-12-01 17:54:47 +0800

    Add subsquare to external links (#6579)
    * Add subsquare to external links
    
    * Fix lint

--- packages/apps-config/src/ui/logos/index.ts
+++ packages/apps-config/src/ui/logos/index.ts
@@ -48,6 +48,7 @@ import externalPolkastats from './external/polkastats.png';
 import externalStatescan from './external/statescan.svg';
 import externalSubId from './external/subid.svg';
 import externalSubscan from './external/subscan.svg';
+import externalSubsquare from './external/subsquare.svg';
 import nodeApron from './nodes/apron.png';
 import nodeAres from './nodes/ares.svg';
 import nodeAresGladios from './nodes/ares-gladios.svg';
@@ -693,7 +694,8 @@ export const externalLogos: Record<string, unknown> = {
   polkastats: externalPolkastats,
   statescan: externalStatescan,
   subid: externalSubId,
-  subscan: externalSubscan
+  subscan: externalSubscan,
+  subsquare: externalSubsquare
 };
 
 // empty logos
