d9770269a0cb6c3b7d317eb85fccce91c81ce773
Greg Zaitsev <greg.zaitsev@gmail.com>
2021-12-01 12:45:34 +0300

    Unique: Add empty lines between imports to fix linter errors

--- packages/apps-config/src/api/spec/quartz.ts
+++ packages/apps-config/src/api/spec/quartz.ts
@@ -2,6 +2,7 @@
 // SPDX-License-Identifier: Apache-2.0
 
 import type { OverrideBundleDefinition } from '@polkadot/types/types';
+
 import { unique } from '@unique-nft/types/definitions';
 
 export default {
