65d3ccc51d8e91eaf8e643fbb26d1991b73b6887
Jaco <jacogr@gmail.com>
2021-12-20 12:21:36 +0100

    Bump deps (#6711)

--- packages/apps/src/index.tsx
+++ packages/apps/src/index.tsx
@@ -5,6 +5,7 @@
 import './initSettings';
 import 'semantic-ui-css/semantic.min.css';
 import '@polkadot/react-components/i18n';
+import '@polkadot/api-augment';
 
 import React from 'react';
 import ReactDOM from 'react-dom';
