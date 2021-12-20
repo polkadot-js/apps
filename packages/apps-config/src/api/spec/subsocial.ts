46e21a299b45fb76b1ce61315a8e3157300d8f66
olehmell <oleh.melnechyk@gmail.com>
2021-12-20 20:12:05 +0200

    Update subsocial types

--- packages/apps-config/src/api/spec/subsocial.ts
+++ packages/apps-config/src/api/spec/subsocial.ts
@@ -1,6 +1,6 @@
 // Copyright 2017-2021 @polkadot/apps-config authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
-import { types } from '@subsocial/types/substrate/preparedTypes';
+import { types } from '@subsocial/types/substrate';
 
 export default { types };
