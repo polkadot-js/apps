01f0e5499825bb58a7563fda71a179a306c5dc54
xnorly <xorpepper@protonmail.com>
2021-12-31 14:25:08 +0100

    move conditional outside and duplicate strings

--- packages/page-accounts/src/Accounts/BannerExtension.tsx
+++ packages/page-accounts/src/Accounts/BannerExtension.tsx
@@ -40,7 +40,13 @@ function BannerExtension (): React.ReactElement | null {
 
       return (
         <Banner type='warning'>
-          <p>{t<string>(`You have ${upgradableCount} ${upgradableCount===1? "extension that needs" : "extensions that need"} to be updated with the latest chain properties in order to display the correct information for the chain you are connected to. This update includes chain metadata and chain properties.`)}</p>
+          <p>
+            {upgradableCount===1
+              ? t<string>('You have 1 extension that needs to be updated with the latest chain properties in order to display the correct information for the chain you are connected to.')
+              : t<string>('You have {{upgradableCount}} extensions that need to be updated with the latest chain properties in order to display the correct information for the chain you are connected to.', { replace: { upgradableCount } })
+            }
+            {t<string>(' This update includes chain metadata and chain properties.')}
+          </p>
           <p><Trans key='extensionUpgrade'>Visit your <a href='#/settings/metadata'>settings page</a> to apply the updates to the injected extensions.</Trans></p>
         </Banner>
       );
