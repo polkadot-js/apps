8e1b261f947291cd24fc47f940f0bb33a35b10a7
Jaco <jacogr@gmail.com>
2022-01-01 10:58:33 +0100

    Bump dev (#6765)
    * Bump dev
    
    * Rebuild

--- packages/page-assets/src/index.tsx
+++ packages/page-assets/src/index.tsx
@@ -1,7 +1,7 @@
 // Copyright 2017-2022 @polkadot/app-assets authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
-import '@polkadot/api-augment';
+import '@polkadot/api-augment/substrate';
 
 import type BN from 'bn.js';
 
