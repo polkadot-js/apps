51835328db5f0eb90a9efcc7bf5510704a7ab279
Jaco <jacogr@gmail.com>
2022-01-01 08:30:15 +0100

    2022 (#6764)

--- packages/apps-config/src/settings/ss58.ts
+++ packages/apps-config/src/settings/ss58.ts
@@ -1,4 +1,4 @@
-// Copyright 2017-2021 @polkadot/ui-settings authors & contributors
+// Copyright 2017-2022 @polkadot/ui-settings authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
 import type { TFunction } from 'i18next';
