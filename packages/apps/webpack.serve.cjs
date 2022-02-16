aea176b1d26c4f7f51e4bb1cc54b0ef9d06112e2
Jaco <jacogr@gmail.com>
2022-02-16 09:10:32 +0200

    Bump deps

--- packages/apps/webpack.serve.cjs
+++ packages/apps/webpack.serve.cjs
@@ -26,7 +26,7 @@ module.exports = merge(
     watchOptions: {
       // 5.66.0 work-around (was reduced to 20, then started failing)
       // https://github.com/webpack/webpack/commit/96da7660021e8aa31e163bacd3515960eb253422#diff-13cf5374edb5eced5f3770d5f346c59252f87a90de91bc57306077693c8b95e2R52
-      aggregateTimeout: 200,
+      aggregateTimeout: 600,
       ignored: ['.yarn', 'build', 'node_modules']
     }
   }
