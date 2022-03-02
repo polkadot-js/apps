26afdf702d87f7a86a581d2dd7d09b00144004f1
Jaco <jacogr@gmail.com>
2022-03-02 16:11:53 +0200

    Create value

--- packages/page-staking/src/Pools/Create.tsx
+++ packages/page-staking/src/Pools/Create.tsx
@@ -1,10 +1,13 @@
 // Copyright 2017-2022 @polkadot/app-staking authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
-import React from 'react';
+import type { BN } from '@polkadot/util';
 
-import { Button } from '@polkadot/react-components';
-import { useToggle } from '@polkadot/react-hooks';
+import React, { useState } from 'react';
+
+import { Button, InputAddress, InputBalance, Modal, TxButton } from '@polkadot/react-components';
+import { useApi, useCallMulti, useToggle } from '@polkadot/react-hooks';
+import { bnMax } from '@polkadot/util';
 
 import { useTranslation } from '../translate';
 
@@ -12,17 +15,69 @@ interface Props {
   className?: string;
 }
 
-function Create (): React.ReactElement<Props> {
+function Create ({ className }: Props): React.ReactElement<Props> | null {
   const { t } = useTranslation();
-  const [, toggleCreate] = useToggle();
+  const { api } = useApi();
+  const [isOpen, toggleOpen] = useToggle();
+  const [accountId, setAccount] = useState<string | null>(null);
+  const [amount, setAmount] = useState<BN | undefined>();
+  const [minCreate, minNominate] = useCallMulti<[BN, BN]>([
+    api.query.nominatorPools.minCreateBond,
+    api.query.staking.minNominatorBond
+  ]);
+
+  if (!minCreate || !minNominate) {
+    return null;
+  }
+
+  const startValue = bnMax(minCreate, minNominate);
 
   return (
     <>
       <Button
         icon='plus'
         label={t<string>('Add pool')}
-        onClick={toggleCreate}
+        onClick={toggleOpen}
       />
+      {isOpen && (
+        <Modal
+          className={className}
+          label={t<string>('Create nomination pool')}
+          onClose={toggleOpen}
+          size='large'
+        >
+          <Modal.Content>
+            <Modal.Columns hint={t<string>('The origin account will also be set as the pool admin, nominator and state toggler.')}>
+              <InputAddress
+                label={t<string>('create pool from')}
+                onChange={setAccount}
+                type='account'
+                value={accountId}
+              />
+            </Modal.Columns>
+            <Modal.Columns hint={t<string>('The initial value to assign to the pool. It is set the the maximum of the minimum bond and the minium nomination value.')}>
+              <InputBalance
+                autoFocus
+                defaultValue={startValue}
+                isError={amount}
+                label={t<string>('initial value')}
+                onChange={setAmount}
+              />
+            </Modal.Columns>
+          </Modal.Content>
+          <Modal.Actions>
+            <TxButton
+              accountId={accountId}
+              icon='plus'
+              isDisabled={!accountId}
+              label={t<string>('Create')}
+              onStart={toggleOpen}
+              params={[amount, 0, accountId, accountId, accountId]}
+              tx={api.tx.nominationPools.create}
+            />
+          </Modal.Actions>
+        </Modal>
+      )}
     </>
   );
 }
