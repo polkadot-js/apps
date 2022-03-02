3ba31db94380e276bfc2a2fb6dcb1f2ec7f9605f
Marcin <marcin.radecki@cardinals.cc>
2022-03-02 16:38:10 +0100

    AZ-580 fix some lint errors

--- packages/react-hooks/src/useInflation.ts
+++ packages/react-hooks/src/useInflation.ts
@@ -7,38 +7,40 @@ import type { Inflation } from './types';
 
 import { useEffect, useState } from 'react';
 
-import {getInflationParams, UniformEraPayoutInflationParams} from '@polkadot/apps-config';
-import {BN_BILLION, BN_MILLION, BN_ZERO} from '@polkadot/util';
+import { getInflationParams, UniformEraPayoutInflationParams } from '@polkadot/apps-config';
+import { BN_BILLION, BN_MILLION, BN_ZERO } from '@polkadot/util';
+import { BN_THOUSAND } from '@polkadot/util/bn/consts';
 
 import { createNamedHook } from './createNamedHook';
 import { useApi } from './useApi';
 import { useCall } from './useCall';
-import {BN_THOUSAND} from "@polkadot/util/bn/consts";
 
 const EMPTY: Inflation = { idealInterest: 0, idealStake: 0, inflation: 0, stakedFraction: 0, stakedReturn: 0 };
 
-function calcInflationUniformEraPayout(totalIssuance: BN, inflation_params: UniformEraPayoutInflationParams) : number {
+function calcInflationUniformEraPayout (totalIssuance: BN, inflationParams: UniformEraPayoutInflationParams): number {
   const totalIssuanceInTokens = totalIssuance.div(BN_BILLION).div(BN_THOUSAND);
-  return (totalIssuanceInTokens.isZero() ? 0.0 : inflation_params.yearlyInflationInTokens.toNumber() / totalIssuanceInTokens.toNumber());
+
+  return (totalIssuanceInTokens.isZero() ? 0.0 : inflationParams.yearlyInflationInTokens.toNumber() / totalIssuanceInTokens.toNumber());
 }
 
 function calcInflation (api: ApiPromise, totalStaked: BN, totalIssuance: BN, numAuctions: BN): Inflation {
-  const inflation_params = getInflationParams(api);
-  const {auctionAdjust, auctionMax, falloff, maxInflation, minInflation, stakeTarget} = inflation_params;
+  const inflationParams = getInflationParams(api);
+  const { auctionAdjust, auctionMax, falloff, maxInflation, minInflation, stakeTarget } = inflationParams;
   const stakedFraction = totalStaked.isZero() || totalIssuance.isZero()
     ? 0
     : totalStaked.mul(BN_MILLION).div(totalIssuance).toNumber() / BN_MILLION.toNumber();
   const idealStake = stakeTarget - (Math.min(auctionMax, numAuctions.toNumber()) * auctionAdjust);
   const idealInterest = maxInflation / idealStake;
   let inflationInPercentage = 0;
-  if ('yearlyInflationInTokens' in inflation_params) {
-    inflationInPercentage = 100 * calcInflationUniformEraPayout(totalIssuance, inflation_params as UniformEraPayoutInflationParams);
+
+  if ('yearlyInflationInTokens' in inflationParams) {
+    inflationInPercentage = 100 * calcInflationUniformEraPayout(totalIssuance, inflationParams as UniformEraPayoutInflationParams);
   } else {
     inflationInPercentage = 100 * (minInflation + (
       stakedFraction <= idealStake
         ? (stakedFraction * (idealInterest - (minInflation / idealStake)))
         : (((idealInterest * idealStake) - minInflation) * Math.pow(2, (idealStake - stakedFraction) / falloff))
-     ));
+    ));
   }
 
   return {
