17b859a454166b22cbe8a1b17e12384330a38040
Jaco <jacogr@gmail.com>
2022-03-02 19:10:08 +0200

    Adjust useMapKeys trigger params

--- packages/page-assets/src/useAssetIds.ts
+++ packages/page-assets/src/useAssetIds.ts
@@ -6,17 +6,18 @@ import type { AssetId } from '@polkadot/types/interfaces';
 
 import { createNamedHook, useApi, useEventTrigger, useMapKeys } from '@polkadot/react-hooks';
 
-function extractAssetIds (keys: StorageKey<[AssetId]>[]): AssetId[] {
-  return keys
-    .map(({ args: [assetId] }) => assetId)
-    .sort((a, b) => a.cmp(b));
-}
+const options = {
+  transform: (keys: StorageKey<[AssetId]>[]): AssetId[] =>
+    keys
+      .map(({ args: [assetId] }) => assetId)
+      .sort((a, b) => a.cmp(b))
+};
 
 function useAssetIdsImpl (): AssetId[] | undefined {
   const { api } = useApi();
   const trigger = useEventTrigger([api.events.assets.Created, api.events.assets.Destroyed]);
 
-  return useMapKeys(api.query.assets.asset, { at: trigger.blockHash, transform: extractAssetIds });
+  return useMapKeys(api.query.assets.asset, options, trigger.blockHash);
 }
 
 export default createNamedHook('useAssetIds', useAssetIdsImpl);
--- packages/page-parachains/src/useProposals.ts
+++ packages/page-parachains/src/useProposals.ts
@@ -20,9 +20,10 @@ const optionsMulti = {
   defaultValue: [undefined, undefined] as MultiQuery
 };
 
-function extractProposalIds (keys: StorageKey<[ParaId]>[]): ParaId[] {
-  return keys.map(({ args: [id] }) => id);
-}
+const proposalOpts = {
+  transform: (keys: StorageKey<[ParaId]>[]): ParaId[] =>
+    keys.map(({ args: [id] }) => id)
+};
 
 function extractScheduled (entries: [StorageKey<[SessionIndex]>, ParaId[]][]): Scheduled[] {
   return entries.map(([{ args: [sessionIndex] }, scheduledIds]) => ({
@@ -35,7 +36,7 @@ function useProposalsImpl (): Proposals | undefined {
   const { api } = useApi();
   const mountedRef = useIsMountedRef();
   const trigger = useEventTrigger([api.events.proposeParachain?.ProposeParachain]);
-  const proposalIds = useMapKeys(api.query.proposeParachain?.proposals, { at: trigger.blockHash, transform: extractProposalIds });
+  const proposalIds = useMapKeys(api.query.proposeParachain?.proposals, proposalOpts, trigger.blockHash);
   const scheduled = useMapEntries(api.query.proposeParachain?.scheduledProposals, { at: trigger.blockHash, transform: extractScheduled });
   const [sessionIndex, approvedIds] = useCallMulti<MultiQuery>([
     api.query.session.currentIndex,
