dec2af80f06d24275c3ac52943e7ced353f33bbd
Jaco <jacogr@gmail.com>
2022-03-15 16:30:22 +0100

    useEventChanges for AssetId updates (#7131)
    * useEventChanges for AssetId updates
    
    * Update packages/react-hooks/src/useEventChanges.ts

--- packages/page-assets/src/useAssetIds.ts
+++ packages/page-assets/src/useAssetIds.ts
@@ -1,26 +1,40 @@
 // Copyright 2017-2022 @polkadot/app-assets authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
+import type { Changes } from '@polkadot/react-hooks/useEventChanges';
 import type { StorageKey } from '@polkadot/types';
-import type { AssetId } from '@polkadot/types/interfaces';
+import type { AssetId, EventRecord } from '@polkadot/types/interfaces';
 
-import { createNamedHook, useApi, useEventTrigger, useMapKeys } from '@polkadot/react-hooks';
+import { createNamedHook, useApi, useEventChanges, useMapKeys } from '@polkadot/react-hooks';
 
-const options = {
+const keyOptions = {
   transform: (keys: StorageKey<[AssetId]>[]): AssetId[] =>
-    keys
-      .map(({ args: [assetId] }) => assetId)
-      .sort((a, b) => a.cmp(b))
+    keys.map(({ args: [id] }) => id)
 };
+
+function filter (records: EventRecord[]): Changes<AssetId> {
+  const added: AssetId[] = [];
+  const removed: AssetId[] = [];
+
+  records.forEach(({ event: { data: [id], method } }): void => {
+    if (method === 'Created') {
+      added.push(id as AssetId);
+    } else {
+      removed.push(id as AssetId);
+    }
+  });
+
+  return { added, removed };
+}
 
 function useAssetIdsImpl (): AssetId[] | undefined {
   const { api } = useApi();
-  const trigger = useEventTrigger([
+  const startValue = useMapKeys(api.query.assets.asset, keyOptions);
+
+  return useEventChanges([
     api.events.assets.Created,
     api.events.assets.Destroyed
-  ]);
-
-  return useMapKeys(api.query.assets.asset, options, trigger.blockHash);
+  ], filter, startValue);
 }
 
 export default createNamedHook('useAssetIds', useAssetIdsImpl);
