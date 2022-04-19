a038d0871ab4c4d2e910b7dc3c998e114f0d5f41
Jaco <jacogr@gmail.com>
2022-04-19 15:19:50 +0300

    Fix Subsocial with multi APIs

--- packages/apps-config/src/api/spec/subsocial.ts
+++ packages/apps-config/src/api/spec/subsocial.ts
@@ -1,11 +1,20 @@
 // Copyright 2017-2022 @polkadot/apps-config authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
+import type { OverrideBundleDefinition } from '@polkadot/types/types';
+
 // We only load the definitions here explicitly - if we try to go via
 //   import { types } from '@subsocial/types/substrate';
-// we end up with multiple version of types/API
-import { types } from '@subsocial/types';
+// we end up with multiple version of types/API since it uses CJS
+import previous from '@subsocial/definitions/interfaces/subsocial/definitions'; // KEEP, see above
 
-export default {
-  types
+const defintions: OverrideBundleDefinition = {
+  types: [
+    {
+      minmax: [0, undefined],
+      types: previous.types as Record<string, string>
+    }
+  ]
 };
+
+export default defintions;
