e6d215840b34f24b8ac89ce52656596ff2d7ba1e
wirednkod <wirednkod@gmail.com>
2022-04-19 15:21:56 +0300

    Remove not-necessery components; Revert changes of SummaryBox

--- packages/page-staking/src/Overview/index.tsx
+++ packages/page-staking/src/Overview/index.tsx
@@ -2,43 +2,142 @@
 // SPDX-License-Identifier: Apache-2.0
 
 import type { DeriveStakingOverview } from '@polkadot/api-derive/types';
-import type { StakerState } from '@polkadot/react-hooks/types';
-import type { BN } from '@polkadot/util';
-import type { NominatedByMap, SortedTargets } from '../types';
+import type { SortedTargets } from '../types';
 
-import React, { useEffect } from 'react';
+import React from 'react';
+import styled from 'styled-components';
 
-import Summary from './Summary';
+import SummarySession from '@polkadot/app-explorer/SummarySession';
+import { CardSummary, Spinner, SummaryBox } from '@polkadot/react-components';
+import { formatNumber } from '@polkadot/util';
+
+import { useTranslation } from '../translate';
 
 interface Props {
   className?: string;
-  favorites: string[];
-  hasAccounts: boolean;
-  hasQueries: boolean;
-  minCommission?: BN;
-  nominatedBy?: NominatedByMap;
-  ownStashes?: StakerState[];
-  paraValidators?: Record<string, boolean>;
+  nominators?: string[];
   stakingOverview?: DeriveStakingOverview;
   targets: SortedTargets;
-  toggleFavorite: (address: string) => void;
-  toggleLedger?: () => void;
-  toggleNominatedBy: () => void;
 }
 
-function Overview ({ className = '', stakingOverview, targets, toggleLedger }: Props): React.ReactElement<Props> {
-  useEffect((): void => {
-    toggleLedger && toggleLedger();
-  }, [toggleLedger]);
+function Overview ({ className = '', stakingOverview, targets: { counterForNominators, inflation: { idealStake, inflation, stakedFraction }, nominatorMinActiveThreshold, nominators, validatorMinActiveThreshold, waitingIds } }: Props): React.ReactElement<Props> {
+  const { t } = useTranslation();
 
   return (
     <div className={`staking--Overview ${className}`}>
-      <Summary
-        stakingOverview={stakingOverview}
-        targets={targets}
-      />
+      <SummaryBox className={className}>
+        <section>
+          <CardSummary label={t<string>('validators')}>
+            {stakingOverview
+              ? <>{formatNumber(stakingOverview.validators.length)}&nbsp;/&nbsp;{formatNumber(stakingOverview.validatorCount)}</>
+              : <Spinner noLabel />
+            }
+          </CardSummary>
+          <CardSummary
+            className='media--900'
+            label={t<string>('waiting')}
+          >
+            {waitingIds
+              ? formatNumber(waitingIds.length)
+              : <Spinner noLabel />
+            }
+          </CardSummary>
+          <CardSummary
+            className='media--1000'
+            label={
+              counterForNominators
+                ? t<string>('active / nominators')
+                : t<string>('nominators')
+            }
+          >
+            {nominators
+              ? (
+                <>
+                  {formatNumber(nominators.length)}
+                  {counterForNominators && (
+                    <>&nbsp;/&nbsp;{formatNumber(counterForNominators)}</>
+                  )}
+                </>
+              )
+              : <Spinner noLabel />
+            }
+          </CardSummary>
+        </section>
+        <section>
+          {(idealStake > 0) && Number.isFinite(idealStake) && (
+            <CardSummary
+              className='media--1400'
+              label={t<string>('ideal staked')}
+            >
+              <>{(idealStake * 100).toFixed(1)}%</>
+            </CardSummary>
+          )}
+          {(stakedFraction > 0) && (
+            <CardSummary
+              className='media--1300'
+              label={t<string>('staked')}
+            >
+              <>{(stakedFraction * 100).toFixed(1)}%</>
+            </CardSummary>
+          )}
+          {(inflation > 0) && Number.isFinite(inflation) && (
+            <CardSummary
+              className='media--1200'
+              label={t<string>('inflation')}
+            >
+              <>{inflation.toFixed(1)}%</>
+            </CardSummary>
+          )}
+        </section>
+        <section>
+          <SummarySession />
+        </section>
+      </SummaryBox>
+      <SummaryBox className={className}>
+        <section>
+          <CardSummary
+            help={t<string>('Minimum stake among the active nominators.')}
+            label={
+              <div className='label-floater'>
+                {t<string>('nominators')}
+                <div>{t<string>('min active stake')}</div>
+              </div>
+            }
+          >
+            {nominatorMinActiveThreshold === '' ? <Spinner noLabel /> : nominatorMinActiveThreshold}
+          </CardSummary>
+          <CardSummary
+            help={t<string>('Minimum (total) stake among the active validators.')}
+            label={
+              <div className='label-floater'>
+                {t<string>('validators')}
+                <div>{t<string>('min active stake')}</div>
+              </div>
+            }
+          >
+            {validatorMinActiveThreshold === '' ? <Spinner noLabel /> : validatorMinActiveThreshold}
+          </CardSummary>
+        </section>
+      </SummaryBox>
     </div>
   );
 }
 
-export default React.memo(Overview);
+export default React.memo(styled(Overview)`
+  .label-floater {
+    float: left;
+    margin-bottom: 0.75rem;
+  }
+  .validator--Account-block-icon {
+    display: inline-block;
+    margin-right: 0.75rem;
+    margin-top: -0.25rem;
+    vertical-align: middle;
+  }
+
+  .validator--Summary-authors {
+    .validator--Account-block-icon+.validator--Account-block-icon {
+      margin-left: -1.5rem;
+    }
+  }
+`);
