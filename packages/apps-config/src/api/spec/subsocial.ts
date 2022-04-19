107a446261633ccf3210f1c7226890ee37b9a7ca
Jaco <jacogr@gmail.com>
2022-04-19 15:32:39 +0300

    Re-adjust

--- packages/apps-config/src/api/spec/subsocial.ts
+++ packages/apps-config/src/api/spec/subsocial.ts
@@ -1,20 +1,12 @@
 // Copyright 2017-2022 @polkadot/apps-config authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
-import type { OverrideBundleDefinition } from '@polkadot/types/types';
-
+// IMPORTANT
+//
 // We only load the definitions here explicitly - if we try to go via
-//   import { types } from '@subsocial/types/substrate';
-// we end up with multiple version of types/API since it uses CJS
-import previous from '@subsocial/definitions/interfaces/subsocial/definitions'; // KEEP, see above
+//   import { types } from '@subsocial/types';
+// we end up with multiple version of types/API since it uses CJS,
+// therefore here we explicitly import from the definitions (as re-exported)
+import { types } from '@subsocial/definitions/interfaces/subsocial/definitions'; // KEEP, see above
 
-const defintions: OverrideBundleDefinition = {
-  types: [
-    {
-      minmax: [0, undefined],
-      types: previous.types as Record<string, string>
-    }
-  ]
-};
-
-export default defintions;
+export default types;
