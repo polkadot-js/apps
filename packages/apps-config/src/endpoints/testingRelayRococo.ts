e8cae1840d123448b6d59b0dfd9a0af9f162a320
MucTepDayH16 <denisdrozhzhin1999@gmail.com>
2022-04-23 17:44:45 +0300

    Alphabetical order

--- packages/apps-config/src/endpoints/testingRelayRococo.ts
+++ packages/apps-config/src/endpoints/testingRelayRococo.ts
@@ -102,19 +102,19 @@ export function createRococo (t: TFunction): EndpointOption {
         }
       },
       {
-        info: 'rococoIntegritee',
-        paraId: 3002,
-        text: t('rpc.rococo.integritee', 'Integritee Network', { ns: 'apps-config' }),
-        providers: {
-          Integritee: 'wss://rococo.api.integritee.network'
-        }
-      },
-      {
         info: 'rococoGenshiro',
         paraId: 2024,
         text: t('rpc.rococo.genshiro', 'Genshiro Rococo Testnet', { ns: 'apps-config' }),
         providers: {
           Equilibrium: 'wss://parachain-testnet.equilab.io/rococo/collator/node1/wss'
+        }
+      },
+      {
+        info: 'rococoIntegritee',
+        paraId: 3002,
+        text: t('rpc.rococo.integritee', 'Integritee Network', { ns: 'apps-config' }),
+        providers: {
+          Integritee: 'wss://rococo.api.integritee.network'
         }
       },
       {
