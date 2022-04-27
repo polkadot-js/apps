dd3d57c28e417d99f54c8a1ad00eb04b576e7d29
Jaco <jacogr@gmail.com>
2022-04-27 10:23:40 +0300

    Adjust Jest with crypto

--- jest.config.cjs
+++ jest.config.cjs
@@ -22,7 +22,7 @@ module.exports = {
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': 'empty/object',
     '\\.(md)$': '<rootDir>/jest/mocks/empty.js'
   },
-  setupFilesAfterEnv: ['<rootDir>/jest/jest-setup.ts'],
+  setupFilesAfterEnv: ['<rootDir>/jest/setupEnv.cjs'],
   testEnvironment: 'jsdom',
   testTimeout: 90000
 };
