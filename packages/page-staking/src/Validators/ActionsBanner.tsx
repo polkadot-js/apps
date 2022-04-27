112e3850617bfe42ca90e62420a6f0205f182055
Marcin <marcin.radecki@cardinals.cc>
2022-04-27 14:26:01 +0200

    A0-866 Fixed linter

--- packages/page-staking/src/Validators/ActionsBanner.tsx
+++ packages/page-staking/src/Validators/ActionsBanner.tsx
@@ -10,16 +10,17 @@ import { useTranslation } from '../translate';
 function ActionsBanner (): React.ReactElement<null> | null {
   const { t } = useTranslation();
 
-  let aleph_zero_staking_homepage = "https://alephzero.org/staking";
+  const AlephZeroStakingHomepage = 'https://alephzero.org/staking';
+
   return (
     <MarkWarning
       className='warning centered'
       content={<>
         {t<string>('Use the account actions to create a new validator/nominator stash and bond it to participate in staking. Do not send funds directly via a transfer to a validator. Learn more about the staking process ')}
         {<a
-            href={aleph_zero_staking_homepage}
-            rel='noopener noreferrer'
-            target='_blank'
+          href={AlephZeroStakingHomepage}
+          rel='noopener noreferrer'
+          target='_blank'
         >{t<string>('here.')}</a>}
       </>}
     />
