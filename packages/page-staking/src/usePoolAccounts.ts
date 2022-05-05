08a4cf0069cbabbbaaa0675cdc602432a8c2b2d7
Jaco <jacogr@gmail.com>
2022-05-05 07:32:36 +0300

    HACK for both npols & nopls

--- packages/page-staking/src/usePoolAccounts.ts
+++ packages/page-staking/src/usePoolAccounts.ts
@@ -12,11 +12,22 @@ import { bnToU8a, stringToU8a, u8aConcat } from '@polkadot/util';
 
 const EMPTY_H256 = new Uint8Array(32);
 const ADDR_PREFIX = stringToU8a('modlpy/npols');
+const ADDR_PREFIX_WST = stringToU8a('modlpy/nopls');
 
 export function createAccount (api: ApiPromise, poolId: BN, index: number): string {
   return api.registry.createType(
     'AccountId32',
-    u8aConcat(ADDR_PREFIX, new Uint8Array([index]), bnToU8a(poolId, { bitLength: 32 }), EMPTY_H256)
+    u8aConcat(
+      // HACK: We have a naming issue between Substrate master as well as Westend. Since
+      // we don't quite have access to palletId, we here check the specName and adjust
+      // accordingly
+      api.runtimeVersion.specName.eq('westend')
+        ? ADDR_PREFIX_WST
+        : ADDR_PREFIX,
+      new Uint8Array([index]),
+      bnToU8a(poolId, { bitLength: 32 }),
+      EMPTY_H256
+    )
   ).toString();
 }
 
