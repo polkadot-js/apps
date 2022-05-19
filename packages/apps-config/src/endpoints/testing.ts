1e5137688004ca960261333a3b28229e29691c40
Gabriel Facco de Arruda <arrudagates@gmail.com>
2022-05-19 11:27:03 -0300

    Add comment about endpoint

--- packages/apps-config/src/endpoints/testing.ts
+++ packages/apps-config/src/endpoints/testing.ts
@@ -226,7 +226,7 @@ export const testChains: EndpointOption[] = [
     info: 'brainstorm',
     text: 'InvArch Brainstorm Testnet',
     providers: {
-      'InvArch Team': 'ws://localhost:9944'
+      'InvArch Team': 'ws://localhost:9944' // Placeholder, will be changed before merge
     }
   },
   {
