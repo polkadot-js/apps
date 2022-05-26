cb440ef86f07ecec1d5db528ee07a5714ce4c557
Jaco <jacogr@gmail.com>
2022-05-26 16:49:34 +0300

    Unreachable endpoints

--- packages/apps-config/src/endpoints/productionRelayPolkadot.ts
+++ packages/apps-config/src/endpoints/productionRelayPolkadot.ts
@@ -181,11 +181,12 @@ export const prodParasPolkadot: EndpointOption[] = [
   },
   {
     info: 'litentry',
+    isUnreachable: true,
     homepage: 'https://crowdloan.litentry.com',
     paraId: 2013,
     text: 'Litentry',
     providers: {
-      Litentry: 'wss://rpc.litentry-parachain.litentry.io'
+      Litentry: 'wss://rpc.litentry-parachain.litentry.io' // https://github.com/polkadot-js/apps/issues/7642
     }
   },
   {
