c483719cceebcf949a7489333bfe6e3cd240f558
Jaco <jacogr@gmail.com>
2022-06-10 09:04:57 +0300

    Fix invalid Quartz URL (with additional test)

--- packages/apps-config/src/endpoints/productionRelayKusama.ts
+++ packages/apps-config/src/endpoints/productionRelayKusama.ts
@@ -320,7 +320,7 @@ export const prodParasKusama: EndpointOption[] = [
     paraId: 2095,
     text: 'QUARTZ by UNIQUE',
     providers: {
-      OnFinality: 'wss://quartz.api.onfinality.io%2Fpublic-ws',
+      OnFinality: 'wss://quartz.api.onfinality.io/public-ws',
       'Unique America': 'wss://us-ws-quartz.unique.network',
       'Unique Asia': 'wss://asia-ws-quartz.unique.network',
       'Unique Europe': 'wss://eu-ws-quartz.unique.network'
