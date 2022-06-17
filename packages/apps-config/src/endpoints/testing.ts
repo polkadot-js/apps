319a489d1d1fca8b50c5f950083a2bf27d406810
Jaco <jacogr@gmail.com>
2022-06-17 17:05:28 +0300

    Remove unreachable endpoints

--- packages/apps-config/src/endpoints/testing.ts
+++ packages/apps-config/src/endpoints/testing.ts
@@ -457,8 +457,8 @@ export const testChains: EndpointOption[] = [
     text: 'Shibuya',
     providers: {
       StakeTechnologies: 'wss://rpc.shibuya.astar.network',
-      Dwellir: 'wss://shibuya-rpc.dwellir.com',
-      Pinknode: 'wss://public-rpc.pinknode.io/shibuya'
+      Dwellir: 'wss://shibuya-rpc.dwellir.com'
+      // Pinknode: 'wss://public-rpc.pinknode.io/shibuya' // https://github.com/polkadot-js/apps/issues/7747
     }
   },
   {
