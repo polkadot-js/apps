679050c2d3fbb3058481b84e22b06b2634febb05
Shawn-Eng-888 <89627694+Shawn-Eng-888@users.noreply.github.com>
2022-07-07 15:26:29 +0800

    Remove Pinknode Polkadex Endpoint

--- packages/apps-config/src/endpoints/production.ts
+++ packages/apps-config/src/endpoints/production.ts
@@ -211,8 +211,8 @@ export const prodChains: EndpointOption[] = [
     text: 'Polkadex',
     providers: {
       'Polkadex Team': 'wss://mainnet.polkadex.trade',
-      OnFinality: 'wss://polkadex.api.onfinality.io/public-ws',
-      Pinknode: 'wss://public-rpc.pinknode.io/polkadex'
+      OnFinality: 'wss://polkadex.api.onfinality.io/public-ws'
+     // Pinknode: 'wss://public-rpc.pinknode.io/polkadex'
     }
   },
   {
