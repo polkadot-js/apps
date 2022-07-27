37c1d5fd3b095426873f14c139e01f0260d47a9d
Jaco <jacogr@gmail.com>
2022-07-27 07:45:50 +0200

    Add alliance join (#7915)

--- packages/page-alliance/src/useMembers.ts
+++ packages/page-alliance/src/useMembers.ts
@@ -1,43 +1,47 @@
 // Copyright 2017-2022 @polkadot/app-alliance authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
-import type { StorageKey } from '@polkadot/types';
 import type { AccountId32 } from '@polkadot/types/interfaces';
-import type { PalletAllianceMemberRole } from '@polkadot/types/lookup';
 import type { Member } from './types';
 
-import { createNamedHook, useApi, useCall } from '@polkadot/react-hooks';
+import { useMemo } from 'react';
 
-const OPT_MEM = {
-  transform: (entries: [StorageKey<[PalletAllianceMemberRole]>, AccountId32[]][]) =>
-    entries
-      .reduce((all: Member[], [{ args: [role] }, accountIds]): Member[] => {
-        for (let i = 0; i < accountIds.length; i++) {
-          all.push({
-            accountId: accountIds[i].toString(),
-            role
-          });
-        }
+import { createNamedHook, useApi, useCallMulti } from '@polkadot/react-hooks';
 
-        return all;
-      }, [])
-      .sort((a, b) =>
-        a.role.type === b.role.type
-          ? 0
-          : a.role.type === 'Founder'
-            ? -1
-            : b.role.type === 'Founder'
-              ? 1
-              : a.role.type === 'Fellow'
-                ? -1
-                : 1
-      )
-};
+const ROLES = <const> ['Founder', 'Fellow', 'Ally'];
+
+function addMembers (all: Member[], role: Member['role'], accountIds: AccountId32[] = []): Member[] {
+  for (let i = 0; i < accountIds.length; i++) {
+    all.push({
+      accountId: accountIds[i].toString(),
+      role
+    });
+  }
+
+  return all;
+}
 
 function useMembersImpl (): Member[] | undefined {
   const { api } = useApi();
+  const query = useCallMulti<AccountId32[][]>([
+    [api.query.alliance.members, ROLES[0]],
+    [api.query.alliance.members, ROLES[1]],
+    [api.query.alliance.members, ROLES[2]]
+  ]);
 
-  return useCall<Member[]>(api.query.alliance.members.entries, [], OPT_MEM);
+  return useMemo((): Member[] | undefined => {
+    if (query && query.length === ROLES.length) {
+      const all: Member[] = [];
+
+      for (let i = 0; i < ROLES.length; i++) {
+        addMembers(all, ROLES[i], query[i]);
+      }
+
+      return all;
+    }
+
+    return undefined;
+  }, [query]);
 }
 
 export default createNamedHook('useMembersImpl', useMembersImpl);
