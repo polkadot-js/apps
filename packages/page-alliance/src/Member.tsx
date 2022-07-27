8217506fd12b845beaae9f614a8c3ff8f3e10a81
Jaco <jacogr@gmail.com>
2022-07-27 10:58:47 +0200

    Allow alliance retire (& display member-specific info) (#7916)

--- packages/page-alliance/src/Member.tsx
+++ packages/page-alliance/src/Member.tsx
@@ -3,9 +3,14 @@
 
 import type { Member as MemberType } from './types';
 
-import React from 'react';
+import React, { useCallback, useContext, useMemo } from 'react';
 
-import { AddressSmall } from '@polkadot/react-components';
+import { AddressSmall, Badge, Menu, Popup, StatusContext } from '@polkadot/react-components';
+import { useAccounts, useApi } from '@polkadot/react-hooks';
+import { FormatBalance } from '@polkadot/react-query';
+
+import { useTranslation } from './translate';
+import useMemberInfo from './useMemberInfo';
 
 interface Props {
   className?: string;
@@ -13,14 +18,61 @@ interface Props {
 }
 
 function Member ({ className, info: { accountId, role } }: Props): React.ReactElement<Props> {
+  const { t } = useTranslation();
+  const { api } = useApi();
+  const { allAccounts } = useAccounts();
+  const info = useMemberInfo(accountId);
+  const { queueExtrinsic } = useContext(StatusContext);
+
+  const hasActions = useMemo(
+    () => allAccounts.includes(accountId),
+    [allAccounts, accountId]
+  );
+
+  const doRetire = useCallback(
+    () => queueExtrinsic({
+      accountId,
+      extrinsic: api.tx.alliance.retire()
+    }),
+    [accountId, api, queueExtrinsic]
+  );
+
   return (
     <tr className={className}>
-      <td className='badge' />
+      <td className='badge'>
+        {info && info.isUpForKicking && (
+          <Badge
+            color='red'
+            icon='ban'
+          />
+        )}
+      </td>
       <td className='address all'>
         <AddressSmall value={accountId} />
       </td>
       <td className='number'>
+        {info && info.deposit && (
+          <FormatBalance value={info.deposit} />
+        )}
+      </td>
+      <td className='number'>
         {role}
+      </td>
+      <td className='button'>
+        {hasActions && (
+          <Popup
+            key='settings'
+            value={
+              <Menu>
+                <Menu.Item
+                  isDisabled={info && info.isUpForKicking}
+                  label={t<string>('Retire')}
+                  onClick={doRetire}
+                />
+              </Menu>
+            }
+          />
+        )}
       </td>
     </tr>
   );
