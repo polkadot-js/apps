15f86cc692feed3f74a9d9570fbc7249a4d0d2d2
Jaco <jacogr@gmail.com>
2022-07-30 13:24:21 +0300

    Structure for upcoming apps

---
+++ packages/page-ranked-collective/src/index.tsx
@@ -0,0 +1,17 @@
+// Copyright 2017-2022 @polkadot/app-ranked-collective authors & contributors
+// SPDX-License-Identifier: Apache-2.0
+
+import React from 'react';
+
+interface Props {
+  basePath: string;
+  className?: string;
+}
+
+function App ({ className }: Props): React.ReactElement<Props> {
+  return (
+    <div className={className} />
+  );
+}
+
+export default React.memo(App);
