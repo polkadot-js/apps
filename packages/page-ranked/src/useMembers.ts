ab7392919c809414c25133f8ab28e8882ddad1a2
Jaco <jacogr@gmail.com>
2022-07-31 16:21:05 +0200

    Adjust ranked members loading (#7946)

--- packages/page-ranked/src/useMembers.ts
+++ packages/page-ranked/src/useMembers.ts
@@ -18,34 +18,34 @@ interface Result {
 }
 
 const OPT_MEM = {
-  transform: ([[ids], infos]: [[AccountId32[]], Option<PalletRankedCollectiveMemberRecord>[]]) =>
-    infos
+  transform: ([[ids], infos]: [[AccountId32[]], Option<PalletRankedCollectiveMemberRecord>[]]): Result => {
+    const members = infos
       .map((info, i) => [info.unwrapOr(null), ids[i]])
       .filter((r): r is [PalletRankedCollectiveMemberRecord, AccountId32] => !!r[0])
       .map(([info, accountId]): Member => ({
         accountId: accountId.toString(),
         info
-      })),
+      }));
+    const memberIds = members.map(({ accountId }) => accountId);
+
+    return {
+      memberIds,
+      members
+    };
+  },
   withParamsTransform: true
 };
 
-function createResult (members: Member[]): Result {
-  const memberIds = members.map(({ accountId }) => accountId);
-
-  return {
-    memberIds,
-    members
-  };
-}
-
 function useMembersImpl (collective: PalletColl): Result | undefined {
   const { api } = useApi();
-  const memberIds = useMembersIds(collective);
-  const all = useCall(memberIds && memberIds.length !== 0 && api.query[collective].members.multi, [memberIds], OPT_MEM);
+  const ids = useMembersIds(collective);
+  const result = useCall(ids && ids.length !== 0 && api.query[collective].members.multi, [ids], OPT_MEM);
 
   return useMemo(
-    () => all && createResult(all),
-    [all]
+    () => ids && ids.length === 0
+      ? { memberIds: [], members: [] }
+      : result,
+    [ids, result]
   );
 }
 
