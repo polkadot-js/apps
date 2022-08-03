ed5d253e049526e9c2e0346b6ec4cf87533d3bf8
Jaco <jacogr@gmail.com>
2022-08-03 10:38:15 +0200

    Remove typesChain overrides (#7956)

--- packages/apps-config/src/api/index.ts
+++ packages/apps-config/src/api/index.ts
@@ -3,18 +3,9 @@
 
 import type { OverrideBundleType } from '@polkadot/types/types';
 
-import typesChain from './chain';
 import spec from './spec';
 
 export * from './constants';
 export * from './params';
 
-export function getChainTypes (_specName: string, chainName: string): Record<string, string | Record<string, unknown>> {
-  return {
-    ...(typesChain[chainName as keyof typeof typesChain] || {})
-  };
-}
-
 export const typesBundle: OverrideBundleType = { spec };
-
-export { typesChain };
