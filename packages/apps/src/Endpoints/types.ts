f59dec11d3a7abec230a1c11bb7f7d469324b091
Jaco <jacogr@gmail.com>
2022-08-14 08:49:43 +0200

    Short info blurb for linked chains (#7989)

--- packages/apps/src/Endpoints/types.ts
+++ packages/apps/src/Endpoints/types.ts
@@ -7,8 +7,11 @@ export interface Network {
   icon?: string;
   isChild?: boolean;
   isLightClient?: boolean;
+  isRelay?: boolean;
   isUnreachable?: boolean;
   name: string;
+  nameRelay?: string;
+  paraId?: number;
   providers: {
     name: string;
     url: string;
