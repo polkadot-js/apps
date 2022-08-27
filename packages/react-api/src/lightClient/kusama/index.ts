81d5fbf62eca37b73b06041242fdb88c95fc306d
Jaco <jacogr@gmail.com>
2022-08-27 11:32:52 +0200

    Adjust light relay/para mappings (#8059)
    * Adjust light relay/para mappings
    
    * Adjust resolveJsonModule

--- packages/react-api/src/lightClient/kusama/index.ts
+++ packages/react-api/src/lightClient/kusama/index.ts
@@ -1,15 +1,13 @@
 // Copyright 2017-2022 @polkadot/react-api authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
-/* eslint sort-keys: ["error", "asc", { caseSensitive: false }] */
-
-import gmSpec from './gm.json';
-import tinkernetSpec from './tinkernet.json';
+import gm from './gm.json' assert { type: 'json' };
+import tinkernet from './tinkernet.json' assert { type: 'json' };
 
 // Add your imported spec here in alphabetical order.
 // The key here reflects the URL of the light client endpoint.
 // e.g. light://substrate-connect/kusama/gm
-export const lightSpecs: Record<string, unknown> = {
-  gm: gmSpec,
-  tinkernet: tinkernetSpec
+export const specs: Record<string, unknown> = {
+  gm,
+  tinkernet
 };
