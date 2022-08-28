7f179c46152b71cce013333999ecab62bd4de66b
Jaco <jacogr@gmail.com>
2022-08-28 09:13:10 +0300

    Retrieve light specs via import(...)

--- packages/react-api/src/light/index.ts
+++ packages/react-api/src/light/index.ts
@@ -5,20 +5,9 @@ import { ScProvider } from '@polkadot/api';
 
 import { specs as kusama } from './kusama';
 
-export const lightSpecs: Record<string, Record<string, string>> =
-  Object
-    .entries<Record<string, unknown>>({ kusama })
-    .reduce((relays: Record<string, Record<string, string>>, [k, s]) => {
-      relays[k] = Object
-        .entries<unknown>(s)
-        .reduce((paras: Record<string, string>, [k, v]) => {
-          paras[k] = JSON.stringify(v);
-
-          return paras;
-        }, {});
-
-      return relays;
-    }, {});
+export const lightSpecs: Record<string, Record<string, string>> = {
+  kusama
+};
 
 export const relaySpecs: Record<string, string> = {
   kusama: ScProvider.WellKnownChain.ksmcc3,
