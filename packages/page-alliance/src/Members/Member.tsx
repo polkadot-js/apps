ee5b552373798f9fb8db21158da1004303d6ec00
Jaco <jacogr@gmail.com>
2022-09-03 12:34:15 +0300

    Retire announcement

--- packages/page-alliance/src/Members/Member.tsx
+++ packages/page-alliance/src/Members/Member.tsx
@@ -1,6 +1,7 @@
 // Copyright 2017-2022 @polkadot/app-alliance authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
+import type { BN } from '@polkadot/util';
 import type { Member as MemberType } from '../types';
 
 import React, { useCallback, useContext, useMemo } from 'react';
@@ -13,18 +14,20 @@ import { useTranslation } from '../translate';
 import useMemberInfo from '../useMemberInfo';
 
 interface Props {
+  bestNumber?: BN;
   className?: string;
   info: MemberType;
   isPrime: boolean;
   isVoter: boolean;
 }
 
-function Member ({ className, info: { accountId, role }, isPrime, isVoter }: Props): React.ReactElement<Props> {
+function Member ({ bestNumber, className, info: { accountId, role }, isPrime, isVoter }: Props): React.ReactElement<Props> {
   const { t } = useTranslation();
   const { api } = useApi();
   const { allAccounts } = useAccounts();
   const info = useMemberInfo(accountId);
   const { queueExtrinsic } = useContext(StatusContext);
+  const hasNotice = !!api.tx.alliance.giveRetirementNotice;
 
   const hasActions = useMemo(
     () => allAccounts.includes(accountId),
@@ -39,13 +42,27 @@ function Member ({ className, info: { accountId, role }, isPrime, isVoter }: Pro
     [accountId, api, queueExtrinsic]
   );
 
+  const doNotice = useCallback(
+    () => queueExtrinsic({
+      accountId,
+      extrinsic: api.tx.alliance.giveRetirementNotice()
+    }),
+    [accountId, api, queueExtrinsic]
+  );
+
   return (
     <tr className={className}>
       <td className='address'>
         <AddressSmall value={accountId} />
       </td>
       <td className='all'>
-        {(info && info.isUpForKicking && (
+        {(info && info.isRetiringAt && (
+          <Tag
+            color='yellow'
+            hover={t<string>('Is retiring')}
+            label={t<string>('retirting')}
+          />
+        )) || (info && info.isUpForKicking && (
           <Tag
             color='red'
             hover={t<string>('Up for kicking')}
@@ -79,8 +96,24 @@ function Member ({ className, info: { accountId, role }, isPrime, isVoter }: Pro
             key='settings'
             value={
               <Menu>
+                {hasNotice && (
+                  <Menu.Item
+                    isDisabled={!!(info && info.isRetiringAt)}
+                    label={t<string>('Announce retirement')}
+                    onClick={doNotice}
+                  />
+                )}
                 <Menu.Item
-                  isDisabled={!!(info && (info.isUpForKicking || info.isRetiringAt))}
+                  isDisabled={
+                    !!info && (
+                      info.isUpForKicking ||
+                      (
+                        hasNotice
+                          ? !bestNumber || !info.isRetiringAt || info.isRetiringAt.lt(bestNumber)
+                          : false
+                      )
+                    )
+                  }
                   label={t<string>('Retire')}
                   onClick={doRetire}
                 />
