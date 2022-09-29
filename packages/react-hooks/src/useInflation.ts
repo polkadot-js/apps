f04ed5dfc6defce151b544375b49770c91b97c66
Jaco <jacogr@gmail.com>
2022-09-29 13:22:00 +0300

    Link inflation calculations

--- packages/react-hooks/src/useInflation.ts
+++ packages/react-hooks/src/useInflation.ts
@@ -21,13 +21,27 @@ function calcInflation (api: ApiPromise, totalStaked: BN, totalIssuance: BN, num
   const stakedFraction = totalStaked.isZero() || totalIssuance.isZero()
     ? 0
     : totalStaked.mul(BN_MILLION).div(totalIssuance).toNumber() / BN_MILLION.toNumber();
+  // Ideal is less based on the actual auctions, see
+  // https://github.com/paritytech/polkadot/blob/816cb64ea16102c6c79f6be2a917d832d98df757/runtime/kusama/src/lib.rs#L531
   const idealStake = stakeTarget - (Math.min(auctionMax, numAuctions.toNumber()) * auctionAdjust);
   const idealInterest = maxInflation / idealStake;
+  // inflation calculations, see
+  // https://github.com/paritytech/substrate/blob/0ba251c9388452c879bfcca425ada66f1f9bc802/frame/staking/reward-fn/src/lib.rs#L28-L54
   const inflation = 100 * (minInflation + (
     stakedFraction <= idealStake
       ? (stakedFraction * (idealInterest - (minInflation / idealStake)))
       : (((idealInterest * idealStake) - minInflation) * Math.pow(2, (idealStake - stakedFraction) / falloff))
   ));
+
+  console.log({
+    idealInterest,
+    idealStake,
+    inflation,
+    stakedFraction,
+    stakedReturn: stakedFraction
+      ? (inflation / stakedFraction)
+      : 0
+  }, stakeTarget, Math.min(auctionMax, numAuctions.toNumber()), Math.min(auctionMax, numAuctions.toNumber()) * auctionAdjust);
 
   return {
     idealInterest,
