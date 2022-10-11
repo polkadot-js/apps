23cbf7478aada9a67408c2df7ee4cc53b256c3a3
STAKEWORLD <info@stakeworld.nl>
2022-10-11 21:03:47 +0200

    Add STAKEWORLD endpoints for kusama and polkadot

--- packages/apps-config/src/endpoints/productionRelayKusama.ts
+++ packages/apps-config/src/endpoints/productionRelayKusama.ts
@@ -550,6 +550,7 @@ export const prodRelayKusama: EndpointOption = {
     Pinknode: 'wss://public-rpc.pinknode.io/kusama',
     // 'Geometry Labs': 'wss://kusama.geometry.io/websockets', // https://github.com/polkadot-js/apps/pull/6746
     'Automata 1RPC': 'wss://1rpc.io/ksm',
+    STAKEWORLD: 'wss://ksm.rpc.stakeworld.nl',
     // NOTE: Keep this as the last entry, nothing after it
     'light client': 'light://substrate-connect/kusama' // NOTE: Keep last
   },
