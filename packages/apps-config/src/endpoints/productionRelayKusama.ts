30a945b667ebd8f6c955deaa10406c6be881a391
0xSamsara <94698086+0xSamsara@users.noreply.github.com>
2022-10-17 17:08:23 +0200

    Added Samsara RPC to Robonomics parachain (#8240)

--- packages/apps-config/src/endpoints/productionRelayKusama.ts
+++ packages/apps-config/src/endpoints/productionRelayKusama.ts
@@ -396,7 +396,8 @@ export const prodParasKusama: EndpointOption[] = [
     providers: {
       Airalab: 'wss://kusama.rpc.robonomics.network/',
       OnFinality: 'wss://robonomics.api.onfinality.io/public-ws',
-      Dwellir: 'wss://robonomics-rpc.dwellir.com'
+      Dwellir: 'wss://robonomics-rpc.dwellir.com',
+      Samsara: 'wss://robonomics.0xsamsara.com'
     }
   },
   {
