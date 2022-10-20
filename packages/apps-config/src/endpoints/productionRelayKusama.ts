07c3b337bd3c5b1650aa389df683c610f14c038f
Leemo <89027356+Leemo94@users.noreply.github.com>
2022-10-20 15:54:31 +0400

    Updated Robonomics RPC endpoints (#8256)
    Added Leemo RPC to Robonomics Parachain

--- packages/apps-config/src/endpoints/productionRelayKusama.ts
+++ packages/apps-config/src/endpoints/productionRelayKusama.ts
@@ -397,7 +397,8 @@ export const prodParasKusama: EndpointOption[] = [
       Airalab: 'wss://kusama.rpc.robonomics.network/',
       OnFinality: 'wss://robonomics.api.onfinality.io/public-ws',
       Dwellir: 'wss://robonomics-rpc.dwellir.com',
-      Samsara: 'wss://robonomics.0xsamsara.com'
+      Samsara: 'wss://robonomics.0xsamsara.com',
+      Leemo: 'wss://robonomics.leemo.me'
     }
   },
   {
