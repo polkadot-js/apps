84270d884fe229611b6ffbdecf9552f748fab81a
Marcin <marcin.radecki@cardinals.cc>
2022-11-21 08:22:20 +0100

    Filtered out reserved nodes from suspensions

--- packages/page-staking/src/Suspensions/CurrentList.tsx
+++ packages/page-staking/src/Suspensions/CurrentList.tsx
@@ -28,6 +28,21 @@ function CurrentList ({ suspensions }: Props): React.ReactElement<Props> {
     ]
   );
 
+  // Work-around for BanEvents containing reserved nodes - remove when backend is fixed
+  // Below are AccountIds of Testnet and Mainnet Foundation validators
+  const excludedReservedValidators = [
+    '5Eo5ZxVUGbT6D8cfAvAxQFhzt3ZqBEb5oE8KCWR9vjvTPSMy', '5HNnDD5djTaiUt3A6yf6f1E9oDiM5w5fcNBTLLCoMKf1TEdS',
+    '5DATX2UZZgxAsumbVEsmup2q6LR9Bn81F7KW7PsShgUw8t12', '5GW5kbpuYn8Wa2253xLNLn9dZYWJUPJmW7VwmjnziDWdGxiX',
+    '5Grh6bLQmoxinEeiijAfSbGYrYiKhxnxcM2m96s5A64VyAiF', '5CGTtuqDbBQokPQjpa4mQyNKyvYxKpgtZEskDkJxzho1NhbM',
+    '5FnyjESMB4EBQn1W1vnNKZ5oVUYUmQbTPG4hZbJJm8697TKt', '5GN3rbR41UYWtjoxeuyvBfWEPopH4C2R4z7qhtz2ysF5hmrt',
+    '5HYzfrjAMGB6zWW3oTg7dhGdWB8cawyU84fCpGar9QhupweS', '5Dkh7kuPm4NMfkmDG1LaVZVWXW3WHYwh7BKEFfNvPiGDrARH',
+    '5CK2GZvpmKYxJQXMQzHa2vvHLf5cibuWK4qkcCem2p9PXYx1', '5CcZnvQmvNJviAxpnmZKVM9aYtfoyRsE4YKoTig9jWBTW8zH',
+    '5CoxXHzXRZrnVkmdDhYVHz5b6PJdCRmR5yGSQmQwzasHyzaw', '5DRbk3BimLzBzQtzUYUP35c57iwXtpqyjbnfY6AX48vbGMQC',
+    '5EjX1sVKuvrDAkm3XVS1xBLfboH5GqfLG9WnBwe8Pki8qwR6', '5F4P6AGQPwVUX3mwfmc64fnGd1abEV2QDKdVT3KRRw793WYK',
+    '5GFCZjWGSHas86192H3yiZZFySLtUW74SdHDqTymBEDUUF7T', '5GsqnQjwhayJ1TEtv43ZYQvvvMkG2pEgucGJw39ci96R1pVp',
+    '5H9h84SrX4gdXTxGyB6wtEfTye5Kb7vMcwARNLCZxMa1CruS', '5HdUiPkneLL2dQHvFkp47cfw63uPWSL8gFPzduLw6YXx3cBU'
+  ];
+
   return (
     <Table
       empty={
@@ -48,16 +63,18 @@ function CurrentList ({ suspensions }: Props): React.ReactElement<Props> {
       }
       header={headerRef.current}
     >
-      {suspensions?.map(({ address, era, suspensionLiftsInEra, suspensionReason }): React.ReactNode => (
-        <Address
-          address={address}
-          era={era}
-          filterName={nameFilter}
-          key={address}
-          suspensionLiftsInEra={suspensionLiftsInEra}
-          suspensionReason={suspensionReason}
-        />
-      ))}
+      {suspensions?.filter(({ address }) =>
+        !excludedReservedValidators.find((value) => value === address))
+        .map(({ address, era, suspensionLiftsInEra, suspensionReason }): React.ReactNode => (
+          <Address
+            address={address}
+            era={era}
+            filterName={nameFilter}
+            key={address}
+            suspensionLiftsInEra={suspensionLiftsInEra}
+            suspensionReason={suspensionReason}
+          />
+        ))}
     </Table>
   );
 }
