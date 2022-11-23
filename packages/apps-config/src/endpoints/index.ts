5c7f3d7bf2d42d335272f965376c75b23ba4587b
Quinn Diggity <git@quinn.dev>
2022-11-23 14:42:05 -0800

    fixes build error: `Invalid header definition`
    fixes build error: `unused variables: api, useApi, hasAccounts`
    
    fixes linting errors
    
    fixes linting errors

--- packages/apps-config/src/endpoints/index.ts
+++ packages/apps-config/src/endpoints/index.ts
@@ -4,8 +4,8 @@
 import type { TFunction, TOptions } from '../types.js';
 import type { LinkOption } from './types.js';
 
-import { createCustom, createDev, createOwn } from './development.js';
 import { chains } from './chains.js';
+import { createCustom, createDev, createOwn } from './development.js';
 import { expandEndpoints } from './util.js';
 
 export { CUSTOM_ENDPOINT_KEY } from './development.js';
--- packages/apps/workers-site/index.js
+++ packages/apps/workers-site/index.js
@@ -1,4 +1,7 @@
-import { getAssetFromKV, mapRequestToAsset } from '@cloudflare/kv-asset-handler'
+// Copyright 2017-2022 @polkadot/apps authors & contributors
+// SPDX-License-Identifier: Apache-2.0
+
+import { getAssetFromKV } from '@cloudflare/kv-asset-handler';
 
 /**
  * The DEBUG flag will do two things that help during development:
@@ -7,74 +10,48 @@ import { getAssetFromKV, mapRequestToAsset } from '@cloudflare/kv-asset-handler'
  * 2. we will return an error message on exception in your Response rather
  *    than the default 404.html page.
  */
-const DEBUG = false
+const DEBUG = false;
 
-addEventListener('fetch', event => {
+addEventListener('fetch', (event) => {
   try {
-    event.respondWith(handleEvent(event))
+    event.respondWith(handleEvent(event));
   } catch (e) {
     if (DEBUG) {
       return event.respondWith(
         new Response(e.message || e.toString(), {
-          status: 500,
-        }),
-      )
+          status: 500
+        })
+      );
     }
-    event.respondWith(new Response('Internal Error', { status: 500 }))
+
+    event.respondWith(new Response('Internal Error', { status: 500 }));
   }
-})
+});
 
-async function handleEvent(event) {
-  const url = new URL(event.request.url)
-  let options = {}
-
-  /**
-   * You can add custom logic to how we fetch your assets
-   * by configuring the function `mapRequestToAsset`
-   */
-  // options.mapRequestToAsset = handlePrefix(/^\/docs/)
+async function handleEvent (event) {
+  const options = {};
 
   try {
     if (DEBUG) {
       // customize caching
       options.cacheControl = {
-        bypassCache: true,
-      }
+        bypassCache: true
+      };
     }
-    return await getAssetFromKV(event, options)
+
+    return await getAssetFromKV(event, options);
   } catch (e) {
     // if an error is thrown try to serve the asset at 404.html
     if (!DEBUG) {
       try {
-        let notFoundResponse = await getAssetFromKV(event, {
-          mapRequestToAsset: req => new Request(`${new URL(req.url).origin}/404.html`, req),
-        })
+        const notFoundResponse = await getAssetFromKV(event, {
+          mapRequestToAsset: (req) => new Request(`${new URL(req.url).origin}/404.html`, req)
+        });
 
-        return new Response(notFoundResponse.body, { ...notFoundResponse, status: 404 })
+        return new Response(notFoundResponse.body, { ...notFoundResponse, status: 404 });
       } catch (e) {}
     }
 
-    return new Response(e.message || e.toString(), { status: 500 })
-  }
-}
-
-/**
- * Here's one example of how to modify a request to
- * remove a specific prefix, in this case `/docs` from
- * the url. This can be useful if you are deploying to a
- * route on a zone, or if you only want your static content
- * to exist at a specific path.
- */
-function handlePrefix(prefix) {
-  return request => {
-    // compute the default (e.g. / -> index.html)
-    let defaultAssetKey = mapRequestToAsset(request)
-    let url = new URL(defaultAssetKey.url)
-
-    // strip the prefix from the path for lookup
-    url.pathname = url.pathname.replace(prefix, '/')
-
-    // inherit all other props from the default request
-    return new Request(url.toString(), defaultAssetKey)
+    return new Response(e.message || e.toString(), { status: 500 });
   }
 }
