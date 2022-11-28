735e6f1a01b2212a5e75990d22ccf7b2a80d4a88
Jaco <jacogr@gmail.com>
2022-11-28 10:18:44 +0200

    lowerCase sort

--- packages/apps-config/src/endpoints/util.ts
+++ packages/apps-config/src/endpoints/util.ts
@@ -81,7 +81,7 @@ function expandEndpoint (t: TFunction, { dnslink, genesisHash, homepage, info, i
         ? 1
         : b.isLightClient
           ? -1
-          : a.textBy.localeCompare(b.textBy)
+          : a.textBy.toLocaleLowerCase().localeCompare(b.textBy.toLocaleLowerCase())
     );
 
   if (linked) {
