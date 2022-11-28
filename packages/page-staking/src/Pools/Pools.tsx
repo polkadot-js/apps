93a3ed738d29be42276ad77c6e19781efd0e9f61
Marcin <marcin.radecki@cardinals.cc>
2022-11-28 09:59:49 +0100

    A0-1621 Add Foundation pools button to all envs except Testnet

--- packages/page-staking/src/Pools/Pools.tsx
+++ packages/page-staking/src/Pools/Pools.tsx
@@ -5,9 +5,10 @@ import type { BN } from '@polkadot/util';
 import type { OwnPool } from '../types';
 import type { Params } from './types';
 
-import React, { useMemo, useRef, useState } from 'react';
+import React, { useMemo, useState } from 'react';
 
 import { Button, Table, ToggleGroup } from '@polkadot/react-components';
+import { useApi } from '@polkadot/react-hooks';
 import { arrayFlatten } from '@polkadot/util';
 
 import { useTranslation } from '../translate';
@@ -23,6 +24,7 @@ interface Props {
 }
 
 function Pools ({ className, ids, ownPools, params }: Props): React.ReactElement<Props> {
+  const { api } = useApi();
   const { t } = useTranslation();
   const membersMap = useMembers();
   const [typeIndex, setTypeIndex] = useState(() => ownPools && ownPools.length ? 0 : 1);
@@ -39,9 +41,11 @@ function Pools ({ className, ids, ownPools, params }: Props): React.ReactElement
 
   const filtered = useMemo(
     () => ownPools && ids
-      ? typeIndex
-        ? ids
-        : ids.filter((id) => ownPools.some(({ poolId }) => id.eq(poolId)))
+      ? typeIndex === 2
+        ? ids.filter((id) => id.toNumber() <= 10)
+        : typeIndex === 1
+          ? ids
+          : ids.filter((id) => ownPools.some(({ poolId }) => id.eq(poolId)))
       : undefined,
     [ids, ownPools, typeIndex]
   );
@@ -54,17 +58,25 @@ function Pools ({ className, ids, ownPools, params }: Props): React.ReactElement
     [undefined, undefined, 3]
   ], [t]);
 
-  const poolTypes = useRef([
-    { text: t('Own pools'), value: 'mine' },
-    { text: t('All pools'), value: 'all' }
-  ]);
+  const poolTypes = useMemo(() => {
+    const headers = [
+      { text: t('Own pools'), value: 'mine' },
+      { text: t('All pools'), value: 'all' }];
+
+    if (!api.runtimeChain.toString().includes('Aleph Zero Testnet')) {
+      headers.push({ text: t('Foundation pools'), value: 'all' });
+    }
+
+    return headers;
+  },
+  [api, t]);
 
   return (
     <>
       <Button.Group>
         <ToggleGroup
           onChange={setTypeIndex}
-          options={poolTypes.current}
+          options={poolTypes}
           value={typeIndex}
         />
         <Create
