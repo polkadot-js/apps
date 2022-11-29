6a0a66db3579a60ce29f1560a340daf668aea4f5
Jaco <jacogr@gmail.com>
2022-11-29 12:56:25 +0200

    Skip (& don't break) on previous-gen preimage pallet

--- packages/page-preimages/src/usePreimage.ts
+++ packages/page-preimages/src/usePreimage.ts
@@ -82,8 +82,11 @@ function usePreimageImpl (hashOrBounded?: Hash | HexString | FrameSupportPreimag
   const { api } = useApi();
 
   const hash = useMemo(
-    () => hashOrBounded && getPreimageHash(hashOrBounded),
-    [hashOrBounded]
+    // we need a hash _and_ be on the newset supported version of the pallet
+    // (after the application of bounded calls)
+    () => api.query.preimage?.preimageFor?.creator.meta.type.isMap && api.registry.lookup.getTypeDef(api.query.preimage.preimageFor.creator.meta.type.asMap.key).type === '(H256,u32)' && hashOrBounded &&
+      getPreimageHash(hashOrBounded),
+    [api, hashOrBounded]
   );
 
   const optStatus = useCall<Option<PalletPreimageRequestStatus>>(hash && api.query.preimage?.statusFor, [hash]);
