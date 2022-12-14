92960c935a98f586fe4d04a1ae00ce3db73283d0
Jaco <jacogr@gmail.com>
2022-12-14 13:09:40 +0200

    Display inactive issuance summary (#8534)
    * Display inactive issuance summary
    
    * Referenda issuance
    
    * Adjust item availability
    
    * active only on inactive availability
    
    * optional explore inactive

--- packages/page-referenda/src/Referenda/Summary.tsx
+++ packages/page-referenda/src/Referenda/Summary.tsx
@@ -1,21 +1,27 @@
 // Copyright 2017-2022 @polkadot/app-referenda authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
+import type { BN } from '@polkadot/util';
 import type { Summary as SummaryType } from '../types';
 
 import React from 'react';
 
 import { CardSummary, Spinner, SummaryBox } from '@polkadot/react-components';
+import { FormatBalance } from '@polkadot/react-query';
 import { formatNumber } from '@polkadot/util';
 
 import { useTranslation } from '../translate';
 
 interface Props {
   className?: string;
+  issuanceActive?: BN;
+  issuanceInactive?: BN;
+  issuanceTotal?: BN;
   summary: SummaryType;
+  withIssuance?: boolean;
 }
 
-function Summary ({ className, summary: { refActive, refCount } }: Props): React.ReactElement<Props> {
+function Summary ({ className, issuanceActive, issuanceInactive, issuanceTotal, summary: { refActive, refCount }, withIssuance }: Props): React.ReactElement<Props> {
   const { t } = useTranslation();
 
   return (
@@ -34,6 +40,34 @@ function Summary ({ className, summary: { refActive, refCount } }: Props): React
           }
         </CardSummary>
       </section>
+      {withIssuance && (
+        <section>
+          {issuanceTotal && (
+            <CardSummary label={t<string>('total issuance')}>
+              <FormatBalance
+                value={issuanceTotal}
+                withSi
+              />
+            </CardSummary>
+          )}
+          {issuanceInactive && (
+            <CardSummary label={t<string>('inactive issuance')}>
+              <FormatBalance
+                value={issuanceInactive}
+                withSi
+              />
+            </CardSummary>
+          )}
+          {issuanceActive && issuanceInactive && (
+            <CardSummary label={t<string>('active issuance')}>
+              <FormatBalance
+                value={issuanceActive}
+                withSi
+              />
+            </CardSummary>
+          )}
+        </section>
+      )}
     </SummaryBox>
   );
 }
