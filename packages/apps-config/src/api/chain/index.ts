c703ca412f8de06fdf3bfa1024b24ce05a57ee3e
Antonio Antonino <antonio@kilt.io>
2022-12-21 15:45:18 +0100

    Add support for chain names

---
+++ packages/apps-config/src/api/chain/index.ts
@@ -0,0 +1,9 @@
+// Copyright 2017-2022 @polkadot/apps-config authors & contributors
+// SPDX-License-Identifier: Apache-2.0
+
+import type { OverrideBundleDefinition } from '@polkadot/types/types';
+
+// NOTE: The mapping is done from chain name in system.chain
+const chain: Record<string, OverrideBundleDefinition> = {};
+
+export default chain;
