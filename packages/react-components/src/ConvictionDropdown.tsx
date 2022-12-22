adec4a7fcb9a4c2a1ce188972e1d0a9863fcf765
Jaco <jacogr@gmail.com>
2022-12-22 09:15:43 +0200

    Use convictionVoting votingPeriod in Vote display (#8599)
    * Use per track enactmentPeriod in Vote display
    
    * Pass track
    
    * Adjust time text
    
    * voteLockingPeriod pass-through
    
    * Remove unused track pass-through

--- packages/react-components/src/ConvictionDropdown.tsx
+++ packages/react-components/src/ConvictionDropdown.tsx
@@ -7,7 +7,8 @@ import React, { useRef } from 'react';
 
 import { ApiPromise } from '@polkadot/api';
 import { useApi, useBlockInterval } from '@polkadot/react-hooks';
-import { BN, BN_THOUSAND } from '@polkadot/util';
+import { calcBlockTime } from '@polkadot/react-hooks/useBlockTime';
+import { BN } from '@polkadot/util';
 
 import Dropdown from './Dropdown';
 import { useTranslation } from './translate';
@@ -18,24 +19,23 @@ export interface Props {
   label?: React.ReactNode;
   onChange?: (value: number) => void;
   value?: number;
+  voteLockingPeriod?: BN;
 }
 
 const CONVICTIONS: [number, number, BN][] = [1, 2, 4, 8, 16, 32].map((lock, index) => [index + 1, lock, new BN(lock)]);
-const SEC_DAY = 60 * 60 * 24;
 
-function createOptions (api: ApiPromise, t: TFunction, blockTime: BN): { text: string; value: number }[] {
+function createOptions (api: ApiPromise, t: TFunction, blockTime: BN, voteLockingPeriod?: BN): { text: string; value: number }[] {
   return [
     { text: t<string>('0.1x voting balance, no lockup period'), value: 0 },
     ...CONVICTIONS.map(([value, lock, bnLock]): { text: string; value: number } => ({
-      text: t<string>('{{value}}x voting balance, locked for {{lock}}x duration ({{period}} days)', {
+      text: t<string>('{{value}}x voting balance, locked for {{lock}}x duration ({{period}})', {
         replace: {
           lock,
-          period: (
-            bnLock.mul(
-              api.consts.democracy.voteLockingPeriod ||
-              api.consts.democracy.enactmentPeriod
-            ).mul(blockTime).div(BN_THOUSAND).toNumber() / SEC_DAY
-          ).toFixed(2),
+          period: calcBlockTime(blockTime, bnLock.mul(
+            voteLockingPeriod ||
+            api.consts.democracy.voteLockingPeriod ||
+            api.consts.democracy.enactmentPeriod
+          ), t)[1],
           value
         }
       }),
@@ -44,12 +44,12 @@ function createOptions (api: ApiPromise, t: TFunction, blockTime: BN): { text: s
   ];
 }
 
-function Convictions ({ className = '', help, label, onChange, value }: Props): React.ReactElement<Props> | null {
+function Convictions ({ className = '', help, label, onChange, value, voteLockingPeriod }: Props): React.ReactElement<Props> | null {
   const { api } = useApi();
   const { t } = useTranslation();
   const blockTime = useBlockInterval();
 
-  const optionsRef = useRef(createOptions(api, t, blockTime));
+  const optionsRef = useRef(createOptions(api, t, blockTime, voteLockingPeriod));
 
   return (
     <Dropdown
