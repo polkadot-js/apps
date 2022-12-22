c513df258529fba23aa57d4714201c3b9bc4b0ac
Jaco <jacogr@gmail.com>
2022-12-22 08:14:51 +0200

    Pass track

--- packages/page-referenda/src/Referenda/RefOngoing.tsx
+++ packages/page-referenda/src/Referenda/RefOngoing.tsx
@@ -139,6 +139,7 @@ function Ongoing ({ isMember, members, palletReferenda, palletVote, ranks, track
           palletVote={palletVote}
           preimage={preimage}
           ranks={ranks}
+          track={track}
           trackInfo={trackInfo}
         />
       </td>
