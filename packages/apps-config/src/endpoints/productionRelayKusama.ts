3a047bf2d714059f9019f724f4d2c05e026d37b4
Gabriel Facco de Arruda <arrudagates@gmail.com>
2022-12-23 05:08:14 -0500

    Enable Tinkernet's OnFinality endpoint (#8596)

--- packages/apps-config/src/endpoints/productionRelayKusama.ts
+++ packages/apps-config/src/endpoints/productionRelayKusama.ts
@@ -193,7 +193,7 @@ export const prodParasKusama: EndpointOption[] = [
     text: 'InvArch Tinkernet',
     providers: {
       'InvArch Team': 'wss://tinker.invarch.network',
-      // OnFinality: 'wss://invarch-tinkernet.api.onfinality.io/public-ws', // https://github.com/polkadot-js/apps/issues/8553
+      OnFinality: 'wss://invarch-tinkernet.api.onfinality.io/public-ws',
       // NOTE: Keep this as the last entry, nothing after it
       'light client': 'light://substrate-connect/kusama/tinkernet' // NOTE: Keep last
     }
