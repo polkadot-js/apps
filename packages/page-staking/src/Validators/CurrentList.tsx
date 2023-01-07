5c6c8d84bacd62cd77dae659e847ad73de998f34
Jaco <jacogr@gmail.com>
2023-01-07 05:54:42 +0200

    Adjust class generation

--- packages/page-staking/src/Validators/CurrentList.tsx
+++ packages/page-staking/src/Validators/CurrentList.tsx
@@ -206,20 +206,20 @@ function CurrentList ({ className, favorites, hasQueries, isIntentions, isOwn, m
   );
 }
 
-const ANIM = [
-  0, 1, 2, 3, 4, 5, 6, 7, 8, 9,
-  10, 11, 12, 13, 14, 15, 16, 17, 18, 19,
-  20, 21, 22, 23, 24
-].map((n) => `
-  .greyAnim-${n} {
-    animation: greyAnim${n} 1s;
-  }
+export default React.memo(styled(CurrentList)`${
+  [
+    0, 1, 2, 3, 4, 5, 6, 7, 8, 9,
+    10, 11, 12, 13, 14, 15, 16, 17, 18, 19,
+    20, 21, 22, 23, 24
+  ].map((n) => `
+    .greyAnim-${n} {
+      animation: greyAnim${n} 1s;
+    }
 
-  @keyframes greyAnim${n} {
-    0% { background: #a6a6a6; }
-    50% { background: darkorange; }
-    100% { background: #a6a6a6; }
-  }
-`).join('');
-
-export default React.memo(styled(CurrentList)`${ANIM}`);
+    @keyframes greyAnim${n} {
+      0% { background: #a6a6a6; }
+      50% { background: darkorange; }
+      100% { background: #a6a6a6; }
+    }
+  `).join('')
+}`);
