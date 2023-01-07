ad88c9463241ffa5ae5bb766243367439b1ccbc7
Jaco <jacogr@gmail.com>
2023-01-07 12:40:30 +0200

    Conviction voting delegation

---
+++ packages/page-referenda/src/Referenda/Delegate.tsx
@@ -0,0 +1,18 @@
+// Copyright 2017-2023 @polkadot/app-referenda authors & contributors
+// SPDX-License-Identifier: Apache-2.0
+
+import type { PalletVote, TrackDescription } from '../types';
+
+import React from 'react';
+
+interface Props {
+  palletVote: PalletVote;
+  tracks?: TrackDescription[];
+}
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+function Delegate (_props: Props): React.ReactElement<Props> | null {
+  return null;
+}
+
+export default React.memo(Delegate);
