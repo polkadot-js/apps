db3c6d620a2a854bcaf58771a9fe76168b224f9e
Jaco <jacogr@gmail.com>
2023-01-10 14:05:58 +0200

    Update packages/apps-config/src/endpoints/productionRelayPolkadot.ts

--- packages/apps-config/src/endpoints/productionRelayPolkadot.ts
+++ packages/apps-config/src/endpoints/productionRelayPolkadot.ts
@@ -169,7 +169,7 @@ export const prodParasPolkadot: EndpointOption[] = [
     text: 'Efinity',
     providers: {
       Efinity: 'wss://rpc.efinity.io',
-      Dwellir: 'wss://efinity-rpc.dwellir.com',
+      Dwellir: 'wss://efinity-rpc.dwellir.com'
     }
   },
   {
