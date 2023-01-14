357d969b60150f69934fed0a22aeb49d724d4494
Peter White <23270067+peterwht@users.noreply.github.com>
2023-01-14 01:52:01 -0700

    Small fixes to provide council page compatibility (#8791)
    * small fixes to provide council page compatibility
    
    * Suggestions to get CI to lint
    
    * Update packages/page-democracy/src/Overview/ProposalCell.tsx
    
    Co-authored-by: Jaco <jacogr@gmail.com>

--- packages/page-council/src/Overview/Voters.tsx
+++ packages/page-council/src/Overview/Voters.tsx
@@ -28,19 +28,26 @@ function Voters ({ balance, voters }: Props): React.ReactElement<Props> {
 
   return (
     <>
-      <td className='expand'>
-        <ExpanderScroll
-          className={balance ? '' : '--tmp'}
-          renderChildren={renderVoters}
-          summary={<FormatBalance value={balance} />}
-        />
-      </td>
-      <td className='number'>
-        {voters
-          ? formatNumber(voters.length)
-          : <span className='--tmp'>123</span>
-        }
-      </td>
+      {voters
+        ? (
+          <>
+            <td className='expand'>
+              <ExpanderScroll
+                className={balance ? '' : '--tmp'}
+                renderChildren={renderVoters}
+                summary={<FormatBalance value={balance} />}
+              />
+            </td>
+            <td className='number'>
+              {voters
+                ? formatNumber(voters.length)
+                : <span className='--tmp'>123</span>
+              }
+            </td>
+          </>
+        )
+        : <td colSpan={2} />
+      }
     </>
   );
 }
