5144666f26940acc3c5ef7d998a15a25ab7fa375
Jaco <jacogr@gmail.com>
2023-01-17 10:06:24 +0200

    Don't do additional table split

--- packages/page-staking2/src/Validators/index.tsx
+++ packages/page-staking2/src/Validators/index.tsx
@@ -3,7 +3,7 @@
 
 import type { SessionInfo, Validator } from '../types';
 
-import React, { useMemo, useRef } from 'react';
+import React, { useRef } from 'react';
 import styled from 'styled-components';
 
 import Legend from '@polkadot/app-staking/Legend';
@@ -21,96 +21,39 @@ interface Props {
   validatorsSession?: Validator[];
 }
 
-interface ValidatorSplit {
-  validatorsActive?: Validator[],
-  validatorsFavorite?: Validator[]
-}
-
-function splitValidators (validators?: Validator[]): ValidatorSplit {
-  if (!validators) {
-    return {};
-  }
-
-  const validatorsFavorite = validators.filter((v) => v.isFavorite);
-
-  return validatorsFavorite.length
-    ? {
-      validatorsActive: validators.filter((v) => !v.isFavorite),
-      validatorsFavorite
-    }
-    : { validatorsActive: validators };
-}
-
 function Validators ({ className = '', isRelay, sessionInfo, toggleFavorite, validatorsSession }: Props): React.ReactElement<Props> {
   const { t } = useTranslation();
   const points = usePoints(sessionInfo);
-
-  const { validatorsActive, validatorsFavorite } = useMemo(
-    () => splitValidators(validatorsSession),
-    [validatorsSession]
-  );
 
   const headerActive = useRef<[string?, string?, number?][]>([
     // favorite, badges, details, expand
     [t<string>('validators'), 'start', 4]
   ]);
 
-  const headerFavorite = useRef<[string?, string?, number?][]>([
-    // favorite, badges, details, expand
-    [t<string>('favorites'), 'start', 4]
-  ]);
-
   return (
-    <>
-      {validatorsFavorite && (
-        <StyledTable
-          className={className}
-          header={headerFavorite.current}
-          isSplit
-          legend={
-            <Legend
-              isRelay={isRelay}
-            />
-          }
-        >
-          {validatorsFavorite.map((v) => (
-            <ValidatorRow
-              isRelay={isRelay}
-              key={v.key}
-              points={points?.[v.stashId]}
-              sessionInfo={sessionInfo}
-              toggleFavorite={toggleFavorite}
-              validator={v}
-            />
-          ))}
-        </StyledTable>
-      )}
-      <StyledTable
-        className={className}
-        empty={validatorsActive && t<string>('No session validators found')}
-        emptySpinner={t<string>('Retrieving session validators')}
-        header={headerActive.current}
-        isSplit
-        legend={
-          !validatorsFavorite && (
-            <Legend
-              isRelay={isRelay}
-            />
-          )
-        }
-      >
-        {validatorsActive?.map((v) => (
-          <ValidatorRow
-            isRelay={isRelay}
-            key={v.key}
-            points={points?.[v.stashId]}
-            sessionInfo={sessionInfo}
-            toggleFavorite={toggleFavorite}
-            validator={v}
-          />
-        ))}
-      </StyledTable>
-    </>
+    <StyledTable
+      className={className}
+      empty={validatorsSession && t<string>('No session validators found')}
+      emptySpinner={t<string>('Retrieving session validators')}
+      header={headerActive.current}
+      isSplit
+      legend={
+        <Legend
+          isRelay={isRelay}
+        />
+      }
+    >
+      {validatorsSession?.map((v) => (
+        <ValidatorRow
+          isRelay={isRelay}
+          key={v.key}
+          points={points?.[v.stashId]}
+          sessionInfo={sessionInfo}
+          toggleFavorite={toggleFavorite}
+          validator={v}
+        />
+      ))}
+    </StyledTable>
   );
 }
 
