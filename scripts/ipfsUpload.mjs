ba88c8e709299f375c4b9f4f5b650192fddb950c
Jaco <jacogr@gmail.com>
2023-01-30 13:18:44 +0200

    CHANGELOG (#8892)

--- scripts/ipfsUpload.mjs
+++ scripts/ipfsUpload.mjs
@@ -6,7 +6,7 @@ import pinataSDK from '@pinata/sdk';
 import cloudflare from 'dnslink-cloudflare';
 import fs from 'fs';
 
-import { execSync } from '@polkadot/dev/scripts/execute.mjs';
+import { execSync } from '@polkadot/dev/scripts/util.mjs';
 
 import { createWsEndpoints } from '../packages/apps-config/build/endpoints/index.js';
 
