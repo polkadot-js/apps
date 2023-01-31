b6b8fb3d8080614ddab7036a94a8b587d0bf51b3
Jaco <jacogr@gmail.com>
2023-01-31 11:50:48 +0200

    Adjust

--- packages/react-hooks/src/useWeight.ts
+++ packages/react-hooks/src/useWeight.ts
@@ -45,11 +45,18 @@ const EMPTY_STATE: Partial<Result> = {
 // return both v1 & v2 weight structures (would depend on actual use)
 export function convertWeight (weight: V1Weight | V2Weight): { v1Weight: BN, v2Weight: V2WeightConstruct } {
   if ((weight as V2Weight).proofSize) {
+    // V2 weight
     const refTime = (weight as V2Weight).refTime.toBn();
 
     return { v1Weight: refTime, v2Weight: weight as V2Weight };
+  } else if ((weight as V2Weight).refTime) {
+    // V1.5 weight (when not converted)
+    const refTime = (weight as V2Weight).refTime.toBn();
+
+    return { v1Weight: refTime, v2Weight: { refTime } };
   }
 
+  // V1 weight
   const refTime = (weight as V1Weight).toBn();
 
   return { v1Weight: refTime, v2Weight: { refTime } };
