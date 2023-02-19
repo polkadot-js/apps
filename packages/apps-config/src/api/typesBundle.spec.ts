dba79caea75453c65268ec2b9869fd9c78dfbeef
Jaco <jacogr@gmail.com>
2023-02-19 18:14:23 +0200

    Fix frequency package name

--- packages/apps-config/src/api/typesBundle.spec.ts
+++ packages/apps-config/src/api/typesBundle.spec.ts
@@ -11,7 +11,7 @@ import chain from './chain';
 import spec from './spec';
 
 // incorrect packages without type specifier
-const PKG_FIX = ['@frequency/api-augment'];
+const PKG_FIX = ['@frequency-chain/api-augment'];
 
 it('generates the typesBundle', (): void => {
   PKG_FIX.forEach((f) => {
