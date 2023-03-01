7946f405afd6ebd0aeb1f5136f66b6d3349d7b9a
Jaco <jacogr@gmail.com>
2023-03-01 15:27:21 +0200

    Disable unreachable endpoints

--- packages/apps-config/src/endpoints/productionRelayPolkadot.ts
+++ packages/apps-config/src/endpoints/productionRelayPolkadot.ts
@@ -608,8 +608,8 @@ export const prodParasPolkadotCommon: EndpointOption[] = [
       Dwellir: 'wss://statemint-rpc.dwellir.com',
       'Dwellir Tunisia': 'wss://statemint-rpc-tn.dwellir.com',
       OnFinality: 'wss://statemint.api.onfinality.io/public-ws',
-      Parity: 'wss://statemint-rpc.polkadot.io',
-      RadiumBlock: 'wss://statemint.public.curie.radiumblock.co/ws'
+      Parity: 'wss://statemint-rpc.polkadot.io'
+      // RadiumBlock: 'wss://statemint.public.curie.radiumblock.co/ws' // https://github.com/polkadot-js/apps/issues/9089
     },
     teleport: [-1],
     text: 'Statemint',
