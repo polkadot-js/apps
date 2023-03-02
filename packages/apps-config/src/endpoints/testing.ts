2030ed34d82cd2356f0128f2812e4b2eb1360541
James Bayly <46693720+jamesbayly@users.noreply.github.com>
2023-03-02 17:42:39 +1300

    Added OnFinality endpoints after testing liveness against GitHub checks (#9084)
    * Restore OnFinality Endpoints
    
    * Update chain-endpoints.yml
    
    * Update chain-endpoints.yml
    
    * Update productionRelayPolkadot.ts
    
    * Final update
    
    * Fix linting issues

--- packages/apps-config/src/endpoints/testing.ts
+++ packages/apps-config/src/endpoints/testing.ts
@@ -488,6 +488,7 @@ export const testChains: EndpointOption[] = [
   },
   {
     info: 'acala',
+    isUnreachable: true,
     providers: {
       // Acala: 'wss://mandala.polkawallet.io' // https://github.com/polkadot-js/apps/issues/9005
       // OnFinality: 'wss://acala-mandala.api.onfinality.io/public-ws' // https://github.com/polkadot-js/apps/issues/8105
