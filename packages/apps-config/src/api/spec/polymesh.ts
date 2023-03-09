3a51c3c160491534c05e193b20808a825599c72f
F-OBrien <francis@polymesh.network>
2023-03-09 13:59:09 +0000

    Use @polymeshassociation/polymesh-types

--- packages/apps-config/src/api/spec/polymesh.ts
+++ packages/apps-config/src/api/spec/polymesh.ts
@@ -3,15 +3,16 @@
 
 import type { OverrideBundleDefinition } from '@polkadot/types/types';
 
-import schema from '@polymathnetwork/polymesh-types';
+import schema from '@polymeshassociation/polymesh-types';
 
 const definitions: OverrideBundleDefinition = {
-  rpc: schema.rpc,
+  rpc: schema.default.rpc,
+  signedExtensions: schema.default.signedExtensions,
   types: [
     {
       // on all versions
       minmax: [0, undefined],
-      types: schema.types
+      types: schema.default.types
     }
   ]
 };
