3373cf9b0cfc569178a2037c7618b0a9053356fc
Jaco <jacogr@gmail.com>
2023-03-15 09:27:55 +0200

    Adjust

--- packages/apps-config/src/api/spec/polymesh.ts
+++ packages/apps-config/src/api/spec/polymesh.ts
@@ -1,10 +1,6 @@
 // Copyright 2017-2023 @polkadot/apps-config authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
-import type { OverrideBundleDefinition } from '@polkadot/types/types';
-
 import { typesBundle } from '@polymeshassociation/polymesh-types';
 
-const definitions: OverrideBundleDefinition = typesBundle.spec;
-
-export default definitions;
+export default typesBundle.spec.polymesh;
