d26c6a7f20b151ec13cae4102e6f1796d3b8af50
Alex <56650632+alexxxkny@users.noreply.github.com>
2023-03-18 12:54:11 +0300

    Add Curio parachain (#9183)
    * curio-devnet, curio-testnet runtime-api definitions
    
    * curio testnet ui settings
    
    * lint

---
+++ packages/apps-config/src/api/spec/curio.ts
@@ -0,0 +1,48 @@
+// Copyright 2017-2023 @polkadot/apps-config authors & contributors
+// SPDX-License-Identifier: Apache-2.0
+
+import type { OverrideBundleDefinition } from '@polkadot/types/types';
+
+/* eslint-disable sort-keys */
+const definitions: OverrideBundleDefinition = {
+  types: [
+    {
+      // on all versions
+      minmax: [0, undefined],
+      types: {
+        StakingRates: {
+          collatorStakingRate: 'Perquintill',
+          collatorRewardRate: 'Perquintill',
+          delegatorStakingRate: 'Perquintill',
+          delegatorRewardRate: 'Perquintill'
+        }
+      }
+    }
+  ],
+  runtime: {
+    Staking: [
+      {
+        methods: {
+          get_staking_rates: {
+            description: 'Calculate the current staking and reward rates for collators and delegators',
+            params: [],
+            type: 'StakingRates'
+          },
+          get_unclaimed_staking_rewards: {
+            description: 'Calculate the claimable staking rewards for a given account address',
+            params: [
+              {
+                name: 'account',
+                type: 'AccountId32'
+              }
+            ],
+            type: 'Balance'
+          }
+        },
+        version: 1
+      }
+    ]
+  }
+};
+
+export default definitions;
