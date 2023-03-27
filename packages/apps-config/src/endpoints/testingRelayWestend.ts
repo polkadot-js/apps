6de8ed030b9d561c8e6e1ee253da258b6afc3ef5
Yiwei <yli497@aucklanduni.ac.nz>
2023-03-28 11:26:47 +1300

    revert

--- packages/apps-config/src/endpoints/testingRelayWestend.ts
+++ packages/apps-config/src/endpoints/testingRelayWestend.ts
@@ -145,6 +145,7 @@ export const testParasWestendCommon: EndpointOption[] = [
       'Dwellir Tunisia': 'wss://westmint-rpc-tn.dwellir.com',
       'IBP-GeoDNS1': 'wss://sys.ibp.network/westmint',
       'IBP-GeoDNS2': 'wss://sys.dotters.network/westmint',
+      OnFinality: 'wss://westmint.api.onfinality.io/public-ws',
       Parity: 'wss://westmint-rpc.polkadot.io'
     },
     teleport: [-1],
