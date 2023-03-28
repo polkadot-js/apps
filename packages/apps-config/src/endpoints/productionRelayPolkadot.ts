e2c5fd87d0020885f240026730ea4e2f252e1f06
Oleh Mell <oleh.melnechyk@gmail.com>
2023-03-28 11:44:16 +0300

    Move Subsocial RPC from Kusama to Polkadot (#9218)
    * Add Subsocial Polkadot parachain
    
    * Move Subsocial RPC from Kusama to Polkadot
    
    * Update after linter check

--- packages/apps-config/src/endpoints/productionRelayPolkadot.ts
+++ packages/apps-config/src/endpoints/productionRelayPolkadot.ts
@@ -593,11 +593,11 @@ export const prodParasPolkadot: EndpointOption[] = [
     info: 'subsocial',
     paraId: 2101,
     providers: {
-      // 'Dappforce': 'wss://para.subsocial.network'
+      Dappforce: 'wss://para.subsocial.network'
     },
     text: 'Subsocial',
     ui: {
-      color: '#69058C',
+      color: '#b9018c',
       logo: nodesSubsocialSVG
     }
   },
