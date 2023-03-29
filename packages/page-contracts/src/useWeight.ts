d317ee88881025c63cf90cd80ef9ee04f25ac8d8
Nikhil Ranjan <niklabh811@gmail.com>
2023-03-29 14:19:47 +0200

    WeightsV2 support for contracts pallet (#8538)
    * Weights V2 support
    
    * estimated v2
    
    * instantiate
    
    * [CI Skip] bump/beta 0.124.2-109-x
    
    skip-checks: true
    
    * contract call dry run
    
    * lint fix
    
    * uploading raw wasm type
    
    ---------
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- packages/page-contracts/src/useWeight.ts
+++ packages/page-contracts/src/useWeight.ts
@@ -1,7 +1,7 @@
 // Copyright 2017-2023 @polkadot/react-hooks authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
-import type { Weight } from '@polkadot/types/interfaces';
+import type { Weight, WeightV2 } from '@polkadot/types/interfaces';
 import type { BN } from '@polkadot/util';
 import type { UseWeight } from './types.js';
 
@@ -9,17 +9,28 @@ import { useCallback, useMemo, useState } from 'react';
 
 import { createNamedHook, useApi, useBlockInterval } from '@polkadot/react-hooks';
 import { convertWeight } from '@polkadot/react-hooks/useWeight';
-import { BN_MILLION, BN_TEN, BN_ZERO } from '@polkadot/util';
+import { BN_MILLION, BN_ONE, BN_TEN, BN_ZERO } from '@polkadot/util';
 
 function useWeightImpl (): UseWeight {
   const { api } = useApi();
   const blockTime = useBlockInterval();
+  const isWeightV2 = !!api.registry.createType<WeightV2>('Weight').proofSize;
   const [megaGas, _setMegaGas] = useState<BN>(
     convertWeight(
       api.consts.system.blockWeights
         ? api.consts.system.blockWeights.maxBlock
         : api.consts.system.maximumBlockWeight as Weight
     ).v1Weight.div(BN_MILLION).div(BN_TEN)
+  );
+  const [megaRefTime, _setMegaRefTime] = useState<BN>(
+    api.consts.system.blockWeights
+      ? api.consts.system.blockWeights.perClass.normal.maxExtrinsic.unwrapOrDefault().refTime.toBn().div(BN_MILLION).div(BN_TEN)
+      : BN_ZERO
+  );
+  const [proofSize, _setProofSize] = useState<BN>(
+    api.consts.system.blockWeights
+      ? api.consts.system.blockWeights.perClass.normal.maxExtrinsic.unwrapOrDefault().proofSize.toBn()
+      : BN_ZERO
   );
   const [isEmpty, setIsEmpty] = useState(false);
 
@@ -32,11 +43,33 @@ function useWeightImpl (): UseWeight {
       ).v1Weight.div(BN_MILLION).div(BN_TEN)),
     [api]
   );
+  const setMegaRefTime = useCallback(
+    (value?: BN | undefined) =>
+      _setMegaRefTime(
+        value || api.consts.system.blockWeights
+          ? api.consts.system.blockWeights.perClass.normal.maxExtrinsic.unwrapOrDefault().refTime.toBn().div(BN_MILLION).div(BN_TEN)
+          : BN_ZERO
+      ),
+    [api]
+  );
+  const setProofSize = useCallback(
+    (value?: BN | undefined) =>
+      _setProofSize(
+        value || api.consts.system.blockWeights
+          ? api.consts.system.blockWeights.perClass.normal.maxExtrinsic.unwrapOrDefault().proofSize.toBn()
+          : BN_ZERO
+      ),
+    [api]
+  );
 
   return useMemo((): UseWeight => {
     let executionTime = 0;
     let percentage = 0;
     let weight = BN_ZERO;
+    let weightV2 = api.registry.createType('WeightV2', {
+      proofSize: BN_ZERO,
+      refTime: BN_ZERO
+    });
     let isValid = false;
 
     if (megaGas) {
@@ -53,17 +86,40 @@ function useWeightImpl (): UseWeight {
       isValid = !megaGas.isZero() && percentage < 65;
     }
 
+    if (isWeightV2 && megaRefTime && proofSize) {
+      weightV2 = api.registry.createType('WeightV2', {
+        proofSize,
+        refTime: megaRefTime.mul(BN_MILLION)
+      });
+      executionTime = megaRefTime.mul(BN_MILLION).mul(blockTime).div(
+        api.consts.system.blockWeights
+          ? api.consts.system.blockWeights.perClass.normal.maxExtrinsic.unwrapOrDefault().refTime.toBn()
+          : BN_ONE
+      ).toNumber();
+      percentage = (executionTime / blockTime.toNumber()) * 100;
+
+      // execution is 2s of 6s blocks, i.e. 1/3
+      executionTime = executionTime / 3000;
+      isValid = !megaRefTime.isZero(); // && percentage < 65;
+    }
+
     return {
       executionTime,
       isEmpty,
       isValid: isEmpty || isValid,
+      isWeightV2,
       megaGas: megaGas || BN_ZERO,
+      megaRefTime: megaRefTime || BN_ZERO,
       percentage,
+      proofSize: proofSize || BN_ZERO,
       setIsEmpty,
       setMegaGas,
-      weight
+      setMegaRefTime,
+      setProofSize,
+      weight,
+      weightV2
     };
-  }, [api, blockTime, isEmpty, megaGas, setIsEmpty, setMegaGas]);
+  }, [api, blockTime, isEmpty, isWeightV2, megaGas, megaRefTime, proofSize, setIsEmpty, setMegaGas, setMegaRefTime, setProofSize]);
 }
 
 export default createNamedHook('useWeight', useWeightImpl);
