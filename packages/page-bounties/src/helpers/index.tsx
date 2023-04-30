451d4e318f051ba8f3f399cfd9b5ed2de1cda2a2
Jaco <jacogr@gmail.com>
2023-04-30 10:20:05 +0300

    0.129.1 (#9387)
    * 0.129.1
    
    * linting

--- packages/page-bounties/src/helpers/index.tsx
+++ packages/page-bounties/src/helpers/index.tsx
@@ -2,8 +2,8 @@
 // SPDX-License-Identifier: Apache-2.0
 
 export * from './calculateBountyBond.js';
+export * from './determineUnassignCuratorAction.js';
 export * from './getBountyStatus.js';
 export * from './isClaimable.js';
-export * from './stringHelpers.js';
-export * from './determineUnassignCuratorAction.js';
 export * from './permillOf.js';
+export * from './stringHelpers.js';
