d05c393cf577c305f1c3a9ffadadf00a37cc83f6
Jaco <jacogr@gmail.com>
2023-05-12 10:20:55 +0300

    Basic NIS dashboard

---
+++ packages/page-nis/src/index.tsx
@@ -0,0 +1,48 @@
+// Copyright 2017-2023 @polkadot/app-nis authors & contributors
+// SPDX-License-Identifier: Apache-2.0
+
+import React, { useRef } from 'react';
+import { Route, Routes } from 'react-router';
+
+import { Tabs } from '@polkadot/react-components';
+
+import Overview from './Overview/index.js';
+import { useTranslation } from './translate.js';
+
+interface Props {
+  basePath: string;
+  className?: string;
+}
+
+function NisApp ({ basePath, className }: Props): React.ReactElement<Props> {
+  const { t } = useTranslation();
+
+  const tabsRef = useRef([
+    {
+      isRoot: true,
+      name: 'overview',
+      text: t<string>('Overview')
+    }
+  ]);
+
+  return (
+    <main className={className}>
+      <Tabs
+        basePath={basePath}
+        items={tabsRef.current}
+      />
+      <Routes>
+        <Route path={basePath}>
+          <Route
+            element={
+              <Overview />
+            }
+            index
+          />
+        </Route>
+      </Routes>
+    </main>
+  );
+}
+
+export default React.memo(NisApp);
