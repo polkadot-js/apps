eb4f9c37072c5838d48c1e6011a21d39023e5a16
3ierratango <118272976+3ierratango@users.noreply.github.com>
2023-05-16 10:57:40 +0400

    fix wrong AccountId32 map

--- packages/apps-config/src/api/spec/ferrum.ts
+++ packages/apps-config/src/api/spec/ferrum.ts
@@ -14,7 +14,6 @@ const definitions: OverrideBundleDefinition = {
       types: {
         AccountId: 'EthereumAccountId',
         AccountId20: 'EthereumAccountId',
-        AccountId32: 'EthereumAccountId',
         Address: 'AccountId',
         LookupSource: 'AccountId',
         Lookup0: 'AccountId',
