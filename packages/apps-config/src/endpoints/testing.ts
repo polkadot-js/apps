912a5a363726a285e637979d09f9171b631b9d43
Mell <oleh.melnechyk@gmail.com>
2023-05-24 17:12:07 +0300

    change order for xsocial

--- packages/apps-config/src/endpoints/testing.ts
+++ packages/apps-config/src/endpoints/testing.ts
@@ -792,16 +792,6 @@ export const testChains: Omit<EndpointOption, 'teleport'>[] = [
     }
   },
   {
-    info: 'xsocial',
-    providers: {
-      DappForce: 'wss://xsocial.subsocial.network' // https://github.com/polkadot-js/apps/issues/8315
-    },
-    text: 'xSocial',
-    ui: {
-      logo: nodesXSocialPNG
-    }
-  },
-  {
     info: 'sora-substrate',
     providers: {
       'Soramitsu #1': 'wss://ws.framenode-1.s1.stg1.sora2.soramitsu.co.jp',
@@ -985,6 +975,16 @@ export const testChains: Omit<EndpointOption, 'teleport'>[] = [
     ui: {
       color: '#000000',
       logo: nodesWeb3gamesSVG
+    }
+  },
+  {
+    info: 'xsocial',
+    providers: {
+      DappForce: 'wss://xsocial.subsocial.network' // https://github.com/polkadot-js/apps/issues/8315
+    },
+    text: 'xSocial',
+    ui: {
+      logo: nodesXSocialPNG
     }
   },
   {
