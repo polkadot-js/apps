163d518980b1fe33b0606ad127891675f8753fba
Jaco <jacogr@gmail.com>
2023-05-25 12:05:27 +0300

    All non-pruned sessions

--- packages/page-parachains/src/Disputes/useSessionDisputes.ts
+++ packages/page-parachains/src/Disputes/useSessionDisputes.ts
@@ -1,63 +1,71 @@
 // Copyright 2017-2023 @polkadot/app-parachains authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
+import type { ApiPromise } from '@polkadot/api';
 import type { Option, StorageKey, u32 } from '@polkadot/types';
 import type { PolkadotPrimitivesV4DisputeState } from '@polkadot/types/lookup';
 import type { Codec } from '@polkadot/types/types';
 import type { HexString } from '@polkadot/util/types';
 import type { DisputeInfo, SessionInfo } from './types.js';
 
-import { useMemo } from 'react';
+import { useEffect, useState } from 'react';
 
-import { createNamedHook, useApi, useMapEntries } from '@polkadot/react-hooks';
+import { createNamedHook, useApi } from '@polkadot/react-hooks';
 import { formatNumber } from '@polkadot/util';
 
 import useSessionInfo from './useSessionInfo.js';
 
-const OPT_ENTRIES = {
-  transform: (entries: [StorageKey<[u32, Codec]>, Option<PolkadotPrimitivesV4DisputeState>][]): [string, HexString, boolean[]][] =>
-    entries
-      .map(([{ args: [session, id] }, optInfo]): [string, HexString, PolkadotPrimitivesV4DisputeState | null] => [formatNumber(session), id.toHex(), optInfo.unwrapOr(null)])
-      .filter((d): d is [string, HexString, PolkadotPrimitivesV4DisputeState] => !!d[2])
-      .map(([s, k, d]) => [s, k, d.validatorsAgainst.toBoolArray()])
-};
+function queryDisputes (api: ApiPromise, sessionInfo: SessionInfo): Promise<DisputeInfo> {
+  return Promise
+    .all(
+      sessionInfo.sessionIndexes.map((index) =>
+        api.query.parasDisputes.disputes.entries(index) as unknown as Promise<[StorageKey<[u32, Codec]>, Option<PolkadotPrimitivesV4DisputeState>][]>
+      )
+    )
+    .then((entries) => ({
+      disputes: entries.reduce<Record<string, Record<HexString, string[]>>>((all, list) => {
+        list
+          .map(([{ args: [session, id] }, optInfo]): [string, HexString, PolkadotPrimitivesV4DisputeState | null] => [formatNumber(session), id.toHex(), optInfo.unwrapOr(null)])
+          .filter((d): d is [string, HexString, PolkadotPrimitivesV4DisputeState] => !!d[2])
+          .map(([s, k, d]): [string, HexString, boolean[]] => [s, k, d.validatorsAgainst.toBoolArray()])
+          .forEach(([s, k, v]) => {
+            if (!all[s]) {
+              all[s] = {};
+            }
 
-function extractDisputes (sessionInfo?: SessionInfo, disputes?: [string, HexString, boolean[]][]): DisputeInfo | undefined {
-  if (!sessionInfo) {
-    return undefined;
-  } else if (!disputes) {
-    return { sessionInfo };
-  }
+            all[s][k] = v
+              .map((f, index) =>
+                f
+                  ? sessionInfo.paraValidators[index]
+                  : null
+              )
+              .filter((v): v is string => !!v);
+          });
 
-  return {
-    disputes: disputes.reduce<Record<string, Record<HexString, string[]>>>((all, [s, k, v]) => {
-      if (!all[s]) {
-        all[s] = {};
-      }
-
-      all[s][k] = v
-        .map((f, index) =>
-          f
-            ? sessionInfo.paraValidators[index]
-            : null
-        )
-        .filter((v): v is string => !!v);
-
-      return all;
-    }, {}),
-    sessionInfo
-  };
+        return all;
+      }, {}),
+      sessionInfo
+    }));
 }
 
 function useSessionDisputesImpl (): DisputeInfo | undefined {
   const { api } = useApi();
-  const session = useSessionInfo();
-  const disputedBv = useMapEntries(session && api.query.parasDisputes?.disputes, [session?.sessionCurrentIndex], OPT_ENTRIES);
+  const [state, setState] = useState<DisputeInfo | undefined>();
+  const sessionInfo = useSessionInfo();
 
-  return useMemo(
-    () => extractDisputes(session, disputedBv),
-    [disputedBv, session]
-  );
+  useEffect((): void => {
+    if (sessionInfo) {
+      if (sessionInfo.sessionIndexes) {
+        queryDisputes(api, sessionInfo)
+          .then(setState)
+          .catch(console.error);
+      } else {
+        setState({ sessionInfo });
+      }
+    }
+  }, [api, sessionInfo]);
+
+  return state;
 }
 
 export default createNamedHook('useSessionDisputes', useSessionDisputesImpl);
