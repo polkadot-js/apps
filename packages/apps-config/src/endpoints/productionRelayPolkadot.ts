e1905b146739abcc51004f5fd5e181a91b6b7f77
Stakeworld <info@stakeworld.io>
2023-06-04 10:56:58 +0200

    Add statemint RPC

--- packages/apps-config/src/endpoints/productionRelayPolkadot.ts
+++ packages/apps-config/src/endpoints/productionRelayPolkadot.ts
@@ -661,7 +661,8 @@ export const prodParasPolkadotCommon: EndpointOption[] = [
       'IBP-GeoDNS2': 'wss://sys.dotters.network/statemint',
       OnFinality: 'wss://statemint.api.onfinality.io/public-ws',
       Parity: 'wss://statemint-rpc.polkadot.io',
-      RadiumBlock: 'wss://statemint.public.curie.radiumblock.co/ws'
+      RadiumBlock: 'wss://statemint.public.curie.radiumblock.co/ws',
+      Stakeworld: 'wss://dot-rpc.stakeworld.io/statemint'
     },
     teleport: [-1],
     text: 'Statemint',
