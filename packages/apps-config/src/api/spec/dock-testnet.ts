12cde37562227d91b298ee7a6d5cdd87126abe6b
Jaco <jacogr@gmail.com>
2023-06-10 08:05:20 +0300

    Remove Efinity providers (#9561)

--- packages/apps-config/src/api/spec/dock-testnet.ts
+++ packages/apps-config/src/api/spec/dock-testnet.ts
@@ -4,9 +4,6 @@
 import type { OverrideBundleDefinition } from '@polkadot/types/types';
 
 // @ts-expect-error No definitions provided in package
-import types from '@docknetwork/node-types';
+import { spec } from '@docknetwork/node-types';
 
-// eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
-const definitions = (types as { spec: { 'dock-test-runtime': OverrideBundleDefinition } }).spec['dock-test-runtime'];
-
-export default definitions;
+export default (spec as { 'dock-test-runtime': OverrideBundleDefinition })['dock-test-runtime'];
