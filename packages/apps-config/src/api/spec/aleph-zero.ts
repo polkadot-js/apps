ac67ebf44af69f32142c8096b783d7db7d8f170b
Piotr Mikołajczyk <piomiko41@gmail.com>
2023-06-14 12:51:07 +0200

    A0-2682: Expose runtime API (#68)
    * Expose runtime API
    
    * Linter, missing call

--- packages/apps-config/src/api/spec/aleph-zero.ts
+++ packages/apps-config/src/api/spec/aleph-zero.ts
@@ -1,6 +1,9 @@
 // Copyright 2017-2023 @polkadot/apps-config authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
+// Structs and enum variants need to be in order
+/* eslint-disable sort-keys */
+
 export default {
   rpc: {
     alephNode: {
@@ -33,5 +36,94 @@ export default {
         type: 'Option<AccountId>'
       }
     }
-  }
+  },
+  runtime: {
+    AlephSessionApi: [
+      {
+        methods: {
+          authorities: {
+            description: '',
+            params: [],
+            type: 'Vec<AuthorityId>'
+          },
+          authority_data: {
+            description: '',
+            params: [],
+            type: 'SessionAuthorityData'
+          },
+          finality_version: {
+            description: '',
+            params: [],
+            type: 'Version'
+          },
+          millisecs_per_block: {
+            description: '',
+            params: [],
+            type: 'u64'
+          },
+          next_session_authorities: {
+            description: '',
+            params: [],
+            type: 'Result<Vec<AuthorityId>, ApiError>'
+          },
+          next_session_authority_data: {
+            description: '',
+            params: [],
+            type: 'Result<SessionAuthorityData, ApiError>'
+          },
+          next_session_finality_version: {
+            description: '',
+            params: [],
+            type: 'Version'
+          },
+          predict_session_committee: {
+            description: '',
+            params: [
+              {
+                name: 'session',
+                type: 'SessionIndex'
+              }
+            ],
+            type: 'Result<SessionCommittee, SessionValidatorError>'
+          },
+          session_period: {
+            description: '',
+            params: [],
+            type: 'u32'
+          }
+        },
+        version: 1
+      }
+    ]
+  },
+  types: [
+    {
+      // supported on all versions
+      minmax: [0, undefined],
+      types: {
+        ApiError: {
+          _enum: ['DecodeKey']
+        },
+        SessionAuthorityData: {
+          authorities: 'Vec<AuthorityId>',
+          emergency_finalizer: 'Option<AuthorityId>'
+        },
+        SessionCommittee: {
+          finality_committee: 'Vec<AccountId>',
+          block_producers: 'Vec<AccountId>'
+        },
+        SessionNotWithinRangeError: {
+          lower_limit: 'SessionIndex',
+          upper_limit: 'SessionIndex'
+        },
+        SessionValidatorError: {
+          _enum: {
+            SessionNotWithinRange: 'SessionNotWithinRangeError',
+            Other: 'Vec<u8>'
+          }
+        },
+        Version: 'u32'
+      }
+    }
+  ]
 };
