742c15e5e8327e360e7e5674324e9c38ac394183
ghalielouarzazi@hotmail.com <ghalielouarzazi@hotmail.com>
2023-07-12 14:43:55 +0200

    add header to config.ts

--- packages/apps-config/src/variables/config.ts
+++ packages/apps-config/src/variables/config.ts
@@ -1,4 +1,12 @@
-export default {
-  LCURL: process.env['LCURL'] as string || 'https://kate.avail.tools',
-  TESTNETURL: process.env['TESTNETURL'] as `wss://${string}` || 'wss://kate.avail.tools/ws'
-};
+// Copyright 2017-2023 @polkadot/apps-config authors & contributors
+// SPDX-License-Identifier: Apache-2.0
+
+const LCURL = process.env['LCURL'] as string || 'https://kate.avail.tools'
+const TESTNETURL = process.env['TESTNETURL'] as `wss://${string}` || 'wss://kate.avail.tools/ws'
+
+const config = {
+  LCURL,
+  TESTNETURL
+}
+
+export default config
