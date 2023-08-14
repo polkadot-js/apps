62ba66097051e21149376b8aed99d042543bf830
Xing <mikespook@gmail.com>
2023-08-14 19:09:23 +1200

    added OnFinality API for Bitgreen (#9782)
    * Add OnFinality Westmint endpoint
    
    * added OnFinality API for BridgeHub Kusama
    
    * added OnFinality API for BridgeHub Westend
    
    * added OnFinality BridgeHub Polkadot API
    
    * Added multiple OnFinality API
    
    * Added OnFinality Equilibrium API
    
    * added OnFinality API for Bitgreen
    
    * added OnFinality API for Darwinia
    
    ---------
    
    Co-authored-by: Mike Xing <mike.xing@onfinality.io>

--- packages/apps-config/src/endpoints/productionRelayPolkadot.ts
+++ packages/apps-config/src/endpoints/productionRelayPolkadot.ts
@@ -115,7 +115,8 @@ export const prodParasPolkadot: Omit<EndpointOption, 'teleport'>[] = [
     info: 'bitgreen',
     paraId: 2048,
     providers: {
-      Bitgreen: 'wss://mainnet.bitgreen.org'
+      Bitgreen: 'wss://mainnet.bitgreen.org',
+      OnFinality: 'wss://bitgreen.api.onfinality.io/public-ws'
     },
     text: 'Bitgreen',
     ui: {
@@ -200,7 +201,8 @@ export const prodParasPolkadot: Omit<EndpointOption, 'teleport'>[] = [
     providers: {
       Darwinia: 'wss://rpc.darwinia.network',
       'Darwinia Community': 'wss://darwinia-rpc.darwiniacommunitydao.xyz',
-      Dwellir: 'wss://darwinia-rpc.dwellir.com'
+      Dwellir: 'wss://darwinia-rpc.dwellir.com',
+      OnFinality: 'wss://darwinia2.api.onfinality.io/public-ws'
     },
     text: 'Darwinia2',
     ui: {
