bb75a7b27fc404143af3c9529b340e3c15deccb9
Jaco <jacogr@gmail.com>
2023-08-23 14:36:40 +0300

    Bump deps (#9815)
    * Bump deps
    
    * Adjust withTranslation usage
    
    * Omit<T, 't'>
    
    * Adjust translate Props
    
    * Adjust straglers

--- packages/react-components/src/translate.ts
+++ packages/react-components/src/translate.ts
@@ -1,10 +1,14 @@
 // Copyright 2017-2023 @polkadot/react-components authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
+import type React from 'react';
+
 import { useTranslation as useTranslationBase, withTranslation } from 'react-i18next';
 
 export function useTranslation (): { t: (key: string, options?: { replace: Record<string, unknown> }) => string } {
   return useTranslationBase('react-components');
 }
 
-export default withTranslation(['react-components']);
+export default function translate <T, P = Omit<T, 't'>> (C: React.ComponentClass<T>): React.ComponentType<P> {
+  return withTranslation(['react-components'])(C) as unknown as React.ComponentType<P>;
+}
