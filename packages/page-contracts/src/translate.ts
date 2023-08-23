6539fd0358deac169b10a18d7f53e4fcf5827854
Jaco <jacogr@gmail.com>
2023-08-23 14:24:43 +0300

    Adjust straglers

--- packages/page-contracts/src/translate.ts
+++ packages/page-contracts/src/translate.ts
@@ -1,10 +1,8 @@
 // Copyright 2017-2023 @polkadot/app-contracts authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
-import { useTranslation as useTranslationBase, withTranslation } from 'react-i18next';
+import { useTranslation as useTranslationBase } from 'react-i18next';
 
 export function useTranslation (): { t: (key: string, options?: { replace: Record<string, unknown> }) => string } {
   return useTranslationBase('app-contracts');
 }
-
-export default withTranslation(['app-contracts']);
