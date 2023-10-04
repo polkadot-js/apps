fe6f779228ea522781fb5a91b38474cfb1caa78d
MinjieTian <97288160+MinjieTian@users.noreply.github.com>
2023-10-05 06:44:40 +1300

    Add onfinaliy Aleph zero endpoint (#9926)
    Co-authored-by: Philip Tian <philip.tian@onfinality.io>

--- packages/apps-config/src/endpoints/production.ts
+++ packages/apps-config/src/endpoints/production.ts
@@ -32,8 +32,8 @@ export const prodChains: Omit<EndpointOption, 'teleport'>[] = [
     info: 'aleph',
     providers: {
       'Aleph Zero Foundation': 'wss://ws.azero.dev',
-      Dwellir: 'wss://aleph-zero-rpc.dwellir.com'
-      // OnFinality: 'wss://aleph-zero.api.onfinality.io/public-ws'
+      Dwellir: 'wss://aleph-zero-rpc.dwellir.com',
+      OnFinality: 'wss://aleph-zero.api.onfinality.io/public-ws'
     },
     text: 'Aleph Zero',
     ui: {
