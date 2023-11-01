47de649d1ef1a62aed3fb187fff9adb4b81d21e4
Douglas Acosta <douglasacostaaquino@gmail.com>
2023-11-01 12:31:29 -0400

    added new OnFinality testnet wss (#37)

--- packages/apps-config/src/endpoints/nodle.ts
+++ packages/apps-config/src/endpoints/nodle.ts
@@ -28,7 +28,7 @@ export function createNodle (t: TFunction, firstOnly: boolean, withSort: boolean
       info: 'nodle',
       text: t('rpc.nodle-paradis', 'Testnet', { ns: 'apps-config' }),
       providers: {
-        OnFinality: 'wss://nodle-paradis.api.onfinality.io/public-ws'
+        OnFinality: 'wss://node-6957502816543653888.lh.onfinality.io/ws?apikey=09b04494-3139-4b57-a5d1-e1c4c18748ce'
       },
       ui: {
         color: '#1ab394',
