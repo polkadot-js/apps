a56f1c88f46f2a8033cdf42273ab6b305bfa5382
Douglas Acosta <douglasacostaaquino@gmail.com>
2023-11-02 05:12:13 -0400

    Update new testnet wss url for Nodle - testingRelayRococo.ts (#10005)
    As Nodle will stop the support for "wss://nodle-paradis.api.onfinality.io/public-ws", is planned to use this one on public clients
    - wss://node-6957502816543653888.lh.onfinality.io/ws?apikey=09b04494-3139-4b57-a5d1-e1c4c18748ce

--- packages/apps-config/src/endpoints/testingRelayRococo.ts
+++ packages/apps-config/src/endpoints/testingRelayRococo.ts
@@ -356,7 +356,7 @@ export const testParasRococo: Omit<EndpointOption, 'teleport'>[] = [
     info: 'rococoNodle',
     paraId: 2026,
     providers: {
-      OnFinality: 'wss://nodle-paradis.api.onfinality.io/public-ws'
+      OnFinality: 'wss://node-6957502816543653888.lh.onfinality.io/ws?apikey=09b04494-3139-4b57-a5d1-e1c4c18748ce'
     },
     text: 'Nodle',
     ui: {
