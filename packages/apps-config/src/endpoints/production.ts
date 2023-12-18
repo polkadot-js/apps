6f7be56299e2148bdacfcd27b8e5234d35e417a4
Jaco <jacogr@gmail.com>
2023-12-18 09:47:32 +0200

    Update packages/apps-config/src/endpoints/production.ts

--- packages/apps-config/src/endpoints/production.ts
+++ packages/apps-config/src/endpoints/production.ts
@@ -525,7 +525,7 @@ export const prodChains: Omit<EndpointOption, 'teleport'>[] = [
   {
     info: 'ternoa',
     providers: {
-       CapsuleCorp: 'wss://mainnet.ternoa.network'
+      CapsuleCorp: 'wss://mainnet.ternoa.network'
     },
     text: 'Ternoa',
     ui: {
