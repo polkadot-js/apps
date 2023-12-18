06bf6f710e1e116ad45dd4a43c6e56c53d5f0bd2
Jaco <jacogr@gmail.com>
2023-12-18 19:26:31 +0200

    Remove unused import

--- packages/apps-routing/src/staking.ts
+++ packages/apps-routing/src/staking.ts
@@ -2,7 +2,7 @@
 // SPDX-License-Identifier: Apache-2.0
 
 import type { ApiPromise } from '@polkadot/api';
-import type { Option, u32, Vec } from '@polkadot/types';
+import type { u32, Vec } from '@polkadot/types';
 import type { PalletStakingExposure, PalletStakingStakingLedger } from '@polkadot/types/lookup';
 import type { Route, TFunction } from './types.js';
 
