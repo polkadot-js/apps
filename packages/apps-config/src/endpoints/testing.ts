f2b281b50ca1b5deb2d9889274e91ef547fa0373
Jaco <jacogr@gmail.com>
2023-12-19 11:13:25 +0200

    Disable unreachable endpoints (#10111)

--- packages/apps-config/src/endpoints/testing.ts
+++ packages/apps-config/src/endpoints/testing.ts
@@ -669,7 +669,7 @@ export const testChains: Omit<EndpointOption, 'teleport'>[] = [
   {
     info: 'pangoro',
     providers: {
-      'Darwinia Network': 'wss://pangoro-rpc.darwinia.network'
+      // 'Darwinia Network': 'wss://pangoro-rpc.darwinia.network' // https://github.com/polkadot-js/apps/issues/10109
     },
     text: 'Pangoro2',
     ui: {
