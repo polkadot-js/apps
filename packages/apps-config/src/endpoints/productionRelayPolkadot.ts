f69ffcd6d1a7f2cc250101b6a08ca60f17c1ddd7
Jaco <jacogr@gmail.com>
2024-01-09 13:32:11 +0200

    Linting

--- packages/apps-config/src/endpoints/productionRelayPolkadot.ts
+++ packages/apps-config/src/endpoints/productionRelayPolkadot.ts
@@ -595,7 +595,7 @@ export const prodParasPolkadot: Omit<EndpointOption, 'teleport'>[] = [
     info: 'polimec',
     paraId: 3344,
     providers: {
-      "Polimec Foundation": 'wss://rpc.polimec.org'
+      'Polimec Foundation': 'wss://rpc.polimec.org'
     },
     text: 'Polimec',
     ui: {
