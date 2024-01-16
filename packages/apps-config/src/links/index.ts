472cd984b8bb9a886f7b99e6010dbb6cb66a1e0b
Shankar D. Warang <shankar@edgeware.agency>
2024-01-16 12:20:03 +0530

    add edgscan.ink explorer (2/5)

--- packages/apps-config/src/links/index.ts
+++ packages/apps-config/src/links/index.ts
@@ -6,6 +6,7 @@ import type { ExternalDef } from './types.js';
 import { CereStats } from './cerestats.js';
 import { Commonwealth } from './commonwealth.js';
 import { Dotreasury } from './dotreasury.js';
+import { Edgscan } from './edgscan';
 import { KodaDot } from './kodadot.js';
 import { MoonbeamApps } from './moonbeamApps.js';
 import { Polkaholic } from './polkaholic.js';
@@ -21,6 +22,7 @@ export const externalLinks: Record<string, ExternalDef> = {
   CereStats,
   Commonwealth,
   Dotreasury,
+  Edgscan,
   KodaDot,
   MoonbeamApps,
   Polkaholic,
