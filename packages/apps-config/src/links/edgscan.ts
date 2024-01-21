17396df7819022610d34f00ac5901cc53d14a52e
Shankar D. Warang <shankar.w@outlook.com>
2024-01-21 16:06:38 +0530

    feat: add edgscan.ink explorer  (#10186)
    * add edgscan.ink explorer (1/5)
    
    * add edgscan.ink explorer (2/5)
    
    * add edgscan.ink explorer (3/5)
    
    * add edgscan.ink explorer (4/5)
    
    * add edgscan.ink explorer (5/5)
    
    * typo fix

---
+++ packages/apps-config/src/links/edgscan.ts
@@ -0,0 +1,25 @@
+// Copyright 2017-2024 @polkadot/apps-config authors & contributors
+// SPDX-License-Identifier: Apache-2.0
+
+import type { BN } from '@polkadot/util';
+import type { ExternalDef } from './types.js';
+
+import { externalEdgscanPNG } from '../ui/logos/external/index.js';
+
+export const Edgscan: ExternalDef = {
+  chains: {
+    Edgeware: 'edgeware'
+  },
+  create: (path: string, data: BN | number | string): string =>
+    `https://edgscan.ink/#/${path}/${data.toString()}`,
+  homepage: 'https://edgscan.ink/',
+  isActive: true,
+  paths: {
+    address: 'accounts',
+    block: 'blocks',
+    extrinsic: 'extrinsics'
+  },
+  ui: {
+    logo: externalEdgscanPNG
+  }
+};
