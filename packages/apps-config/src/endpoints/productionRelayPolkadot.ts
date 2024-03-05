69197a4a8f35e9ea42c5c15b0e28469943998f4c
brenzi <brenzi@users.noreply.github.com>
2024-03-05 14:05:11 +0100

    add metadata for integritee crowdloan (#10249)
    * add metadata for integritee crowdloan
    
    * swap order of entries
    
    * deactivate rpc on 2039 in favor of 3359
    
    * lint

--- packages/apps-config/src/endpoints/productionRelayPolkadot.ts
+++ packages/apps-config/src/endpoints/productionRelayPolkadot.ts
@@ -349,12 +349,26 @@ export const prodParasPolkadot: Omit<EndpointOption, 'teleport'>[] = [
     }
   },
   {
+    homepage: 'https://dot.crowdloan.integritee.network/',
+    info: 'integritee',
+    paraId: 3359,
+    providers: {
+      Dwellir: 'wss://integritee-rpc.dwellir.com',
+      Integritee: 'wss://polkadot.api.integritee.network'
+    },
+    text: 'Integritee Network',
+    ui: {
+      color: '#658ea9',
+      logo: nodesIntegriteeSVG
+    }
+  },
+  {
     homepage: 'https://integritee.network',
     info: 'integritee',
     paraId: 2039,
     providers: {
-      Dwellir: 'wss://integritee-rpc.dwellir.com',
-      Integritee: 'wss://polkadot.api.integritee.network'
+      // Dwellir: 'wss://integritee-rpc.dwellir.com',
+      // Integritee: 'wss://polkadot.api.integritee.network'
     },
     text: 'Integritee Shell',
     ui: {
