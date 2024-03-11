5d2d7714e7339d77f4e35158b6a921c32c98fb08
serkixenos <serki.xenos@gmail.com>
2024-03-11 18:00:41 +0100

    Peerplays types bundle

--- packages/apps-config/src/api/typesBundle.ts
+++ packages/apps-config/src/api/typesBundle.ts
@@ -70918,6 +70918,90 @@ export const typesBundle = {
         }
       }
     },
+    "peerplays": {
+      "rpc": {
+        "validatormanager": {
+          "activeValidators": {
+            "description": "Get the list of active validators",
+            "params": [],
+            "type": "Vec<AccountId>",
+            "isSubscription": false,
+            "jsonrpc": "validatormanager_activeValidators",
+            "method": "activeValidators",
+            "section": "validatormanager"
+          },
+          "currentMaintenanceIndex": {
+            "description": "Get current maintenance index",
+            "params": [],
+            "type": "MaintenanceIndex",
+            "isSubscription": false,
+            "jsonrpc": "validatormanager_currentMaintenanceIndex",
+            "method": "currentMaintenanceIndex",
+            "section": "validatormanager"
+          },
+          "nextMaintenanceIndex": {
+            "description": "Get next maintenance index",
+            "params": [],
+            "type": "MaintenanceIndex",
+            "isSubscription": false,
+            "jsonrpc": "validatormanager_nextMaintenanceIndex",
+            "method": "nextMaintenanceIndex",
+            "section": "validatormanager"
+          },
+          "offenceValidators": {
+            "description": "Get the list of active validators who made the offence",
+            "params": [],
+            "type": "Vec<(AccountId, AuthIndex)>",
+            "isSubscription": false,
+            "jsonrpc": "validatormanager_offenceValidators",
+            "method": "offenceValidators",
+            "section": "validatormanager"
+          },
+          "offlineValidators": {
+            "description": "Get the list of active validators who went offline",
+            "params": [],
+            "type": "Vec<(AccountId, AuthIndex)>",
+            "isSubscription": false,
+            "jsonrpc": "validatormanager_offlineValidators",
+            "method": "offlineValidators",
+            "section": "validatormanager"
+          },
+          "validatorsPool": {
+            "description": "Get the list of validator candidates",
+            "params": [],
+            "type": "Vec<AccountId>",
+            "isSubscription": false,
+            "jsonrpc": "validatormanager_validatorsPool",
+            "method": "validatorsPool",
+            "section": "validatormanager"
+          },
+        },
+      },
+      "types": [
+        {
+          "minmax": [
+            0,
+            null
+          ],
+          "types": {
+            "AccountId": "EthereumAccountId",
+            "AccountId20": "EthereumAccountId",
+            "AccountId32": "EthereumAccountId",
+            "Address": "AccountId",
+            "AuthIndex": "u32",
+            "EthereumSignature": {
+              "r": "H256",
+              "s": "H256",
+              "v": "U8"
+            },
+            "ExtrinsicSignature": "EthereumSignature",
+            "Lookup0": "AccountId",
+            "LookupSource": "AccountId",
+            "MaintenanceIndex": "u32",
+          }
+        }
+      ]
+    },
     "pendulum": {
       "rpc": {
         "issue": {
