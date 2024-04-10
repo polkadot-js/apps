48cc5e64a8ebce4a69b1d6e10bf82fbfdf60d50e
José Molina <jose@blockdeep.io>
2024-04-10 16:56:41 +0200

    use runtime APIs to query block time

--- packages/react-hooks/src/useBlockInterval.ts
+++ packages/react-hooks/src/useBlockInterval.ts
@@ -2,48 +2,29 @@
 // SPDX-License-Identifier: Apache-2.0
 
 import type { ApiPromise } from '@polkadot/api';
+import type { BabeGenesisConfiguration } from '@polkadot/types/interfaces/babe';
 
 import { useMemo } from 'react';
 
-import { BN, BN_THOUSAND, BN_TWO, bnMin } from '@polkadot/util';
+import { BN } from '@polkadot/util';
 
 import { createNamedHook } from './createNamedHook.js';
 import { useApi } from './useApi.js';
-import { A_DAY } from './useBlocksPerDays.js';
-
-// Some chains incorrectly use these, i.e. it is set to values such as 0 or even 2
-// Use a low minimum validity threshold to check these against
-const THRESHOLD = BN_THOUSAND.div(BN_TWO);
-const DEFAULT_TIME = new BN(6_000);
-
-function calcInterval (api: ApiPromise): BN {
-  return bnMin(A_DAY, (
-    // Babe, e.g. Relay chains (Substrate defaults)
-    api.consts.babe?.expectedBlockTime ||
-    // POW, eg. Kulupu
-    api.consts.difficulty?.targetBlockTime ||
-    // Subspace
-    api.consts.subspace?.expectedBlockTime || (
-      // Check against threshold to determine value validity
-      api.consts.timestamp?.minimumPeriod.gte(THRESHOLD)
-        // Default minimum period config
-        ? api.consts.timestamp.minimumPeriod.mul(BN_TWO)
-        : api.query.parachainSystem
-          // default guess for a parachain
-          ? api.consts.aura?.slotDuration ?? DEFAULT_TIME.mul(BN_TWO)
-          // default guess for others
-          : DEFAULT_TIME
-    )
-  ));
-}
+import { useCall } from './useCall.js';
 
 function useBlockIntervalImpl (apiOverride?: ApiPromise | null): BN {
-  const { api } = useApi();
+  let { api } = useApi();
+
+  api = apiOverride || api;
+  const blockTimeAura = useCall(api.call.auraApi?.slotDuration && api.call.auraApi.slotDuration, []);
+  const blockTimeBabe = useCall(api.call.babeApi?.configuration && api.call.babeApi.configuration, [], {
+    transform: (data: BabeGenesisConfiguration | undefined) => data?.slotDuration
+  });
 
   return useMemo(
-    () => calcInterval(apiOverride || api),
-    [api, apiOverride]
-  );
+    () => (blockTimeAura || blockTimeBabe) ?? new BN(6_000),
+    [blockTimeAura, blockTimeBabe]
+  ) as BN;
 }
 
 export const useBlockInterval = createNamedHook('useBlockInterval', useBlockIntervalImpl);
