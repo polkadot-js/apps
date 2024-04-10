c3383e9710d6a6783b758290dbec11df36e9cef3
José Molina <jose@blockdeep.io>
2024-04-10 18:12:45 +0200

    restore calcInterval function

--- packages/react-hooks/src/useBlockInterval.ts
+++ packages/react-hooks/src/useBlockInterval.ts
@@ -6,11 +6,38 @@ import type { BabeGenesisConfiguration } from '@polkadot/types/interfaces/babe';
 
 import { useMemo } from 'react';
 
-import { BN } from '@polkadot/util';
+import { A_DAY } from '@polkadot/react-hooks/useBlocksPerDays';
+import { BN, BN_THOUSAND, BN_TWO, bnMin } from '@polkadot/util';
 
 import { createNamedHook } from './createNamedHook.js';
 import { useApi } from './useApi.js';
 import { useCall } from './useCall.js';
+
+// Some chains incorrectly use these, i.e. it is set to values such as 0 or even 2
+// Use a low minimum validity threshold to check these against
+const THRESHOLD = BN_THOUSAND.div(BN_TWO);
+const DEFAULT_TIME = new BN(6_000);
+
+function calcInterval (api: ApiPromise): BN {
+  return bnMin(A_DAY, (
+    // Babe, e.g. Relay chains (Substrate defaults)
+    api.consts.babe?.expectedBlockTime ||
+    // POW, eg. Kulupu
+    api.consts.difficulty?.targetBlockTime ||
+    // Subspace
+    api.consts.subspace?.expectedBlockTime || (
+      // Check against threshold to determine value validity
+      api.consts.timestamp?.minimumPeriod.gte(THRESHOLD)
+        // Default minimum period config
+        ? api.consts.timestamp.minimumPeriod.mul(BN_TWO)
+        : api.query.parachainSystem
+          // default guess for a parachain
+          ? api.consts.aura?.slotDuration ?? DEFAULT_TIME.mul(BN_TWO)
+          // default guess for others
+          : DEFAULT_TIME
+    )
+  ));
+}
 
 function useBlockIntervalImpl (apiOverride?: ApiPromise | null): BN {
   const { api } = useApi();
@@ -22,8 +49,8 @@ function useBlockIntervalImpl (apiOverride?: ApiPromise | null): BN {
   });
 
   return useMemo(
-    () => (blockTimeAura || blockTimeBabe) ?? new BN(6_000),
-    [blockTimeAura, blockTimeBabe]
+    () => (blockTimeAura || blockTimeBabe) ?? calcInterval(currApi),
+    [blockTimeAura, blockTimeBabe, currApi]
   ) as BN;
 }
 
