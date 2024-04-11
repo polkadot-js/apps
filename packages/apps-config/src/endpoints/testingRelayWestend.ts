27f5676d8a1928e4229b511862d3fe16e53bfc57
Tom <tsenseless@gmail.com>
2024-04-11 07:11:18 -0400

    Update testingRelayWestend.ts

--- packages/apps-config/src/endpoints/testingRelayWestend.ts
+++ packages/apps-config/src/endpoints/testingRelayWestend.ts
@@ -167,8 +167,8 @@ export const testParasWestendCommon: EndpointOption[] = [
     info: 'westendCoretime',
     paraId: 1005,
     providers: {
-      Parity: 'wss://westend-coretime-rpc.polkadot.io',
-      'IBP-GeoDNS2': 'wss://sys.dotters.network/coretime-westend'
+      'IBP-GeoDNS2': 'wss://sys.dotters.network/coretime-westend',
+      Parity: 'wss://westend-coretime-rpc.polkadot.io'
     },
     teleport: [-1],
     text: 'Coretime',
@@ -178,8 +178,8 @@ export const testParasWestendCommon: EndpointOption[] = [
     info: 'westendPeople',
     paraId: 1004,
     providers: {
-      Parity: 'wss://westend-people-rpc.polkadot.io',
-      'IBP-GeoDNS2': 'wss://sys.dotters.network/people-westend'
+      'IBP-GeoDNS2': 'wss://sys.dotters.network/people-westend',
+      Parity: 'wss://westend-people-rpc.polkadot.io'
     },
     teleport: [-1],
     text: 'People',
