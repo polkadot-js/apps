59508a4019dab237269b1512be77399ec44be016
tarikgul <tariksnow37@gmail.com>
2024-04-18 11:42:57 -0400

    lint

--- packages/apps/src/endpoint.ts
+++ packages/apps/src/endpoint.ts
@@ -12,7 +12,7 @@ function hasEndpoint (api: ApiPromise, endpoint: string, needsApiInstances: bool
   const [section] = (needsApiInstances && api.registry.getModuleInstances(api.runtimeVersion.specName.toString(), _section)) || [_section];
   const resolvedSection = (api as unknown as ApiMapper)[area][section]
     ? section
-    : _section
+    : _section;
 
   try {
     return area === 'consts'
