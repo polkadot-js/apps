27c1cfaa12130c062a3a16219441382cc9de8af3
Oleg Nosov <olegnosov1@gmail.com>
2024-04-23 12:42:20 +0900

    Update production.ts

--- packages/apps-config/src/endpoints/production.ts
+++ packages/apps-config/src/endpoints/production.ts
@@ -20,7 +20,7 @@ export const prodChains: EndpointOption[] = [
     info: 'dock-pos-mainnet',
     text: 'Dock PoS Mainnet',
     providers: {
-      'Dock Association': 'wss://mainnet-val6.docknode.io'
+      'Dock Association': 'wss://mainnet-node.dock.io'
     }
   }
 ];
