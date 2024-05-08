b5588ebea73311858078eb6a1041139be017155a
Tarik Gul <47201679+TarikGul@users.noreply.github.com>
2024-05-08 13:28:35 -0700

    Fix staking rewards, and update polkadot deps (#10552)
    * Bump pjs deps
    
    * reactive withClaimedRewardsEras
    
    * remove comments

--- packages/page-staking/src/useSortedTargets.ts
+++ packages/page-staking/src/useSortedTargets.ts
@@ -36,9 +36,7 @@ interface OldLedger {
 }
 
 const EMPTY_PARTIAL: Partial<SortedTargets> = {};
-// To get claimedRewards, activate `withClaimedRewardsEras` to true.
-// Currently its turned off because of performance issues.
-const DEFAULT_FLAGS_ELECTED = { withClaimedRewardsEras: false, withController: true, withExposure: true, withExposureMeta: true, withPrefs: true };
+const DEFAULT_FLAGS_ELECTED = { withClaimedRewardsEras: true, withController: true, withExposure: true, withExposureMeta: true, withPrefs: true };
 const DEFAULT_FLAGS_WAITING = { withController: true, withPrefs: true };
 
 const OPT_ERA = {
