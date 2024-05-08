eb3cc7cdecab68866e8292fec6f52068286ac949
Bulat Saifullin <bulat@parity.io>
2024-05-08 14:30:36 +0300

    Add people-kusama RPC (#10546)
    * Add people-kusama RPC
    
    * sort

--- packages/apps-config/src/endpoints/productionRelayKusama.ts
+++ packages/apps-config/src/endpoints/productionRelayKusama.ts
@@ -969,6 +969,16 @@ export const prodParasKusamaCommon: EndpointOption[] = [
       color: '#0000cc',
       logo: nodesEncointerBlueSVG
     }
+  },
+  {
+    info: 'kusamaPeople',
+    paraId: 1004,
+    providers: {
+      Parity: 'wss://kusama-people-rpc.polkadot.io'
+    },
+    teleport: [-1],
+    text: 'People',
+    ui: {}
   }
 ];
 
