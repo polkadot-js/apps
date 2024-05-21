e7b454a63dc25169058c06c6f375a28ac7e9224c
Jim Counter <jim@subspace.network>
2024-05-21 13:15:17 +0100

    Add Subspace Gemini 3h Nova endpoint.

--- packages/apps-config/src/endpoints/testing.ts
+++ packages/apps-config/src/endpoints/testing.ts
@@ -961,6 +961,17 @@ export const testChains: Omit<EndpointOption, 'teleport'>[] = [
     }
   },
   {
+    info: 'subspace-gemini-3h-nova',
+    providers: {
+      Subspace: 'wss://nova-0.gemini-3h.subspace.network/ws'
+    },
+    text: 'Subspace Gemini 3h Nova',
+    ui: {
+      color: '#562b8e',
+      logo: nodesSubspacePNG
+    }
+  },
+  {
     info: 'subspace',
     providers: {
       // 'Subspace Network': 'wss://test-rpc.subspace.network' // https://github.com/polkadot-js/apps/issues/8598
