08f4318189ad5e015e8309a0913f1b8995d5ac65
Faraz Ahmad <65384170+farazahmadvaival@users.noreply.github.com>
2024-05-21 18:00:12 +0500

    update the title and logo for elysium (#10600)
    * added elysium chain as prod
    
    added elysium chain as prod
    
    * added elysium in index.ts file
    
    added elysium in index.ts file
    
    * run the yarn lint
    
    * updated the logo and title for elysium

--- packages/apps-config/src/endpoints/production.ts
+++ packages/apps-config/src/endpoints/production.ts
@@ -233,8 +233,9 @@ export const prodChains: Omit<EndpointOption, 'teleport'>[] = [
     providers: {
       Elysium: 'wss://ws.elysiumchain.tech'
     },
-    text: 'ELysium',
+    text: 'Elysium',
     ui: {
+      color: '#140533',
       logo: nodesElysiumPNG
     }
   },
