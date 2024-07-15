9d51e7c5f04b8f96a7f55e70c020d3c1e3cadb3c
tarikgul <tariksnow37@gmail.com>
2024-07-15 16:33:14 -0400

    Cleanup LedgerSigner

--- packages/react-signer/src/signers/LedgerSigner.ts
+++ packages/react-signer/src/signers/LedgerSigner.ts
@@ -12,38 +12,51 @@ import { merkleizeMetadata } from '@polkadot-api/merkleize-metadata';
 let id = 0;
 
 export class LedgerSigner implements Signer {
-  readonly #accountOffset: number;
+  readonly #accountIndex: number;
+  readonly #addressOffset: number;
   readonly #getLedger: () => LedgerGeneric;
   readonly #registry: Registry;
   readonly #api: ApiPromise;
 
-  constructor (api: ApiPromise, getLedger: () => LedgerGeneric, accountOffset: number) {
-    this.#accountOffset = accountOffset;
+  constructor (api: ApiPromise, getLedger: () => LedgerGeneric, accountIndex: number, addressOffset: number) {
+    this.#accountIndex = accountIndex;
+    this.#addressOffset = addressOffset;
     this.#getLedger = getLedger;
     this.#registry = api.registry;
     this.#api = api;
   }
 
-  public async signPayload (payload: SignerPayloadJSON): Promise<SignerResult> {
-    // TODO hardcoded just for testing, will remove or fix soon!
-    const { address } = await this.#getLedger().getAddress(0, false, 0, 0);
+  private async getMetadataProof (payload: SignerPayloadJSON) {
     const m = await this.#api.call.metadata.metadataAtVersion(15);
-    const { specName, specVersion } = await this.#api.rpc.state.getRuntimeVersion();
+    const { specName, specVersion } = this.#api.runtimeVersion;
     const merkleizedMetadata = merkleizeMetadata(m.toHex(), {
       base58Prefix: this.#api.consts.system.ss58Prefix.toNumber(),
-      decimals: 10,
+      decimals: this.#api.registry.chainDecimals[0],
       specName: specName.toString(),
       specVersion: specVersion.toNumber(),
-      tokenSymbol: 'DOT'
+      tokenSymbol: this.#api.registry.chainTokens[0]
     });
-
     const metadataHash = u8aToHex(merkleizedMetadata.digest());
-
     const newPayload = objectSpread({}, payload, { metadataHash, mode: 1 });
     const raw = this.#registry.createType('ExtrinsicPayload', newPayload);
-    const txMetadata = u8aToHex(merkleizedMetadata.getProofForExtrinsicPayload(u8aToHex(raw.toU8a(true))));
+
+    return {
+      raw,
+      txMetadata: u8aToHex(merkleizedMetadata.getProofForExtrinsicPayload(u8aToHex(raw.toU8a(true))))
+    };
+  }
+
+  public async signPayload (payload: SignerPayloadJSON): Promise<SignerResult> {
+    const { address } = await this.#getLedger().getAddress(
+      this.#api.consts.system.ss58Prefix.toNumber(),
+      false,
+      this.#accountIndex,
+      this.#addressOffset
+    );
+    const { raw, txMetadata } = await this.getMetadataProof(payload);
+
     const buff = Buffer.from(hexToU8a(txMetadata));
-    const { signature } = await this.#getLedger().signWithMetadata(raw.toU8a(true), 0, this.#accountOffset, { metadata: buff });
+    const { signature } = await this.#getLedger().signWithMetadata(raw.toU8a(true), 0, this.#accountIndex, { metadata: buff });
 
     const extrinsic = this.#registry.createType(
       'Extrinsic',
