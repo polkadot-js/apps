c3e72679892e295ce2f9b52a2c425e1122da4b9a
Alberto Nicolas Penayo <74352651+bee344@users.noreply.github.com>
2024-07-29 09:39:41 -0300

    feat: enable uploading `rawMetadata` to the extension (#10780)
    * first commit
    
    * test commit
    
    * removed old logging
    
    * more cleanup
    
    * disabled update metadata button until metadata is available
    
    * removed logs
    
    * dedupe
    
    * updated metadata warning
    
    * allow rawMetadata to be falsy on _updateMeta

--- packages/page-settings/src/Metadata/index.tsx
+++ packages/page-settings/src/Metadata/index.tsx
@@ -6,17 +6,22 @@ import React from 'react';
 import { useApi } from '@polkadot/react-hooks';
 
 import useChainInfo from '../useChainInfo.js';
+import useRawMetadata from '../useRawMetadata.js';
 import Extensions from './Extensions.js';
 import NetworkSpecs from './NetworkSpecs.js';
 
 export default function Metadata (): React.ReactElement {
   const { isDevelopment } = useApi();
+  const rawMetadata = useRawMetadata();
   const chainInfo = useChainInfo();
 
   return (
     <>
       {!isDevelopment && (
-        <Extensions chainInfo={chainInfo} />
+        <Extensions
+          chainInfo={chainInfo}
+          rawMetadata={rawMetadata}
+        />
       )}
       <NetworkSpecs chainInfo={chainInfo} />
     </>
