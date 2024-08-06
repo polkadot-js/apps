56d520b00c789dc509d4bcba30e46264a3848ffd
Mike Spook <mikespook@gmail.com>
2024-08-07 11:33:35 +1200

    added Onfinality CommuneAI endpoint

--- packages/apps-config/src/endpoints/production.ts
+++ packages/apps-config/src/endpoints/production.ts
@@ -125,6 +125,7 @@ export const prodChains: Omit<EndpointOption, 'teleport'>[] = [
   {
     info: 'communeai',
     providers: {
+      OnFinality: 'wss://commune.api.onfinality.io/public-ws',
       Bitconnect: 'wss://commune-api-node-1.communeai.net'
     },
     text: 'Commune AI',
