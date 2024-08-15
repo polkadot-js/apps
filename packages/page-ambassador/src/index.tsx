d76ba97433537a22add434fb6680a444be5201b6
Nikos Kontakis <wirednkod@gmail.com>
2024-08-15 17:24:06 +0300

    Ambassador Base (Members and Referenda)

---
+++ packages/page-ambassador/src/index.tsx
@@ -0,0 +1,26 @@
+// Copyright 2017-2024 @polkadot/app-fellowship authors & contributors
+// SPDX-License-Identifier: Apache-2.0
+
+import React from 'react';
+
+import Main from '@polkadot/app-ranked/App';
+
+export { default as useCounter } from './useCounter.js';
+
+interface Props {
+  basePath: string;
+  className?: string;
+}
+
+function App ({ basePath, className }: Props): React.ReactElement<Props> {
+  return (
+    <Main
+      basePath={basePath}
+      className={className}
+      palletColl='ambassadorCollective'
+      palletPoll='ambassadorReferenda'
+    />
+  );
+}
+
+export default React.memo(App);
