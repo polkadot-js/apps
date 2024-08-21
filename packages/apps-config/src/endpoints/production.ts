a1458985ddbe1084fbf0e8e5f167dd5f17080bd3
BradleyOlson64 <lotrftw9@gmail.com>
2024-08-21 14:24:05 -0700

    Adding new creditcoin mainnet, without endpoint

--- packages/apps-config/src/endpoints/production.ts
+++ packages/apps-config/src/endpoints/production.ts
@@ -146,6 +146,17 @@ export const prodChains: Omit<EndpointOption, 'teleport'>[] = [
     }
   },
   {
+    info: 'creditcoin',
+    providers: {
+      // TODO: Get official endpoint.
+    },
+    text: 'Creditcoin',
+    ui: {
+      color: '#2D353F',
+      logo: chainsCreditcoinPNG
+    }
+  },
+  {
     info: 'creditcoin-classic',
     providers: {
       'Creditcoin Foundation': 'wss://mainnet.creditcoin.network/ws'
