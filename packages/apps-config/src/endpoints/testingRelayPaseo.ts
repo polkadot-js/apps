6695389cdd4e6a881743f47f2c083e0dc8b700f3
Ganesh oli <ganesh@thaiofficefurniture.com>
2024-09-06 14:51:24 +0545

    remove changes

--- packages/apps-config/src/endpoints/testingRelayPaseo.ts
+++ packages/apps-config/src/endpoints/testingRelayPaseo.ts
@@ -4,7 +4,7 @@
 import type { EndpointOption } from './types.js';
 
 import { chainsFrequencyPaseoSVG, chainsLaosSigmaPNG, chainsPaseoPNG, chainsPopNetworkSVG, chainsXcavatePNG } from '@polkadot/apps-config/ui/logos/chains';
-import { nodesAjunaPNG, nodesAssetHubSVG, nodesAventusSVG, nodesBajunPNG, nodesBifrostSVG, nodesDarwiniaKoiSVG, nodesHyperbridgePNG, nodesIntegriteeSVG, nodesKiltPNG, nodesMandalaPNG, nodesMusePNG, nodesNodleSVG, nodesRexSVG } from '@polkadot/apps-config/ui/logos/nodes';
+import { nodesAjunaPNG, nodesAssetHubSVG, nodesAventusSVG, nodesBajunPNG, nodesBifrostSVG, nodesDarwiniaKoiSVG, nodesHyperbridgePNG, nodesIntegriteeSVG, nodesKiltPNG, nodesMandalaPNG, nodesMusePNG, nodesNodleSVG } from '@polkadot/apps-config/ui/logos/nodes';
 
 import { PASEO_GENESIS } from '../api/constants.js';
 // import { testnetParachainSVG } from '../ui/logos/nodes/index.js';
@@ -97,6 +97,19 @@ export const testParasPaseo: Omit<EndpointOption, 'teleport'>[] = [
     }
   },
 
+  {
+    homepage: 'https://xcavate.io/',
+    info: 'Xcavate',
+    paraId: 4003,
+    providers: {
+      Xcavate: 'wss://rpc-paseo.xcavate.io:443'
+    },
+    text: 'Xcavate',
+    ui: {
+      color: '#FF0083',
+      logo: chainsXcavatePNG
+    }
+  },
   {
     homepage: 'https://www.frequency.xyz',
     info: 'Frequency',
@@ -201,18 +214,6 @@ export const testParasPaseo: Omit<EndpointOption, 'teleport'>[] = [
     }
   },
   {
-    info: 'paseoEwx',
-    paraId: 3345,
-    providers: {
-      'Energy Web': 'wss://public-rpc.testnet.energywebx.com/'
-    },
-    text: 'PEX',
-    ui: {
-      color: '#452E66',
-      logo: nodesRexSVG
-    }
-  },
-  {
     homepage: 'https://popnetwork.xyz/',
     info: 'Pop Network',
     paraId: 4001,
@@ -225,19 +226,6 @@ export const testParasPaseo: Omit<EndpointOption, 'teleport'>[] = [
     ui: {
       color: 'linear-gradient(to right, rgb(230, 0, 122), rgb(83, 15, 160))',
       logo: chainsPopNetworkSVG
-    }
-  },
-  {
-    homepage: 'https://xcavate.io/',
-    info: 'Xcavate',
-    paraId: 4003,
-    providers: {
-      Xcavate: 'wss://rpc-paseo.xcavate.io:443'
-    },
-    text: 'Xcavate',
-    ui: {
-      color: '#FF0083',
-      logo: chainsXcavatePNG
     }
   }
 ];
