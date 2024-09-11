adb11c5df9efb5d37b4245536a6198e6646dce10
Ganesh oli <ganesh@thaiofficefurniture.com>
2024-09-11 19:19:36 +0545

    update xcav logo

--- packages/apps-config/src/ui/logos/chains/index.ts
+++ packages/apps-config/src/ui/logos/chains/index.ts
@@ -53,6 +53,7 @@ export { chainsMangataPNG } from './generated/mangataPNG.js';
 export { chainsMangataxPNG } from './generated/mangataxPNG.js';
 export { chainsMoonsamaPNG } from './generated/moonsamaPNG.js';
 export { chainsMyriadPNG } from './generated/myriadPNG.js';
+export { chainsMyxcavPNG } from './generated/myxcavPNG.js';
 export { chainsNeurowebTestnetPNG } from './generated/neuroweb-testnetPNG.js';
 export { chainsNeurowebPNG } from './generated/neurowebPNG.js';
 export { chainsOakPNG } from './generated/oakPNG.js';
