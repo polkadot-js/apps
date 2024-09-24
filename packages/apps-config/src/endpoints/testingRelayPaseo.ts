315781ea305edb4c5ce1b335dc509c33e922be00
Agustinus Theodorus <37565691+agustinustheo@users.noreply.github.com>
2024-09-24 13:46:21 +0700

    Add Myriad Social testnet to Paseo (#10936)
    * feat: add myriad paseo svg para id 4005
    
    * fix: linter
    
    * fix: change myriad paseo location
    
    * fix: formatting

--- packages/apps-config/src/endpoints/testingRelayPaseo.ts
+++ packages/apps-config/src/endpoints/testingRelayPaseo.ts
@@ -4,7 +4,7 @@
 import type { EndpointOption } from './types.js';
 
 import { chainsCoretimeKusamaSVG, chainsFrequencyPaseoSVG, chainsHydrationPaseoSVG, chainsLaosSigmaPNG, chainsMyxcavPNG, chainsPaseoPNG, chainsPeoplePolkadotSVG, chainsPopNetworkSVG } from '@polkadot/apps-config/ui/logos/chains';
-import { nodesAjunaPNG, nodesAssetHubSVG, nodesAventusSVG, nodesBajunPNG, nodesBifrostSVG, nodesBridgeHubSVG, nodesDarwiniaKoiSVG, nodesHyperbridgePNG, nodesIdealNetworkSVG, nodesIntegriteeSVG, nodesKiltPNG, nodesMandalaPNG, nodesMusePNG, nodesNodleSVG, nodesRexSVG, nodesZeitgeistPNG } from '@polkadot/apps-config/ui/logos/nodes';
+import { nodesAjunaPNG, nodesAssetHubSVG, nodesAventusSVG, nodesBajunPNG, nodesBifrostSVG, nodesBridgeHubSVG, nodesDarwiniaKoiSVG, nodesHyperbridgePNG, nodesIdealNetworkSVG, nodesIntegriteeSVG, nodesKiltPNG, nodesMandalaPNG, nodesMusePNG, nodesMyriadPaseoSVG, nodesNodleSVG, nodesRexSVG, nodesZeitgeistPNG } from '@polkadot/apps-config/ui/logos/nodes';
 
 import { PASEO_GENESIS } from '../api/constants.js';
 // import { testnetParachainSVG } from '../ui/logos/nodes/index.js';
@@ -196,6 +196,19 @@ export const testParasPaseo: Omit<EndpointOption, 'teleport'>[] = [
     ui: {
       color: '#110ff9',
       logo: nodesMusePNG
+    }
+  },
+  {
+    homepage: 'https://myriad.social',
+    info: 'Myriad Social',
+    paraId: 4005,
+    providers: {
+      myriadPaseo: 'wss://ws-rpc.paseo.myriad.social'
+    },
+    text: 'Myriad Social Testnet',
+    ui: {
+      color: '#d5e3e4',
+      logo: nodesMyriadPaseoSVG
     }
   },
   {
