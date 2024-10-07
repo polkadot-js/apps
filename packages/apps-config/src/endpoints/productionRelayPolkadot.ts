666fba14de12f02e2f14c8f741915f092ee85f6f
dio <ryan@parallel.fi>
2024-10-07 13:45:11 +0800

    Update productionRelayPolkadot.ts

--- packages/apps-config/src/endpoints/productionRelayPolkadot.ts
+++ packages/apps-config/src/endpoints/productionRelayPolkadot.ts
@@ -626,8 +626,8 @@ export const prodParasPolkadot: Omit<EndpointOption, 'teleport'>[] = [
     info: 'parallel',
     paraId: 2012,
     providers: {
-      Parallel: 'wss://polkadot-parallel-rpc.parallel.fi',
-      Dwellir: 'wss://parallel-rpc.dwellir.com'
+      Dwellir: 'wss://parallel-rpc.dwellir.com',
+      Parallel: 'wss://polkadot-parallel-rpc.parallel.fi'
       // OnFinality: 'wss://parallel.api.onfinality.io/public-ws', // https://github.com/polkadot-js/apps/issues/9986
     },
     text: 'Parallel',
