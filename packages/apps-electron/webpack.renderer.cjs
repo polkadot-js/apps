242389d82105da6ae3c26ff9e7aa22faa9a14e5b
AlexanderVTr <tro.alex80@gmail.com>
2024-12-23 15:50:52 +0000

    Rename title of the page

--- packages/apps-electron/webpack.renderer.cjs
+++ packages/apps-electron/webpack.renderer.cjs
@@ -27,7 +27,7 @@ module.exports = merge(
         }]
       }),
       new HtmlWebpackPlugin({
-        PAGE_TITLE: 'Polkadot/Substrate Portal',
+        PAGE_TITLE: 'QFNetwork',
         minify: false,
         template: path.join(context, '../apps/public/index.html')
       })
