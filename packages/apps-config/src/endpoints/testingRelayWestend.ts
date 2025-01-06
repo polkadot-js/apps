0962dbe0efd135b6b0586a896cf3985b74af82ec
Francisco Aguirre <franciscoaguirreperez@gmail.com>
2025-01-06 14:35:35 -0300

    fix: order

--- packages/apps-config/src/endpoints/testingRelayWestend.ts
+++ packages/apps-config/src/endpoints/testingRelayWestend.ts
@@ -63,6 +63,19 @@ export const testParasWestend: Omit<EndpointOption, 'teleport'>[] = [
     }
   },
   {
+    info: 'westendPenpal',
+    isPeopleForIdentity: true,
+    paraId: 2042,
+    providers: {
+      Parity: 'wss://westend-penpal-rpc.polkadot.io'
+    },
+    relayName: 'westend',
+    text: 'Penpal',
+    ui: {
+      color: '#964b00'
+    }
+  },
+  {
     homepage: 'https://kylin.network/',
     info: 'westendPichiu',
     paraId: 2112,
@@ -203,20 +216,6 @@ export const testParasWestendCommon: EndpointOption[] = [
     ui: {
       color: '#ec03fc',
       logo: chainsPeoplePolkadotSVG
-    }
-  },
-  {
-    info: 'westendPenpal',
-    isPeopleForIdentity: true,
-    paraId: 2042,
-    providers: {
-      Parity: 'wss://westend-penpal-rpc.polkadot.io'
-    },
-    relayName: 'westend',
-    teleport: [-1],
-    text: 'Penpal',
-    ui: {
-      color: '#964b00'
     }
   }
 ];
