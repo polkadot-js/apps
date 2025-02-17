7eb9b5f291a087a33c963bd0737799c4f91f8500
Arjun Porwal <porwalarjun95@gmail.com>
2025-02-17 12:36:16 +0530

    feat: create hook for quering foreign assets locations

---
+++ packages/page-assets/src/useForeignAssets.ts
@@ -0,0 +1,24 @@
+// Copyright 2017-2025 @polkadot/app-assets authors & contributors
+// SPDX-License-Identifier: Apache-2.0
+
+import type { StorageKey, u32 } from '@polkadot/types';
+import type { AnyJson } from '@polkadot/types-codec/types';
+
+import { createNamedHook, useApi, useMapEntries } from '@polkadot/react-hooks';
+
+const EMPTY_PARAMS: unknown[] = [];
+
+const OPT_KEY = {
+  transform: (keys: StorageKey<[u32]>[]): AnyJson[] =>
+    keys.map((entry) => entry.toHuman())
+};
+
+function useForeignAssetsImpl () {
+  const { api, isApiReady } = useApi();
+
+  const values = useMapEntries(isApiReady && api.query.foreignAssets?.asset, EMPTY_PARAMS, OPT_KEY) || [];
+
+  return values;
+}
+
+export const useForeignAssets = createNamedHook('useForeignAssets', useForeignAssetsImpl);
