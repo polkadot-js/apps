9c649388131b604a4ca0b8a2022498ee4c3706f1
Ernest Nnamdi <ernestnamdi66@gmail.com>
2025-03-05 13:10:24 +0200

    chore: fixed all eslint issues

--- packages/react-components/src/Chart/index.ts
+++ packages/react-components/src/Chart/index.ts
@@ -1,6 +1,8 @@
 // Copyright 2017-2025 @polkadot/react-components authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
+import type { ChartComponentLike } from 'chart.js';
+
 import { CategoryScale, Chart, LinearScale, LineElement, PointElement, Title, Tooltip } from 'chart.js';
 import annotationPlugin from 'chartjs-plugin-annotation';
 import crosshairPlugin from 'chartjs-plugin-crosshair';
@@ -9,17 +11,30 @@ import Doughnut from './Doughnut.js';
 import HorizBar from './HorizBar.js';
 import Line from './Line.js';
 
-const CustomCrosshairPlugin = function (plugin: any) {  
-  const originalAfterDraw = plugin.afterDraw;  
-  plugin.afterDraw = function(chart:any, easing: any) {  
-      if (chart && chart.crosshair) {  
-        originalAfterDraw.call(this, chart, easing);  
-      }  
-  };  
-  return plugin;  
-};
-Chart.register(CategoryScale, LinearScale, LineElement, PointElement, Title, Tooltip, annotationPlugin, CustomCrosshairPlugin(crosshairPlugin));
+function CustomCrosshairPlugin (plugin: Record<string, unknown>): Record<string, unknown> {
+  const originalAfterDraw = plugin.afterDraw as (chart: unknown, easing: unknown) => void;
 
+  plugin.afterDraw = function (chart: unknown, easing: unknown): void {
+    const chartWithCrosshair = chart as { crosshair?: unknown };
+
+    if (chartWithCrosshair.crosshair) {
+      originalAfterDraw.call(this, chart, easing);
+    }
+  };
+
+  return plugin;
+}
+
+Chart.register(
+  CategoryScale,
+  LinearScale,
+  LineElement,
+  PointElement,
+  Title,
+  Tooltip,
+  annotationPlugin,
+  CustomCrosshairPlugin(crosshairPlugin) as unknown as ChartComponentLike
+);
 
 export default {
   Doughnut,
