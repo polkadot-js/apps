eab9cefef64129a6494116a0579f79ba7fa95c29
Ernest Nnamdi <ernestnamdi66@gmail.com>
2025-04-07 04:12:43 +0100

    lint

--- packages/react-components/src/Chart/index.ts
+++ packages/react-components/src/Chart/index.ts
@@ -1,7 +1,7 @@
 // Copyright 2017-2025 @polkadot/react-components authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
-import type { ChartComponentLike } from 'chart.js';
+import type { ChartType, Plugin } from 'chart.js';
 
 import { CategoryScale, Chart, LinearScale, LineElement, PointElement, Title, Tooltip } from 'chart.js';
 import annotationPlugin from 'chartjs-plugin-annotation';
@@ -11,19 +11,35 @@ import Doughnut from './Doughnut.js';
 import HorizBar from './HorizBar.js';
 import Line from './Line.js';
 
-const CustomCrosshairPlugin = function (plugin: any) {
+interface CrosshairChart {
+  crosshair?: boolean;
+}
+
+function CustomCrosshairPlugin (plugin: Plugin<ChartType>): Plugin<ChartType> {
   const originalAfterDraw = plugin.afterDraw;
 
-  plugin.afterDraw = function (chart: any, easing: any) {
-    if (chart?.crosshair) {
-      originalAfterDraw.call(this, chart, easing);
-    }
-  };
+  if (originalAfterDraw) {
+    plugin.afterDraw = function (chart, args): void {
+      if ((chart as CrosshairChart).crosshair) {
+        // @ts-expect-error - Pass exactly what the original plugin expects
+        originalAfterDraw.call(this, chart, args);
+      }
+    };
+  }
 
   return plugin;
-};
+}
 
-Chart.register(CategoryScale, LinearScale, LineElement, PointElement, Title, Tooltip, annotationPlugin, CustomCrosshairPlugin(crosshairPlugin));
+Chart.register(
+  CategoryScale,
+  LinearScale,
+  LineElement,
+  PointElement,
+  Title,
+  Tooltip,
+  annotationPlugin,
+  CustomCrosshairPlugin(crosshairPlugin as Plugin<ChartType>)
+);
 
 export default {
   Doughnut,
