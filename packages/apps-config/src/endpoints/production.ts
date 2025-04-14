987d3b332982dc79dd4ce47ebd4b17af0cea902b
Arjun Porwal <65214523+ap211unitech@users.noreply.github.com>
2025-04-14 17:13:57 +0530

    chore: disable unreachable endpoints (#11478)

--- packages/apps-config/src/endpoints/production.ts
+++ packages/apps-config/src/endpoints/production.ts
@@ -32,7 +32,7 @@ export const prodChains: Omit<EndpointOption, 'teleport'>[] = [
     info: 'aleph',
     providers: {
       'Aleph Zero Foundation': 'wss://ws.azero.dev',
-      Dwellir: 'wss://aleph-zero-rpc.dwellir.com',
+      // Dwellir: 'wss://aleph-zero-rpc.dwellir.com', https://github.com/polkadot-js/apps/issues/11477
       OnFinality: 'wss://aleph-zero.api.onfinality.io/public-ws'
     },
     text: 'Aleph Zero',
