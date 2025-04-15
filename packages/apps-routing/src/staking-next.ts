477159d52bdd79234639938b29e1de58d4a5151a
Arjun Porwal <porwalarjun95@gmail.com>
2025-04-15 12:34:15 +0530

    feat: implement API check for staking-next route

--- packages/apps-routing/src/staking-next.ts
+++ packages/apps-routing/src/staking-next.ts
@@ -1,16 +1,25 @@
 // Copyright 2017-2025 @polkadot/apps-routing authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
+import type { ApiPromise } from '@polkadot/api';
 import type { Route, TFunction } from './types.js';
 
 import Component from '@polkadot/app-staking-next';
+
+function needsApiCheck (api: ApiPromise): boolean {
+  try {
+    return !!((api.tx.stakingNextAhClient) || (api.tx.staking && api.tx.stakingNextRcClient));
+  } catch {
+    return false;
+  }
+}
 
 export default function create (t: TFunction): Route {
   return {
     Component,
     display: {
-      // TODO: Add check when to show this page
-      needsApi: []
+      needsApi: [],
+      needsApiCheck
     },
     group: 'network',
     icon: 'certificate',
