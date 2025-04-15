11e1691de89de3cd79074f55cda81acb05f29ea0
Arjun Porwal <porwalarjun95@gmail.com>
2025-04-15 10:44:31 +0530

    feat: storage fetch for ah api

--- packages/page-staking-next/src/CommandCenter/index.tsx
+++ packages/page-staking-next/src/CommandCenter/index.tsx
@@ -1,39 +1,34 @@
 // Copyright 2017-2025 @polkadot/app-staking-next authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
-import type { ApiPromise } from '@polkadot/api';
 import type { AccountId32, Event } from '@polkadot/types/interfaces';
 import type { IEventData } from '@polkadot/types/types';
 
 import React, { useEffect, useMemo, useState } from 'react';
 
+import { ApiPromise, WsProvider } from '@polkadot/api';
 import { useApi } from '@polkadot/react-hooks';
 
 import RelaySection from './relay.js';
 
 const MAX_EVENTS = 10;
 
-// const getApi = async (url: string) => {
-//   const api = await ApiPromise.create({
-//     provider: new WsProvider(url)
-//   });
+const getApi = async (url: string) => {
+  const api = await ApiPromise.create({
+    provider: new WsProvider(url)
+  });
 
-//   await api.isReadyOrError;
+  await api.isReadyOrError;
 
-//   return api;
-// };
+  return api;
+};
 
-const commandCenterHandler = async (rcApi: ApiPromise, setRcOutout: React.Dispatch<React.SetStateAction<IRcOutput[]>>): Promise<void> => {
-  // const rcApi = await getApi('ws://127.0.0.1:54169');
-  // const ahApi = await getApi('ws://127.0.0.1:54175');
-
-  //   const manager = UpdateManager.getInstance();
-  // manager.hook();
-
-  // let ahOutput: string[] = [];
-  // const rcEvents: string[] = [];
-  // const ahEvents: string[] = [];
-
+const commandCenterHandler = async (
+  rcApi: ApiPromise,
+  ahApi: ApiPromise,
+  setRcOutout: React.Dispatch<React.SetStateAction<IRcOutput[]>>,
+  setAhOutout: React.Dispatch<React.SetStateAction<IAhOutput[]>>
+): Promise<void> => {
   await rcApi.rpc.chain.subscribeFinalizedHeads(async (header) => {
     // --- RC:
     // current session index
@@ -106,72 +101,90 @@ const commandCenterHandler = async (rcApi: ApiPromise, setRcOutout: React.Dispat
     // rcOutput.concat(ahOutput);
   });
 
-  // AH:
-  // await ahApi.rpc.chain.subscribeFinalizedHeads(async (header) => {
-  //   // the current planned era
-  //   const currentEra = await ahApi.query.staking.currentEra();
-  //   // the active era
-  //   const activeEra = await ahApi.query.staking.activeEra();
-  //   // the starting index of the active era
-  //   const erasStartSessionIndex =
-  //     // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-  //     // @ts-ignore
-  //     await ahApi.query.staking.erasStartSessionIndex(activeEra.unwrap().index);
+  await ahApi.rpc.chain.subscribeFinalizedHeads(async (header) => {
+    // the current planned era
+    const currentEra = await ahApi.query.staking.currentEra();
+    // the active era
+    const activeEra = await ahApi.query.staking.activeEra();
+    // the starting index of the active era
+    const erasStartSessionIndex =
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-ignore
+      await ahApi.query.staking.erasStartSessionIndex(activeEra.unwrap().index);
 
-  //   // the basic state of the election provider
-  //   const phase = await ahApi.query.multiBlock.currentPhase();
-  //   const round = await ahApi.query.multiBlock.round();
-  //   const snapshotRange = (
-  //     await ahApi.query.multiBlock.pagedVoterSnapshotHash.entries()
-  //   )
-  //     .map(([k]) => k.args[0])
-  //     .sort();
-  //   const queuedScore =
-  //     await ahApi.query.multiBlockVerifier.queuedSolutionScore();
-  //   const signedSubmissions =
-  //     await ahApi.query.multiBlockSigned.sortedScores(round);
+    // the basic state of the election provider
+    const phase = await ahApi.query.multiBlock.currentPhase();
+    const round = await ahApi.query.multiBlock.round();
+    const snapshotRange = (
+      await ahApi.query.multiBlock.pagedVoterSnapshotHash.entries()
+    )
+      .map(([k]) => k.args[0])
+      .sort();
+    const queuedScore =
+      await ahApi.query.multiBlockVerifier.queuedSolutionScore();
+    const signedSubmissions =
+      await ahApi.query.multiBlockSigned.sortedScores(round);
 
-  //   // Events that we are interested in from RC:
-  //   const eventsOfInterest = (await ahApi.query.system.events())
-  //     // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-  //     // @ts-ignore
-  //     .map((e) => e.event)
-  //     .filter((e) => {
-  //       const election = (e: IEventData) =>
-  //         e.section === 'multiBlock' ||
-  //         e.section === 'multiBlockVerifier' ||
-  //         e.section === 'multiBlockSigned' ||
-  //         e.section === 'multiBlockUnsigned';
-  //       const rcClient = (e: IEventData) => e.section === 'stakingNextRcClient';
-  //       const staking = (e: IEventData) =>
-  //         e.section === 'staking' &&
-  //         (e.method === 'EraPaid' ||
-  //           e.method === 'SessionRotated' ||
-  //           e.method === 'PagedElectionProceeded');
+    // Events that we are interested in from RC:
+    const eventsOfInterest = (await (await ahApi.at(header.hash.toHex())).query.system.events())
+      .map((e) => e.event)
+      .filter((e) => {
+        const election = (e: IEventData) =>
+          e.section === 'multiBlock' ||
+          e.section === 'multiBlockVerifier' ||
+          e.section === 'multiBlockSigned' ||
+          e.section === 'multiBlockUnsigned';
+        const rcClient = (e: IEventData) => e.section === 'stakingNextRcClient';
+        const staking = (e: IEventData) =>
+          e.section === 'staking' &&
+          (e.method === 'EraPaid' ||
+            e.method === 'SessionRotated' ||
+            e.method === 'PagedElectionProceeded');
 
-  //       return election(e.data) || rcClient(e.data) || staking(e.data);
-  //     })
-  //     .map(
-  //       (e) =>
-  //         `${e.section.toString()}::${e.method.toString()}(${e.data.toString()})`
-  //     );
+        return election(e.data) || rcClient(e.data) || staking(e.data);
+      });
 
-  //   ahEvents.push(...eventsOfInterest);
+    setAhOutout((prev) => {
+      return [
+        {
+          events: eventsOfInterest,
+          finalizedBlock: header.number.toNumber(),
+          multiblock: {
+            phase: phase.toString(),
+            queuedScore: ahApi.createType('Option<SpNposElectionsElectionScore>', queuedScore).unwrap().toString(),
+            round: ahApi.createType('u32', round).toNumber(),
+            signedSubmissions: ahApi.createType('Vec<(AccountId32,SpNposElectionsElectionScore)>', signedSubmissions).length,
+            snapshotRange: snapshotRange.map((a) => a.toString())
+          },
+          staking: {
+            activeEra: {
+              index: activeEra.unwrap().index.toNumber(),
+              start: activeEra.unwrap().toString()
+            },
+            currentEra: currentEra.unwrap().toNumber(),
+            erasStartSessionIndex: erasStartSessionIndex.unwrap().toNumber()
+          }
+        },
+        ...prev.slice(0, MAX_EVENTS - 1)
+      ];
+    });
 
-  //   ahOutput = [
-  //     'AH:',
-  //     `finalized block ${header.number.toNumber()}`,
-  //     // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-  //     // @ts-ignore
-  //     `AH.staking: currentEra=${currentEra.unwrap().toNumber()}, activeEra=${activeEra.unwrap().toString()}, erasStartSessionIndex(${activeEra.unwrap().index.toNumber()})=${erasStartSessionIndex.unwrap().toNumber()}`,
-  //     `multiBlock: phase=${phase.toString()}, round=${round.toString()}, snapshotRange=${JSON.stringify(snapshotRange)}, queuedScore=${queuedScore.toString()}, signedSubmissions=${signedSubmissions.toString()}`,
-  //     `AH.events: ${JSON.stringify(ahEvents)}`,
-  //     '----'
-  //   ];
+    // ahEvents.push(...eventsOfInterest);
 
-  //   // manager.update(rcOutput.concat(ahOutput));
-  //   rcOutput.concat(ahOutput);
-  // });
+    // ahOutput = [
+    //   'AH:',
+    //   `finalized block ${header.number.toNumber()}`,
+    //   // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    //   // @ts-ignore
+    //   `AH.staking: currentEra=${currentEra.unwrap().toNumber()}, activeEra=${activeEra.unwrap().toString()}, erasStartSessionIndex(${activeEra.unwrap().index.toNumber()})=${erasStartSessionIndex.unwrap().toNumber()}`,
+    //   `multiBlock: phase=${phase.toString()}, round=${round.toString()}, snapshotRange=${JSON.stringify(snapshotRange)}, queuedScore=${queuedScore.toString()}, signedSubmissions=${signedSubmissions.toString()}`,
+    //   `AH.events: ${JSON.stringify(ahEvents)}`,
+    //   '----'
+    // ];
+
+    // manager.update(rcOutput.concat(ahOutput));
+    // rcOutput.concat(ahOutput);
+  });
 };
 
 export interface IRcOutput {
@@ -189,15 +202,39 @@ export interface IRcOutput {
   events: Event[]
 }
 
+export interface IAhOutput {
+  finalizedBlock: number,
+  staking: {
+    currentEra: number,
+    activeEra: {index: number, start: string},
+    erasStartSessionIndex: number
+  },
+  multiblock: {
+    phase: string,
+    round: number,
+    snapshotRange: string[]
+    queuedScore: string,
+    signedSubmissions: number
+  },
+  events: Event[]
+}
+
 function CommandCenter () {
   const { api } = useApi();
   const [rcOutput, setRcOutput] = useState<IRcOutput[]>([]);
+  const [_ahOutput, setAhOutput] = useState<IAhOutput[]>([]);
 
   const rcApi = useMemo(() => api, [api]);
 
+  const [ahApi, setAhApi] = useState<ApiPromise>();
+
   useEffect(() => {
-    commandCenterHandler(rcApi, setRcOutput).catch((e) => console.log(e));
-  }, [rcApi]);
+    getApi('ws://127.0.0.1:57357').then((ahApi) => setAhApi(ahApi)).catch((e) => console.log(e));
+  }, []);
+
+  useEffect(() => {
+    ahApi && commandCenterHandler(rcApi, ahApi, setRcOutput, setAhOutput).catch((e) => console.log(e));
+  }, [ahApi, rcApi]);
 
   return (
     <>
