2a0a23d5284772ca2e20d7f235b149a896536e97
danil_sitdikov <quadrad4@gmail.com>
2025-04-23 16:17:16 +0600

    fix github link and remove wiki links

--- packages/apps/src/Menu/index.tsx
+++ packages/apps/src/Menu/index.tsx
@@ -27,17 +27,17 @@ interface Props {
 function createExternals (t: (key: string, optionsOrText?: string | { replace: Record<string, unknown> }, options?: { ns: string }) => string): ItemRoute[] {
   return [
     {
-      href: 'https://github.com/polkadot-js/apps',
+      href: 'https://github.com/QuantumFusion-network/apps',
       icon: 'code-branch',
       name: 'github',
       text: t('nav.github', 'GitHub', { ns: 'apps-routing' })
     },
-    {
-      href: 'https://wiki.polkadot.network',
-      icon: 'book',
-      name: 'wiki',
-      text: t('nav.wiki', 'Wiki', { ns: 'apps-routing' })
-    }
+    // {
+    //   href: 'https://wiki.polkadot.network',
+    //   icon: 'book',
+    //   name: 'wiki',
+    //   text: t('nav.wiki', 'Wiki', { ns: 'apps-routing' })
+    // }
   ];
 }
 
