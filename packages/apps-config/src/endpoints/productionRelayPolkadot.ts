d2264634db0483dc440e739a48a0fb9048594d43
Arneil Paul Polican <policanarneilpaul@gmail.com>
2025-04-28 12:32:10 +0800

    fix: change Xode RPC Endpoint under Polkadot (#11512)
    Co-authored-by: KenSombreo <156393067+KenSombreo@users.noreply.github.com>

--- packages/apps-config/src/endpoints/productionRelayPolkadot.ts
+++ packages/apps-config/src/endpoints/productionRelayPolkadot.ts
@@ -883,7 +883,7 @@ export const prodParasPolkadot: Omit<EndpointOption, 'teleport'>[] = [
     info: 'xode',
     paraId: 3417,
     providers: {
-      XodeCommunity: 'wss://polkadot-bootnode03.xode.net/rpc'
+      XodeCommunity: 'wss://polkadot-rpcnode.xode.net'
     },
     text: 'Xode',
     ui: {
