41d1408fb956450c81d3960e0d01c3a4e965e91c
rajk93 <raj@blockdeep.io>
2025-06-07 10:50:18 +0530

    fix: disable unreachable endpoints (#11612)

--- packages/apps-config/src/endpoints/testing.ts
+++ packages/apps-config/src/endpoints/testing.ts
@@ -619,7 +619,7 @@ export const testChains: Omit<EndpointOption, 'teleport'>[] = [
   {
     info: 'moonbaseAlpha',
     providers: {
-      Blast: 'wss://moonbase-alpha.public.blastapi.io',
+      // Blast: 'wss://moonbase-alpha.public.blastapi.io', // https://github.com/polkadot-js/apps/issues/11608
       Dwellir: 'wss://moonbase-rpc.n.dwellir.com',
       'Moonbeam Foundation': 'wss://wss.api.moonbase.moonbeam.network',
       // OnFinality: 'wss://moonbeam-alpha.api.onfinality.io/public-ws', https://github.com/polkadot-js/apps/issues/11026
