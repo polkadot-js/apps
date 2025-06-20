3827fb411dc903c16e9e395e0285cb691d180666
Raj <raj@blockdeep.io>
2025-06-20 11:42:01 +0530

    fix: disable unreachable endpoints

--- packages/apps-config/src/endpoints/productionRelayPolkadot.ts
+++ packages/apps-config/src/endpoints/productionRelayPolkadot.ts
@@ -27,10 +27,10 @@ export const prodParasPolkadot: Omit<EndpointOption, 'teleport'>[] = [
       'Acala Foundation 3': 'wss://acala-rpc-3.aca-api.network/ws',
       Dwellir: 'wss://acala-rpc.n.dwellir.com',
       IBP1: 'wss://acala.ibp.network',
-      IBP2: 'wss://acala.dotters.network',
+      IBP2: 'wss://acala.dotters.network'
       // LuckyFriday: 'wss://rpc-acala.luckyfriday.io', // https://github.com/polkadot-js/apps/issues/10728
       // 'Automata 1RPC': 'wss://1rpc.io/aca' // https://github.com/polkadot-js/apps/issues/8648
-      OnFinality: 'wss://acala-polkadot.api.onfinality.io/public-ws'
+      // OnFinality: 'wss://acala-polkadot.api.onfinality.io/public-ws' // https://github.com/polkadot-js/apps/issues/11634
       // 'Polkawallet 0': 'wss://acala.polkawallet.io' // https://github.com/polkadot-js/apps/issues/9760
     },
     text: 'Acala',
