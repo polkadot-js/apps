901f972cfe4ff8f0e75bd65f9aeec032270638c3
Arjun Porwal <65214523+ap211unitech@users.noreply.github.com>
2025-06-23 15:52:04 +0530

    fix: register app protocol

--- packages/apps-electron/src/electron/index.ts
+++ packages/apps-electron/src/electron/index.ts
@@ -2,6 +2,8 @@
 // SPDX-License-Identifier: Apache-2.0
 
 import { app, protocol } from 'electron';
+import mime from 'mime';
+import { readFile } from 'node:fs/promises';
 import path from 'path';
 
 import { registerAccountStoreHandlers } from '../main/account-store.js';
@@ -19,11 +21,38 @@ app
   .whenReady()
   .then(async (): Promise<void> => {
     // eslint-disable-next-line deprecation/deprecation
-    protocol.registerFileProtocol('app', (request, callback) => {
-      const url = request.url.slice(6); // strip off "app://"
-      const resolvedPath = path.join(__dirname, '..', 'build', url); // adjust "build" as needed
+    // protocol.registerFileProtocol('app', (request, callback) => {
+    //   const url = request.url.slice(6); // strip off "app://"
+    //   const resolvedPath = path.join(__dirname, '..', 'build', url); // adjust "build" as needed
 
-      callback({ path: resolvedPath });
+    //   callback({ path: resolvedPath });
+    // });
+
+    protocol.handle('app', async (request) => {
+      const url = new URL(request.url);
+
+      console.log(request.url, url, 'request url');
+      const pathname = url.pathname === '/' ? '/index.html' : url.pathname;
+
+      const filePath = path.join(__dirname, request.url.slice(6));
+      const mimeType = mime.getType(filePath) || 'text/plain';
+
+      console.log(filePath, 'file path');
+
+      try {
+        const data = await readFile(filePath);
+
+        return new Response(data, {
+          headers: {
+            // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+            'Content-Type': mimeType
+          }
+        });
+      } catch (err) {
+        console.error('Failed to load:', filePath, err);
+
+        return new Response('Not found', { status: 404 });
+      }
     });
 
     registerAccountStoreHandlers();
