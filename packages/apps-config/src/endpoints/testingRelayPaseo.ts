73759588f7bbee48fe64c37cd05b8a1709c5996f
Rusinskiy Vsevolod <100481804+Vsevolod-Rusinskiy@users.noreply.github.com>
2025-07-10 19:16:43 +0300

    Add RPC endpoints for Quantum Fusion testnet (replaces #11454) (#11687)
    * feat: add Quantum Fusion logo
    
    * feat: generate quantum-fusionPNG.ts from logo file
    
    * feat: add Quantum Fusion RPC endpoints to testing networks
    
    * feat: add Quantum Fusion to testingRelayPaseo networks
    
    * Fix formatting issues in CONTRIBUTORS file
    
    * revert: remove CONTRIBUTORS formatting changes unrelated to RPC endpoints
    
    * Update Quantum Fusion endpoints to qfnetwork.xyz
    
    * Fix test networks alphabetical sorting order

--- packages/apps-config/src/endpoints/testingRelayPaseo.ts
+++ packages/apps-config/src/endpoints/testingRelayPaseo.ts
@@ -3,7 +3,7 @@
 
 import type { EndpointOption } from './types.js';
 
-import { chainsAmplitudeSVG, chainsCoretimeKusamaSVG, chainsFrequencyPaseoSVG, chainsHydrationPaseoSVG, chainsKreivoSVG, chainsLaosSigmaPNG, chainsMyxcavPNG, chainsNeurowebTestnetPNG, chainsPaseoPNG, chainsPeoplePolkadotSVG, chainsPopNetworkSVG, chainsWatrPNG, chainsWeTEESVG } from '@polkadot/apps-config/ui/logos/chains';
+import { chainsAmplitudeSVG, chainsCoretimeKusamaSVG, chainsFrequencyPaseoSVG, chainsHydrationPaseoSVG, chainsKreivoSVG, chainsLaosSigmaPNG, chainsMyxcavPNG, chainsNeurowebTestnetPNG, chainsPaseoPNG, chainsPeoplePolkadotSVG, chainsPopNetworkSVG, chainsQuantumFusionPNG, chainsWatrPNG, chainsWeTEESVG } from '@polkadot/apps-config/ui/logos/chains';
 import { nodesAjunaPNG, nodesAssetHubSVG, nodesAventusSVG, nodesBajunPNG, nodesBifrostSVG, nodesBridgeHubSVG, nodesDarwiniaKoiSVG, nodesHeimaPaseoPNG, nodesHyperbridgePNG, nodesIdealNetworkSVG, nodesIntegriteeSVG, nodesKiltIconSVG, nodesMandalaPNG, nodesMusePNG, nodesMyriadPaseoSVG, nodesNodleSVG, nodesRegionxPNG, nodesRexSVG, nodesXodePNG, nodesZeitgeistPNG } from '@polkadot/apps-config/ui/logos/nodes';
 
 import { PASEO_GENESIS } from '../api/constants.js';
@@ -317,6 +317,19 @@ export const testParasPaseo: Omit<EndpointOption, 'teleport'>[] = [
     ui: {
       color: 'linear-gradient(to right, rgb(230, 0, 122), rgb(83, 15, 160))',
       logo: chainsPopNetworkSVG
+    }
+  },
+  {
+    homepage: 'https://qfnetwork.xyz/',
+    info: 'quantum-fusion-paseo',
+    paraId: 4775,
+    providers: {
+      'Quantum Fusion': 'wss://para-test.qfnetwork.xyz'
+    },
+    text: 'Quantum Fusion (Paseo)',
+    ui: {
+      color: '#2E2E5C',
+      logo: chainsQuantumFusionPNG
     }
   },
   {
