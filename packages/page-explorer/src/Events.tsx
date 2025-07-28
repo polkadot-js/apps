370203d6d7c2c885eb67947d61e76e57a076fa0a
Arjun Porwal <porwalarjun95@gmail.com>
2025-07-28 13:15:48 +0530

    feat: created toggle component for events

--- packages/page-explorer/src/Events.tsx
+++ packages/page-explorer/src/Events.tsx
@@ -6,7 +6,8 @@ import type { KeyedEvent } from '@polkadot/react-hooks/ctx/types';
 import React, { useMemo } from 'react';
 import { Link } from 'react-router-dom';
 
-import { MarkError, Table } from '@polkadot/react-components';
+import { MarkError, styled, Table, Toggle } from '@polkadot/react-components';
+import { useToggle } from '@polkadot/react-hooks';
 import { formatNumber } from '@polkadot/util';
 
 import Event from './Event.js';
@@ -19,6 +20,7 @@ interface Props {
   events?: KeyedEvent[] | null;
   eventClassName?: string;
   label?: React.ReactNode;
+  showToggle?: boolean
 }
 
 function renederEvent (className: string | undefined, { blockHash, blockNumber, indexes, key, record }: KeyedEvent): React.ReactNode {
@@ -40,7 +42,7 @@ function renederEvent (className: string | undefined, { blockHash, blockNumber, 
   );
 }
 
-function Events ({ className = '', emptyLabel, error, eventClassName, events, label }: Props): React.ReactElement<Props> {
+function Events ({ className = '', emptyLabel, error, eventClassName, events, label, showToggle = false }: Props): React.ReactElement<Props> {
   const { t } = useTranslation();
 
   const header = useMemo<[React.ReactNode?, string?, number?][]>(
@@ -51,24 +53,51 @@ function Events ({ className = '', emptyLabel, error, eventClassName, events, la
   );
 
   return (
-    <Table
-      className={className}
-      empty={emptyLabel || t('No events available')}
-      header={header}
-    >
-      {error
-        ? (
-          <tr
-            className={eventClassName}
-            key='error'
-          >
-            <td><MarkError content={t('Unable to decode the block events. {{error}}', { replace: { error: error.message } })} /></td>
-          </tr>
-        )
-        : events?.map((e) => renederEvent(eventClassName, e))
-      }
-    </Table>
+    <StyledSection>
+      <Table
+        className={className}
+        empty={emptyLabel || t('No events available')}
+        header={header}
+      >
+        {error
+          ? (
+            <tr
+              className={eventClassName}
+              key='error'
+            >
+              <td><MarkError content={t('Unable to decode the block events. {{error}}', { replace: { error: error.message } })} /></td>
+            </tr>
+          )
+          : events?.map((e) => renederEvent(eventClassName, e))
+        }
+      </Table>
+      {showToggle && <ShowRelevantEventsToggle />}
+    </StyledSection>
   );
 }
+
+const ShowRelevantEventsToggle = () => {
+  const { t } = useTranslation();
+  const [showRelevantEvents, setShowRelevantEvents] = useToggle();
+
+  return (
+    <Toggle
+      label={t('Show my events')}
+      onChange={setShowRelevantEvents}
+      value={showRelevantEvents}
+    />
+  );
+};
+
+const StyledSection = styled.section`
+  position: relative;
+
+  .ui--Toggle {
+    position: absolute;
+    top: 1.4rem;
+    right: 1rem;
+    z-index: 999;
+  }
+`;
 
 export default React.memo(Events);
