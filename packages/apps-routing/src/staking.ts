6ae4fd30f5e16fac71ae25ced819d0b6be66bbe4
Arjun Porwal <porwalarjun95@gmail.com>
2025-09-03 11:01:32 +0530

    fix: update needsApiCheck to hide staking route for specific Relay chains

--- packages/apps-routing/src/staking.ts
+++ packages/apps-routing/src/staking.ts
@@ -7,22 +7,14 @@ import type { SpStakingPagedExposureMetadata } from '@polkadot/types/lookup';
 import type { Route, TFunction } from './types.js';
 
 import Component from '@polkadot/app-staking';
-import { getGenesis } from '@polkadot/apps-config';
 import { ZERO_ACCOUNT } from '@polkadot/react-hooks/useWeight';
 import { unwrapStorageType } from '@polkadot/types/util';
 import { assert, BN_ONE } from '@polkadot/util';
 
-const PASEO_RELAY_GENESIS = '0x77afd6190f1554ad45fd0d31aee62aacc33c6db0ea801129acb813f913e0764f';
-
 function needsApiCheck (api: ApiPromise): boolean {
   try {
-    // Hide it always for Westend relay
-    if ([getGenesis('westend'), PASEO_RELAY_GENESIS].includes(api.genesisHash.toHex())) {
-      return false;
-    }
-
-    // Hide for every Asset Hub chain
-    if (api.tx.stakingRcClient) {
+    // Hide for every Asset Hub chain and for Relay chains which have stakingAhClient storagr
+    if (api.query.stakingAhClient || api.tx.stakingRcClient) {
       return false;
     }
 
