f79bcdec631b8b41b5e6421cc2c0f64fdd23b025
Buddy Glass <104383456+BuddyGlas@users.noreply.github.com>
2025-10-23 12:14:26 +0700

    Change endpoints for the Unique Network chains: Unique, Quartz, Opal (#11937)
    * disable Unique Europe Quartz endpoint
    
    * Disable regoins endpoints for Unique Network
    
    * Move Opal to testing Relay Paseo
    
    * fix lint CI errors

--- packages/apps-config/src/endpoints/productionRelayPolkadot.ts
+++ packages/apps-config/src/endpoints/productionRelayPolkadot.ts
@@ -789,11 +789,11 @@ export const prodParasPolkadot: Omit<EndpointOption, 'teleport'>[] = [
       // Dwellir: 'wss://unique-rpc.n.dwellir.com', // https://github.com/polkadot-js/apps/issues/11531
       'Geo Load Balancer': 'wss://ws.unique.network',
       IBP1: 'wss://unique.ibp.network',
-      IBP2: 'wss://unique.dotters.network',
+      IBP2: 'wss://unique.dotters.network'
       // OnFinality: 'wss://unique.api.onfinality.io/public-ws', // https://github.com/polkadot-js/apps/issues/10030
-      'Unique America': 'wss://us-ws.unique.network',
-      'Unique Asia': 'wss://asia-ws.unique.network',
-      'Unique Europe': 'wss://eu-ws.unique.network'
+      // 'Unique America': 'wss://us-ws.unique.network',
+      // 'Unique Asia': 'wss://asia-ws.unique.network',
+      // 'Unique Europe': 'wss://eu-ws.unique.network'
     },
     text: 'Unique Network',
     ui: {
