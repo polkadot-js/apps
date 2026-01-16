2699fd250afbc7d3f7bc6352358fa9d3425a4f03
leoonf <leo@onfinality.io>
2026-01-16 15:36:49 +0800

    add onfinality xvacate rpc to the list

--- packages/apps-config/src/endpoints/productionRelayPolkadot.ts
+++ packages/apps-config/src/endpoints/productionRelayPolkadot.ts
@@ -818,7 +818,8 @@ export const prodParasPolkadot: Omit<EndpointOption, 'teleport'>[] = [
     providers: {
       IBP1: 'wss://xcavate.ibp.network',
       IBP2: 'wss://xcavate.dotters.network',
-      Xcavate: 'wss://rpc1-polkadot.xcavate.io'
+      Xcavate: 'wss://rpc1-polkadot.xcavate.io',
+      Onfinality: 'wss://xcavate-paseo.api.onfinality.io/public-ws',
     },
     relayName: 'polkadot',
     text: 'Xcavate',
