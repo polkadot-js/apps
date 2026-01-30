aab016e9ee6e2c3848b2f34c7415090637485f4f
Bastian Köcher <git@kchr.de>
2026-01-30 11:03:02 +0100

    Update copyright year to make CI happy (#12142)

--- packages/page-council/src/Motions/ProposeExternal.tsx
+++ packages/page-council/src/Motions/ProposeExternal.tsx
@@ -1,4 +1,4 @@
-// Copyright 2017-2025 @polkadot/app-council authors & contributors
+// Copyright 2017-2026 @polkadot/app-council authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
 import type { SubmittableExtrinsic } from '@polkadot/api/types';
