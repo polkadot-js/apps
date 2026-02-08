381d51357067e37983c1a177c3090a323471452a
Milos Kriz <82968568+miloskriz@users.noreply.github.com>
2026-02-08 18:21:51 +0000

    Remove IBP support to `xcavate` parachain

--- packages/apps-config/src/endpoints/productionRelayPolkadot.ts
+++ packages/apps-config/src/endpoints/productionRelayPolkadot.ts
@@ -814,8 +814,6 @@ export const prodParasPolkadot: Omit<EndpointOption, 'teleport'>[] = [
     info: 'xcavate',
     paraId: 3413,
     providers: {
-      IBP1: 'wss://xcavate.ibp.network',
-      IBP2: 'wss://xcavate.dotters.network',
       Xcavate: 'wss://rpc1-polkadot.xcavate.io'
     },
     relayName: 'polkadot',
