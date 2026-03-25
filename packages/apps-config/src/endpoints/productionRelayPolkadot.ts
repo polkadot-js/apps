1a5e67bd9b8290532643c27416fb37b901a76976
Arneil Paul Polican <policanarneilpaul@gmail.com>
2026-03-25 13:31:56 +0800

    Update productionRelayPolkadot.ts

--- packages/apps-config/src/endpoints/productionRelayPolkadot.ts
+++ packages/apps-config/src/endpoints/productionRelayPolkadot.ts
@@ -841,9 +841,9 @@ export const prodParasPolkadot: Omit<EndpointOption, 'teleport'>[] = [
     info: 'xode',
     paraId: 3417,
     providers: {
-      XodeCommunity: 'wss://polkadot-rpcnode.xode.net',
       RakSonCebu: 'wss://rakson-ceb-rpc.xode.net',
       RakSonKorea: 'wss://rakson-rpc.xode.net',
+      XodeCommunity: 'wss://polkadot-rpcnode.xode.net'
     },
     text: 'Xode',
     ui: {
