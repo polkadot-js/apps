10033a48c016011a86f882c60a3318e2e293f34a
Rbdude25 <114830736+Rbdude25@users.noreply.github.com>
2026-08-26 05:56:05 -0400

    Update testingRelayPaseo.ts

--- packages/apps-config/src/endpoints/testingRelayPaseo.ts
+++ packages/apps-config/src/endpoints/testingRelayPaseo.ts
@@ -385,10 +385,10 @@ export const testRelayPaseo: EndpointOption = {
   ],
   providers: {
     Dwellir: 'wss://paseo-rpc.n.dwellir.com',
+    Paranodes: 'wss://rpc-paseo.paranodes.io',
     StakeWorld: 'wss://rpc-paseo.stakeworld.io',
     TurboFlakes: 'wss://paseo-v2.rpc.turboflakes.io',
-    interweb: 'wss://rpc.interweb-it.com/paseo',
-    Paranodes: 'wss://rpc-paseo.paranodes.io'
+    interweb: 'wss://rpc.interweb-it.com/paseo'
     // 'light client': 'light://substrate-connect/paseo'
   },
   teleport: getTeleports(testParasPaseoCommon),
